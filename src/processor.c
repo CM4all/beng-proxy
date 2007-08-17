e55bdab31e71202a6fd7cf0ebfec2e3a80a8693e
Max Kellermann <mk@cm4all.com>
2007-08-17 12:05:37 +0000

    added struct substitution, parse <c:embed/> elements

--- src/processor.c
+++ src/processor.c
@@ -5,12 +5,28 @@
  */
 
 #include "processor.h"
+#include "strutil.h"
 
 #include <sys/mman.h>
 #include <assert.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <string.h>
+
+struct substitution {
+    struct substitution *next;
+    off_t start, end;
+};
+
+enum parser_state {
+    PARSER_NONE,
+    PARSER_START,
+    PARSER_NAME,
+    PARSER_ELEMENT,
+    PARSER_SHORT,
+    PARSER_INSIDE,
+};
 
 struct processor {
     pool_t pool;
@@ -19,9 +35,20 @@ struct processor {
     off_t content_length, position;
     char *map;
 
+    enum parser_state parser_state;
+    off_t element_offset;
+    size_t match_length;
+    char element_name[64];
+    size_t element_name_length;
+
+    struct substitution *first_substitution, **append_substitution_p;
+
     const struct processor_handler *handler;
     void *handler_ctx;
 };
+
+static const char element_start[] = "<c:";
+static const char element_end[] = "</c:";
 
 processor_t
 processor_new(pool_t pool,
@@ -39,6 +66,10 @@ processor_new(pool_t pool,
     processor->fd = -1;
     processor->content_length = 0;
     processor->map = NULL;
+
+    processor->parser_state = PARSER_NONE;
+    processor->first_substitution = NULL;
+    processor->append_substitution_p = &processor->first_substitution;
 
     processor->handler = handler;
     processor->handler_ctx = ctx;
@@ -91,6 +122,134 @@ processor_free(processor_t *processor_r)
     processor_close(processor);
 }
 
+static void
+processor_element_finished(processor_t processor, off_t end)
+{
+    struct substitution *s = p_malloc(processor->pool, sizeof(*s));
+
+    s->next = NULL;
+    s->start = processor->element_offset;
+    s->end = end;
+
+    *processor->append_substitution_p = s;
+    processor->append_substitution_p = &s->next;
+}
+
+static void
+processor_parse_input(processor_t processor, const char *start, size_t length)
+{
+    const char *buffer = start, *end = start + length, *p;
+
+    assert(processor != NULL);
+    assert(buffer != NULL);
+    assert(length > 0);
+
+    while (buffer < end) {
+        switch (processor->parser_state) {
+        case PARSER_NONE:
+            /* find first character */
+            p = memchr(buffer, element_start[0], end - buffer);
+            if (p == NULL)
+                return;
+
+            processor->parser_state = PARSER_START;
+            processor->element_offset = processor->content_length + (off_t)(p - start);
+            processor->match_length = 1;
+            buffer = p + 1;
+            break;
+
+        case PARSER_START:
+            /* compare more characters */
+            assert(processor->match_length > 0);
+            assert(processor->match_length < sizeof(element_start) - 1);
+
+            do {
+                if (*buffer != element_start[processor->match_length]) {
+                    processor->parser_state = PARSER_NONE;
+                    break;
+                }
+
+                ++processor->match_length;
+                ++buffer;
+
+                if (processor->match_length == sizeof(element_start) - 1) {
+                    processor->parser_state = PARSER_NAME;
+                    processor->element_name_length = 0;
+                    break;
+                }
+            } while (unlikely(buffer < end));
+
+            break;
+
+        case PARSER_NAME:
+            /* copy element name */
+            while (buffer < end) {
+                if (char_is_alphanumeric(*buffer)) {
+                    if (processor->element_name_length == sizeof(processor->element_name)) {
+                        /* name buffer overflowing */
+                        processor->parser_state = PARSER_NONE;
+                        break;
+                    }
+
+                    processor->element_name[processor->element_name_length++] = *buffer++;
+                } else if ((char_is_whitespace(*buffer) || *buffer == '/' || *buffer == '>') &&
+                           processor->element_name_length > 0) {
+                    processor->parser_state = PARSER_ELEMENT;
+                    break;
+                } else {
+                    processor->parser_state = PARSER_NONE;
+                    break;
+                }
+            }
+
+            break;
+
+        case PARSER_ELEMENT:
+            do {
+                if (char_is_whitespace(*buffer)) {
+                    ++buffer;
+                } else if (*buffer == '/') {
+                    processor->parser_state = PARSER_SHORT;
+                    ++buffer;
+                    break;
+                } else if (*buffer == '>') {
+                    processor->parser_state = PARSER_INSIDE;
+                    ++buffer;
+                    processor_element_finished(processor, processor->content_length + (off_t)(buffer - start));
+                    break;
+                } else {
+                    processor->parser_state = PARSER_NONE;
+                    break;
+                }
+            } while (buffer < end);
+
+            break;
+
+        case PARSER_SHORT:
+            do {
+                if (char_is_whitespace(*buffer)) {
+                    ++buffer;
+                } else if (*buffer == '>') {
+                    processor->parser_state = PARSER_NONE;
+                    ++buffer;
+                    processor_element_finished(processor, processor->content_length + (off_t)(buffer - start));
+                    break;
+                } else {
+                    processor->parser_state = PARSER_NONE;
+                    break;
+                }
+            } while (buffer < end);
+
+            break;
+
+        case PARSER_INSIDE:
+            /* XXX */
+            processor->parser_state = PARSER_NONE;
+            break;
+        }
+    }
+}
+
 size_t
 processor_input(processor_t processor, const void *buffer, size_t length)
 {
@@ -113,6 +272,8 @@ processor_input(processor_t processor, const void *buffer, size_t length)
         processor_close(processor);
         return 0;
     }
+
+    processor_parse_input(processor, (const char*)buffer, (size_t)nbytes);
 
     processor->content_length += (off_t)nbytes;
 
