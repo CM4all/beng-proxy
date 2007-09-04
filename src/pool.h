883355298568206236ec780e48657240507d1da6
Max Kellermann <mk@cm4all.com>
2007-09-04 09:20:47 +0000

    removed misplaced attr_printf

--- src/pool.h
+++ src/pool.h
@@ -69,7 +69,7 @@ p_strndup(pool_t pool, const char *src, size_t length);
 char * attr_malloc attr_printf(2, 3)
 p_sprintf(pool_t pool, const char *fmt, ...);
 
-char * attr_malloc attr_printf(2, 3)
+char * attr_malloc
 p_strcat(pool_t pool, const char *s, ...);
 
 #endif
