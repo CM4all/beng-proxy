dd16d741ce87f0d6adad538e39d06cd4caa4d289
Max Kellermann <mk@cm4all.com>
2007-09-05 09:12:42 +0000

    use istream_delayed in embed.c

--- src/embed.c
+++ src/embed.c
@@ -12,117 +12,27 @@
 #include <string.h>
 
 struct embed {
-    struct istream output;
-
     const struct widget *widget;
     const struct processor_env *env;
 
     url_stream_t url_stream;
-    istream_t input;
-    int input_eof, direct_mode;
+
+    istream_t delayed;
 };
 
-static size_t
-embed_input_data(const void *data, size_t length, void *ctx)
-{
-    struct embed *embed = ctx;
-
-    return istream_invoke_data(&embed->output, data, length);
-}
-
-static ssize_t
-embed_input_direct(int fd, size_t max_length, void *ctx)
-{
-    struct embed *embed = ctx;
-
-    return istream_invoke_direct(&embed->output, fd, max_length);
-}
-
 static void
-embed_input_eof(void *ctx)
+embed_abort(void *ctx)
 {
-    struct embed *embed = ctx;
+    struct embed *embed = (struct embed *)ctx;
 
-    embed->input->handler = NULL;
-    pool_unref(embed->input->pool);
-    embed->input = NULL;
-    embed->input_eof = 1;
+    if (embed->url_stream == NULL)
+        return;
 
-    pool_ref(embed->output.pool);
-    istream_invoke_eof(&embed->output);
-    istream_close(&embed->output);
-    pool_unref(embed->output.pool);
+    embed->delayed = NULL;
+
+    url_stream_close(embed->url_stream);
+    assert(embed->url_stream == NULL);
 }
-
-static void
-embed_input_free(void *ctx)
-{
-    struct embed *embed = ctx;
-
-    if (!embed->input_eof && embed->input != NULL) {
-        /* abort the transfer */
-        pool_unref(embed->input->pool);
-        embed->input = NULL;
-        /* XXX */
-    }
-}
-
-static const struct istream_handler embed_input_handler = {
-    .data = embed_input_data,
-    .direct = embed_input_direct,
-    .eof = embed_input_eof,
-    .free = embed_input_free,
-};
-
-static inline struct embed *
-istream_to_embed(istream_t istream)
-{
-    return (struct embed *)(((char*)istream) - offsetof(struct embed, output));
-}
-
-static void
-istream_embed_read(istream_t istream)
-{
-    struct embed *embed = istream_to_embed(istream);
-
-    if (embed->input != NULL)
-        istream_read(embed->input);
-}
-
-static void
-istream_embed_direct(istream_t istream)
-{
-    struct embed *embed = istream_to_embed(istream);
-
-    if (embed->input != NULL)
-        istream_direct(embed->input);
-    else
-        embed->direct_mode = 1;
-}
-
-static void
-istream_embed_close(istream_t istream)
-{
-    struct embed *embed = istream_to_embed(istream);
-
-    if (embed->url_stream != NULL) {
-        assert(embed->input == NULL);
-        url_stream_close(embed->url_stream);
-        assert(embed->url_stream == NULL);
-    } else if (embed->input != NULL) {
-        assert(embed->url_stream == NULL);
-        istream_close(embed->input);
-        assert(embed->input == NULL);
-    }
-
-    istream_invoke_free(&embed->output);
-}
-
-static const struct istream istream_embed = {
-    .read = istream_embed_read,
-    .direct = istream_embed_direct,
-    .close = istream_embed_close,
-};
 
 static void 
 embed_http_client_callback(http_status_t status, strmap_t headers,
@@ -131,6 +41,7 @@ embed_http_client_callback(http_status_t status, strmap_t headers,
 {
     struct embed *embed = ctx;
     const char *value;
+    istream_t input;
 
     (void)content_length;
 
@@ -139,32 +50,26 @@ embed_http_client_callback(http_status_t status, strmap_t headers,
 
     if (status == 0) {
         /* XXX */
-        istream_close(&embed->output);
+        if (embed->delayed != NULL)
+            istream_free(&embed->delayed);
         return;
     }
 
     value = strmap_get(headers, "content-type");
     if (value != NULL && strncmp(value, "text/html", 9) == 0) {
-        embed->input = processor_new(embed->output.pool, body,
-                                     embed->widget, embed->env);
-        if (embed->input == NULL) {
+        input = processor_new(embed->delayed->pool, body,
+                              embed->widget, embed->env);
+        if (input == NULL) {
             istream_close(body);
-            embed->input = istream_string_new(embed->output.pool, "Failed to create processor object.");
+            input = istream_string_new(embed->delayed->pool, "Failed to create processor object.");
         }
     } else {
         istream_close(body);
 
-        embed->input = istream_string_new(embed->output.pool, "Not an HTML document");
+        input = istream_string_new(embed->delayed->pool, "Not an HTML document");
     }
 
-    pool_ref(embed->input->pool);
-    embed->input->handler = &embed_input_handler;
-    embed->input->handler_ctx = embed;
-
-    if (embed->direct_mode)
-        istream_direct(embed->input);
-    else
-        istream_read(embed->input);
+    istream_delayed_set(embed->delayed, input);
 }
 
 istream_t
@@ -176,24 +81,16 @@ embed_new(pool_t pool, const char *url, const struct widget *widget,
     assert(url != NULL);
 
     embed = p_malloc(pool, sizeof(*embed));
-    embed->output = istream_embed;
-    embed->output.pool = pool;
     embed->widget = widget;
     embed->env = env;
-    embed->input = NULL;
-    embed->input_eof = 0;
-    embed->direct_mode = 0;
+    embed->delayed = istream_delayed_new(pool, embed_abort, embed);
 
     embed->url_stream = url_stream_new(pool,
                                        HTTP_METHOD_GET, url, NULL,
                                        embed_http_client_callback, embed);
-    if (embed->url_stream == NULL) {
-        embed->input = istream_string_new(pool, "Failed to create url_stream object.");
+    if (embed->url_stream == NULL)
+        istream_delayed_set(embed->delayed,
+                            istream_string_new(pool, "Failed to create url_stream object."));
 
-        pool_ref(embed->input->pool);
-        embed->input->handler = &embed_input_handler;
-        embed->input->handler_ctx = embed;
-    }
-
-    return &embed->output;
+    return embed->delayed;
 }
