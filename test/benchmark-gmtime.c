9308d063defa564ada61777bcd6c7786d802725c
Max Kellermann <mk@cm4all.com>
2007-09-19 09:06:27 +0000

    dont let icc optimize all code away by really using calculcation result

--- test/benchmark-gmtime.c
+++ test/benchmark-gmtime.c
@@ -3,21 +3,30 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <stdio.h>
 
 int main(int argc, char **argv) {
     time_t now = time(NULL);
     int i;
+    unsigned foo = 0;
 
     if (argc != 2)
         abort();
 
     if (strcmp(argv[1], "libc") == 0) {
-        for (i = 10000000; i > 0; --i)
-            gmtime(&now);
+        struct tm tm;
+        for (i = 10000000; i > 0; --i) {
+            gmtime_r(&now, &tm);
+            foo += tm.tm_sec + tm.tm_year + tm.tm_mday + tm.tm_mon + tm.tm_hour + tm.tm_min;
+        }
     } else if (strcmp(argv[1], "babak") == 0) {
         struct tm tm;
-        for (i = 10000000; i > 0; --i)
+        for (i = 10000000; i > 0; --i) {
             sysx_time_gmtime(now, &tm);
+            foo += tm.tm_sec + tm.tm_year + tm.tm_mday + tm.tm_mon + tm.tm_hour + tm.tm_min;
+        }
     } else
         abort();
+
+    printf("%u\n", foo);
 }
