7cc6374905caac188081dd3cf801ecab532a45e1
Max Kellermann <mk@cm4all.com>
2007-10-09 07:43:50 +0000

    added struct istream_null

--- src/istream-null.c
+++ src/istream-null.c
@@ -6,17 +6,31 @@
 
 #include "istream.h"
 
+struct istream_null {
+    struct istream stream;
+};
+
+static inline struct istream_null *
+istream_to_null(istream_t istream)
+{
+    return (struct istream_null *)(((char*)istream) - offsetof(struct istream_null, stream));
+}
+
 static void
 istream_null_read(istream_t istream)
 {
-    istream_invoke_eof(istream);
+    struct istream_null *null = istream_to_null(istream);
+
+    istream_invoke_eof(&null->stream);
     istream_close(istream);
 }
 
 static void
 istream_null_close(istream_t istream)
 {
-    istream_invoke_free(istream);
+    struct istream_null *null = istream_to_null(istream);
+
+    istream_invoke_free(&null->stream);
 }
 
 static const struct istream istream_null = {
@@ -27,10 +41,10 @@ static const struct istream istream_null = {
 istream_t
 istream_null_new(pool_t pool)
 {
-    istream_t istream = p_malloc(pool, sizeof(*istream));
+    struct istream_null *null = p_malloc(pool, sizeof(*null));
 
-    *istream = istream_null;
-    istream->pool = pool;
+    null->stream = istream_null;
+    null->stream.pool = pool;
 
-    return istream;
+    return &null->stream;
 }
