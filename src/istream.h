208fc6f1b936784d09d87e75e13b5a3a777eed1a
Max Kellermann <mk@cm4all.com>
2007-10-09 06:13:47 +0000

    api docs

--- src/istream.h
+++ src/istream.h
@@ -26,23 +26,67 @@ typedef unsigned istream_direct_t;
 
 typedef struct istream *istream_t;
 
+/** data sink for an istream */
 struct istream_handler {
+    /**
+     * Data is available as a buffer.
+     *
+     * @param data the buffer
+     * @param length the number of bytes available in the buffer, greater than 0
+     * @param ctx the istream_handler context pointer
+     * @return the number of bytes consumed
+     */
     size_t (*data)(const void *data, size_t length, void *ctx);
+
+    /**
+     * Data is available in a file descriptor.
+     *
+     * @param type what kind of file descriptor?
+     * @param fd the file descriptor
+     * @param max_length don't read more than this number of bytes
+     * @param ctx the istream_handler context pointer
+     * @return the number of bytes consumed, -1 on error (errno set),
+     *   -2 if writing would block
+     */
     ssize_t (*direct)(istream_direct_t type, int fd, size_t max_length, void *ctx);
+
+    /**
+     * End of file encountered.
+     *
+     * @param ctx the istream_handler context pointer
+     */
     void (*eof)(void *ctx);
+
+    /**
+     * The istream is being disposed.
+     *
+     * @param ctx the istream_handler context pointer
+     */
     void (*free)(void *ctx);
 };
 
+/** an input stream */
 struct istream {
+    /** the memory pool which allocated this object */
     pool_t pool;
+
+    /** data sink */
     const struct istream_handler *handler;
+
+    /** context pointer for the handler */
     void *handler_ctx;
+
+    /** which types of file descriptors are accepted by the handler? */
     istream_direct_t handler_direct;
+
 #ifndef NDEBUG
     int reading;
 #endif
 
+    /** try to read from the stream */
     void (*read)(istream_t istream);
+
+    /** close the stream and free resources */
     void (*close)(istream_t istream);
 };
 
