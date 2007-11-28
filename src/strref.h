e92439f19438e179b02cd63b16d70439df2c70a6
Max Kellermann <mk@cm4all.com>
2007-11-28 10:18:09 +0000

    added more strref functions

--- src/strref.h
+++ src/strref.h
@@ -20,6 +20,15 @@ struct strref {
     const char *data;
 };
 
+static attr_always_inline const char *
+strref_end(const struct strref *s)
+{
+    assert(s != NULL);
+    assert(s->data != NULL || s->length == 0);
+
+    return s->data + s->length;
+}
+
 static attr_always_inline void
 strref_clear(struct strref *s)
 {
@@ -29,6 +38,15 @@ strref_clear(struct strref *s)
 #ifndef NDEBUG
     s->data = (const char*)0x02020202;
 #endif
+}
+
+static attr_always_inline void
+strref_null(struct strref *s)
+{
+    assert(s != NULL);
+
+    s->length = 0;
+    s->data = NULL;
 }
 
 static attr_always_inline void
@@ -49,6 +67,29 @@ strref_set_c(struct strref *s, const char *p)
 
     s->length = strlen(p);
     s->data = p;
+}
+
+static attr_always_inline void
+strref_set2(struct strref *s, const char *start, const char *end)
+{
+    assert(s != NULL);
+    assert(start != NULL);
+    assert(end != NULL);
+    assert(start <= end);
+
+    s->length = end - start;
+    s->data = start;
+}
+
+static attr_always_inline void
+strref_right(struct strref *dest, const struct strref *src, const char *start)
+{
+    assert(dest != NULL);
+    assert(src != NULL);
+    assert(src->data != NULL || src->length == 0);
+    assert(start >= src->data && start <= strref_end(src));
+
+    strref_set2(dest, start, strref_end(src));
 }
 
 static inline int
@@ -120,6 +161,15 @@ strref_ends_with_n(const struct strref *s,
 
     return s->length >= length &&
         memcmp(s->data + s->length - length, p, length) == 0;
+}
+
+static attr_always_inline const char *
+strref_chr(const struct strref *s, char ch)
+{
+    assert(s != NULL);
+    assert(s->data != NULL || s->length == 0);
+
+    return memchr(s->data, ch, s->length);
 }
 
 #endif
