feccc0f4aa6e1bb0c5093cce3a3aa7ac7e366a84
Max Kellermann <mk@cm4all.com>
2007-11-28 09:22:23 +0000

    added assertions

--- src/strref.h
+++ src/strref.h
@@ -11,6 +11,7 @@
 #include "compiler.h"
 #include "pool.h"
 
+#include <assert.h>
 #include <stddef.h>
 #include <string.h>
 
@@ -22,6 +23,8 @@ struct strref {
 static attr_always_inline void
 strref_clear(struct strref *s)
 {
+    assert(s != NULL);
+
     s->length = 0;
 #ifndef NDEBUG
     s->data = (const char*)0x02020202;
@@ -31,6 +34,9 @@ strref_clear(struct strref *s)
 static attr_always_inline void
 strref_set(struct strref *s, const char *p, size_t length)
 {
+    assert(s != NULL);
+    assert(p != NULL);
+
     s->length = length;
     s->data = p;
 }
@@ -38,6 +44,9 @@ strref_set(struct strref *s, const char *p, size_t length)
 static attr_always_inline void
 strref_set_c(struct strref *s, const char *p)
 {
+    assert(s != NULL);
+    assert(p != NULL);
+
     s->length = strlen(p);
     s->data = p;
 }
@@ -45,18 +54,27 @@ strref_set_c(struct strref *s, const char *p)
 static attr_always_inline int
 strref_is_empty(const struct strref *s)
 {
+    assert(s != NULL);
+
     return s->length == 0;
 }
 
 static attr_always_inline char
 strref_last(const struct strref *s)
 {
+    assert(s != NULL);
+    assert(s->length > 0);
+    assert(s->data != NULL);
+
     return s->data[s->length - 1];
 }
 
 static attr_always_inline char *
 strref_dup(pool_t pool, const struct strref *s)
 {
+    assert(pool != NULL);
+    assert(s != NULL);
+
     return p_strndup(pool, s->data, s->length);
 }
 
@@ -64,6 +82,10 @@ static attr_always_inline int
 strref_cmp(const struct strref *s,
            const char *p, size_t length)
 {
+    assert(s != NULL);
+    assert(s->data != NULL || s->length == 0);
+    assert(p != NULL || length == 0);
+
     return s->length == length &&
         memcmp(s->data, p, length);
 }
@@ -71,6 +93,9 @@ strref_cmp(const struct strref *s,
 static attr_always_inline int
 strref_cmp2(const struct strref *a, const struct strref *b)
 {
+    assert(a != NULL);
+    assert(b != NULL);
+
     return strref_cmp(a, b->data, b->length);
 }
 
@@ -78,6 +103,9 @@ static attr_always_inline int
 strref_ends_with_n(const struct strref *s,
                    const char *p, size_t length)
 {
+    assert(s != NULL);
+    assert(s->data != NULL || s->length == 0);
+
     return s->length >= length &&
         memcmp(s->data + s->length - length, p, length) == 0;
 }
