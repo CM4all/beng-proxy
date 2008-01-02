9dce86e1c469b13996470014f38eb4d41956a4e8
Max Kellermann <mk@cm4all.com>
2008-01-02 16:54:21 +0000

    store sessions in hash table

--- src/session.c
+++ src/session.c
@@ -14,6 +14,8 @@
 #define SESSION_TTL_NEW 120
 #define SESSION_TTL 600
 
+#define SESSION_SLOTS 1024
+
 static struct timeval cleanup_interval = {
     .tv_sec = 60,
     .tv_usec = 0,
@@ -21,8 +23,10 @@ static struct timeval cleanup_interval = {
 
 static struct {
     pool_t pool;
-    struct list_head sessions; /* XXX use hash table */
+
+    struct list_head sessions[SESSION_SLOTS];
     unsigned num_sessions;
+
     struct event cleanup_event;
 } session_manager;
 
@@ -30,18 +34,21 @@ static void
 cleanup_event_callback(int fd, short event, void *ctx)
 {
     time_t now = time(NULL);
+    unsigned i;
     session_t session, next;
 
     (void)fd;
     (void)event;
     (void)ctx;
 
-    for (session = (session_t)session_manager.sessions.next;
-         session != (session_t)&session_manager.sessions;
-         session = next) {
-        next = (session_t)session->hash_siblings.next;
-        if (now >= session->expires)
-            session_remove(session);
+    for (i = 0; i < SESSION_SLOTS; ++i) {
+        for (session = (session_t)session_manager.sessions[i].next;
+             session != (session_t)&session_manager.sessions[i];
+             session = next) {
+            next = (session_t)session->hash_siblings.next;
+            if (now >= session->expires)
+                session_remove(session);
+        }
     }
 
     if (session_manager.num_sessions > 0) {
@@ -53,10 +60,14 @@ cleanup_event_callback(int fd, short event, void *ctx)
 void
 session_manager_init(pool_t pool)
 {
+    unsigned i;
+
     assert(session_manager.pool == NULL);
 
     session_manager.pool = pool_new_libc(pool, "session_manager");
-    list_init(&session_manager.sessions);
+
+    for (i = 0; i < SESSION_SLOTS; ++i)
+        list_init(&session_manager.sessions[i]);
 
     evtimer_set(&session_manager.cleanup_event, cleanup_event_callback, NULL);
 }
@@ -64,17 +75,27 @@ session_manager_init(pool_t pool)
 void
 session_manager_deinit(void)
 {
+    unsigned i;
+
     assert(session_manager.pool != NULL);
 
     event_del(&session_manager.cleanup_event);
 
-    while (session_manager.sessions.next != &session_manager.sessions) {
-        session_t session = (session_t)session_manager.sessions.next;
-        session_remove(session);
+    for (i = 0; i < SESSION_SLOTS; ++i) {
+        while (!list_empty(&session_manager.sessions[i])) {
+            session_t session = (session_t)session_manager.sessions[i].next;
+            session_remove(session);
+        }
     }
 
     pool_unref(session_manager.pool);
     session_manager.pool = NULL;
+}
+
+static struct list_head *
+session_slot(session_id_t id)
+{
+    return &session_manager.sessions[id % SESSION_SLOTS];
 }
 
 session_id_t
@@ -116,7 +137,7 @@ session_new(void)
     session->translate = NULL;
     session->widgets = NULL;
 
-    list_add(&session->hash_siblings, &session_manager.sessions);
+    list_add(&session->hash_siblings, session_slot(session->id));
     ++session_manager.num_sessions;
 
     if (session_manager.num_sessions == 1) {
@@ -130,10 +151,10 @@ session_new(void)
 session_t
 session_get(session_id_t id)
 {
+    struct list_head *head = session_slot(id);
     session_t session;
 
-    for (session = (session_t)session_manager.sessions.next;
-         session != (session_t)&session_manager.sessions;
+    for (session = (session_t)head->next; session != (session_t)head;
          session = (session_t)session->hash_siblings.next) {
         if (session->id == id) {
             session->expires = time(NULL) + SESSION_TTL;
