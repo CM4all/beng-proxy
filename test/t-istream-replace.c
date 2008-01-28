fcfb8706937880948f701ab9afcca01af0afdb42
Max Kellermann <mk@cm4all.com>
2008-01-28 11:05:46 +0000

    another replace test

--- test/t-istream-replace.c
+++ test/t-istream-replace.c
@@ -11,6 +11,7 @@ create_test(pool_t pool, istream_t input)
 {
     istream_t istream = istream_replace_new(pool, input, 0);
     istream_replace_add(istream, 0, 0, NULL);
+    istream_replace_add(istream, 3, 3, NULL);
     istream_replace_finish(istream);
     return istream;
 }
