99f7ce8b68f0015b01dc20d9a0425ab743193044
Max Kellermann <mk@cm4all.com>
2008-03-11 10:02:11 +0000

    reverted last commit

--- src/istream-tee.c
+++ src/istream-tee.c
@@ -9,9 +9,7 @@
 #include <assert.h>
 
 struct istream_tee {
-    struct {
-        struct istream istream;
-    } outputs[2];
+    struct istream outputs[2];
     istream_t input;
 };
 
@@ -31,15 +29,15 @@ tee_input_data(const void *data, size_t length, void *ctx)
     int denotify;
 #endif
 
-    nbytes1 = istream_invoke_data(&tee->outputs[0].istream, data, length);
+    nbytes1 = istream_invoke_data(&tee->outputs[0], data, length);
     if (nbytes1 == 0)
         return 0;
 
 #ifndef NDEBUG
-    pool_notify(tee->outputs[1].istream.pool, &notify);
+    pool_notify(tee->outputs[1].pool, &notify);
 #endif
 
-    nbytes2 = istream_invoke_data(&tee->outputs[1].istream, data, nbytes1);
+    nbytes2 = istream_invoke_data(&tee->outputs[1], data, nbytes1);
 
 #ifndef NDEBUG
     denotify = pool_denotify(&notify);
@@ -60,13 +58,13 @@ tee_input_eof(void *ctx)
 
     assert(tee->input != NULL);
 
-    pool_ref(tee->outputs[0].istream.pool);
+    pool_ref(tee->outputs[0].pool);
 
     tee->input = NULL;
-    istream_deinit_eof(&tee->outputs[0].istream);
-    istream_deinit_eof(&tee->outputs[1].istream);
+    istream_deinit_eof(&tee->outputs[0]);
+    istream_deinit_eof(&tee->outputs[1]);
 
-    pool_unref(tee->outputs[0].istream.pool);
+    pool_unref(tee->outputs[0].pool);
 }
 
 static void
@@ -76,13 +74,13 @@ tee_input_abort(void *ctx)
 
     assert(tee->input != NULL);
 
-    pool_ref(tee->outputs[0].istream.pool);
+    pool_ref(tee->outputs[0].pool);
 
     tee->input = NULL;
-    istream_deinit_abort(&tee->outputs[0].istream);
-    istream_deinit_abort(&tee->outputs[1].istream);
+    istream_deinit_abort(&tee->outputs[0]);
+    istream_deinit_abort(&tee->outputs[1]);
 
-    pool_unref(tee->outputs[0].istream.pool);
+    pool_unref(tee->outputs[0].pool);
 }
 
 static const struct istream_handler tee_input_handler = {
@@ -101,7 +99,7 @@ static const struct istream_handler tee_input_handler = {
 static inline struct istream_tee *
 istream_to_tee1(istream_t istream)
 {
-    return (struct istream_tee *)(((char*)istream) - offsetof(struct istream_tee, outputs[0].istream));
+    return (struct istream_tee *)(((char*)istream) - offsetof(struct istream_tee, outputs[0]));
 }
 
 static void
@@ -120,8 +118,8 @@ istream_tee_close1(istream_t istream)
     assert(tee->input != NULL);
 
     istream_free_handler(&tee->input);
-    istream_deinit_abort(&tee->outputs[1].istream);
-    istream_deinit_abort(&tee->outputs[0].istream);
+    istream_deinit_abort(&tee->outputs[1]);
+    istream_deinit_abort(&tee->outputs[0]);
 }
 
 static const struct istream istream_tee1 = {
@@ -138,7 +136,7 @@ static const struct istream istream_tee1 = {
 static inline struct istream_tee *
 istream_to_tee2(istream_t istream)
 {
-    return (struct istream_tee *)(((char*)istream) - offsetof(struct istream_tee, outputs[1].istream));
+    return (struct istream_tee *)(((char*)istream) - offsetof(struct istream_tee, outputs[1]));
 }
 
 static void
@@ -157,8 +155,8 @@ istream_tee_close2(istream_t istream)
     assert(tee->input != NULL);
 
     istream_free_handler(&tee->input);
-    istream_deinit_abort(&tee->outputs[1].istream);
-    istream_deinit_abort(&tee->outputs[0].istream);
+    istream_deinit_abort(&tee->outputs[1]);
+    istream_deinit_abort(&tee->outputs[0]);
 }
 
 static const struct istream istream_tee2 = {
@@ -181,13 +179,13 @@ istream_tee_new(pool_t pool, istream_t input)
     assert(input != NULL);
     assert(!istream_has_handler(input));
 
-    istream_init(&tee->outputs[1].istream, &istream_tee2, pool);
+    istream_init(&tee->outputs[1], &istream_tee2, pool);
 
     istream_assign_handler(&tee->input, input,
                            &tee_input_handler, tee,
                            0);
 
-    return istream_struct_cast(&tee->outputs[0].istream);
+    return istream_struct_cast(&tee->outputs[0]);
 }
 
 istream_t
@@ -195,6 +193,6 @@ istream_tee_second(istream_t istream)
 {
     struct istream_tee *tee = istream_to_tee1(istream);
 
-    return istream_struct_cast(&tee->outputs[1].istream);
+    return istream_struct_cast(&tee->outputs[1]);
 }
 
