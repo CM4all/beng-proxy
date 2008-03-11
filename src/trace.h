e33e2356c6a7e620294f53507f63802c7e323ab6
Max Kellermann <mk@cm4all.com>
2008-03-11 09:46:09 +0000

    added TRACE_ARGS_IGNORE

--- src/trace.h
+++ src/trace.h
@@ -12,12 +12,14 @@
 #define TRACE_ARGS_DECL , const char *file, unsigned line
 #define TRACE_ARGS_FWD , file, line
 #define TRACE_ARGS , __FILE__, __LINE__
+#define TRACE_ARGS_IGNORE { (void)file; (void)line; }
 
 #else
 
 #define TRACE_ARGS_DECL
 #define TRACE_ARGS_FWD
 #define TRACE_ARGS
+#define TRACE_ARGS_IGNORE
 
 #endif
 
