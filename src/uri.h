5a8bcf7d99e4467146614cdc9b01de7848f4807a
Max Kellermann <mk@cm4all.com>
2008-04-01 14:32:56 +0000

    added uri_escape_dup()

--- src/uri.h
+++ src/uri.h
@@ -19,6 +19,14 @@ struct parsed_uri {
 size_t
 uri_escape(char *dest, const char *src, size_t src_length);
 
+static inline const char *
+uri_escape_dup(pool_t pool, const char *src, size_t src_length)
+{
+    char *dest = p_malloc(pool, src_length * 3 + 1);
+    uri_escape(dest, src, src_length);
+    return dest;
+}
+
 size_t
 uri_unescape_inplace(char *src, size_t length);
 
