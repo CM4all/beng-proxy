ce95afabd3798199a3faf1b806010c4d20eced36
Max Kellermann <mk@cm4all.com>
2008-04-16 07:46:09 +0000

    internal renames

--- src/http-request.c
+++ src/http-request.c
@@ -17,7 +17,7 @@
 
 #include <string.h>
 
-struct url_stream {
+struct http_request {
     pool_t pool;
 
     http_method_t method;
@@ -36,22 +36,22 @@ struct url_stream {
  */
 
 static void
-url_stream_stock_callback(void *ctx, struct stock_item *item)
+http_request_stock_callback(void *ctx, struct stock_item *item)
 {
-    struct url_stream *us = ctx;
+    struct http_request *hr = ctx;
 
     if (item == NULL) {
-        http_response_handler_invoke_abort(&us->handler);
+        http_response_handler_invoke_abort(&hr->handler);
 
-        if (us->body != NULL)
-            istream_close(us->body);
+        if (hr->body != NULL)
+            istream_close(hr->body);
     } else
         http_client_request(http_stock_item_get(item),
-                            us->method, us->uri, us->headers, us->body,
-                            us->handler.handler, us->handler.ctx,
-                            us->async_ref);
+                            hr->method, hr->uri, hr->headers, hr->body,
+                            hr->handler.handler, hr->handler.ctx,
+                            hr->async_ref);
 
-    pool_unref(us->pool);
+    pool_unref(hr->pool);
 }
 
 
@@ -71,7 +71,7 @@ http_request(pool_t pool,
              void *handler_ctx,
              struct async_operation_ref *async_ref)
 {
-    struct url_stream *us;
+    struct http_request *hr;
     const char *host_and_port;
 
     assert(uwa != NULL);
@@ -82,23 +82,23 @@ http_request(pool_t pool,
 
     pool_ref(pool);
 
-    us = p_malloc(pool, sizeof(*us));
-    us->pool = pool;
-    us->method = method;
+    hr = p_malloc(pool, sizeof(*hr));
+    hr->pool = pool;
+    hr->method = method;
 
-    us->headers = headers;
-    if (us->headers == NULL)
-        us->headers = growing_buffer_new(pool, 512);
+    hr->headers = headers;
+    if (hr->headers == NULL)
+        hr->headers = growing_buffer_new(pool, 512);
 
     if (body == NULL)
-        us->body = NULL;
+        hr->body = NULL;
     else
         /* XXX remove istream_hold(), it is only here because
            http-client.c resets istream->pool after the response is
            ready */
-        us->body = istream_hold_new(pool, body);
-    http_response_handler_set(&us->handler, handler, handler_ctx);
-    us->async_ref = async_ref;
+        hr->body = istream_hold_new(pool, body);
+    http_response_handler_set(&hr->handler, handler, handler_ctx);
+    hr->async_ref = async_ref;
 
     if (memcmp(uwa->uri, "http://", 7) == 0) {
         /* HTTP over TCP */
@@ -107,37 +107,37 @@ http_request(pool_t pool,
         p = uwa->uri + 7;
         slash = strchr(p, '/');
         if (slash == NULL || slash == p) {
-            http_response_handler_invoke_abort(&us->handler);
-            pool_unref(us->pool);
+            http_response_handler_invoke_abort(&hr->handler);
+            pool_unref(hr->pool);
             return;
         }
 
-        host_and_port = p_strndup(us->pool, p, slash - p);
-        header_write(us->headers, "host", host_and_port);
+        host_and_port = p_strndup(hr->pool, p, slash - p);
+        header_write(hr->headers, "host", host_and_port);
 
-        us->uri = slash;
+        hr->uri = slash;
     } else if (memcmp(uwa->uri, "unix:/", 6) == 0) {
         /* HTTP over Unix socket */
         const char *p, *qmark;
 
         p = uwa->uri + 5;
-        us->uri = p;
+        hr->uri = p;
 
         qmark = strchr(p, '?');
         if (qmark == NULL)
             host_and_port = p;
         else
-            host_and_port = p_strndup(us->pool, p, qmark - p);
+            host_and_port = p_strndup(hr->pool, p, qmark - p);
     } else {
-        http_response_handler_invoke_abort(&us->handler);
-        pool_unref(us->pool);
+        http_response_handler_invoke_abort(&hr->handler);
+        pool_unref(hr->pool);
         return;
     }
 
-    header_write(us->headers, "connection", "keep-alive");
+    header_write(hr->headers, "connection", "keep-alive");
 
     hstock_get(http_client_stock,
                host_and_port, uwa,
-               url_stream_stock_callback, us,
+               http_request_stock_callback, hr,
                async_ref);
 }
