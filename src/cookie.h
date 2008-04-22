52469872c4b95b83088f21a55140e1204a4fd2cf
Max Kellermann <mk@cm4all.com>
2008-04-22 08:59:14 +0000

    cookie.h apidocs

--- src/cookie.h
+++ src/cookie.h
@@ -24,13 +24,25 @@ struct cookie {
     time_t valid_until;
 };
 
+/**
+ * Parse a Set-Cookie2 response header and insert new cookies into the
+ * linked list.
+ */
 void
 cookie_list_set_cookie2(pool_t pool, struct list_head *head, const char *value);
 
+/**
+ * Generate HTTP request headers passing for all cookies in the linked
+ * list.
+ */
 void
 cookie_list_http_header(struct strmap *headers, struct list_head *head,
                         pool_t pool);
 
+/**
+ * Parse a Cookie request header and store all cookies in the
+ * specified strmap.
+ */
 void
 cookie_map_parse(struct strmap *cookies, const char *p, pool_t pool);
 
