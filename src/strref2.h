2e3408f7e2509d0021bebed40b7385d201b537dc
Max Kellermann <mk@cm4all.com>
2008-04-22 12:18:51 +0000

    added lower-case strref comparison functions

--- src/strref2.h
+++ src/strref2.h
@@ -37,4 +37,37 @@ strref_trim(struct strref *s)
     strref_rtrim(s);
 }
 
+static __attr_always_inline int
+strref_lower_cmp(const struct strref *s, const char *p, size_t length)
+{
+    size_t i;
+
+    assert(s != NULL);
+    assert(s->data != NULL || s->length == 0);
+    assert(p != NULL || length == 0);
+
+    if (s->length != length)
+        return 1; /* XXX -1 or 1? */
+
+    for (i = 0; i < length; ++i) {
+        char ch = char_to_lower(s->data[i]);
+        if (ch < p[i])
+            return -1;
+        if (ch > p[i])
+            return 1;
+    }
+
+    return 0;
+}
+
+static __attr_always_inline int
+strref_lower_cmp_c(const struct strref *s, const char *p)
+{
+    assert(p != NULL);
+
+    return strref_lower_cmp(s, p, strlen(p));
+}
+
+#define strref_lower_cmp_literal(s, l) strref_lower_cmp((s), (l), sizeof(l) - 1)
+
 #endif
