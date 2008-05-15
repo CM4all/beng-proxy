0288c971e40647a4634c9ad3431914d382e27e5b
Max Kellermann <mk@cm4all.com>
2008-05-15 09:24:11 +0000

    added lock_is_locked()

--- src/lock.h
+++ src/lock.h
@@ -7,6 +7,7 @@
 #ifndef __BENG_LOCK_H
 #define __BENG_LOCK_H
 
+#include <stdbool.h>
 #include <semaphore.h>
 
 struct lock {
@@ -35,6 +36,15 @@ static inline void
 lock_unlock(struct lock *lock)
 {
     sem_post(&lock->semaphore);
+}
+
+static inline bool
+lock_is_locked(struct lock *lock)
+{
+    int ret, value;
+
+    ret = sem_getvalue(&lock->semaphore, &value);
+    return ret == 0 && value <= 0;
 }
 
 #endif
