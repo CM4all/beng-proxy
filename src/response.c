225205befff53a0035ebf8976e74851d31a4dfe8
Max Kellermann <mk@cm4all.com>
2008-05-15 06:40:58 +0000

    root widget id is uri base

--- src/response.c
+++ src/response.c
@@ -103,12 +103,11 @@ response_invoke_processor(struct request *request2,
 
     widget = p_malloc(request->pool, sizeof(*widget));
     widget_init(widget, &root_widget_class);
+    widget->id = strref_dup(request->pool, &request2->uri.base);
     widget->lazy.path = "";
     widget->lazy.prefix = "__";
     widget->from_request.session = session_get_widget(request2->env.session,
-                                                      strref_dup(request->pool,
-                                                                 &request2->uri.base),
-                                                      true);
+                                                      widget->id, true);
 
     widget->from_request.focus_ref = widget_ref_parse(request->pool,
                                                       strmap_remove(request2->env.args, "focus"));
