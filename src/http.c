b3bd76206761563e4016367e3f9b4eac3f8925da
Max Kellermann <mk@cm4all.com>
2008-05-23 10:41:48 +0000

    added status 412

--- src/http.c
+++ src/http.c
@@ -34,6 +34,7 @@ const char *http_status_to_string_data[6][20] = {
         [HTTP_STATUS_FORBIDDEN - 400] = "403 Forbidden",
         [HTTP_STATUS_NOT_FOUND - 400] = "404 Not Found",
         [HTTP_STATUS_METHOD_NOT_ALLOWED - 400] = "405 Method Not Allowed",
+        [HTTP_STATUS_PRECONDITION_FAILED - 400] = "412 Precondition Failed",
     },
     [5] = {
         [HTTP_STATUS_INTERNAL_SERVER_ERROR - 500] = "500 Internal Server Error",
