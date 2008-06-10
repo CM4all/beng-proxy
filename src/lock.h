b4929fb213e179149bec1001d829fe01d742d3c4
Max Kellermann <mk@cm4all.com>
2008-06-10 08:54:26 +0000

    protected lock struct with 2 magic words

--- src/lock.h
+++ src/lock.h
@@ -7,34 +7,65 @@
 #ifndef __BENG_LOCK_H
 #define __BENG_LOCK_H
 
+#include <assert.h>
 #include <stdbool.h>
 #include <semaphore.h>
 
+#define LOCK_MAGIC1 (*(const unsigned*)"lck1")
+#define LOCK_MAGIC2 (*(const unsigned*)"lck2")
+
 struct lock {
+#ifndef NDEBUG
+    unsigned magic1;
+#endif
+
     sem_t semaphore;
+
+#ifndef NDEBUG
+    unsigned magic2;
+#endif
 };
 
 static inline void
 lock_init(struct lock *lock)
 {
     sem_init(&lock->semaphore, 1, 1);
+
+#ifndef NDEBUG
+    lock->magic1 = LOCK_MAGIC1;
+    lock->magic2 = LOCK_MAGIC2;
+#endif
 }
 
 static inline void
 lock_destroy(struct lock *lock)
 {
+    assert(lock->magic1 == LOCK_MAGIC1);
+    assert(lock->magic2 == LOCK_MAGIC2);
+
+#ifndef NDEBUG
+    lock->magic1 = 0;
+    lock->magic2 = 0;
+#endif
+
     sem_destroy(&lock->semaphore);
 }
 
 static inline void
 lock_lock(struct lock *lock)
 {
+    assert(lock->magic1 == LOCK_MAGIC1);
+    assert(lock->magic2 == LOCK_MAGIC2);
+
     sem_wait(&lock->semaphore);
 }
 
 static inline void
 lock_unlock(struct lock *lock)
 {
+    assert(lock->magic1 == LOCK_MAGIC1);
+    assert(lock->magic2 == LOCK_MAGIC2);
+
     sem_post(&lock->semaphore);
 }
 
@@ -42,6 +73,9 @@ static inline bool
 lock_is_locked(struct lock *lock)
 {
     int ret, value;
+
+    assert(lock->magic1 == LOCK_MAGIC1);
+    assert(lock->magic2 == LOCK_MAGIC2);
 
     ret = sem_getvalue(&lock->semaphore, &value);
     return ret == 0 && value <= 0;
