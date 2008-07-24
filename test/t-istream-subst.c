982aea963186de109ac50c477cd1770a01044f08
Max Kellermann <mk@cm4all.com>
2008-07-24 13:16:13 +0000

    longer subst test

--- test/t-istream-subst.c
+++ test/t-istream-subst.c
@@ -1,11 +1,11 @@
 #include "istream.h"
 
-#define EXPECTED_RESULT "bar"
+#define EXPECTED_RESULT "bar fo fo bar bla!"
 
 static istream_t
 create_input(pool_t pool)
 {
-    return istream_string_new(pool, "foo");
+    return istream_string_new(pool, "foo fo fo bar blablablablubb");
 }
 
 static istream_t
@@ -13,6 +13,7 @@ create_test(pool_t pool, istream_t input)
 {
     istream_t istream = istream_subst_new(pool, input);
     istream_subst_add(istream, "foo", "bar");
+    istream_subst_add(istream, "blablablubb", "!");
     return istream;
 }
 
