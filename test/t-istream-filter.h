2a881f74d439c7feda08fa94e54b991ed07c11ea
Max Kellermann <mk@cm4all.com>
2008-09-08 16:49:52 +0000

    abort_after signed, check >=0

--- test/t-istream-filter.h
+++ test/t-istream-filter.h
@@ -21,7 +21,7 @@ struct ctx {
     size_t buffer_length;
 #endif
     istream_t abort_istream;
-    unsigned abort_after;
+    int abort_after;
 };
 
 /*
@@ -349,7 +349,7 @@ test_abort_in_handler_half(pool_t pool)
         event_loop(EVLOOP_ONCE|EVLOOP_NONBLOCK);
     }
 
-    assert(ctx.abort_istream == NULL || ctx.abort_after > 0);
+    assert(ctx.abort_istream == NULL || ctx.abort_after >= 0);
 
     pool_trash(pool);
     pool_unref(pool);
