b89b4719a87a8e82bd117c4713fa1d5c68a881ee
Max Kellermann <mk@cm4all.com>
2008-09-11 09:33:57 +0000

    created python library beng_proxy.translation

--- prototypes/hosting.py
+++ prototypes/hosting.py
@@ -10,109 +10,9 @@ from twisted.internet import reactor
 from twisted.internet.protocol import Protocol, Factory
 import struct
 from urlparse import urlparse
-from socket import gethostbyname
-
-TRANSLATE_BEGIN = 1
-TRANSLATE_END = 2
-TRANSLATE_HOST = 3
-TRANSLATE_URI = 4
-TRANSLATE_STATUS = 5
-TRANSLATE_PATH = 6
-TRANSLATE_CONTENT_TYPE = 7
-TRANSLATE_PROXY = 8
-TRANSLATE_REDIRECT = 9
-TRANSLATE_FILTER = 10
-TRANSLATE_PROCESS = 11
-TRANSLATE_SESSION = 12
-TRANSLATE_PARAM = 13
-TRANSLATE_USER = 14
-TRANSLATE_LANGUAGE = 15
-TRANSLATE_REMOTE_HOST = 16
-TRANSLATE_PATH_INFO = 17
-TRANSLATE_SITE = 18
-TRANSLATE_CGI = 19
-TRANSLATE_DOCUMENT_ROOT = 20
-TRANSLATE_WIDGET_TYPE = 21
-TRANSLATE_CONTAINER = 22
-TRANSLATE_ADDRESS = 23
-TRANSLATE_ADDRESS_STRING = 24
-TRANSLATE_JAILCGI = 26
-TRANSLATE_INTERPRETER = 27
-TRANSLATE_ACTION = 28
-TRANSLATE_SCRIPT_NAME = 29
-TRANSLATE_AJP = 30
-TRANSLATE_DOMAIN = 31
-TRANSLATE_STATEFUL = 32
-TRANSLATE_FASTCGI = 33
+from beng_proxy.translation import *
 
 cgi_re = re.compile('\.(?:sh|rb|py|pl|cgi|php\d?)$')
-
-class PacketReader:
-    def __init__(self):
-        self._header = ''
-        self.complete = False
-
-    def consume(self, data):
-        assert not self.complete
-        assert isinstance(data, str)
-        # read header
-        if len(self._header) < 4:
-            # append to header
-            size = 4 - len(self._header)
-            self._header += data[0:size]
-            data = data[size:]
-            if len(self._header) < 4:
-                return data
-
-            # header is finished, decode it
-            self._length, self.command = struct.unpack('HH', self._header)
-            self.payload = ''
-            if self._length == 0:
-                # no payload, we're done
-                self.complete = True
-                return data
-
-        # read payload
-        if len(self.payload) < self._length:
-            # append to payload
-            size = self._length - len(self.payload)
-            self.payload += data[0:size]
-            data = data[size:]
-
-            # done yet?
-            if len(self.payload) == self._length:
-                self.complete = True
-
-        # return data chunk without the consumed part
-        return data
-
-class Request:
-    def __init__(self):
-        self.host = None
-        self.uri = None
-        self.widget_type = None
-        self.session = None
-        self.param = None
-        self.remote_host = None
-
-    def packetReceived(self, packet):
-        if packet.command == TRANSLATE_END:
-            return True
-        elif packet.command == TRANSLATE_HOST:
-            self.host = packet.payload
-        elif packet.command == TRANSLATE_URI:
-            self.uri = packet.payload
-        elif packet.command == TRANSLATE_WIDGET_TYPE:
-            self.widget_type = packet.payload
-        elif packet.command == TRANSLATE_SESSION:
-            self.session = packet.payload
-        elif packet.command == TRANSLATE_PARAM:
-            self.param = packet.payload
-        elif packet.command == TRANSLATE_REMOTE_HOST:
-            self.remote_host = packet.payload
-        else:
-            print "Invalid command:", packet.command
-        return False
 
 class Translation(Protocol):
     def connectionMade(self):
@@ -122,8 +22,7 @@ class Translation(Protocol):
 
     def _write_packet(self, command, payload = ''):
         assert isinstance(payload, str)
-        self.transport.write(struct.pack('HH', len(payload), command))
-        self.transport.write(payload)
+        write_packet(self.transport, command, payload)
 
     def _host_account_id(self, host):
         if not host: return None
---
+++ python/beng_proxy/translation.py
@@ -0,0 +1,109 @@
+# Author: Max Kellermann <mk@cm4all.com>
+
+import struct
+
+TRANSLATE_BEGIN = 1
+TRANSLATE_END = 2
+TRANSLATE_HOST = 3
+TRANSLATE_URI = 4
+TRANSLATE_STATUS = 5
+TRANSLATE_PATH = 6
+TRANSLATE_CONTENT_TYPE = 7
+TRANSLATE_PROXY = 8
+TRANSLATE_REDIRECT = 9
+TRANSLATE_FILTER = 10
+TRANSLATE_PROCESS = 11
+TRANSLATE_SESSION = 12
+TRANSLATE_PARAM = 13
+TRANSLATE_USER = 14
+TRANSLATE_LANGUAGE = 15
+TRANSLATE_REMOTE_HOST = 16
+TRANSLATE_PATH_INFO = 17
+TRANSLATE_SITE = 18
+TRANSLATE_CGI = 19
+TRANSLATE_DOCUMENT_ROOT = 20
+TRANSLATE_WIDGET_TYPE = 21
+TRANSLATE_CONTAINER = 22
+TRANSLATE_ADDRESS = 23
+TRANSLATE_ADDRESS_STRING = 24
+TRANSLATE_JAILCGI = 26
+TRANSLATE_INTERPRETER = 27
+TRANSLATE_ACTION = 28
+TRANSLATE_SCRIPT_NAME = 29
+TRANSLATE_AJP = 30
+TRANSLATE_DOMAIN = 31
+TRANSLATE_STATEFUL = 32
+TRANSLATE_FASTCGI = 33
+
+class PacketReader:
+    def __init__(self):
+        self._header = ''
+        self.complete = False
+
+    def consume(self, data):
+        assert not self.complete
+        assert isinstance(data, str)
+        # read header
+        if len(self._header) < 4:
+            # append to header
+            size = 4 - len(self._header)
+            self._header += data[0:size]
+            data = data[size:]
+            if len(self._header) < 4:
+                return data
+
+            # header is finished, decode it
+            self._length, self.command = struct.unpack('HH', self._header)
+            self.payload = ''
+            if self._length == 0:
+                # no payload, we're done
+                self.complete = True
+                return data
+
+        # read payload
+        if len(self.payload) < self._length:
+            # append to payload
+            size = self._length - len(self.payload)
+            self.payload += data[0:size]
+            data = data[size:]
+
+            # done yet?
+            if len(self.payload) == self._length:
+                self.complete = True
+
+        # return data chunk without the consumed part
+        return data
+
+class Request:
+    def __init__(self):
+        self.host = None
+        self.uri = None
+        self.widget_type = None
+        self.session = None
+        self.param = None
+        self.remote_host = None
+
+    def packetReceived(self, packet):
+        if packet.command == TRANSLATE_END:
+            return True
+        elif packet.command == TRANSLATE_HOST:
+            self.host = packet.payload
+        elif packet.command == TRANSLATE_URI:
+            self.uri = packet.payload
+        elif packet.command == TRANSLATE_WIDGET_TYPE:
+            self.widget_type = packet.payload
+        elif packet.command == TRANSLATE_SESSION:
+            self.session = packet.payload
+        elif packet.command == TRANSLATE_PARAM:
+            self.param = packet.payload
+        elif packet.command == TRANSLATE_REMOTE_HOST:
+            self.remote_host = packet.payload
+        else:
+            print "Invalid command:", packet.command
+        return False
+
+def write_packet(f, command, payload = ''):
+    assert isinstance(payload, str)
+    assert len(payload) <= 0xffff
+    f.write(struct.pack('HH', len(payload), command))
+    f.write(payload)
