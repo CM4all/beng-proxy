5c29a18f5f244bd6a411e7d515790e53df1bdce8
Max Kellermann <mk@cm4all.com>
2009-04-27 14:23:30 +0000

    fork: added event for writing to the child process

--- src/fork.c
+++ src/fork.c
@@ -32,6 +32,7 @@ struct fork {
 
     istream_t input;
     int input_fd;
+    struct event2 input_event;
 
     pid_t pid;
 
@@ -48,6 +49,7 @@ fork_close(struct fork *f)
     if (f->input != NULL) {
         assert(f->input_fd >= 0);
 
+        event2_set(&f->input_event, 0);
         close(f->input_fd);
         istream_close_handler(f->input);
     }
@@ -80,11 +82,14 @@ fork_input_data(const void *data, size_t length, void *ctx)
 
     nbytes = write(f->input_fd, data, length);
     if (nbytes < 0) {
-        if (errno == EAGAIN)
+        if (errno == EAGAIN) {
+            event2_or(&f->input_event, EV_WRITE);
             return 0;
+        }
 
         daemon_log(1, "write() to subprocess failed: %s\n",
                    strerror(errno));
+        event2_set(&f->input_event, 0);
         close(f->input_fd);
         f->input_fd = -1;
         istream_free_handler(&f->input);
@@ -108,8 +113,10 @@ fork_input_direct(__attr_unused istream_direct_t type,
                     SPLICE_F_NONBLOCK | SPLICE_F_MORE | SPLICE_F_MOVE);
     if (nbytes < 0) {
         if (errno == EAGAIN) {
-            if (!fd_ready_for_writing(f->input_fd))
+            if (!fd_ready_for_writing(f->input_fd)) {
+                event2_or(&f->input_event, EV_WRITE);
                 return -2;
+            }
 
             /* try again, just in case connection->fd has become ready
                between the first splice() call and
@@ -130,6 +137,7 @@ fork_input_eof(void *ctx)
 
     assert(f->input_fd >= 0);
 
+    event2_set(&f->input_event, 0);
     close(f->input_fd);
     f->input_fd = -1;
 
@@ -229,6 +237,19 @@ fork_read_from_output(struct fork *f)
             istream_deinit_abort(&f->output);
         }
     }
+}
+
+static void
+fork_input_event_callback(int fd __attr_unused, short event __attr_unused,
+                          void *ctx)
+{
+    struct fork *f = ctx;
+
+    assert(f->input_fd == fd);
+    assert(f->input != NULL);
+
+    event2_reset(&f->input_event);
+    istream_read(f->input);
 }
 
 static void
@@ -370,6 +391,10 @@ beng_fork(pool_t pool, istream_t input, istream_t *output_r,
             close(stdin_pipe[0]);
             fd_set_cloexec(stdin_pipe[1]);
             f->input_fd = stdin_pipe[1];
+
+            event2_init(&f->input_event, f->input_fd,
+                        fork_input_event_callback, f, NULL);
+            event2_set(&f->input_event, EV_WRITE);
 
             istream_assign_handler(&f->input, input,
                                    &fork_input_handler, f,
