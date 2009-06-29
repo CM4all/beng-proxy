126f96237255c829fd957416a9cd1d1339aaf120
Max Kellermann <mk@cm4all.com>
2009-06-29 16:18:35 +0000

    inline-widget.c: don't use widget_stream

--- src/inline-widget.c
+++ src/inline-widget.c
@@ -9,7 +9,7 @@
 #include "processor.h"
 #include "widget.h"
 #include "widget-resolver.h"
-#include "widget-stream.h"
+#include "async.h"
 #include "global.h"
 
 #include <assert.h>
@@ -19,8 +19,61 @@ struct inline_widget {
     struct processor_env *env;
     struct widget *widget;
 
-    struct widget_stream *stream;
+    istream_t delayed;
 };
+
+
+static void
+inline_widget_close(struct inline_widget *iw)
+{
+    /* clear the delayed async_ref object: we didn't provide an
+       istream to the delayed object, and if we close it right now, it
+       will trigger the async_abort(), unless we clear its
+       async_ref */
+    async_ref_clear(istream_delayed_async_ref(iw->delayed));
+
+    istream_free(&iw->delayed);
+}
+
+/*
+ * HTTP response handler
+ *
+ */
+
+static void
+inline_widget_response(__attr_unused http_status_t status,
+                       __attr_unused struct strmap *headers,
+                       istream_t body, void *ctx)
+{
+    struct inline_widget *iw = ctx;
+
+    if (body == NULL)
+        body = istream_null_new(iw->pool);
+
+    istream_delayed_set(iw->delayed, body);
+
+    if (istream_has_handler(iw->delayed))
+        istream_read(iw->delayed);
+}
+
+static void
+inline_widget_abort(void *ctx)
+{
+    struct inline_widget *iw = ctx;
+
+    inline_widget_close(iw);
+}
+
+const struct http_response_handler inline_widget_response_handler = {
+    .response = inline_widget_response,
+    .abort = inline_widget_abort,
+};
+
+
+/*
+ * internal
+ *
+ */
 
 static void
 inline_widget_set(struct inline_widget *iw)
@@ -34,9 +87,15 @@ inline_widget_set(struct inline_widget *iw)
     }
 
     widget_http_request(iw->pool, iw->widget, iw->env,
-                        &widget_stream_response_handler, iw->stream,
-                        widget_stream_async_ref(iw->stream));
+                        &inline_widget_response_handler, iw,
+                        istream_delayed_async_ref(iw->delayed));
 }
+
+
+/*
+ * Widget resolver callback
+ *
+ */
 
 static void
 class_lookup_callback(void *_ctx)
@@ -46,10 +105,15 @@ class_lookup_callback(void *_ctx)
     if (iw->widget->class != NULL) {
         inline_widget_set(iw);
     } else {
-        async_ref_clear(widget_stream_async_ref(iw->stream));
-        istream_free(&iw->stream->delayed);
+        inline_widget_close(iw);
     }
 }
+
+
+/*
+ * Constructor
+ *
+ */
 
 istream_t
 embed_inline_widget(pool_t pool, struct processor_env *env,
@@ -68,15 +132,15 @@ embed_inline_widget(pool_t pool, struct processor_env *env,
     iw->pool = pool;
     iw->env = env;
     iw->widget = widget;
-    iw->stream = widget_stream_new(pool);
-    hold = istream_hold_new(pool, iw->stream->delayed);
+    iw->delayed = istream_delayed_new(pool);
+    hold = istream_hold_new(pool, iw->delayed);
 
     if (widget->class == NULL)
         widget_resolver_new(pool, env->pool,
                             widget,
                             global_translate_cache,
                             class_lookup_callback, iw,
-                            widget_stream_async_ref(iw->stream));
+                            istream_delayed_async_ref(iw->delayed));
     else
         inline_widget_set(iw);
 
