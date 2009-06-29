85c3eca65bc30e6b35c64d460c9175fccbf1169f
Max Kellermann <mk@cm4all.com>
2009-06-29 16:18:37 +0000

    widget-http.c: moved widget_response_format() to inline-widget.c

--- src/inline-widget.c
+++ src/inline-widget.c
@@ -11,6 +11,11 @@
 #include "widget-resolver.h"
 #include "async.h"
 #include "global.h"
+#include "http-util.h"
+#include "strref-pool.h"
+#include "strref2.h"
+
+#include <daemon/log.h>
 
 #include <assert.h>
 
@@ -35,6 +40,73 @@ inline_widget_close(struct inline_widget *iw)
     istream_free(&iw->delayed);
 }
 
+/**
+ * Ensure that a widget has the correct type for embedding it into a
+ * HTML/XML document.  Returns NULL (and closes body) if that is
+ * impossible.
+ */
+static istream_t
+widget_response_format(pool_t pool, const struct widget *widget,
+                       struct strmap **headers_r, istream_t body)
+{
+    struct strmap *headers = *headers_r;
+    const char *content_type;
+    struct strref *charset, charset_buffer;
+
+    assert(body != NULL);
+
+    content_type = strmap_get_checked(headers, "content-type");
+
+    if (content_type == NULL || strncmp(content_type, "text/", 5) != 0) {
+        daemon_log(2, "widget '%s' sent non-text response\n",
+                   widget_path(widget));
+        istream_close(body);
+        return NULL;
+    }
+
+    charset = http_header_param(&charset_buffer, content_type, "charset");
+    if (charset != NULL && strref_lower_cmp_literal(charset, "utf-8") != 0 &&
+        strref_lower_cmp_literal(charset, "utf8") != 0) {
+        /* beng-proxy expects all widgets to send their HTML code in
+           utf-8; this widget however used a different charset.
+           Automatically convert it with istream_iconv */
+        const char *charset2 = strref_dup(pool, charset);
+        istream_t ic = istream_iconv_new(pool, body, "utf-8", charset2);
+        if (ic == NULL) {
+            daemon_log(2, "widget '%s' sent unknown charset '%s'\n",
+                       widget_path(widget), charset2);
+            istream_close(body);
+            return NULL;
+        }
+
+        daemon_log(6, "widget '%s': charset conversion '%s' -> utf-8\n",
+                   widget_path(widget), charset2);
+        body = ic;
+
+        headers = strmap_dup(pool, headers);
+        strmap_set(headers, "content-type", "text/html; charset=utf-8");
+    }
+
+    if (strncmp(content_type + 5, "html", 4) != 0 &&
+        strncmp(content_type + 5, "xml", 3) != 0) {
+        /* convert text to HTML */
+
+        daemon_log(6, "widget '%s': converting text to HTML\n",
+                   widget_path(widget));
+
+        body = istream_html_escape_new(pool, body);
+        body = istream_cat_new(pool,
+                               istream_string_new(pool,
+                                                  "<pre class=\"beng_text_widget\">"),
+                               body,
+                               istream_string_new(pool, "</pre>"),
+                               NULL);
+    }
+
+    *headers_r = headers;
+    return body;
+}
+
 /*
  * HTTP response handler
  *
@@ -42,12 +114,21 @@ inline_widget_close(struct inline_widget *iw)
 
 static void
 inline_widget_response(__attr_unused http_status_t status,
-                       __attr_unused struct strmap *headers,
+                       struct strmap *headers,
                        istream_t body, void *ctx)
 {
     struct inline_widget *iw = ctx;
 
-    if (body == NULL)
+    if (body != NULL) {
+        /* check if the content-type is correct for embedding into
+           a template, and convert if possible */
+        body = widget_response_format(iw->pool, iw->widget,
+                                      &headers, body);
+        if (body == NULL) {
+            inline_widget_close(iw);
+            return;
+        }
+    } else
         body = istream_null_new(iw->pool);
 
     istream_delayed_set(iw->delayed, body);
--- src/widget-http.c
+++ src/widget-http.c
@@ -10,9 +10,6 @@
 #include "session.h"
 #include "cookie-client.h"
 #include "async.h"
-#include "http-util.h"
-#include "strref2.h"
-#include "strref-pool.h"
 #include "dpool.h"
 #include "get.h"
 #include "fcache.h"
@@ -39,12 +36,6 @@ struct embed {
      * the next transformation to be applied to the widget response
      */
     const struct transformation *transformation;
-
-    /**
-     * is this widget standalone, i.e. not embedded in another
-     * container?
-     */
-    bool standalone;
 
     /**
      * An identifier for the source stream of the current
@@ -256,73 +247,6 @@ widget_response_redirect(struct embed *embed, const char *location,
     return true;
 }
 
-/**
- * Ensure that a widget has the correct type for embedding it into a
- * HTML/XML document.  Returns NULL (and closes body) if that is
- * impossible.
- */
-static istream_t
-widget_response_format(pool_t pool, const struct widget *widget,
-                       struct strmap **headers_r, istream_t body)
-{
-    struct strmap *headers = *headers_r;
-    const char *content_type;
-    struct strref *charset, charset_buffer;
-
-    assert(body != NULL);
-
-    content_type = strmap_get_checked(headers, "content-type");
-
-    if (content_type == NULL || strncmp(content_type, "text/", 5) != 0) {
-        daemon_log(2, "widget '%s' sent non-text response\n",
-                   widget_path(widget));
-        istream_close(body);
-        return NULL;
-    }
-
-    charset = http_header_param(&charset_buffer, content_type, "charset");
-    if (charset != NULL && strref_lower_cmp_literal(charset, "utf-8") != 0 &&
-        strref_lower_cmp_literal(charset, "utf8") != 0) {
-        /* beng-proxy expects all widgets to send their HTML code in
-           utf-8; this widget however used a different charset.
-           Automatically convert it with istream_iconv */
-        const char *charset2 = strref_dup(pool, charset);
-        istream_t ic = istream_iconv_new(pool, body, "utf-8", charset2);
-        if (ic == NULL) {
-            daemon_log(2, "widget '%s' sent unknown charset '%s'\n",
-                       widget_path(widget), charset2);
-            istream_close(body);
-            return NULL;
-        }
-
-        daemon_log(6, "widget '%s': charset conversion '%s' -> utf-8\n",
-                   widget_path(widget), charset2);
-        body = ic;
-
-        headers = strmap_dup(pool, headers);
-        strmap_set(headers, "content-type", "text/html; charset=utf-8");
-    }
-
-    if (strncmp(content_type + 5, "html", 4) != 0 &&
-        strncmp(content_type + 5, "xml", 3) != 0) {
-        /* convert text to HTML */
-
-        daemon_log(6, "widget '%s': converting text to HTML\n",
-                   widget_path(widget));
-
-        body = istream_html_escape_new(pool, body);
-        body = istream_cat_new(pool,
-                               istream_string_new(pool,
-                                                  "<pre class=\"beng_text_widget\">"),
-                               body,
-                               istream_string_new(pool, "</pre>"),
-                               NULL);
-    }
-
-    *headers_r = headers;
-    return body;
-}
-
 static bool
 processable(const struct strmap *headers)
 {
@@ -423,18 +347,6 @@ widget_response_dispatch(struct embed *embed, http_status_t status,
                                   body, transformation);
     } else {
         /* no transformation left */
-
-        if (body != NULL && !embed->widget->from_request.raw &&
-            !embed->standalone) {
-            /* check if the content-type is correct for embedding into
-               a template, and convert if possible */
-            body = widget_response_format(embed->pool, embed->widget,
-                                          &headers, body);
-            if (body == NULL) {
-                http_response_handler_invoke_abort(&embed->handler_ref);
-                return;
-            }
-        }
 
         /* finally pass the response to our handler */
         http_response_handler_invoke_response(&embed->handler_ref,
@@ -557,8 +469,6 @@ widget_http_request(pool_t pool, struct widget *widget,
         : NULL;
     embed->transformation = embed->widget->from_request.raw
         ? NULL : view->transformation;
-    embed->standalone = embed->widget->from_request.proxy ||
-        embed->widget->from_request.proxy_ref != NULL;
 
     headers = widget_request_headers(embed, widget->from_request.body != NULL);
 
