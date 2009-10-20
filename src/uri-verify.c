e6507c05771de041570fdda6766e87b81e4d119f
Max Kellermann <mk@cm4all.com>
2009-10-20 13:38:49 +0000

    uri-verify: added uri_path_verify_paranoid()

--- src/uri-verify.c
+++ src/uri-verify.c
@@ -51,3 +51,69 @@ uri_path_verify(const char *src, size_t length)
 
     return true;
 }
+
+static bool
+is_encoded_dot(const char *p)
+{
+    return p[0] == '%' && p[1] == '2' &&
+        (p[2] == 'e' || p[2] == 'E');
+}
+
+bool
+uri_path_verify_paranoid(const char *uri)
+{
+    if (uri[0] == '.' &&
+        (uri[1] == 0 || uri[1] == '/' ||
+         (uri[1] == '.' && (uri[2] == 0 || uri[2] == '/')) ||
+         is_encoded_dot(uri + 1)))
+        /* no ".", "..", "./", "../" */
+        return false;
+
+    if (is_encoded_dot(uri))
+        return false;
+
+    while (*uri != 0) {
+        if (*uri == '%') {
+            ++uri;
+
+            if (uri[0] == '0' && uri[1] == '0')
+                /* don't allow an encoded NUL character */
+                return false;
+
+            if (uri[0] == '2' && (uri[1] == 'f' || uri[1] == 'F'))
+                /* don't allow an encoded slash (somebody trying to
+                   hide a hack?) */
+                return false;
+        } else if (*uri == '/') {
+            ++uri;
+
+            if (*uri == '/')
+                return false;
+
+            if (is_encoded_dot(uri))
+                /* encoded dot after a slash - what's this client
+                   trying to hide? */
+                return false;
+
+            if (*uri == '.') {
+                ++uri;
+
+                if (is_encoded_dot(uri))
+                    /* encoded dot after a real dot - smells fishy */
+                    return false;
+
+                if (*uri == 0 || *uri == '/')
+                    return false;
+
+                if (*uri == '.')
+                    /* disallow two dots after a slash, even if
+                       something else follows - this is the paranoid
+                       function after all! */
+                    return false;
+            }
+        } else
+            ++uri;
+    }
+
+    return true;
+}
--- src/uri-verify.h
+++ src/uri-verify.h
@@ -22,4 +22,19 @@ uri_segment_verify(const char *src, const char *end);
 bool
 uri_path_verify(const char *src, size_t length);
 
+/**
+ * Performs some paranoid checks on the URI; the following is not
+ * allowed:
+ *
+ * - %00
+ * - %2f (encoded slash)
+ * - "/../", "/./"
+ * - "/..", "/." at the end
+ *
+ * It is assumed that the URI was already verified with
+ * uri_path_verify().
+ */
+bool
+uri_path_verify_paranoid(const char *uri);
+
 #endif
