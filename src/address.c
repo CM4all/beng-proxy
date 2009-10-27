9a3720ae6efdfbc59bac19435ec6808fd6d14b10
Max Kellermann <mk@cm4all.com>
2009-10-27 15:09:51 +0000

    address: use libsocket, removed duplicate code

--- src/address.c
+++ src/address.c
@@ -6,45 +6,16 @@
 
 #include "address.h"
 
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <string.h>
-
-static const struct sockaddr *
-ipv64_normalize_mapped(const struct sockaddr *addr, size_t *len) {
-    const struct sockaddr_in6 *a6 = (const struct sockaddr_in6 *)addr;
-    static struct sockaddr_in a4;
-    struct in_addr inaddr;
-    u_int16_t port;
-
-    if (addr->sa_family != AF_INET6 || !IN6_IS_ADDR_V4MAPPED(&a6->sin6_addr))
-        return addr;
-
-    memcpy(&inaddr, ((const char *)&a6->sin6_addr) + 12, sizeof(inaddr));
-    port = a6->sin6_port;
-
-    a4.sin_family = AF_INET;
-    *len = sizeof(a4);
-    memcpy(&a4.sin_addr, &inaddr, sizeof(inaddr));
-    a4.sin_port = (in_port_t)port;
-
-    return (const struct sockaddr *)&a4;
-}                                                  
+#include <socket/address.h>
 
 const char *
 address_to_string(pool_t pool, const struct sockaddr *addr, size_t addrlen)
 {
-    int ret;
-    char host[512], serv[16];
+    bool success;
+    char host[512];
 
-    addr = ipv64_normalize_mapped(addr, &addrlen);
-
-    ret = getnameinfo(addr, addrlen,
-                      host, sizeof(host),
-                      serv, sizeof(serv),
-                      NI_NUMERICHOST | NI_NUMERICSERV);
-    if (ret != 0)
+    success = socket_address_to_string(host, sizeof(host), addr, addrlen);
+    if (!success)
         return NULL;
 
     return p_strdup(pool, host);
