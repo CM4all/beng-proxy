3f2b71f202e1087351add4ecf40982f3856b9d24
Max Kellermann <mk@cm4all.com>
2009-11-05 13:59:39 +0000

    header-forward: added header_forward_settings

--- src/header-forward.h
+++ src/header-forward.h
@@ -9,6 +9,56 @@
 
 #include "pool.h"
 
+/**
+ * How is a specific set of headers forwarded?
+ */
+enum header_forward_mode {
+    /**
+     * Do not forward at all.
+     */
+    HEADER_FORWARD_NO,
+
+    /**
+     * Forward it as-is.
+     */
+    HEADER_FORWARD_YES,
+
+    /**
+     * Forward it, but mangle it.  Example: cookie headers are handled
+     * by
+     */
+    HEADER_FORWARD_MANGLE,
+};
+
+struct header_forward_settings {
+    /**
+     * Reveal the identity of the real communication partner?  This
+     * affects "Via", "X-Forwarded-For".
+     */
+    enum header_forward_mode identity;
+
+    /**
+     * Forward headers showing the capabilities of the real
+     * communication partner?  Affects "Server", "User-Agent",
+     * "Accept-*" and others.
+     *
+     * Note that the "Server" response header is always sent, even
+     * when this attribute is set to #HEADER_FORWARD_NO.
+     */
+    enum header_forward_mode capabilities;
+
+    /**
+     * Forward cookie headers?
+     */
+    enum header_forward_mode cookie;
+
+    /**
+     * Forwarding mode for "other" headers: headers not explicitly
+     * handled here.  This does not include hop-by-hop headers.
+     */
+    enum header_forward_mode other;
+};
+
 struct session;
 
 struct strmap *
