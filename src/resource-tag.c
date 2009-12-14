5fb7ca6a1fdbe83f4c7ea004b761ad6664568bfd
Max Kellermann <mk@cm4all.com>
2009-12-14 12:38:42 +0000

    resource-tag: check for "Cache-Control: no-store"

--- src/resource-tag.c
+++ src/resource-tag.c
@@ -6,18 +6,25 @@
 
 #include "resource-tag.h"
 #include "strmap.h"
+#include "http-util.h"
 
 const char *
 resource_tag_append_etag(pool_t pool, const char *tag,
                          const struct strmap *headers)
 {
-    const char *etag;
+    const char *etag, *p;
 
     if (tag == NULL || headers == NULL)
         return NULL;
 
     etag = strmap_get(headers, "etag");
     if (etag == NULL)
+        return NULL;
+
+    p = strmap_get(headers, "cache-control");
+    if (p != NULL && http_list_contains(p, "no-store"))
+        /* generating a resource tag for the cache is pointless,
+           because we are not allowed to store the response anyway */
         return NULL;
 
     return p_strcat(pool, tag, "|etag=", etag, NULL);
