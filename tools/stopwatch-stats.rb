eba18893b52243e8e6042d1e6356af109a52ab3f
Max Kellermann <mk@cm4all.com>
2009-12-16 11:41:18 +0000

    tools/stopwatch-stats.rb: show pipe filters

--- tools/stopwatch-stats.rb
+++ tools/stopwatch-stats.rb
@@ -12,39 +12,44 @@ ARGF.each do |line|
     line.sub!(/^@[0-9a-f]+ /, '')
 
     # only stopwatch lines
-    next unless line =~ /^stopwatch\[(\S+) ([^\]]+)\]: .*headers=(\d+)ms/
-    host, uri, t = $1, $2, $3.to_i
+    if line =~ /^stopwatch\[(\S+) ([^\]]+)\]: .*headers=(\d+)ms/
+        host, uri, t = $1, $2, $3.to_i
 
-    # no translation server stats
-    next if host =~ /^localhost:/
+        # no translation server stats
+        next if host =~ /^localhost:/
 
-    # remove some useless query parameters
-    uri.gsub!(/(\/(?:config|preview|show|thumbnail)).*/, '\1')
-    uri.gsub!(/(?:beng_session|step|u|d|kc|k|\w+Id|max\w+|domain|path|address|companyname|type|guestbookId|plz|width|page_no|cc|wiid|effect|generic|search|firstResult|maxResults|descending|orderBy|header|project|parentStateId|_statemachineId|parentStatemachineId|color|trigger|direction|isWizard|preset|output|referrer|redirect|ctimestamp|name|email|entries_per_page|text|homepage|cm-[-\w]+)=[-\w\+%\$]*/, '')
-    uri.gsub!(/\/(pageId|skin|wcid|initParams|bodyonly)=[\w.$]*/, '')
+        # remove some useless query parameters
+        uri.gsub!(/(\/(?:config|preview|show|thumbnail)).*/, '\1')
+        uri.gsub!(/(?:beng_session|step|u|d|kc|k|\w+Id|max\w+|domain|path|address|companyname|type|guestbookId|plz|width|page_no|cc|wiid|effect|generic|search|firstResult|maxResults|descending|orderBy|header|project|parentStateId|_statemachineId|parentStatemachineId|color|trigger|direction|isWizard|preset|output|referrer|redirect|ctimestamp|name|email|entries_per_page|text|homepage|cm-[-\w]+)=[-\w\+%\$]*/, '')
+        uri.gsub!(/\/(pageId|skin|wcid|initParams|bodyonly)=[\w.$]*/, '')
 
-    # little bit of cleanup
-    uri.gsub!(/&{2,}/, '&')
-    uri.gsub!(/\/{2,}/, '/')
-    uri.gsub!(/\?&/, '?')
-    uri.gsub!(/[&?]$/, '')
+        # little bit of cleanup
+        uri.gsub!(/&{2,}/, '&')
+        uri.gsub!(/\/{2,}/, '/')
+        uri.gsub!(/\?&/, '?')
+        uri.gsub!(/[&?]$/, '')
 
-    # some special cases
-    uri.sub!(/((?:Widget|statistics)\/show|sam\/jump\.cls|photo\/(?:CONFIG|SHOW))\?.*/, '\1')
-    uri.sub!(/(GuestbookPublic\.cls\?subaction=(?:addEntry|getList))&.*/, '\1')
-    uri.sub!(/(Download\.cls)\?.*/, '\1')
-    uri.sub!(/(wcid=cm4all.com.widgets.PhotoAlbum\/).*/, '\1')
-    uri.sub!(/(SM_smID_\d+.cls).*/, '\1')
-    uri.sub!(/(Editor\.cls\/action=[^\/]+).*/, '\1')
-    uri.sub!(/(\/action=(?:getThumbnail|create))\/.*/, '\1')
-    uri.sub!(/(\/res\/).*/, '\1')
-    uri.sub!(/(\/pid)\/p\d+/, '\1')
-    uri.sub!(/^(\/widgetserver\/.+\.action).*/, '\1')
-    uri.sub!(/^(\/widgetserver\/\w+\/static\/).*/, '\1')
-    uri.sub!(/^(\/login-service\/service\/(?:cred|i|sam)\/).*/, '\1')
+        # some special cases
+        uri.sub!(/((?:Widget|statistics)\/show|sam\/jump\.cls|photo\/(?:CONFIG|SHOW))\?.*/, '\1')
+        uri.sub!(/(GuestbookPublic\.cls\?subaction=(?:addEntry|getList))&.*/, '\1')
+        uri.sub!(/(Download\.cls)\?.*/, '\1')
+        uri.sub!(/(wcid=cm4all.com.widgets.PhotoAlbum\/).*/, '\1')
+        uri.sub!(/(SM_smID_\d+.cls).*/, '\1')
+        uri.sub!(/(Editor\.cls\/action=[^\/]+).*/, '\1')
+        uri.sub!(/(\/action=(?:getThumbnail|create))\/.*/, '\1')
+        uri.sub!(/(\/res\/).*/, '\1')
+        uri.sub!(/(\/pid)\/p\d+/, '\1')
+        uri.sub!(/^(\/widgetserver\/.+\.action).*/, '\1')
+        uri.sub!(/^(\/widgetserver\/\w+\/static\/).*/, '\1')
+        uri.sub!(/^(\/login-service\/service\/(?:cred|i|sam)\/).*/, '\1')
 
-    # no static files
-    next if uri =~ /\.(?:png|jpg|js|css|gif|conf|ico)(?:\?.*)?$/
+        # no static files
+        next if uri =~ /\.(?:png|jpg|js|css|gif|conf|ico)(?:\?.*)?$/
+    elsif line =~ /^stopwatch\[(\/[^\]]+)\]: fork=.* end=(\d+)ms/
+        uri, t = $1, $2.to_i
+    else
+        next
+    end
 
     # insert this data point into the hash
     times = data[uri]
