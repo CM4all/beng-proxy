efd90f8df7e24eb1ae15b0d99d45a97a27b3cc5c
Max Kellermann <mk@cm4all.com>
2010-03-18 16:37:30 +0000

    fcgi-client: support chunked request body
    Disable the check, which I suppose was not correct.

--- src/fcgi-client.c
+++ src/fcgi-client.c
@@ -661,16 +661,6 @@ fcgi_client_request(pool_t caller_pool, int fd,
                     void *handler_ctx,
                     struct async_operation_ref *async_ref)
 {
-    off_t available = -1;
-    if (body != NULL) {
-        available = istream_available(body, false);
-        if (available == -1) {
-            daemon_log(1, "Unknown FastCGI request body length\n");
-            http_response_handler_direct_abort(handler, handler_ctx);
-            return;
-        }
-    }
-
     static unsigned next_request_id = 1;
     ++next_request_id;
 
@@ -733,6 +723,9 @@ fcgi_client_request(pool_t caller_pool, int fd,
                           "SERVER_SOFTWARE", "beng-proxy v" VERSION,
                           NULL);
 
+    off_t available = body != NULL
+        ? istream_available(body, false)
+        : -1;
     if (available >= 0) {
         char value[64];
         snprintf(value, sizeof(value),
