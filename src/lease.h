53440fcbdee67886472fc00e5ced578722fcea66
Max Kellermann <mk@cm4all.com>
2010-05-17 22:59:04 +0200

    lease: added lease_direct_release()

--- src/lease.h
+++ src/lease.h
@@ -41,12 +41,18 @@ lease_ref_poison(struct lease_ref *lease_ref)
 }
 
 static inline void
+lease_direct_release(const struct lease *lease, void *ctx, bool reuse)
+{
+    lease->release(reuse, ctx);
+}
+
+static inline void
 lease_release(struct lease_ref *lease_ref, bool reuse)
 {
     assert(lease_ref != NULL);
     assert(lease_ref->lease != NULL);
 
-    lease_ref->lease->release(reuse, lease_ref->ctx);
+    lease_direct_release(lease_ref->lease, lease_ref->ctx, reuse);
     lease_ref_poison(lease_ref);
 }
 
