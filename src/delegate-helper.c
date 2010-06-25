badef46fb7387c13a3d4d3e01b639267a5b3c672
Max Kellermann <max@duempel.org>
2010-06-25 12:06:02 +0200

    delegate-helper: move code to functions

--- src/delegate-helper.c
+++ src/delegate-helper.c
@@ -26,13 +26,104 @@ enum {
 };
 #endif
 
+static bool
+delegate_send(const void *data, size_t length)
+{
+    ssize_t nbytes = send(0, data, length, 0);
+    if (nbytes < 0) {
+        fprintf(stderr, "send() on delegate socket failed: %s\n",
+                strerror(errno));
+        return false;
+    }
+
+    if ((size_t)nbytes != length) {
+        fprintf(stderr, "short send() on delegate socket\n");
+        return false;
+    }
+
+    return true;
+}
+
+static bool
+delegate_send_header(unsigned command)
+{
+    const struct delegate_header header = {
+        .length = 0,
+        .command = command,
+    };
+
+    return delegate_send(&header, sizeof(header));
+}
+
+static bool
+delegate_send_fd(unsigned command, int fd)
+{
+    struct delegate_header header = {
+        .length = 0,
+        .command = command,
+    };
+    struct iovec vec = {
+        .iov_base = &header,
+        .iov_len = sizeof(header),
+    };
+
+    char ccmsg[CMSG_SPACE(sizeof(fd))];
+    struct msghdr msg = {
+        .msg_name = NULL,
+        .msg_namelen = 0,
+        .msg_iov = &vec,
+        .msg_iovlen = 1,
+        .msg_control = ccmsg,
+        .msg_controllen = CMSG_LEN(sizeof(fd)),
+        .msg_flags = 0,
+    };
+    struct cmsghdr *cmsg;
+
+    cmsg = CMSG_FIRSTHDR(&msg);
+    cmsg->cmsg_level = SOL_SOCKET;
+    cmsg->cmsg_type = SCM_RIGHTS;
+    cmsg->cmsg_len = msg.msg_controllen;
+
+    *(int *)CMSG_DATA(cmsg) = fd;
+
+    if (sendmsg(0, &msg, 0) < 0) {
+        fprintf(stderr, "failed to send fd: %s\n", strerror(errno));
+        return false;
+    }
+
+    return true;
+}
+
+static bool
+delegate_handle_open(const char *payload)
+{
+    int fd = open(payload, O_RDONLY|O_CLOEXEC|O_NOCTTY);
+    if (fd >= 0) {
+        bool success = delegate_send_fd(0, fd);
+        close(fd);
+        return success;
+    } else {
+        /* error: send error code to client */
+
+        return delegate_send_header(errno);
+    }
+}
+
+static bool
+delegate_handle(unsigned command, const char *payload, size_t length)
+{
+    (void)command;
+    (void)length;
+
+    return delegate_handle_open(payload);
+}
+
 int main(int argc __attr_unused, char **argv __attr_unused)
 {
     struct delegate_header header;
     ssize_t nbytes;
     char payload[4096];
     size_t length;
-    int fd;
 
     while (true) {
         nbytes = recv(0, &header, sizeof(header), 0);
@@ -74,60 +165,9 @@ int main(int argc __attr_unused, char **argv __attr_unused)
 
         payload[length] = 0;
 
-        fd = open(payload, O_RDONLY|O_CLOEXEC|O_NOCTTY);
-        if (fd >= 0) {
-            struct iovec vec = {
-                .iov_base = &header,
-                .iov_len = sizeof(header),
-            };
-            char ccmsg[CMSG_SPACE(sizeof(fd))];
-            struct msghdr msg = {
-                .msg_name = NULL,
-                .msg_namelen = 0,
-                .msg_iov = &vec,
-                .msg_iovlen = 1,
-                .msg_control = ccmsg,
-                .msg_controllen = CMSG_LEN(sizeof(fd)),
-                .msg_flags = 0,
-            };
-            struct cmsghdr *cmsg;
-            int ret;
-
-            header.length = 0;
-            header.command = 0;
-
-            cmsg = CMSG_FIRSTHDR(&msg);
-            cmsg->cmsg_level = SOL_SOCKET;
-            cmsg->cmsg_type = SCM_RIGHTS;
-            cmsg->cmsg_len = msg.msg_controllen;
-
-            int *fd_p = (int *)CMSG_DATA(cmsg);
-            *fd_p = fd;
-
-            ret = sendmsg(0, &msg, 0);
-            if (ret < 0) {
-                fprintf(stderr, "failed to send fd: %s\n", strerror(errno));
-                return 2;
-            }
-
-            close(fd);
-        } else {
-            /* error: send error code to client */
-
-            header.length = 0;
-            header.command = errno;
-
-            nbytes = send(0, &header, sizeof(header), 0);
-            if (nbytes < 0) {
-                fprintf(stderr, "send() on delegate socket failed: %s\n",
-                        strerror(errno));
-                return 2;
-            }
-
-            if ((size_t)nbytes != sizeof(header)) {
-                fprintf(stderr, "short send() on delegate socket\n");
-                return 2;
-            }
-        }
+        if (!delegate_handle(header.command, payload, length))
+            return 2;
     }
+
+    return 0;
 }
