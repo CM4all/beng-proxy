e1ade49a4d5ae440ad49cae2adb83e84e4501630
Max Kellermann <mk@cm4all.com>
2010-08-09 18:59:45 +0200

    control-handler: use udp_listener directly

--- src/control-handler.c
+++ src/control-handler.c
@@ -6,10 +6,12 @@
 
 #include "control-handler.h"
 #include "control-server.h"
+#include "udp-listener.h"
 #include "instance.h"
 
 #include <daemon/log.h>
 
+#include <glib.h>
 #include <assert.h>
 #include <arpa/inet.h>
 
@@ -31,7 +33,18 @@ static const struct control_handler global_control_handler = {
     .packet = global_control_packet,
 };
 
-static struct control_server *global_control_server;
+static void
+global_control_udp_callback(const void *data, size_t length,
+                            G_GNUC_UNUSED const struct sockaddr *addr,
+                            G_GNUC_UNUSED size_t addrlen,
+                            void *ctx)
+{
+    struct instance *instance = ctx;
+
+    control_server_decode(data, length, &global_control_handler, instance);
+}
+
+static struct udp_listener *global_udp_listener;
 
 bool
 global_control_handler_init(pool_t pool, struct instance *instance)
@@ -39,21 +52,29 @@ global_control_handler_init(pool_t pool, struct instance *instance)
     if (instance->config.control_listen == NULL)
         return true;
 
-    struct in_addr group;
-    if (instance->config.multicast_group != NULL)
-        group.s_addr = inet_addr(instance->config.multicast_group);
+    global_udp_listener =
+        udp_listener_port_new(pool, instance->config.control_listen, 5478,
+                              global_control_udp_callback, instance);
+    if (global_udp_listener == NULL)
+        return false;
 
-    global_control_server =
-        control_server_new(pool, instance->config.control_listen, 5478,
-                           instance->config.multicast_group != NULL
-                           ? &group : NULL,
-                           &global_control_handler, instance);
-    return global_control_server != NULL;
+    if (instance->config.multicast_group != NULL) {
+        const struct in_addr group = {
+            .s_addr = inet_addr(instance->config.multicast_group),
+        };
+
+        if (!udp_listener_join4(global_udp_listener, &group)) {
+            udp_listener_free(global_udp_listener);
+            return false;
+        }
+    }
+
+    return true;
 }
 
 void
 global_control_handler_deinit(void)
 {
-    if (global_control_server != NULL)
-        control_server_free(global_control_server);
+    if (global_udp_listener != NULL)
+        udp_listener_free(global_udp_listener);
 }
