73d7138f1f69fb51382b07b68fcfbdb0cacd051f
Max Kellermann <mk@cm4all.com>
2010-12-02 08:06:19 +0100

    tcache: add API documentation

--- src/tcache.h
+++ src/tcache.h
@@ -19,9 +19,17 @@ translate_cache_new(pool_t pool, struct tstock *stock,
 void
 translate_cache_close(struct tcache *tcache);
 
+/**
+ * Flush all items from the cache.
+ */
 void
 translate_cache_flush(struct tcache *tcache);
 
+/**
+ * Query an item from the cache.  If not present, the request is
+ * forwarded to the "real" translation server, and its response is
+ * added to the cache.
+ */
 void
 translate_cache(pool_t pool, struct tcache *tcache,
                 const struct translate_request *request,
@@ -29,6 +37,13 @@ translate_cache(pool_t pool, struct tcache *tcache,
                 void *ctx,
                 struct async_operation_ref *async_ref);
 
+/**
+ * Flush selected items from the cache.
+ *
+ * @param request a request with parameters to compare with
+ * @param vary a list of #beng_translation_command codes which define
+ * the cache item filter
+ */
 void
 translate_cache_invalidate(struct tcache *tcache,
                            const struct translate_request *request,
