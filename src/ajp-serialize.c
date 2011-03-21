434fc2fa2f57fab73253fc8c78badf6127ad0ae6
Max Kellermann <mk@cm4all.com>
2011-03-21 08:40:24 +0100

    ajp-serialize: support NULL strings

--- src/ajp-serialize.c
+++ src/ajp-serialize.c
@@ -15,11 +15,20 @@
 void
 serialize_ajp_string(struct growing_buffer *gb, const char *s)
 {
+    if (s == NULL) {
+        /* 0xffff means NULL; this is not documented, I have
+           determined it from a wireshark dump */
+
+        uint16_t *p = growing_buffer_write(gb, 2);
+        *p = 0xffff;
+        return;
+    }
+
     size_t length = strlen(s);
     char *p;
 
-    if (length > 0xffff)
-        length = 0xffff; /* XXX too long, cut off */
+    if (length > 0xfffe)
+        length = 0xfffe; /* XXX too long, cut off */
 
     p = growing_buffer_write(gb, 2 + length + 1);
     *(uint16_t*)p = htons(length);
@@ -45,6 +54,11 @@ const char *
 deserialize_ajp_string(struct strref *input)
 {
     size_t length = deserialize_uint16(input);
+    if (length == 0xffff)
+        /* 0xffff means NULL; this is not documented, I have
+           determined it from a wireshark dump */
+        return NULL;
+
     const char *value;
 
     if (input->length <= length || input->data[length] != 0) {
