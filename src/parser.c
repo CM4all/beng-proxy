a429ad1dbc2c10c3fe4ed8670d18894e2508c4b3
Max Kellermann <mk@cm4all.com>
2011-08-31 15:28:08 +0200

    parser: retain TAG_PI for finishing the processing instruction

--- src/parser.c
+++ src/parser.c
@@ -233,9 +233,12 @@ parser_feed(struct parser *parser, const char *start, size_t length)
             do {
                 if (char_is_whitespace(*buffer)) {
                     ++buffer;
-                } else if ((*buffer == '/' && parser->tag.type == TAG_OPEN) ||
-                           (*buffer == '?' && parser->tag.type == TAG_PI)) {
+                } else if (*buffer == '/' && parser->tag.type == TAG_OPEN) {
                     parser->tag.type = TAG_SHORT;
+                    parser->state = PARSER_SHORT;
+                    ++buffer;
+                    break;
+                } else if (*buffer == '?' && parser->tag.type == TAG_PI) {
                     parser->state = PARSER_SHORT;
                     ++buffer;
                     break;
