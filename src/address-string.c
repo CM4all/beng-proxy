2170cdca4b58d113c6968bf5716fdf4fdc38df9f
Max Kellermann <mk@cm4all.com>
2011-09-07 10:36:57 +0200

    address-string: add support for local sockets

--- src/address-string.c
+++ src/address-string.c
@@ -10,10 +10,31 @@
 
 #include <netdb.h>
 #include <string.h>
+#include <sys/un.h>
+
+static struct address_envelope *
+address_envelope_sun(struct pool *pool, const char *path)
+{
+    struct sockaddr_un *sun;
+
+    size_t length = sizeof(*sun) - sizeof(sun->sun_path) + strlen(path) + 1;
+    struct address_envelope *envelope =
+        p_malloc(pool, sizeof(*envelope) - sizeof(envelope->address) + length);
+    envelope->length = length;
+
+    sun = (struct sockaddr_un *)&envelope->address;
+    sun->sun_family = AF_UNIX;
+    strcpy(sun->sun_path, path);
+
+    return envelope;
+}
 
 struct address_envelope *
 address_envelope_parse(struct pool *pool, const char *p, int default_port)
 {
+    if (*p == '/')
+        return address_envelope_sun(pool, p);
+
     static const struct addrinfo hints = {
         .ai_flags = AI_NUMERICHOST,
     };
