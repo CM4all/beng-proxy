be43fa2e734013727cca538f16557b716ea178f6
Max Kellermann <mk@cm4all.com>
2011-11-16 13:09:59 +0100

    widget: un-inline widget_init()
    This function is rather large, and should be called.

--- src/widget.h
+++ src/widget.h
@@ -116,35 +116,9 @@ struct widget_ref {
 #define WIDGET_REF_SEPARATOR ':'
 #define WIDGET_REF_SEPARATOR_S ":"
 
-static inline void
+void
 widget_init(struct widget *widget, struct pool *pool,
-            const struct widget_class *class)
-{
-    list_init(&widget->children);
-    widget->parent = NULL;
-    widget->pool = pool;
-
-    widget->class_name = NULL;
-    widget->class = class;
-    widget->resolver = NULL;
-    widget->id = NULL;
-    widget->display = WIDGET_DISPLAY_INLINE;
-    widget->path_info = "";
-    widget->query_string = NULL;
-    widget->headers = NULL;
-    widget->view = NULL;
-    widget->session = WIDGET_SESSION_RESOURCE;
-    widget->from_request.focus_ref = NULL;
-    widget->from_request.path_info = NULL;
-    strref_clear(&widget->from_request.query_string);
-    widget->from_request.method = HTTP_METHOD_GET;
-    widget->from_request.body = NULL;
-    widget->from_request.view = NULL;
-    widget->lazy.path = NULL;
-    widget->lazy.prefix = NULL;
-    widget->lazy.address = NULL;
-    widget->lazy.stateless_address = NULL;
-}
+            const struct widget_class *class);
 
 void
 widget_set_id(struct widget *widget, struct pool *pool, const struct strref *id);
