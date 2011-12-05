370075314c343b1c5a696353347345bdc3c8829f
Max Kellermann <mk@cm4all.com>
2011-12-05 17:58:47 +0100

    test/t-http-client: check for fatal errors in child processes

--- test/t-http-client.c
+++ test/t-http-client.c
@@ -14,6 +14,7 @@
 
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -872,4 +873,9 @@ int main(int argc, char **argv) {
 
     event_base_free(event_base);
     direct_global_deinit();
+
+    int status;
+    while (wait(&status) > 0) {
+        assert(!WIFSIGNALED(status));
+    }
 }
