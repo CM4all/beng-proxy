b2439dc2c9abfe3bc3b0bcc19b595a6d42dcd7c5
Max Kellermann <mk@cm4all.com>
2012-02-15 11:33:50 +0100

    cgi: move code to cgi-client.c

---
+++ src/cgi-client.h
@@ -0,0 +1,27 @@
+/*
+ * Communicate with a CGI script.
+ *
+ * author: Max Kellermann <mk@cm4all.com>
+ */
+
+#ifndef BENG_PROXY_CGI_CLIENT_H
+#define BENG_PROXY_CGI_CLIENT_H
+
+#include <http/method.h>
+
+struct pool;
+struct stopwatch;
+struct istream;
+struct http_response_handler;
+struct async_operation_ref;
+
+/**
+ * @param input the stream received from the child process
+ */
+void
+cgi_client_new(struct pool *pool, struct stopwatch *stopwatch,
+               struct istream *input,
+               const struct http_response_handler *handler, void *handler_ctx,
+               struct async_operation_ref *async_ref);
+
+#endif
