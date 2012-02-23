5ee5c959f53e3c0ce1f674ad1a99b3e3560634f0
Max Kellermann <mk@cm4all.com>
2012-02-23 17:56:42 +0100

    resource-address: fix regression when CGI URI is not set

--- src/resource-address.c
+++ src/resource-address.c
@@ -245,7 +245,9 @@ resource_address_save_base(pool_t pool, struct resource_address *dest,
         if (src->u.cgi.path_info == NULL)
             return NULL;
 
-        uri_length = base_string_unescape(pool, src->u.cgi.uri, suffix);
+        uri_length = src->u.cgi.uri != NULL
+            ? base_string_unescape(pool, src->u.cgi.uri, suffix)
+            : 0;
         if (uri_length == (size_t)-1)
             return NULL;
 
@@ -254,7 +256,8 @@ resource_address_save_base(pool_t pool, struct resource_address *dest,
             return NULL;
 
         resource_address_copy(pool, dest, src);
-        dest->u.cgi.uri = p_strndup(pool, dest->u.cgi.uri, uri_length);
+        if (dest->u.cgi.uri != NULL)
+            dest->u.cgi.uri = p_strndup(pool, dest->u.cgi.uri, uri_length);
         dest->u.cgi.path_info = p_strndup(pool, dest->u.cgi.path_info, length);
         return dest;
 
@@ -314,8 +317,9 @@ resource_address_load_base(pool_t pool, struct resource_address *dest,
         unescaped[uri_unescape_inplace(unescaped, strlen(unescaped), '%')] = 0;
 
         resource_address_copy(pool, dest, src);
-        dest->u.cgi.uri = p_strcat(pool, dest->u.cgi.uri,
-                                   unescaped, NULL);
+        if (dest->u.cgi.uri != NULL)
+            dest->u.cgi.uri = p_strcat(pool, dest->u.cgi.uri,
+                                       unescaped, NULL);
         dest->u.cgi.path_info = p_strcat(pool, dest->u.cgi.path_info,
                                          unescaped, NULL);
         return dest;
