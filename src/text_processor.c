62556b18127031332109331decf086ca19ec7845
Max Kellermann <mk@cm4all.com>
2012-03-23 11:47:32 +0100

    processor: add entities &c:id; &c:type; &c:class;

--- src/text_processor.c
+++ src/text_processor.c
@@ -88,7 +88,11 @@ processor_subst_beng_widget(struct istream *istream,
                             const struct widget *widget,
                             const struct processor_env *env)
 {
+    istream_subst_add(istream, "&c:type;", widget->class_name);
+    istream_subst_add(istream, "&c:class;",
+                      widget_get_quoted_class_name(widget));
     istream_subst_add(istream, "&c:local;", widget->class->local_uri);
+    istream_subst_add(istream, "&c:id;", widget->id);
     istream_subst_add(istream, "&c:path;", widget_path(widget));
     istream_subst_add(istream, "&c:prefix;", widget_prefix(widget));
     istream_subst_add(istream, "&c:uri;", env->absolute_uri);
