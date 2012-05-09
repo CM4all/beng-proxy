942e9ba47c1c249ccff52b78ef759a049c7c4794
Max Kellermann <mk@cm4all.com>
2012-05-09 13:34:24 +0200

    test/t-uri-address: new unit test

---
+++ test/t-uri-address.c
@@ -0,0 +1,51 @@
+#include "uri-address.h"
+#include "pool.h"
+
+#include <assert.h>
+#include <string.h>
+
+static void
+test_apply(struct pool *pool)
+{
+    struct uri_with_address *a =
+        uri_address_parse(pool, "http://localhost/foo", NULL);
+    assert(a != NULL);
+    assert(a->scheme == URI_SCHEME_HTTP);
+    assert(a->host_and_port != NULL);
+    assert(strcmp(a->host_and_port, "localhost") == 0);
+    assert(strcmp(a->path, "/foo") == 0);
+
+    const struct uri_with_address *b = uri_address_apply(pool, a, "", 0);
+    assert(b != NULL);
+    assert(b->scheme == a->scheme);
+    assert(strcmp(b->host_and_port, a->host_and_port) == 0);
+    assert(strcmp(b->path, "/foo") == 0);
+
+    b = uri_address_apply(pool, a, "bar", 3);
+    assert(b != NULL);
+    assert(b->scheme == a->scheme);
+    assert(strcmp(b->host_and_port, a->host_and_port) == 0);
+    assert(strcmp(b->path, "/bar") == 0);
+
+    b = uri_address_apply(pool, a, "/", 1);
+    assert(b != NULL);
+    assert(b->scheme == a->scheme);
+    assert(strcmp(b->host_and_port, a->host_and_port) == 0);
+    assert(strcmp(b->path, "/") == 0);
+
+    b = uri_address_apply(pool, a, "http://example.com/", 29);
+    assert(b == NULL);
+}
+
+int
+main(G_GNUC_UNUSED int argc, G_GNUC_UNUSED char **argv)
+{
+    struct pool *pool = pool_new_libc(NULL, "root");
+
+    test_apply(pool);
+
+    pool_unref(pool);
+    pool_commit();
+
+    pool_recycler_clear();
+}
