851a29ec56221016ce6860b226b06a17b9b4d026
Max Kellermann <mk@cm4all.com>
2012-05-09 17:29:39 +0200

    demo: new redirection demo widget

---
+++ demo/cgi-bin/redirect.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if test "${REQUEST_URI: -4}" = "/foo"; then
+    :
+elif test "${REQUEST_URI: -5}" = "/path"; then
+    echo "Status: 303"
+    echo "Location: ${REQUEST_URI}/foo"
+elif test "${REQUEST_URI: -9}" = "/absolute"; then
+    echo "Status: 303"
+    echo "Location: http://cfatest01.intern.cm-ag${REQUEST_URI}/foo"
+else
+    echo "Status: 303"
+    echo "Location: foo"
+fi
+
+echo "Content-Type: text/plain"
+echo ""
+echo $REQUEST_URI
