2f5085fe85f7451e5db39c5c51122668bc7ebb87
Max Kellermann <mk@cm4all.com>
2012-05-23 14:57:29 +0200

    uri-parser: add API documentation

--- src/uri-parser.h
+++ src/uri-parser.h
@@ -11,12 +11,32 @@
 
 #include <stdbool.h>
 
+/**
+ * A splitted URI.
+ */
 struct parsed_uri {
+    /**
+     * The "base" URI that points to the real resource, without
+     * dynamic arguments.
+     */
     struct strref base;
+
+    /**
+     * The beng-proxy arguments, which were introduced by a semicolon
+     * (without the semicolon).
+     */
     struct strref args;
+
+    /**
+     * The query string (without the question mark).
+     */
     struct strref query;
 };
 
+/**
+ * Split the URI into its parts.  The result contains pointers into
+ * the original string.
+ */
 bool
 uri_parse(struct parsed_uri *dest, const char *src);
 
