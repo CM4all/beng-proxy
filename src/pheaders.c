42f14f023e686eb7901e260f403ee1bf524ac9c9
Max Kellermann <mk@cm4all.com>
2012-06-26 11:36:01 +0200

    pheaders: emit Cache-Control:no-store to work around IE quirk
    Work around for ticket 0001163.

--- src/pheaders.c
+++ src/pheaders.c
@@ -11,26 +11,31 @@
 struct strmap *
 processor_header_forward(struct pool *pool, struct strmap *headers)
 {
-    if (headers == NULL)
-        return NULL;
+    struct strmap *headers2 = strmap_new(pool, 17);
 
-    static const char *const copy_headers[] = {
-        "content-language",
-        "content-type",
-        "content-disposition",
-        "location",
-        NULL,
-    };
+    if (headers != NULL) {
+        static const char *const copy_headers[] = {
+            "content-language",
+            "content-type",
+            "content-disposition",
+            "location",
+            NULL,
+        };
 
-    struct strmap *headers2 = strmap_new(pool, 8);
-    header_copy_list(headers, headers2, copy_headers);
+        header_copy_list(headers, headers2, copy_headers);
 
 #ifndef NDEBUG
-    /* copy Wildfire headers if present (debug build only, to avoid
-       overhead on production servers) */
-    if (strmap_get(headers, "x-wf-protocol-1") != NULL)
-        header_copy_prefix(headers, headers2, "x-wf-");
+        /* copy Wildfire headers if present (debug build only, to avoid
+           overhead on production servers) */
+        if (strmap_get(headers, "x-wf-protocol-1") != NULL)
+            header_copy_prefix(headers, headers2, "x-wf-");
 #endif
+    }
+
+    /* reportedly, the Internet Explorer caches uncacheable resources
+       without revalidating them; only Cache-Control will prevent him
+       from showing stale data to the user */
+    strmap_add(headers2, "cache-control", "no-store");
 
     return headers2;
 }
