f5c090b2120d4d6bbbf704f648bfdace9adfae52
Max Kellermann <mk@cm4all.com>
2012-08-14 15:54:20 +0200

    clock: un-inline now_us()
    Reduce header dependencies.

--- src/clock.h
+++ src/clock.h
@@ -5,21 +5,15 @@
 #ifndef BENG_PROXY_CLOCK_H
 #define BENG_PROXY_CLOCK_H
 
+#include <inline/compiler.h>
+
 #include <stdint.h>
-#include <time.h>
 
 /**
  * Returns the current monotonic time stamp in microseconds.
  */
 gcc_pure
-static uint64_t
-now_us(void)
-{
-    struct timespec t;
-    if (clock_gettime(CLOCK_MONOTONIC, &t) < 0)
-        return 0;
-
-    return t.tv_sec * 1000000 + t.tv_nsec / 1000;
-}
+uint64_t
+now_us(void);
 
 #endif
