2cf21551cff2c04263bb595d6ba31f1fba6ab1de
Max Kellermann <mk@cm4all.com>
2012-10-04 16:58:13 +0200

    pool: allocate attachments from libc memory
    This caused long-running pools to grow.

--- src/pool.c
+++ src/pool.c
@@ -455,6 +455,14 @@ pool_destroy(struct pool *pool, struct pool *reparent_to)
     }
 #endif
 
+#ifndef NDEBUG
+    while (!list_empty(&pool->attachments)) {
+        struct list_head *next = pool->unrefs.next;
+        list_remove(next);
+        free(next);
+    }
+#endif
+
     switch (pool->type) {
     case POOL_LIBC:
         while (!list_empty(&pool->current_area.libc)) {
@@ -1016,7 +1024,7 @@ pool_attach(struct pool *pool, const void *p, const char *name)
     assert(p != NULL);
     assert(name != NULL);
 
-    struct attachment *attachment = p_malloc(pool, sizeof(*attachment));
+    struct attachment *attachment = xmalloc(sizeof(*attachment));
     attachment->value = p;
     attachment->name = name;
 
@@ -1055,6 +1063,7 @@ pool_detach(struct pool *pool, const void *p)
     assert(attachment != NULL);
 
     list_remove(&attachment->siblings);
+    free(attachment);
 }
 
 void
@@ -1065,6 +1074,7 @@ pool_detach_checked(struct pool *pool, const void *p)
         return;
 
     list_remove(&attachment->siblings);
+    free(attachment);
 }
 
 const char *
