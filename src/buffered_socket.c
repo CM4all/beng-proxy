1d95f1704111d66798a75a36f7ab0ed978f7a0d9
Max Kellermann <mk@cm4all.com>
2012-11-27 17:00:23 +0100

    buffered_socket: new high-level socket library
    Based on the existing socket_wrapper library.

---
+++ src/buffered_socket.c
@@ -0,0 +1,310 @@
+/*
+ * Wrapper for a socket file descriptor with input and output
+ * buffer management.
+ *
+ * author: Max Kellermann <mk@cm4all.com>
+ */
+
+#include "buffered_socket.h"
+#include "fifo-buffer.h"
+#include "gerrno.h"
+
+#include <limits.h>
+#include <errno.h>
+
+static bool
+buffered_socket_input_empty(const struct buffered_socket *s)
+{
+    assert(s != NULL);
+    assert(!s->ended);
+
+    return s->input == NULL || fifo_buffer_empty(s->input);
+}
+
+static bool
+buffered_socket_input_full(const struct buffered_socket *s)
+{
+    assert(s != NULL);
+    assert(!s->ended);
+
+    return s->input != NULL && fifo_buffer_full(s->input);
+}
+
+static bool
+buffered_socket_submit_from_buffer(struct buffered_socket *s)
+{
+    if (s->input == NULL)
+        return true;
+
+    size_t length;
+    const void *data = fifo_buffer_read(s->input, &length);
+    if (data == NULL) {
+        assert(buffered_socket_connected(s));
+        return true;
+    }
+
+    pool_ref(s->base.pool);
+
+    size_t consumed = s->handler->data(data, length, s->handler_ctx);
+    assert(consumed == 0 || buffered_socket_valid(s));
+
+    bool result = consumed > 0 || buffered_socket_valid(s);
+    pool_unref(s->base.pool);
+
+    if (consumed > 0) {
+        fifo_buffer_consume(s->input, consumed);
+
+        if (fifo_buffer_empty(s->input) && !buffered_socket_connected(s)) {
+            assert(s->handler->end != NULL);
+
+#ifndef NDEBUG
+            s->ended = true;
+#endif
+
+            s->handler->end(s->handler_ctx);
+            result = false;
+        }
+    }
+
+    if (result && !buffered_socket_connected(s))
+        result = false;
+
+    return result;
+}
+
+/**
+ * @return true if more data should be read from the socket
+ */
+static bool
+buffered_socket_submit_direct(struct buffered_socket *s)
+{
+    assert(buffered_socket_connected(s));
+    assert(buffered_socket_input_empty(s));
+
+    return s->handler->direct(s->base.fd, s->base.fd_type, s->handler_ctx);
+}
+
+static bool
+buffered_socket_fill_buffer(struct buffered_socket *s)
+{
+    assert(buffered_socket_connected(s));
+
+    struct fifo_buffer *buffer = s->input;
+    if (buffer == NULL)
+        buffer = s->input = fifo_buffer_new(s->base.pool, 8192);
+
+    ssize_t nbytes = socket_wrapper_read_to_buffer(&s->base, buffer, INT_MAX);
+    if (gcc_likely(nbytes > 0))
+        /* success: data was added to the buffer */
+        return true;
+
+    if (nbytes == 0) {
+        /* socket closed */
+
+        if (!s->handler->closed(s->handler_ctx))
+            return false;
+
+        assert(!buffered_socket_connected(s));
+        assert(s->input == buffer);
+        assert(s->handler->end != NULL);
+
+        if (fifo_buffer_empty(buffer)) {
+#ifndef NDEBUG
+            s->ended = true;
+#endif
+
+            s->handler->end(s->handler_ctx);
+            return false;
+        }
+
+        return true;
+    }
+
+    if (nbytes == -2) {
+        /* input buffer is full */
+        socket_wrapper_unschedule_read(&s->base);
+        return true;
+    }
+
+    if (nbytes == -1) {
+        if (errno == EAGAIN) {
+            socket_wrapper_schedule_read(&s->base);
+            return true;
+        } else {
+            GError *error = new_error_errno_msg("recv() failed");
+            s->handler->error(error, s->handler_ctx);
+            return false;
+        }
+    }
+
+    return true;
+}
+
+static bool
+buffered_socket_try_read2(struct buffered_socket *s)
+{
+    assert(!s->destroyed);
+    assert(!s->ended);
+    assert(s->reading);
+
+    if (!buffered_socket_connected(s)) {
+        assert(!buffered_socket_input_empty(s));
+
+        buffered_socket_submit_from_buffer(s);
+        return false;
+    } else if (s->direct) {
+        /* empty the remaining buffer before doing direct transfer */
+        if (!buffered_socket_submit_from_buffer(s))
+            return false;
+
+        if (!s->direct)
+            /* meanwhile, the "direct" flag was reverted by the
+               handler - try again */
+            return buffered_socket_try_read2(s);
+
+        if (!buffered_socket_input_empty(s)) {
+            /* there's still data in the buffer, but our handler isn't
+               ready for consuming it - stop reading from the
+               socket */
+            socket_wrapper_unschedule_read(&s->base);
+            return true;
+        }
+
+        if (!buffered_socket_submit_direct(s))
+            return false;
+
+        socket_wrapper_schedule_read(&s->base);
+        return true;
+    } else {
+        if (!buffered_socket_fill_buffer(s))
+            return false;
+
+        if (!buffered_socket_submit_from_buffer(s))
+            return false;
+
+        socket_wrapper_schedule_read(&s->base);
+        return true;
+    }
+}
+
+static bool
+buffered_socket_try_read(struct buffered_socket *s)
+{
+    assert(buffered_socket_connected(s));
+    assert(!s->destroyed);
+    assert(!s->ended);
+    assert(!s->reading);
+
+#ifndef NDEBUG
+    struct pool_notify notify;
+    pool_notify(s->base.pool, &notify);
+    s->reading = true;
+#endif
+
+    const bool result = buffered_socket_try_read2(s);
+
+#ifndef NDEBUG
+    if (!pool_denotify(&notify)) {
+        assert(s->reading);
+        s->reading = false;
+    }
+#endif
+
+    return result;
+}
+
+/*
+ * socket_wrapper handler
+ *
+ */
+
+static bool
+buffered_socket_wrapper_write(void *ctx)
+{
+    struct buffered_socket *s = ctx;
+    assert(!s->destroyed);
+    assert(!s->ended);
+
+    return s->handler->write(s->handler_ctx);
+}
+
+static bool
+buffered_socket_wrapper_read(void *ctx)
+{
+    struct buffered_socket *s = ctx;
+    assert(!s->destroyed);
+    assert(!s->ended);
+
+    return buffered_socket_try_read(s);
+}
+
+static bool
+buffered_socket_wrapper_timeout(void *ctx)
+{
+    struct buffered_socket *s = ctx;
+    assert(!s->destroyed);
+    assert(!s->ended);
+
+    if (s->handler->timeout != NULL)
+        return s->handler->timeout(s->handler_ctx);
+
+    s->handler->error(g_error_new_literal(errno_quark(), ETIMEDOUT, "Timeout"),
+                      s->handler_ctx);
+    return false;
+}
+
+static const struct socket_handler buffered_socket_handler = {
+    .read = buffered_socket_wrapper_read,
+    .write = buffered_socket_wrapper_write,
+    .timeout = buffered_socket_wrapper_timeout,
+};
+
+/*
+ * public API
+ *
+ */
+
+void
+buffered_socket_init(struct buffered_socket *s, struct pool *pool,
+                     int fd, enum istream_direct fd_type,
+                     const struct timeval *read_timeout,
+                     const struct timeval *write_timeout,
+                     const struct buffered_socket_handler *handler, void *ctx)
+{
+    assert(handler != NULL);
+    assert(handler->data != NULL);
+    /* handler method closed() is optional */
+    assert(handler->write != NULL);
+    assert(handler->error != NULL);
+
+    socket_wrapper_init(&s->base, pool, fd, fd_type,
+                        read_timeout, write_timeout,
+                        &buffered_socket_handler, s);
+
+    s->handler = handler;
+    s->handler_ctx = ctx;
+    s->input = NULL;
+    s->direct = false;
+
+#ifndef NDEBUG
+    s->reading = false;
+    s->ended = false;
+    s->destroyed = false;
+#endif
+}
+
+bool
+buffered_socket_full(const struct buffered_socket *s)
+{
+    return buffered_socket_input_full(s);
+}
+
+bool
+buffered_socket_read(struct buffered_socket *s)
+{
+    assert(!s->reading);
+    assert(!s->destroyed);
+    assert(!s->ended);
+
+    return buffered_socket_try_read(s);
+}
---
+++ src/buffered_socket.h
@@ -0,0 +1,265 @@
+/*
+ * Wrapper for a socket file descriptor with input buffer management.
+ *
+ * author: Max Kellermann <mk@cm4all.com>
+ */
+
+#ifndef BENG_PROXY_BUFFERED_SOCKET_H
+#define BENG_PROXY_BUFFERED_SOCKET_H
+
+#include "socket_wrapper.h"
+
+#include <glib.h>
+
+struct fifo_buffer;
+
+struct buffered_socket_handler {
+    /**
+     * Data has been read from the socket.
+     *
+     * @return the number of bytes consumed, 0 if the socket has been
+     * closed (or if nothing could be used)
+     */
+    size_t (*data)(const void *buffer, size_t size, void *ctx);
+
+    /**
+     * The socket is ready for reading.  It is suggested to attempt a
+     * "direct" tansfer.
+     *
+     * @return true if more data shall be read from the socket, false
+     * when the socket has been closed or if the output is currently
+     * unable to consume data
+     */
+    bool (*direct)(int fd, enum istream_direct fd_type, void *ctx);
+
+    /**
+     * The peer has finished sending and has closed the socket.  The
+     * method must close/abandon the socket.  There may still be data
+     * in the input buffer, so don't give up on this object yet.
+     *
+     * @return false if no more data shall be delivered to the
+     * handler; the #end method will also not be invoked
+     */
+    bool (*closed)(void *ctx);
+
+    /**
+     * The buffer has become empty after the socket has been closed by
+     * the peer.  This may be called right after #closed if the input
+     * buffer was empty.
+     */
+    void (*end)(void *ctx);
+
+    /**
+     * The socket is ready for writing.
+     *
+     * @return false when the socket has been closed
+     */
+    bool (*write)(void *ctx);
+
+    /**
+     * @return false when the socket has been closed
+     */
+    bool (*timeout)(void *ctx);
+
+    /**
+     * An I/O error on the socket has occurred.  After returning, it
+     * is assumed that the #buffered_socket object has been closed.
+     *
+     * @param error a description of the error, to be freed by the
+     * callee
+     */
+    void (*error)(GError *error, void *ctx);
+};
+
+/**
+ * A wrapper for #socket_wrapper that manages an optional input
+ * buffer.
+ *
+ * The object can have the following states:
+ *
+ * - uninitialised
+ *
+ * - connected (after buffered_socket_init())
+ *
+ * - disconnected (after buffered_socket_close() or
+ * buffered_socket_abandon()); in this state, the remaining data
+ * from the input buffer will be delivered
+ *
+ * - ended (when the handler method end() is invoked)
+ *
+ * - destroyed (after buffered_socket_destroy())
+ */
+struct buffered_socket {
+    struct socket_wrapper base;
+
+    const struct buffered_socket_handler *handler;
+    void *handler_ctx;
+
+    struct fifo_buffer *input;
+
+    /**
+     * Attempt to do "direct" transfers?
+     */
+    bool direct;
+
+#ifndef NDEBUG
+    bool reading, ended, destroyed;
+#endif
+};
+
+void
+buffered_socket_init(struct buffered_socket *s, struct pool *pool,
+                     int fd, enum istream_direct fd_type,
+                     const struct timeval *read_timeout,
+                     const struct timeval *write_timeout,
+                     const struct buffered_socket_handler *handler, void *ctx);
+
+/**
+ * Close the physical socket, but do not destroy the input buffer.  To
+ * do the latter, call buffered_socket_destroy().
+ */
+static inline void
+buffered_socket_close(struct buffered_socket *s)
+{
+    assert(!s->ended);
+    assert(!s->destroyed);
+
+    socket_wrapper_close(&s->base);
+}
+
+/**
+ * Just like buffered_socket_close(), but do not actually close the
+ * socket.  The caller is responsible for closing the socket (or
+ * scheduling it for reuse).
+ */
+static inline void
+buffered_socket_abandon(struct buffered_socket *s)
+{
+    assert(!s->ended);
+    assert(!s->destroyed);
+
+    socket_wrapper_abandon(&s->base);
+}
+
+/**
+ * Destroy the object.  Prior to that, the socket must be removed by
+ * calling either buffered_socket_close() or
+ * buffered_socket_abandon().
+ */
+static inline void
+buffered_socket_destroy(struct buffered_socket *s)
+{
+    assert(!socket_wrapper_valid(&s->base));
+    assert(!s->destroyed);
+
+    s->input = NULL;
+
+#ifndef NDEBUG
+    s->destroyed = true;
+#endif
+}
+
+/**
+ * Is the socket still connected?  This does not actually check
+ * whether the socket is connected, just whether it is known to be
+ * closed.
+ */
+static inline bool
+buffered_socket_connected(const struct buffered_socket *s)
+{
+    assert(s != NULL);
+    assert(!s->ended);
+    assert(!s->destroyed);
+
+    return socket_wrapper_valid(&s->base);
+}
+
+/**
+ * Is the object still usable?  The socket may be closed already, but
+ * the input buffer may still have data.
+ */
+static inline bool
+buffered_socket_valid(const struct buffered_socket *s)
+{
+    assert(s != NULL);
+
+    /* the object is valid if there is either a valid socket or a
+       buffer that may have more data; in the latter case, the socket
+       may be closed already because no more data is needed from
+       there */
+    return socket_wrapper_valid(&s->base) || s->input != NULL;
+}
+
+/**
+ * Is the input buffer full?
+ */
+gcc_pure
+bool
+buffered_socket_full(const struct buffered_socket *s);
+
+/**
+ * Returns the istream_direct mask for splicing data into this socket.
+ */
+static inline enum istream_direct
+buffered_socket_direct_mask(const struct buffered_socket *s)
+{
+    assert(s != NULL);
+    assert(!s->ended);
+    assert(!s->destroyed);
+
+    return socket_wrapper_direct_mask(&s->base);
+}
+
+/**
+ * The caller wants to read more data from the socket.  There are four
+ * possible outcomes: a call to buffered_socket_handler.read, a call
+ * to buffered_socket_handler.direct, a call to
+ * buffered_socket_handler.error or (if there is no data available
+ * yet) an event gets scheduled and the function returns immediately.
+ */
+bool
+buffered_socket_read(struct buffered_socket *s);
+
+static inline ssize_t
+buffered_socket_write(struct buffered_socket *s,
+                      const void *data, size_t length)
+{
+    return socket_wrapper_write(&s->base, data, length);
+}
+
+static inline ssize_t
+buffered_socket_write_from(struct buffered_socket *s,
+                           int fd, enum istream_direct fd_type,
+                           size_t length)
+{
+    return socket_wrapper_write_from(&s->base, fd, fd_type, length);
+}
+
+gcc_pure
+static inline bool
+buffered_socket_ready_for_writing(const struct buffered_socket *s)
+{
+    assert(!s->destroyed);
+
+    return socket_wrapper_ready_for_writing(&s->base);
+}
+
+static inline void
+buffered_socket_schedule_write(struct buffered_socket *s)
+{
+    assert(!s->ended);
+    assert(!s->destroyed);
+
+    socket_wrapper_schedule_write(&s->base);
+}
+
+static inline void
+buffered_socket_unschedule_write(struct buffered_socket *s)
+{
+    assert(!s->ended);
+    assert(!s->destroyed);
+
+    socket_wrapper_unschedule_write(&s->base);
+}
+
+#endif
