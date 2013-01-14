1665e2ff8c4008f48f094483b908c4e3e79b8bfe
Max Kellermann <mk@cm4all.com>
2013-01-14 14:29:49 +0100

    http-server: eliminate redundant check

--- src/http-server-request.c
+++ src/http-server-request.c
@@ -34,10 +34,10 @@ http_server_consume_body(struct http_server_connection *connection)
     if (connection->request.read_state == READ_BODY &&
         http_body_eof(&connection->request.body_reader)) {
         connection->request.read_state = READ_END;
-        if (connection->request.read_state == READ_END)
-            /* re-enable the event, to detect client disconnect while
-               we're processing the request */
-            http_server_schedule_read(connection);
+
+        /* re-enable the event, to detect client disconnect while
+           we're processing the request */
+        http_server_schedule_read(connection);
 
         istream_deinit_eof(&connection->request.body_reader.output);
         if (!http_server_connection_valid(connection))
