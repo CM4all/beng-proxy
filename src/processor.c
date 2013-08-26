18369afaba39d760f75d07c1c035575243df8d91
Max Kellermann <mk@cm4all.com>
2013-08-26 20:20:59 +0200

    processor: check replace!=NULL
    Fix widget lookup regression introduced by 3.0.27.

--- src/processor.c
+++ src/processor.c
@@ -1537,7 +1537,7 @@ processor_parser_cdata(const char *p gcc_unused, size_t length,
         if (length > 0)
             istream_replace_extend(processor->replace, processor->cdata_start,
                                    start + length);
-    } else if (processor->widget.widget == NULL)
+    } else if (processor->replace != NULL && processor->widget.widget == NULL)
         istream_replace_settle(processor->replace, start + length);
 
     return length;
