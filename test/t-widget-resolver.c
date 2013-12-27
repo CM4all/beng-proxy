dd785c93cd42bb3806c4fe059d1d0d5808dc2297
Max Kellermann <mk@cm4all.com>
2013-12-27 17:34:54 +0100

    test/t-widget-resolver: ignore clang's -Wextended-offsetof

--- test/t-widget-resolver.c
+++ test/t-widget-resolver.c
@@ -81,6 +81,10 @@ widget_resolver_callback2(void *ctx)
  *
  */
 
+#ifdef __clang__
+#pragma GCC diagnostic ignored "-Wextended-offsetof"
+#endif
+
 static struct data *
 async_to_data(struct async_operation *ao)
 {
