5accdc615dde6e13e4d968580a07804e54e6dda7
Max Kellermann <mk@cm4all.com>
2014-02-26 16:10:15 +0100

    tvary: convert to C++

--- src/tvary.cxx
+++ src/tvary.cxx
@@ -13,7 +13,7 @@
 #include <assert.h>
 
 static const char *
-translation_vary_name(enum beng_translation_command cmd)
+translation_vary_name(beng_translation_command cmd)
 {
     switch (cmd) {
     case TRANSLATE_SESSION:
@@ -31,20 +31,21 @@ translation_vary_name(enum beng_translation_command cmd)
         return "user-agent";
 
     default:
-        return NULL;
+        return nullptr;
     }
 }
 
 static const char *
 translation_vary_header(const struct translate_response *response)
 {
-    assert(response != NULL);
+    assert(response != nullptr);
 
     static char buffer[256];
     char *p = buffer;
     for (unsigned i = 0, n = response->num_vary; i != n; ++i) {
-        const char *name = translation_vary_name(response->vary[i]);
-        if (name == NULL)
+        const auto cmd = beng_translation_command(response->vary[i]);
+        const char *name = translation_vary_name(cmd);
+        if (name == nullptr)
             continue;
 
         if (p > buffer)
@@ -55,30 +56,30 @@ translation_vary_header(const struct translate_response *response)
         p += length;
     }
 
-    return p > buffer ? buffer : NULL;
+    return p > buffer ? buffer : nullptr;
 }
 
 struct strmap *
 add_translation_vary_header(struct pool *pool, struct strmap *headers,
                             const struct translate_response *response)
 {
-    assert(pool != NULL);
-    assert(response != NULL);
+    assert(pool != nullptr);
+    assert(response != nullptr);
 
     const char *value = translation_vary_header(response);
-    if (value == NULL)
+    if (value == nullptr)
         return headers;
 
     const char *old;
-    if (headers == NULL) {
-        old = NULL;
+    if (headers == nullptr) {
+        old = nullptr;
         headers = strmap_new(pool, 5);
     } else {
         old = strmap_get(headers, "vary");
     }
 
-    if (old != NULL)
-        value = p_strcat(pool, old, ",", value, NULL);
+    if (old != nullptr)
+        value = p_strcat(pool, old, ",", value, nullptr);
 
     strmap_set(headers, "vary", value);
     return headers;
@@ -88,13 +89,14 @@ void
 write_translation_vary_header(struct growing_buffer *headers,
                               const struct translate_response *response)
 {
-    assert(headers != NULL);
-    assert(response != NULL);
+    assert(headers != nullptr);
+    assert(response != nullptr);
 
     bool active = false;
     for (unsigned i = 0, n = response->num_vary; i != n; ++i) {
-        const char *name = translation_vary_name(response->vary[i]);
-        if (name == NULL)
+        const auto cmd = beng_translation_command(response->vary[i]);
+        const char *name = translation_vary_name(cmd);
+        if (name == nullptr)
             continue;
 
         if (active) {
