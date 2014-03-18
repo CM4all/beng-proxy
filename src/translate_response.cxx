cc5b663ce2ff2cf90d472e1f81a702162c698e0b
Max Kellermann <mk@cm4all.com>
2014-03-18 12:45:34 +0100

    translate_response: handle null/empty difference in Copy()

--- src/translate_response.cxx
+++ src/translate_response.cxx
@@ -22,8 +22,11 @@ template<typename T>
 static ConstBuffer<T>
 Copy(pool *p, ConstBuffer<T> src)
 {
-    if (src.IsEmpty())
+    if (src.IsNull())
         return ConstBuffer<T>::Null();
+
+    if (src.IsEmpty())
+        return ConstBuffer<T>::FromVoid({"", 0});
 
     ConstBuffer<void> src_v = src.ToVoid();
     ConstBuffer<void> dest_v(p_memdup(p, src_v.data, src_v.size), src_v.size);
