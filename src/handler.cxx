CM4all/beng-proxy

commit 702e2d71c46cfcf40d2b2456652d2b1e8a8a092b

handler: use translate_response copy

src/handler.cxx
@@ -189,17 +189,14 @@ apply_translate_response_session(request &request,
     return session;
 }
 
+/**
+ * Called by handle_translated_request() with the #translate_response
+ * copy.
+ */
 static void
-handle_translated_request(request &request, const translate_response &response)
+handle_translated_request2(request &request,
+                           const translate_response &response)
 {
-    /* copy the translate_response just in case the cache item is
-       freed before we send the final response */
-    /* TODO: use cache_item_lock() instead */
-    translate_response *response2 = (translate_response *)
-        p_malloc(request.request->pool, sizeof(*response2));
-    *response2 = response;
-    translate_response_copy(request.request->pool, response2, &response);
-    request.translate.response = response2;
     request.translate.transformation = response.views != nullptr
         ? response.views->transformation
         : nullptr;
@@ -276,6 +273,21 @@ handle_translated_request(request &request, const translate_response &response)
         response_dispatch_message(&request, HTTP_STATUS_INTERNAL_SERVER_ERROR,
                                   "Internal server error");
     }
+}
+
+static void
+handle_translated_request(request &request, const translate_response &response)
+{
+    /* copy the translate_response just in case the cache item is
+       freed before we send the final response */
+    /* TODO: use cache_item_lock() instead */
+    translate_response *response2 = (translate_response *)
+        p_malloc(request.request->pool, sizeof(*response2));
+    *response2 = response;
+    translate_response_copy(request.request->pool, response2, &response);
+    request.translate.response = response2;
+
+    handle_translated_request2(request, *response2);
 }
 
 extern const struct translate_handler handler_translate_handler;

commit 944bfcc2d944a7892756273b35463f24c0945dbb

handler: copy TranslateResponse::session
TranslateResponse::CopyFrom() omits this attribute because it should
not be in the cache.  Fixes a regression from commit 702e2d71
(v4.0.17).

src/handler.cxx
@@ -403,6 +403,12 @@ handle_translated_request(request &request, const TranslateResponse &response)
     auto response2 = NewFromPool<TranslateResponse>(request.request->pool);
     *response2 = response;
     response2->CopyFrom(request.request->pool, response);
+
+    /* copy TRANSLATE_SESSION because TranslateResponse::CopyFrom()
+       clears it */
+    response2->session = p_strdup_checked(request.request->pool,
+                                          response.session);
+
     request.translate.response = response2;
 
     if (!do_content_type_lookup(request, *response2))