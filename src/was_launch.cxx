4b643089d7314cbe8f57151f3722ca3b8e7c2e53
Max Kellermann <mk@cm4all.com>
2014-05-08 22:53:37 +0200

    was_launch: store child_options pointer

--- src/was_launch.cxx
+++ src/was_launch.cxx
@@ -29,8 +29,7 @@
 struct was_run_args {
     sigset_t signals;
 
-    const struct namespace_options *ns;
-    const struct rlimit_options *rlimits;
+    const struct child_options *options;
 
     int control_fd, input_fd, output_fd;
     struct exec exec;
@@ -45,8 +44,8 @@ was_run(void *ctx)
     install_default_signal_handlers();
     leave_signal_section(&args->signals);
 
-    namespace_options_setup(args->ns);
-    rlimit_options_apply(args->rlimits);
+    namespace_options_setup(&args->options->ns);
+    rlimit_options_apply(&args->options->rlimits);
 
     dup2(args->input_fd, 0);
     dup2(args->output_fd, 1);
@@ -91,8 +90,7 @@ was_launch(struct was_process *process,
     }
 
     struct was_run_args run_args = {
-        .ns = &options->ns,
-        .rlimits = &options->rlimits,
+        .options = options,
         .control_fd = control_fds[1],
         .output_fd = input_fds[1],
         .input_fd = output_fds[0],
