b7dbabd014b9f8c49c80406453e1ab8fe22f7f29
Max Kellermann <mk@cm4all.com>
2014-06-05 09:01:26 +0200

    widget_uri, cgi_address: fix length check in HasTrailingSlash()
    The wrong variable was being checked.  D'oh!

--- src/widget_uri.cxx
+++ src/widget_uri.cxx
@@ -65,7 +65,7 @@ static bool
 HasTrailingSlash(const char *p)
 {
     size_t length = strlen(p);
-    return p > 0 && p[length - 1] == '/';
+    return length > 0 && p[length - 1] == '/';
 }
 
 const struct resource_address *
