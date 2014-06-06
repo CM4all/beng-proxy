af4e5888e88bb85f667693f91aaf30f29b9ddf5b
Max Kellermann <mk@cm4all.com>
2014-06-06 13:50:51 +0200

    cookie_server: remove C++ check

--- src/cookie_server.hxx
+++ src/cookie_server.hxx
@@ -10,19 +10,11 @@
 struct pool;
 struct strmap;
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 /**
  * Parse a Cookie request header and store all cookies in the
  * specified strmap.
  */
 void
 cookie_map_parse(struct strmap *cookies, const char *p, struct pool *pool);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif
