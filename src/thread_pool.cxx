8e1722deb69c9754446d1e0c79d1969480d6060d
Max Kellermann <mk@cm4all.com>
2014-07-02 22:55:49 +0200

    thread_pool: use std::array

--- src/thread_pool.cxx
+++ src/thread_pool.cxx
@@ -10,13 +10,13 @@
 
 #include <daemon/log.h>
 
-#include <glib.h>
+#include <array>
 
 #include <assert.h>
 #include <stdlib.h>
 
 static ThreadQueue *global_thread_queue;
-static struct thread_worker worker_threads[8];
+static std::array<struct thread_worker, 8> worker_threads;
 
 static void
 thread_pool_init()
@@ -29,8 +29,8 @@ thread_pool_start(void)
 {
     assert(global_thread_queue != nullptr);
 
-    for (unsigned i = 0; i < G_N_ELEMENTS(worker_threads); ++i) {
-        if (!thread_worker_create(&worker_threads[i], global_thread_queue)) {
+    for (auto &i : worker_threads) {
+        if (!thread_worker_create(&i, global_thread_queue)) {
             daemon_log(1, "Failed to launch worker thread\n");
             exit(EXIT_FAILURE);
         }
@@ -65,8 +65,8 @@ thread_pool_join(void)
     if (global_thread_queue == nullptr)
         return;
 
-    for (unsigned i = 0; i < G_N_ELEMENTS(worker_threads); ++i)
-        thread_worker_join(&worker_threads[i]);
+    for (auto &i : worker_threads)
+        thread_worker_join(&i);
 }
 
 void
