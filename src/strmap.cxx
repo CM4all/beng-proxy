3f4205bd062d9036bd45fc2b713894f6162b820c
Max Kellermann <mk@cm4all.com>
2014-07-10 19:50:01 +0200

    strmap: expose the class

--- src/strmap.cxx
+++ src/strmap.cxx
@@ -12,111 +12,89 @@
 #include <assert.h>
 #include <string.h>
 
-struct strmap {
-    struct Item : strmap_pair {
-        typedef boost::intrusive::set_member_hook<boost::intrusive::link_mode<boost::intrusive::normal_link>> Hook;
-        Hook hook;
+inline bool
+strmap::Item::Compare::Less(const char *a, const char *b) const
+{
+    return strcmp(a, b) < 0;
+}
 
-        Item(const char *_key, const char *_value)
-            :strmap_pair(_key, _value) {}
-
-        struct Compare {
-            gcc_pure
-            bool operator()(const char *a, const Item &b) const {
-                return strcmp(a, b.key) < 0;
-            }
-
-            gcc_pure
-            bool operator()(const Item &a, const char *b) const {
-                return strcmp(a.key, b) < 0;
-            }
-
-            gcc_pure
-            bool operator()(const Item &a, const Item &b) const {
-                return strcmp(a.key, b.key) < 0;
-            }
-        };
-    };
-
-    struct pool &pool;
-
-    typedef boost::intrusive::multiset<Item,
-                                       boost::intrusive::member_hook<Item, Item::Hook, &Item::hook>,
-                                       boost::intrusive::compare<Item::Compare>,
-                                       boost::intrusive::constant_time_size<false>> Map;
-    Map map;
-
-    mutable Map::const_iterator cursor;
-
-    explicit strmap(struct pool &_pool):pool(_pool) {}
-
-    strmap(struct pool &_pool, const strmap &src):pool(_pool) {
-        const auto hint = map.end();
-        for (auto &i : src.map) {
-            Item *item = NewFromPool<Item>(&pool,
-                                           p_strdup(&pool, i.key),
-                                           p_strdup(&pool, i.value));
-            map.insert(hint, *item);
-        }
+strmap::strmap(struct pool &_pool, const strmap &src)
+    :pool(_pool) {
+    const auto hint = map.end();
+    for (auto &i : src.map) {
+        Item *item = NewFromPool<Item>(&pool,
+                                       p_strdup(&pool, i.key),
+                                       p_strdup(&pool, i.value));
+        map.insert(hint, *item);
     }
+}
 
-    strmap(const strmap &) = delete;
+void
+strmap::Add(const char *key, const char *value)
+{
+    Item *item = NewFromPool<Item>(&pool, key, value);
+    map.insert(*item);
+}
 
-    void Add(const char *key, const char *value) {
-        Item *item = NewFromPool<Item>(&pool, key, value);
-        map.insert(*item);
+const char *
+strmap::Set(const char *key, const char *value)
+{
+    const Item item(key, value);
+    auto i = map.find(item);
+    if (i != map.end()) {
+        const char *old_value = i->value;
+        i->value = value;
+        return old_value;
+    } else {
+        map.insert(*NewFromPool<Item>(&pool, item));
+        return nullptr;
     }
+}
 
-    const char *Set(const char *key, const char *value) {
-        const Item item(key, value);
-        auto i = map.find(item);
-        if (i != map.end()) {
-            const char *old_value = i->value;
-            i->value = value;
-            return old_value;
-        } else {
-            map.insert(*NewFromPool<Item>(&pool, item));
-            return nullptr;
-        }
-    }
+const char *
+strmap::Remove(const char *key)
+{
+    auto i = map.find(key, Item::Compare());
+    if (i == map.end())
+        return nullptr;
 
-    const char *Remove(const char *key) {
-        auto i = map.find(key, Item::Compare());
-        if (i == map.end())
-            return nullptr;
+    Item *found = &*i;
+    map.erase(i);
 
-        Item *found = &*i;
-        map.erase(i);
+    const char *value = found->value;
+    DeleteFromPool(&pool, found);
+    return value;
+}
 
-        const char *value = found->value;
-        DeleteFromPool(&pool, found);
-        return value;
-    }
+const char *
+strmap::Get(const char *key) const
+{
+    auto i = map.find(key, Item::Compare());
+    if (i == map.end())
+        return nullptr;
 
-    const char *Get(const char *key) const {
-        auto i = map.find(key, Item::Compare());
-        if (i == map.end())
-            return nullptr;
+    return i->value;
+}
 
-        return i->value;
-    }
+const struct strmap_pair *
+strmap::LookupFirst(const char *key) const
+{
+    auto i = map.find(key, Item::Compare());
+    if (i == map.end())
+        return nullptr;
 
-    const struct strmap_pair *LookupFirst(const char *key) const {
-        auto i = map.find(key, Item::Compare());
-        if (i == map.end())
-            return nullptr;
+    return &*i;
+}
 
-        return &*i;
-    }
-
-    const struct strmap_pair *LookupNext(const struct strmap_pair *pair) const {
-        const Item &item = *(const Item *)pair;
-        const auto i = std::next(map.iterator_to(item));
-        return i != map.end() && strcmp(i->key, pair->key) == 0
-            ? &*i
-            : nullptr;
-    }
-};
+const struct strmap_pair *
+strmap::LookupNext(const struct strmap_pair *pair) const
+{
+    const Item &item = *(const Item *)pair;
+    const auto i = std::next(map.iterator_to(item));
+    return i != map.end() && strcmp(i->key, pair->key) == 0
+        ? &*i
+        : nullptr;
+}
 
 struct strmap *
 strmap_new(struct pool *pool)
@@ -128,42 +106,6 @@ struct strmap *gcc_malloc
 strmap_dup(struct pool *pool, struct strmap *src)
 {
     return NewFromPool<struct strmap>(pool, *pool, *src);
-}
-
-void
-strmap_add(struct strmap *map, const char *key, const char *value)
-{
-    map->Add(key, value);
-}
-
-const char *
-strmap_set(struct strmap *map, const char *key, const char *value)
-{
-    return map->Set(key, value);
-}
-
-const char *
-strmap_remove(struct strmap *map, const char *key)
-{
-    return map->Remove(key);
-}
-
-const char *
-strmap_get(const struct strmap *map, const char *key)
-{
-    return map->Get(key);
-}
-
-const struct strmap_pair *
-strmap_lookup_first(const struct strmap *map, const char *key)
-{
-    return map->LookupFirst(key);
-}
-
-const struct strmap_pair *
-strmap_lookup_next(const struct strmap *map, const struct strmap_pair *pair)
-{
-    return map->LookupNext(pair);
 }
 
 void
--- src/strmap.hxx
+++ src/strmap.hxx
@@ -9,6 +9,8 @@
 
 #include <inline/compiler.h>
 
+#include <boost/intrusive/set.hpp>
+
 struct pool;
 
 struct strmap_pair {
@@ -18,38 +20,124 @@ struct strmap_pair {
         :key(_key), value(_value) {}
 };
 
+struct strmap {
+    struct Item : strmap_pair {
+        typedef boost::intrusive::set_member_hook<boost::intrusive::link_mode<boost::intrusive::normal_link>> Hook;
+        Hook hook;
+
+        Item(const char *_key, const char *_value)
+            :strmap_pair(_key, _value) {}
+
+        class Compare {
+            gcc_pure
+            bool Less(const char *a, const char *b) const;
+
+        public:
+            gcc_pure
+            bool operator()(const char *a, const Item &b) const {
+                return Less(a, b.key);
+            }
+
+            gcc_pure
+            bool operator()(const Item &a, const char *b) const {
+                return Less(a.key, b);
+            }
+
+            gcc_pure
+            bool operator()(const Item &a, const Item &b) const {
+                return Less(a.key, b.key);
+            }
+        };
+    };
+
+    struct pool &pool;
+
+    typedef boost::intrusive::multiset<Item,
+                                       boost::intrusive::member_hook<Item, Item::Hook, &Item::hook>,
+                                       boost::intrusive::compare<Item::Compare>,
+                                       boost::intrusive::constant_time_size<false>> Map;
+
+    typedef Map::const_iterator const_iterator;
+
+    Map map;
+
+    mutable Map::const_iterator cursor;
+
+    explicit strmap(struct pool &_pool):pool(_pool) {}
+
+    strmap(struct pool &_pool, const strmap &src);
+
+    strmap(const strmap &) = delete;
+
+    const_iterator begin() const {
+        return map.begin();
+    }
+
+    const_iterator end() const {
+        return map.end();
+    }
+
+    void Add(const char *key, const char *value);
+    const char *Set(const char *key, const char *value);
+    const char *Remove(const char *key);
+
+    gcc_pure
+    const char *Get(const char *key) const;
+
+    gcc_pure
+    const struct strmap_pair *LookupFirst(const char *key) const;
+
+    gcc_pure
+    const struct strmap_pair *LookupNext(const struct strmap_pair *pair) const;
+};
+
 struct strmap *gcc_malloc
 strmap_new(struct pool *pool);
 
 struct strmap *gcc_malloc
 strmap_dup(struct pool *pool, struct strmap *src);
 
-void
-strmap_add(struct strmap *map, const char *key, const char *value);
+static inline void
+strmap_add(struct strmap *map, const char *key, const char *value)
+{
+    map->Add(key, value);
+}
 
-const char *
-strmap_set(struct strmap *map, const char *key, const char *value);
+static inline const char *
+strmap_set(struct strmap *map, const char *key, const char *value)
+{
+    return map->Set(key, value);
+}
 
-const char *
-strmap_remove(struct strmap *map, const char *key);
+static inline const char *
+strmap_remove(struct strmap *map, const char *key)
+{
+    return map->Remove(key);
+}
 
-gcc_pure
-const char *
-strmap_get(const struct strmap *map, const char *key);
+static inline const char *
+strmap_get(const struct strmap *map, const char *key)
+{
+    return map->Get(key);
+}
 
 /**
  * @see hashmap_lookup_first()
  */
-gcc_pure
-const struct strmap_pair *
-strmap_lookup_first(const struct strmap *map, const char *key);
+static inline const struct strmap_pair *
+strmap_lookup_first(const struct strmap *map, const char *key)
+{
+    return map->LookupFirst(key);
+}
 
 /**
  * @see hashmap_lookup_next()
  */
-gcc_pure
-const struct strmap_pair *
-strmap_lookup_next(const struct strmap *map, const struct strmap_pair *pair);
+static inline const struct strmap_pair *
+strmap_lookup_next(const struct strmap *map, const struct strmap_pair *pair)
+{
+    return map->LookupNext(pair);
+}
 
 void
 strmap_rewind(struct strmap *map);
