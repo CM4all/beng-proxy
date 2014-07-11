e6614fe25cc7523973b8f3bcac874e42b11bc0c0
Max Kellermann <mk@cm4all.com>
2014-07-11 12:33:24 +0200

    net/AllocatedSocketAddress: new class replacing struct address_envelope

---
+++ src/net/AllocatedSocketAddress.cxx
@@ -0,0 +1,96 @@
+/*
+ * author: Max Kellermann <mk@cm4all.com>
+ */
+
+#include "AllocatedSocketAddress.hxx"
+#include "address_quark.h"
+
+#include <socket/resolver.h>
+
+#include <glib.h>
+
+#include <assert.h>
+#include <sys/un.h>
+#include <netdb.h>
+#include <string.h>
+
+void
+AllocatedSocketAddress::SetSize(size_t new_size)
+{
+    if (size == new_size)
+        return;
+
+    free(address);
+    size = new_size;
+    address = (struct sockaddr *)malloc(size);
+}
+
+void
+AllocatedSocketAddress::SetLocal(const char *path)
+{
+    const size_t path_length = strlen(path);
+
+    struct sockaddr_un *sun;
+    SetSize(sizeof(*sun) - sizeof(sun->sun_path) + path_length);
+    sun = (struct sockaddr_un *)address;
+    sun->sun_family = AF_UNIX;
+    memcpy(sun->sun_path, path, path_length + 1);
+}
+
+bool
+AllocatedSocketAddress::Parse(const char *p, int default_port,
+                              bool passive, GError **error_r)
+{
+    if (*p == '/') {
+        SetLocal(p);
+        return true;
+    }
+
+    if (*p == '@') {
+#ifdef __linux
+        /* abstract unix domain socket */
+
+        SetLocal(p);
+
+        /* replace the '@' with a null byte to make it "abstract" */
+        struct sockaddr_un *sun = (struct sockaddr_un *)address;
+        assert(sun->sun_path[0] == '@');
+        sun->sun_path[0] = '\0';
+        return true;
+#else
+        /* Linux specific feature */
+        g_set_error_literal(error_r, resolver_quark(), 0,
+                            "Abstract sockets supported only on Linux");
+        return false;
+#endif
+    }
+
+    static const struct addrinfo hints = {
+        .ai_flags = AI_NUMERICHOST,
+        .ai_family = AF_UNSPEC,
+        .ai_socktype = SOCK_STREAM,
+    };
+    static const struct addrinfo passive_hints = {
+        .ai_flags = AI_NUMERICHOST|AI_PASSIVE,
+        .ai_family = AF_UNSPEC,
+        .ai_socktype = SOCK_STREAM,
+    };
+
+    struct addrinfo *ai;
+    int result = socket_resolve_host_port(p, default_port,
+                                          passive ? &passive_hints : &hints,
+                                          &ai);
+    if (result != 0) {
+        g_set_error(error_r, resolver_quark(), result,
+                    "Failed to resolve '%s': %s",
+                    p, gai_strerror(result));
+        return false;
+    }
+
+    SetSize(ai->ai_addrlen);
+    memcpy(address, ai->ai_addr, size);
+
+    freeaddrinfo(ai);
+
+    return true;
+}
---
+++ src/net/AllocatedSocketAddress.hxx
@@ -0,0 +1,97 @@
+/*
+ * author: Max Kellermann <mk@cm4all.com>
+ */
+
+#ifndef ALLOCATED_SOCKET_ADDRESS_HXX
+#define ALLOCATED_SOCKET_ADDRESS_HXX
+
+#include "SocketAddress.hxx"
+#include "glibfwd.hxx"
+
+#include <inline/compiler.h>
+
+#include <algorithm>
+
+#include <stddef.h>
+#include <stdlib.h>
+
+struct sockaddr;
+class Error;
+
+class AllocatedSocketAddress {
+    friend class SocketDescriptor;
+
+    struct sockaddr *address;
+    socklen_t size;
+
+    AllocatedSocketAddress(struct sockaddr *_address,
+                           socklen_t _size)
+        :address(_address), size(_size) {}
+
+public:
+    AllocatedSocketAddress():address(nullptr), size(0) {}
+
+    AllocatedSocketAddress(AllocatedSocketAddress &&src)
+        :address(src.address), size(src.size) {
+        src.address = nullptr;
+        src.size = 0;
+    }
+
+    ~AllocatedSocketAddress() {
+        free(address);
+    }
+
+    AllocatedSocketAddress &operator=(AllocatedSocketAddress &&src) {
+        std::swap(address, src.address);
+        std::swap(size, src.size);
+        return *this;
+    }
+
+    gcc_const
+    static AllocatedSocketAddress Null() {
+        return AllocatedSocketAddress(nullptr, 0);
+    }
+
+    bool IsNull() const {
+        return address == nullptr;
+    }
+
+    size_t GetSize() const {
+        return size;
+    }
+
+    const struct sockaddr *GetAddress() const {
+        return address;
+    }
+
+    operator SocketAddress() const {
+        return SocketAddress(address, size);
+    }
+
+    operator const struct sockaddr *() const {
+        return address;
+    }
+
+    int GetFamily() const {
+        return address->sa_family;
+    }
+
+    void Clear() {
+        free(address);
+        address = nullptr;
+        size = 0;
+    }
+
+    /**
+     * Make this a "local" address (UNIX domain socket).
+     */
+    void SetLocal(const char *path);
+
+    bool Parse(const char *p, int default_port,
+               bool passive, GError **error_r);
+
+private:
+    void SetSize(size_t new_size);
+};
+
+#endif
