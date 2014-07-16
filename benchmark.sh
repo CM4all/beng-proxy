a7833a3a87467802fb4311debc9a014758defb19
Max Kellermann <mk@cm4all.com>
2014-07-16 13:42:16 +0200

    benchmark.sh: use clang; support C++; enable silent-rules

--- benchmark.sh
+++ benchmark.sh
@@ -2,7 +2,11 @@
 
 set -e
 
-CFLAGS="-O2 -ggdb" ./configure \
+./configure \
+	CC=clang CXX=clang++ \
+	CFLAGS="-O2 -ggdb" CXXFLAGS="-O2 -ggdb -D__STRICT_ANSI__" \
 	--prefix=/usr/local/stow/cm4all-beng-proxy \
 	--disable-debug \
+	--enable-werror \
+	--enable-silent-rules \
 	"$@"
