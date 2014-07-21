7246d7ad0c5409ca69f260ed54edf3c4ebaa6a90
Max Kellermann <mk@cm4all.com>
2014-07-21 15:20:44 +0200

    test/t_widget_registry: use NewFromPool() to allocate struct widget_view

--- test/t_widget_registry.cxx
+++ test/t_widget_registry.cxx
@@ -71,7 +71,8 @@ tstock_translate(gcc_unused struct tstock *stock, struct pool *pool,
         auto response = NewFromPool<TranslateResponse>(*pool);
         response->address.type = RESOURCE_ADDRESS_HTTP;
         response->address.u.http = http_address_parse(pool, "http://foo/", NULL);
-        response->views = (widget_view *)p_calloc(pool, sizeof(*response->views));
+        response->views = NewFromPool<struct widget_view>(*pool);
+        response->views->Init();
         response->views->address = response->address;
         handler->response(response, ctx);
     } else if (strcmp(request->widget_type, "block") == 0) {
