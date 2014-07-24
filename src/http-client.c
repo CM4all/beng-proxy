CM4all/beng-proxy

commit d622d87334a99e7dc6dc01dd385eb363d9ce9b46

http_client: allow comma-separated list in "Connection" response header

src/http-client.c
@@ -6,6 +6,7 @@
 
 #include "http-client.h"
 #include "http-response.h"
+#include "http-util.h"
 #include "fifo-buffer.h"
 #include "strutil.h"
 #include "header-parser.h"
@@ -482,7 +483,7 @@ http_client_headers_finished(struct http_client *client)
     client->keep_alive =
         (header_connection == NULL && !client->response.http_1_0) ||
         (header_connection != NULL &&
-         strcasecmp(header_connection, "keep-alive") == 0);
+         http_list_contains_i(header_connection, "keep-alive") == 0);
 
     if (http_status_is_empty(client->response.status) ||
         client->response.no_body) {

Makefile.am
@@ -1132,6 +1132,7 @@ test_run_http_server_LDADD = $(AM_LDADD) \
 test_t_http_client_SOURCES = test/t-http-client.c \
 	src/t_client.h \
 	src/http-client.c \
+	src/http-util.c \
 	src/socket_wrapper.c src/buffered_socket.c src/filtered_socket.c \
 	src/cleanup_timer.c \
 	$(STOPWATCH_SRC) \
@@ -1628,6 +1629,7 @@ test_t_ajp_client_LDADD = $(AM_LDADD) \
 test_run_client_SOURCES = test/run_client.c \
 	src/client-socket.c \
 	src/http-client.c src/http-body.c \
+	src/http-util.c \
 	src/header-writer.c src/header-parser.c \
 	src/ajp-client.c src/ajp-serialize.c \
 	src/ajp-headers.c src/ajp-protocol.c \

commit 7f42614a9f83945b8d0a0057994454d4fa823f33

http_client: fix bug with HTTP 1.0 Keep-Alive
Broken by commit d622d873

src/http-client.c
@@ -483,7 +483,7 @@ http_client_headers_finished(struct http_client *client)
     client->keep_alive =
         (header_connection == NULL && !client->response.http_1_0) ||
         (header_connection != NULL &&
-         http_list_contains_i(header_connection, "keep-alive") == 0);
+         http_list_contains_i(header_connection, "keep-alive"));
 
     if (http_status_is_empty(client->response.status) ||
         client->response.no_body) {