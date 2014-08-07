CM4all/beng-proxy

commit 7a76417c2a7ccc1c483d3d2cca564e940f0630d1

lb_config: use class AllocatedSocketAddress instead of struct address_envelope

src/lb_config.hxx
@@ -10,6 +10,7 @@
 #include "address_list.hxx"
 #include "sticky.h"
 #include "ssl_config.hxx"
+#include "net/AllocatedSocketAddress.hxx"
 
 #include <http/status.h>
 
@@ -28,9 +29,7 @@ enum lb_protocol {
 };
 
 struct lb_control_config {
-    const struct address_envelope *envelope;
-
-    lb_control_config():envelope(nullptr) {}
+    AllocatedSocketAddress bind_address;
 };
 
 struct lb_monitor_config {
@@ -89,7 +88,7 @@ struct lb_monitor_config {
 struct lb_node_config {
     std::string name;
 
-    const struct address_envelope *envelope;
+    AllocatedSocketAddress address;
 
     /**
      * The Tomcat "jvmRoute" setting of this node.  It is used for
@@ -97,10 +96,15 @@ struct lb_node_config {
      */
     std::string jvm_route;
 
-    lb_node_config(const char *_name,
-                   const struct address_envelope *_envelope=nullptr)
-        :name(_name),
-         envelope(_envelope) {}
+    lb_node_config(const char *_name)
+        :name(_name) {}
+
+    lb_node_config(const char *_name, AllocatedSocketAddress &&_address)
+        :name(_name), address(std::move(_address)) {}
+
+    lb_node_config(lb_node_config &&src)
+        :name(std::move(src.name)), address(std::move(src.address)),
+         jvm_route(std::move(jvm_route)) {}
 };
 
 struct lb_member_config {

src/lb_config.cxx
@@ -285,7 +285,7 @@ config_parser_feed_control(struct config_parser *parser, char *p,
         if (!expect_eol(p + 1))
             return syntax_error(error_r);
 
-        if (control->envelope == NULL)
+        if (control->bind_address.IsNull())
             return _throw(error_r, "Bind address is missing");
 
         parser->config.controls.emplace_back(std::move(*control));
@@ -307,10 +307,7 @@ config_parser_feed_control(struct config_parser *parser, char *p,
         if (!expect_eol(p))
             return syntax_error(error_r);
 
-        control->envelope = address_envelope_parse(parser->pool,
-                                                   address, 80, true,
-                                                   error_r);
-        if (control->envelope == NULL)
+        if (!control->bind_address.Parse(address, 80, true, error_r))
             return false;
 
         return true;
@@ -478,16 +475,13 @@ config_parser_feed_node(struct config_parser *parser, char *p,
         if (!expect_eol(p + 1))
             return syntax_error(error_r);
 
-        if (node->envelope == NULL) {
-            node->envelope = address_envelope_parse(parser->pool,
-                                                    node->name.c_str(),
-                                                    80, false,
-                                                    error_r);
-            if (node->envelope == NULL)
-                return false;
-        }
+        if (node->address.IsNull() &&
+            !node->address.Parse(node->name.c_str(),
+                                 80, false,
+                                 error_r))
+            return false;
 
-        parser->config.nodes.insert(std::make_pair(node->name, *node));
+        parser->config.nodes.insert(std::make_pair(node->name, std::move(*node)));
         delete node;
 
         parser->state = config_parser::State::ROOT;
@@ -506,12 +500,10 @@ config_parser_feed_node(struct config_parser *parser, char *p,
         if (!expect_eol(p))
             return syntax_error(error_r);
 
-        if (node->envelope != NULL)
+        if (!node->address.IsNull())
             return _throw(error_r, "Duplicate node address");
 
-        node->envelope = address_envelope_parse(parser->pool,
-                                                value, 80, false, error_r);
-        if (node->envelope == NULL)
+        if (!node->address.Parse(value, 80, false, error_r))
             return false;
 
         return true;
@@ -536,13 +528,13 @@ static struct lb_node_config *
 auto_create_node(struct config_parser *parser, const char *name,
                  GError **error_r)
 {
-    const struct address_envelope *envelope =
-        address_envelope_parse(parser->pool, name, 80, false, error_r);
-    if (envelope == NULL)
+    AllocatedSocketAddress address;
+    if (!address.Parse(name, 80, false, error_r))
         return NULL;
 
-    lb_node_config node(name, envelope);
-    auto i = parser->config.nodes.insert(std::make_pair(name, node));
+    lb_node_config node(name, std::move(address));
+    auto i = parser->config.nodes.insert(std::make_pair(name,
+                                                        std::move(node)));
 
     return &i.first->second;
 }
@@ -609,11 +601,11 @@ sockaddr_port(const struct sockaddr *address)
 }
 
 static unsigned
-parse_port(const char *p, const struct address_envelope *envelope)
+parse_port(const char *p, SocketAddress address)
 {
     struct addrinfo hints;
     memset(&hints, 0, sizeof(hints));
-    hints.ai_family = envelope->address.sa_family;
+    hints.ai_family = address.GetFamily();
     hints.ai_socktype = SOCK_STREAM;
 
     struct addrinfo *ai;
@@ -776,7 +768,7 @@ config_parser_feed_cluster(struct config_parser *parser, char *p,
                                               error_r);
                 }
 
-                member->port = parse_port(q, member->node->envelope);
+                member->port = parse_port(q, member->node->address);
                 if (member->port == 0)
                     return _throw(error_r, "Malformed port");
             } else
@@ -1318,13 +1310,13 @@ lb_cluster_config_finish(struct pool *pool, lb_cluster_config &config,
     config.address_list.SetStickyMode(config.sticky_mode);
 
     for (auto &member : config.members) {
-        const struct address_envelope *envelope = member.node->envelope;
+        const AllocatedSocketAddress &node_address = member.node->address;
         const struct sockaddr *address = member.port != 0
-            ? sockaddr_set_port(pool, &envelope->address, envelope->length,
+            ? sockaddr_set_port(pool, node_address, node_address.GetSize(),
                                 member.port)
-            : &envelope->address;
+            : node_address;
 
-        if (!config.address_list.Add(pool, address, envelope->length))
+        if (!config.address_list.Add(pool, address, node_address.GetSize()))
             return _throw(error_r, "Too many members");
     }
 

src/lb_control.cxx
@@ -52,15 +52,15 @@ enable_node(const struct lb_instance *instance,
 
     const struct sockaddr *with_port =
         sockaddr_set_port(tpool,
-                          &node->envelope->address, node->envelope->length,
+                          node->address, node->address.GetSize(),
                           port);
 
     char buffer[64];
     socket_address_to_string(buffer, sizeof(buffer), with_port,
-                             node->envelope->length);
+                             node->address.GetSize());
     daemon_log(4, "enabling node %s (%s)\n", node_name, buffer);
 
-    failure_unset(with_port, node->envelope->length, FAILURE_OK);
+    failure_unset(with_port, node->address.GetSize(), FAILURE_OK);
 }
 
 static void
@@ -94,16 +94,16 @@ fade_node(const struct lb_instance *instance,
 
     const struct sockaddr *with_port =
         sockaddr_set_port(tpool,
-                          &node->envelope->address, node->envelope->length,
+                          node->address, node->address.GetSize(),
                           port);
 
     char buffer[64];
     socket_address_to_string(buffer, sizeof(buffer), with_port,
-                             node->envelope->length);
+                             node->address.GetSize());
     daemon_log(4, "fading node %s (%s)\n", node_name, buffer);
 
     /* set status "FADE" for 3 hours */
-    failure_set(with_port, node->envelope->length, FAILURE_FADE, 3 * 3600);
+    failure_set(with_port, node->address.GetSize(), FAILURE_FADE, 3 * 3600);
 }
 
 G_GNUC_CONST
@@ -189,15 +189,15 @@ query_node_status(struct lb_control *control,
 
     const struct sockaddr *with_port =
         sockaddr_set_port(tpool,
-                          &node->envelope->address, node->envelope->length,
+                          node->address, node->address.GetSize(),
                           port);
 
     char buffer[64];
     socket_address_to_string(buffer, sizeof(buffer), with_port,
-                             node->envelope->length);
+                             node->address.GetSize());
 
     enum failure_status status =
-        failure_get_status(with_port, node->envelope->length);
+        failure_get_status(with_port, node->address.GetSize());
     const char *s = failure_status_to_string(status);
 
     GError *error = NULL;
@@ -293,8 +293,8 @@ lb_control_new(struct lb_instance *instance,
     control->instance = instance;
 
     control->server =
-        control_server_new(pool, &config->envelope->address,
-                           config->envelope->length,
+        control_server_new(pool, config->bind_address,
+                           config->bind_address.GetSize(),
                            &lb_control_handler, control,
                            error_r);
     if (control->server == NULL) {

src/lb_hmonitor.cxx
@@ -88,13 +88,14 @@ lb_hmonitor_add(const struct lb_node_config *node, unsigned port,
         struct pool *pool = pool_new_linear(hmonitor_pool, "monitor", 1024);
         key = p_strdup(pool, key);
 
-        const struct sockaddr *address = &node->envelope->address;
+        const struct sockaddr *address = node->address;
         if (port > 0)
             address = sockaddr_set_port(pool, address,
-                                        node->envelope->length, port);
+                                        node->address.GetSize(), port);
 
         monitor = lb_monitor_new(pool, key, config,
-                                 SocketAddress(address, node->envelope->length),
+                                 SocketAddress(address,
+                                               node->address.GetSize()),
                                  class_);
         pool_unref(pool);
         hashmap_add(hmonitor_map, key, monitor);

commit 80413d7cfbe0a17a6d2bd4a4a6eacd590313c013

lb_config: fix typo in move constructor
Breakage by commit 7a76417c

src/lb_config.hxx
@@ -104,7 +104,7 @@ struct lb_node_config {
 
     lb_node_config(lb_node_config &&src)
         :name(std::move(src.name)), address(std::move(src.address)),
-         jvm_route(std::move(jvm_route)) {}
+         jvm_route(std::move(src.jvm_route)) {}
 };
 
 struct lb_member_config {