4d0ef282a7ea4db1d9b09ee35f0834495b359d2b
Max Kellermann <mk@cm4all.com>
2014-08-12 19:05:18 +0200

    address_envelope: add operator SocketAddress

--- src/address_envelope.hxx
+++ src/address_envelope.hxx
@@ -5,12 +5,18 @@
 #ifndef BENG_PROXY_ADDRESS_ENVELOPE_HXX
 #define BENG_PROXY_ADDRESS_ENVELOPE_HXX
 
+#include "net/SocketAddress.hxx"
+
 #include <sys/socket.h>
 
 struct address_envelope {
     socklen_t length;
 
     struct sockaddr address;
+
+    operator SocketAddress() const {
+        return { &address, length };
+    }
 };
 
 #endif
