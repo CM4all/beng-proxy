b9ccb3e5d94a5679f5e45cebc92437fea9130ab1
Max Kellermann <mk@cm4all.com>
2014-09-12 12:04:15 +0200

    SocketWrapper: add method Shutdown()

--- src/socket_wrapper.hxx
+++ src/socket_wrapper.hxx
@@ -58,6 +58,14 @@ public:
               int _fd, enum istream_direct _fd_type,
               const struct socket_handler *_handler, void *_ctx);
 
+    /**
+     * Shut down the socket gracefully, allowing the TCP stack to
+     * complete all pending transfers.  If you call Close() without
+     * Shutdown(), it may reset the connection and discard pending
+     * data.
+     */
+    void Shutdown();
+
     void Close();
 
     /**
