afa9914a8ab446ecf7b165b1075502126e51b9ea
Max Kellermann <mk@cm4all.com>
2015-03-19 11:33:55 +0100

    shm/dstring: convert to C++

--- src/shm/dstring.cxx
+++ src/shm/dstring.cxx
@@ -13,25 +13,25 @@ char *
 d_memdup(struct dpool *pool, const void *src, size_t length)
 {
     void *dest = d_malloc(pool, length);
-    if (dest == NULL)
-        return NULL;
+    if (dest == nullptr)
+        return nullptr;
 
     memcpy(dest, src, length);
-    return dest;
+    return (char *)dest;
 }
 
 char *
 d_strdup(struct dpool *pool, const char *src)
 {
-    return d_memdup(pool, src, strlen(src) + 1);
+    return (char *)d_memdup(pool, src, strlen(src) + 1);
 }
 
 char *
 d_strndup(struct dpool *pool, const char *src, size_t length)
 {
-    char *dest = d_malloc(pool, length + 1);
-    if (dest == NULL)
-        return NULL;
+    char *dest = (char *)d_malloc(pool, length + 1);
+    if (dest == nullptr)
+        return nullptr;
 
     memcpy(dest, src, length);
     dest[length] = 0;
