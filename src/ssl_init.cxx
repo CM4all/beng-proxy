cab2f402ae34151905d8867c36ae0c434bf28576
Max Kellermann <mk@cm4all.com>
2015-03-24 19:09:51 +0100

    ssl_init: compatibility #if for OpenSSL 0.9.8

--- src/ssl_init.cxx
+++ src/ssl_init.cxx
@@ -55,7 +55,13 @@ ssl_global_deinit()
 {
     EVP_cleanup();
     CRYPTO_cleanup_all_ex_data();
+
+#if OPENSSL_VERSION_NUMBER >= 0x10000000L
     ERR_remove_thread_state(nullptr);
+#else
+    ERR_remove_state(0);
+#endif
+
     ERR_free_strings();
 
     CRYPTO_set_id_callback(nullptr);
