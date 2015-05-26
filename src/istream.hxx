0b47cbf4d904bd1770ff0343f1bad72df849f9a0
Max Kellermann <mk@cm4all.com>
2015-05-26 15:53:25 +0200

    istream: move struct istream_class to istream_class.hxx

--- src/istream.hxx
+++ src/istream.hxx
@@ -7,6 +7,7 @@
 #ifndef ISTREAM_HXX
 #define ISTREAM_HXX
 
+#include "istream_class.hxx"
 #include "pool.h"
 #include "istream-direct.h"
 #include "glibfwd.hxx"
@@ -125,57 +126,6 @@ struct istream {
     istream(const struct istream_class &cls, struct pool &pool);
 
     istream(const struct istream &) = delete;
-};
-
-struct istream_class {
-    /**
-     * How much data is available? 
-     *
-     * @param partial if false, the stream must provide the data size
-     * until the end of the stream; for partial, a minimum estimate is
-     * ok
-     * @return the number of bytes available or -1 if the object does
-     * not know
-     */
-    off_t (*available)(struct istream *istream, bool partial);
-
-    /**
-     * Skip data without processing it.  By skipping 0 bytes, you can
-     * test whether the stream is able to skip at all.
-     *
-     * @return the number of bytes skipped or -1 if skipping is not supported
-     */
-    off_t (*skip)(struct istream *istream, off_t length);
-
-    /**
-     * Try to read from the stream.  If the stream can read data
-     * without blocking, it must provide data.  It may invoke the
-     * callbacks any number of times, supposed that the handler itself
-     * doesn't block.
-     *
-     * If the stream does not provide data immediately (and it is not
-     * at EOF yet), it must install an event and invoke the handler
-     * later, whenever data becomes available.
-     *
-     * Whenever the handler reports it is blocking, the responsibility
-     * for calling back (and calling this function) is handed back to
-     * the istream handler.
-     */
-    void (*read)(struct istream *istream);
-
-    /**
-     * Close the istream object, and return the remaining data as a
-     * file descriptor.  This fd can be read until end-of-stream.
-     * Returns -1 if this is not possible (the istream object is still
-     * usable).
-     */
-    int (*as_fd)(struct istream *istream);
-
-    /**
-     * Close the stream and free resources.  This must not be called
-     * after the handler's eof() / abort() callbacks were invoked.
-     */
-    void (*close)(struct istream *istream);
 };
 
 static inline struct istream *
