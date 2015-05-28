13f18f170c1de2d592cd0a6d1ad58f13261ee64f
Max Kellermann <mk@cm4all.com>
2015-05-28 11:09:43 +0200

    istream_optional: use OO API

--- src/istream_optional.cxx
+++ src/istream_optional.cxx
@@ -7,158 +7,75 @@
  */
 
 #include "istream_optional.hxx"
-#include "istream_null.hxx"
-#include "istream_internal.hxx"
 #include "istream_forward.hxx"
-#include "util/Cast.hxx"
+#include "istream_null.hxx"
 
 #include <assert.h>
 
-struct istream_optional {
-    struct istream output;
+class OptionalIstream final : public ForwardIstream {
+    bool resumed = false;
 
-    struct istream *input;
+public:
+    OptionalIstream(struct pool &p, struct istream &_input)
+        :ForwardIstream(p, _input,
+                        MakeIstreamHandler<OptionalIstream>::handler, this) {}
 
-    bool resumed;
-};
-
-
-/*
- * istream handler
- *
- */
-
-static size_t
-optional_input_data(const void *data, size_t length, void *ctx)
-{
-    struct istream_optional &optional = *(struct istream_optional *)ctx;
-
-    return optional.resumed
-        ? istream_invoke_data(&optional.output, data, length)
-        : 0;
-}
-
-static ssize_t
-optional_input_direct(enum istream_direct type, int fd, size_t max_length,
-                      void *ctx)
-{
-    struct istream_optional &optional = *(struct istream_optional *)ctx;
-
-    assert(optional.resumed);
-
-    return istream_invoke_direct(&optional.output, type, fd, max_length);
-}
-
-static const struct istream_handler optional_input_handler = {
-    .data = optional_input_data,
-    .direct = optional_input_direct,
-    .eof = istream_forward_eof,
-    .abort = istream_forward_abort
-};
-
-
-/*
- * istream implementation
- *
- */
-
-static inline struct istream_optional &
-istream_to_optional(struct istream *istream)
-{
-    return ContainerCast2(*istream, &istream_optional::output);
-}
-
-static off_t
-istream_optional_available(struct istream *istream, bool partial)
-{
-    struct istream_optional &optional = istream_to_optional(istream);
-
-    return optional.resumed
-        ? istream_available(optional.input, partial)
-        : -1;
-}
-
-static void
-istream_optional_read(struct istream *istream)
-{
-    struct istream_optional &optional = istream_to_optional(istream);
-
-    if (optional.resumed) {
-        istream_handler_set_direct(optional.input,
-                                   optional.output.handler_direct);
-        istream_read(optional.input);
+    void Resume() {
+        resumed = true;
     }
-}
 
-static int
-istream_optional_as_fd(struct istream *istream)
-{
-    struct istream_optional &optional = istream_to_optional(istream);
+    void Discard() {
+        assert(!resumed);
 
-    return optional.resumed
-        ? istream_as_fd(optional.input)
-        : -1;
-}
+        resumed = true;
 
-static void
-istream_optional_close(struct istream *istream)
-{
-    struct istream_optional &optional = istream_to_optional(istream);
+        /* replace the input with a "null" istream */
+        ReplaceInputDirect(*istream_null_new(&GetPool()),
+                           MakeIstreamHandler<OptionalIstream>::handler, this);
+    }
 
-    istream_close_handler(optional.input);
-    istream_deinit(&optional.output);
-}
+    /* virtual methods from class Istream */
 
-static const struct istream_class istream_optional = {
-    .available = istream_optional_available,
-    .read = istream_optional_read,
-    .as_fd = istream_optional_as_fd,
-    .close = istream_optional_close,
+    off_t GetAvailable(bool partial) override {
+        /* can't respond to this until we're resumed, because the
+           original input can be discarded */
+        return resumed ? ForwardIstream::GetAvailable(partial) : -1;
+    }
+
+    void Read() override {
+        if (resumed)
+            ForwardIstream::Read();
+    }
+
+    int AsFd() override {
+        return resumed
+            ? ForwardIstream::AsFd()
+            : -1;
+    }
+
+    /* handler */
+
+    size_t OnData(const void *data, size_t length) {
+        return resumed ? InvokeData(data, length) : 0;
+    }
 };
-
-
-/*
- * constructor
- *
- */
 
 struct istream *
 istream_optional_new(struct pool *pool, struct istream *input)
 {
-    struct istream_optional *optional = istream_new_macro(pool, optional);
-
-    istream_assign_handler(&optional->input, input,
-                           &optional_input_handler, optional,
-                           0);
-
-    optional->resumed = false;
-
-    return &optional->output;
+    return NewIstream<OptionalIstream>(*pool, *input);
 }
 
 void
 istream_optional_resume(struct istream *istream)
 {
-    struct istream_optional *optional = (struct istream_optional *)istream;
-
-    assert(!optional->resumed);
-
-    optional->resumed = true;
+    auto &optional = (OptionalIstream &)Istream::Cast(*istream);
+    optional.Resume();
 }
 
 void
 istream_optional_discard(struct istream *istream)
 {
-    struct istream_optional *optional = (struct istream_optional *)istream;
-
-    assert(!optional->resumed);
-
-    optional->resumed = true;
-
-    /* replace the input with a "null" istream */
-    istream_close_handler(optional->input);
-    istream_assign_handler(&optional->input,
-                           istream_null_new(optional->output.pool),
-                           &optional_input_handler, optional,
-                           optional->output.handler_direct);
+    auto &optional = (OptionalIstream &)Istream::Cast(*istream);
+    optional.Discard();
 }
