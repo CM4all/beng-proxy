41553e0e7596c16f48b5fdc4015ceeac489a3fba
Max Kellermann <mk@cm4all.com>
2015-05-29 18:01:24 +0200

    istream_delayed: use OO API

--- src/istream_delayed.cxx
+++ src/istream_delayed.cxx
@@ -3,154 +3,113 @@
  */
 
 #include "istream_delayed.hxx"
-#include "istream_internal.hxx"
 #include "istream_forward.hxx"
 #include "async.hxx"
-#include "util/Cast.hxx"
-
-#include <glib.h>
 
 #include <assert.h>
 #include <string.h>
 
-struct istream_delayed {
-    struct istream output;
-    struct istream *input;
+class DelayedIstream final : public ForwardIstream {
     struct async_operation_ref async;
-};
 
-
-/*
- * istream implementation
- *
- */
-
-static inline struct istream_delayed *
-istream_to_delayed(struct istream *istream)
-{
-    return &ContainerCast2(*istream, &istream_delayed::output);
-}
-
-static off_t
-istream_delayed_available(struct istream *istream, bool partial)
-{
-    struct istream_delayed *delayed = istream_to_delayed(istream);
-
-    if (delayed->input == nullptr)
-        return (off_t)-1;
-    else
-        return istream_available(delayed->input, partial);
-}
-
-static void
-istream_delayed_read(struct istream *istream)
-{
-    struct istream_delayed *delayed = istream_to_delayed(istream);
-
-    if (delayed->input != nullptr) {
-        istream_handler_set_direct(delayed->input,
-                                   delayed->output.handler_direct);
-        istream_read(delayed->input);
+public:
+    explicit DelayedIstream(struct pool &p)
+        :ForwardIstream(p) {
     }
-}
 
-static int
-istream_delayed_as_fd(struct istream *istream)
-{
-    struct istream_delayed *delayed = istream_to_delayed(istream);
+    struct async_operation_ref &GetAsyncRef() {
+        return async;
+    }
 
-    if (delayed->input == nullptr)
-        return -1;
+    void Set(struct istream &_input) {
+        assert(!HasInput());
+        assert(!istream_has_handler(&_input));
 
-    int fd = istream_as_fd(delayed->input);
-    if (fd >= 0)
-        istream_deinit(&delayed->output);
+        async.Poison();
+        SetInput(_input,
+                 MakeIstreamHandler<DelayedIstream>::handler, this,
+                 GetHandlerDirect());
+    }
 
-    return fd;
-}
+    void SetEof() {
+        assert(!HasInput());
 
-static void
-istream_delayed_close(struct istream *istream)
-{
-    struct istream_delayed *delayed = istream_to_delayed(istream);
+        async.Poison();
+        DestroyEof();
+    }
 
-    if (delayed->input != nullptr)
-        istream_close_handler(delayed->input);
-    else if (delayed->async.IsDefined())
-        delayed->async.Abort();
+    void SetError(GError *error) {
+        assert(!HasInput());
 
-    istream_deinit(&delayed->output);
-}
+        async.Poison();
+        DestroyError(error);
+    }
 
-static const struct istream_class istream_delayed = {
-    .available = istream_delayed_available,
-    .read = istream_delayed_read,
-    .as_fd = istream_delayed_as_fd,
-    .close = istream_delayed_close,
+    /* virtual methods from class Istream */
+
+    off_t GetAvailable(bool partial) override {
+        return HasInput()
+            ? ForwardIstream::GetAvailable(partial)
+            : -1;
+    }
+
+    void Read() override {
+        if (HasInput())
+            ForwardIstream::Read();
+    }
+
+    int AsFd() override {
+        return HasInput()
+            ? ForwardIstream::AsFd()
+            : -1;
+    }
+
+    void Close() override {
+        if (HasInput())
+            ForwardIstream::Close();
+        else {
+            if (async.IsDefined())
+                async.Abort();
+
+            Destroy();
+        }
+    }
 };
-
-
-/*
- * constructor
- *
- */
 
 struct istream *
 istream_delayed_new(struct pool *pool)
 {
-    struct istream_delayed *delayed = istream_new_macro(pool, delayed);
-
-    delayed->input = nullptr;
-    return &delayed->output;
+    return NewIstream<DelayedIstream>(*pool);
 }
 
 struct async_operation_ref *
 istream_delayed_async_ref(struct istream *i_delayed)
 {
-    struct istream_delayed *delayed = (struct istream_delayed *)i_delayed;
+    auto &delayed = (DelayedIstream &)Istream::Cast(*i_delayed);
 
-    return &delayed->async;
+    return &delayed.GetAsyncRef();
 }
 
 void
 istream_delayed_set(struct istream *i_delayed, struct istream *input)
 {
-    struct istream_delayed *delayed = (struct istream_delayed *)i_delayed;
+    auto &delayed = (DelayedIstream &)Istream::Cast(*i_delayed);
 
-    assert(delayed != nullptr);
-    assert(delayed->input == nullptr);
-    assert(input != nullptr);
-    assert(!istream_has_handler(input));
-
-    delayed->async.Poison();
-
-    istream_assign_handler(&delayed->input, input,
-                           &istream_forward_handler, &delayed->output,
-                           delayed->output.handler_direct);
+    delayed.Set(*input);
 }
 
 void
 istream_delayed_set_eof(struct istream *i_delayed)
 {
-    struct istream_delayed *delayed = (struct istream_delayed *)i_delayed;
+    auto &delayed = (DelayedIstream &)Istream::Cast(*i_delayed);
 
-    assert(delayed != nullptr);
-    assert(delayed->input == nullptr);
-
-    delayed->async.Poison();
-
-    istream_deinit_eof(&delayed->output);
+    delayed.SetEof();
 }
 
 void
 istream_delayed_set_abort(struct istream *i_delayed, GError *error)
 {
-    struct istream_delayed *delayed = (struct istream_delayed *)i_delayed;
+    auto &delayed = (DelayedIstream &)Istream::Cast(*i_delayed);
 
-    assert(delayed != nullptr);
-    assert(delayed->input == nullptr);
-
-    delayed->async.Poison();
-
-    istream_deinit_abort(&delayed->output, error);
+    delayed.SetError(error);
 }
