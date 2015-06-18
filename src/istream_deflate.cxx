73e131c86740fe8defdde9abf080d1ece5f48771
Max Kellermann <mk@cm4all.com>
2015-06-18 21:12:06 +0200

    istream_deflate: move code into the struct

--- src/istream_deflate.cxx
+++ src/istream_deflate.cxx
@@ -25,6 +25,56 @@ struct DeflateIstream {
     StaticFifoBuffer<uint8_t, 4096> buffer;
 
     DeflateIstream(struct pool &pool, struct istream &_input);
+
+    bool InitZlib();
+
+    void DeinitZlib() {
+        if (z_initialized) {
+            z_initialized = false;
+            deflateEnd(&z);
+        }
+    }
+
+    void Abort(GError *error) {
+        DeinitZlib();
+
+        if (input != nullptr)
+            istream_free_handler(&input);
+
+        istream_deinit_abort(&output, error);
+    }
+
+    /**
+     * Submit data from the buffer to our istream handler.
+     *
+     * @return the number of bytes which were handled, or 0 if the
+     * stream was closed
+     */
+    size_t TryWrite();
+
+    /**
+     * Starts to write to the buffer.
+     *
+     * @return a pointer to the writable buffer, or nullptr if there is no
+     * room (our istream handler blocks) or if the stream was closed
+     */
+    WritableBuffer<void> BufferWrite() {
+        auto w = buffer.Write();
+        if (w.IsEmpty() && TryWrite() > 0)
+            w = buffer.Write();
+
+        return w.ToVoid();
+    }
+
+    void TryFlush();
+
+    /**
+     * Read from our input until we have submitted some bytes to our
+     * istream handler.
+     */
+    void ForceRead();
+
+    void TryFinish();
 };
 
 gcc_const
@@ -32,26 +82,6 @@ static GQuark
 zlib_quark(void)
 {
     return g_quark_from_static_string("zlib");
-}
-
-static void
-deflate_close(DeflateIstream *defl)
-{
-    if (defl->z_initialized) {
-        defl->z_initialized = false;
-        deflateEnd(&defl->z);
-    }
-}
-
-static void
-deflate_abort(DeflateIstream *defl, GError *error)
-{
-    deflate_close(defl);
-
-    if (defl->input != nullptr)
-        istream_free_handler(&defl->input);
-
-    istream_deinit_abort(&defl->output, error);
 }
 
 static voidpf z_alloc
@@ -69,168 +99,142 @@ z_free(voidpf opaque, voidpf address)
     (void)address;
 }
 
-static int
-deflate_initialize_z(DeflateIstream *defl)
+bool
+DeflateIstream::InitZlib()
 {
-    if (defl->z_initialized)
-        return Z_OK;
+    if (z_initialized)
+        return true;
 
-    defl->z.zalloc = z_alloc;
-    defl->z.zfree = z_free;
-    defl->z.opaque = defl->output.pool;
+    z.zalloc = z_alloc;
+    z.zfree = z_free;
+    z.opaque = output.pool;
 
-    int err = deflateInit(&defl->z, Z_DEFAULT_COMPRESSION);
+    int err = deflateInit(&z, Z_DEFAULT_COMPRESSION);
     if (err != Z_OK) {
         GError *error =
             g_error_new(zlib_quark(), err,
                         "deflateInit(Z_FINISH) failed: %d", err);
-        deflate_abort(defl, error);
-        return err;
+        Abort(error);
+        return false;
     }
 
-    defl->z_initialized = true;
-    return Z_OK;
+    z_initialized = true;
+    return true;
 }
 
-/**
- * Submit data from the buffer to our istream handler.
- *
- * @return the number of bytes which were handled, or 0 if the stream
- * was closed
- */
-static size_t
-deflate_try_write(DeflateIstream *defl)
+size_t
+DeflateIstream::TryWrite()
 {
-    auto r = defl->buffer.Read();
+    auto r = buffer.Read();
     assert(!r.IsEmpty());
 
-    size_t nbytes = istream_invoke_data(&defl->output, r.data, r.size);
+    size_t nbytes = istream_invoke_data(&output, r.data, r.size);
     if (nbytes == 0)
         return 0;
 
-    defl->buffer.Consume(nbytes);
+    buffer.Consume(nbytes);
 
-    if (nbytes == r.size && defl->input == nullptr && defl->z_stream_end) {
-        deflate_close(defl);
-        istream_deinit_eof(&defl->output);
+    if (nbytes == r.size && input == nullptr && z_stream_end) {
+        DeinitZlib();
+        istream_deinit_eof(&output);
         return 0;
     }
 
     return nbytes;
 }
 
-/**
- * Starts to write to the buffer.
- *
- * @return a pointer to the writable buffer, or nullptr if there is no
- * room (our istream handler blocks) or if the stream was closed
- */
-static WritableBuffer<void>
-deflate_buffer_write(DeflateIstream *defl)
+inline void
+DeflateIstream::TryFlush()
 {
-    auto w = defl->buffer.Write();
-    if (w.IsEmpty() && deflate_try_write(defl) > 0)
-        w = defl->buffer.Write();
+    assert(!z_stream_end);
 
-    return w.ToVoid();
-}
-
-static void
-deflate_try_flush(DeflateIstream *defl)
-{
-    assert(!defl->z_stream_end);
-
-    auto w = deflate_buffer_write(defl);
+    auto w = BufferWrite();
     if (w.IsEmpty())
         return;
 
-    defl->z.next_out = (Bytef *)w.data;
-    defl->z.avail_out = (uInt)w.size;
+    z.next_out = (Bytef *)w.data;
+    z.avail_out = (uInt)w.size;
 
-    defl->z.next_in = nullptr;
-    defl->z.avail_in = 0;
+    z.next_in = nullptr;
+    z.avail_in = 0;
 
-    int err = deflate(&defl->z, Z_SYNC_FLUSH);
+    int err = deflate(&z, Z_SYNC_FLUSH);
     if (err != Z_OK) {
         GError *error =
             g_error_new(zlib_quark(), err,
                         "deflate(Z_SYNC_FLUSH) failed: %d", err);
-        deflate_abort(defl, error);
+        Abort(error);
         return;
     }
 
-    defl->buffer.Append(w.size - (size_t)defl->z.avail_out);
+    buffer.Append(w.size - (size_t)z.avail_out);
 
-    if (!defl->buffer.IsEmpty())
-        deflate_try_write(defl);
+    if (!buffer.IsEmpty())
+        TryWrite();
 }
 
-/**
- * Read from our input until we have submitted some bytes to our
- * istream handler.
- */
-static void
-istream_deflate_force_read(DeflateIstream *defl)
+inline
+void
+DeflateIstream::ForceRead()
 {
-    bool had_input = false;
+    bool had_input2 = false;
+    had_output = false;
 
-    defl->had_output = false;
-
-    pool_ref(defl->output.pool);
+    pool_ref(output.pool);
 
     while (1) {
-        defl->had_input = false;
-        istream_read(defl->input);
-        if (defl->input == nullptr || defl->had_output) {
-            pool_unref(defl->output.pool);
+        had_input = false;
+        istream_read(input);
+        if (input == nullptr || had_output) {
+            pool_unref(output.pool);
             return;
         }
 
-        if (!defl->had_input)
+        if (!had_input)
             break;
 
-        had_input = true;
+        had_input2 = true;
     }
 
-    pool_unref(defl->output.pool);
+    pool_unref(output.pool);
 
-    if (had_input)
-        deflate_try_flush(defl);
+    if (had_input2)
+        TryFlush();
 }
 
-static void
-deflate_try_finish(DeflateIstream *defl)
+void
+DeflateIstream::TryFinish()
 {
-    assert(!defl->z_stream_end);
+    assert(!z_stream_end);
 
-    auto w = deflate_buffer_write(defl);
+    auto w = BufferWrite();
     if (w.IsEmpty())
         return;
 
-    defl->z.next_out = (Bytef *)w.data;
-    defl->z.avail_out = (uInt)w.size;
+    z.next_out = (Bytef *)w.data;
+    z.avail_out = (uInt)w.size;
 
-    defl->z.next_in = nullptr;
-    defl->z.avail_in = 0;
+    z.next_in = nullptr;
+    z.avail_in = 0;
 
-    int err = deflate(&defl->z, Z_FINISH);
+    int err = deflate(&z, Z_FINISH);
     if (err == Z_STREAM_END)
-        defl->z_stream_end = true;
+        z_stream_end = true;
     else if (err != Z_OK) {
         GError *error =
             g_error_new(zlib_quark(), err,
                         "deflate(Z_FINISH) failed: %d", err);
-        deflate_abort(defl, error);
+        Abort(error);
         return;
     }
 
-    defl->buffer.Append(w.size - (size_t)defl->z.avail_out);
+    buffer.Append(w.size - (size_t)z.avail_out);
 
-    if (defl->z_stream_end && defl->buffer.IsEmpty()) {
-        deflate_close(defl);
-        istream_deinit_eof(&defl->output);
+    if (z_stream_end && buffer.IsEmpty()) {
+        DeinitZlib();
+        istream_deinit_eof(&output);
     } else
-        deflate_try_write(defl);
+        TryWrite();
 }
 
 
@@ -246,12 +250,11 @@ deflate_input_data(const void *data, size_t length, void *ctx)
 
     assert(defl->input != nullptr);
 
-    auto w = deflate_buffer_write(defl);
+    auto w = defl->BufferWrite();
     if (w.size < 64) /* reserve space for end-of-stream marker */
         return 0;
 
-    int err = deflate_initialize_z(defl);
-    if (err != Z_OK)
+    if (!defl->InitZlib())
         return 0;
 
     defl->had_input = true;
@@ -263,12 +266,12 @@ deflate_input_data(const void *data, size_t length, void *ctx)
     defl->z.avail_in = (uInt)length;
 
     do {
-        err = deflate(&defl->z, Z_NO_FLUSH);
+        auto err = deflate(&defl->z, Z_NO_FLUSH);
         if (err != Z_OK) {
             GError *error =
                 g_error_new(zlib_quark(), err,
                             "deflate() failed: %d", err);
-            deflate_abort(defl, error);
+            defl->Abort(error);
             return 0;
         }
 
@@ -278,7 +281,7 @@ deflate_input_data(const void *data, size_t length, void *ctx)
             defl->buffer.Append(nbytes);
 
             pool_ref(defl->output.pool);
-            deflate_try_write(defl);
+            defl->TryWrite();
 
             if (!defl->z_initialized) {
                 pool_unref(defl->output.pool);
@@ -289,7 +292,7 @@ deflate_input_data(const void *data, size_t length, void *ctx)
         } else
             break;
 
-        w = deflate_buffer_write(defl);
+        w = defl->BufferWrite();
         if (w.size < 64) /* reserve space for end-of-stream marker */
             break;
 
@@ -308,11 +311,10 @@ deflate_input_eof(void *ctx)
     assert(defl->input != nullptr);
     defl->input = nullptr;
 
-    int err = deflate_initialize_z(defl);
-    if (err != Z_OK)
+    if (!defl->InitZlib())
         return;
 
-    deflate_try_finish(defl);
+    defl->TryFinish();
 }
 
 static void
@@ -323,7 +325,7 @@ deflate_input_abort(GError *error, void *ctx)
     assert(defl->input != nullptr);
     defl->input = nullptr;
 
-    deflate_close(defl);
+    defl->DeinitZlib();
 
     istream_deinit_abort(&defl->output, error);
 }
@@ -352,11 +354,11 @@ istream_deflate_read(struct istream *istream)
     DeflateIstream *defl = istream_to_deflate(istream);
 
     if (!defl->buffer.IsEmpty())
-        deflate_try_write(defl);
+        defl->TryWrite();
     else if (defl->input == nullptr)
-        deflate_try_finish(defl);
+        defl->TryFinish();
     else
-        istream_deflate_force_read(defl);
+        defl->ForceRead();
 }
 
 static void
@@ -364,7 +366,7 @@ istream_deflate_close(struct istream *istream)
 {
     DeflateIstream *defl = istream_to_deflate(istream);
 
-    deflate_close(defl);
+    defl->DeinitZlib();
 
     if (defl->input != nullptr)
         istream_close_handler(defl->input);
