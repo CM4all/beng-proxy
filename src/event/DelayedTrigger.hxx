e493c1d84a4a80df3b438405714a0424a75c2608
Max Kellermann <mk@cm4all.com>
2015-06-24 12:39:55 +0200

    event/DelayedTrigger: new utility class

---
+++ src/event/DelayedTrigger.hxx
@@ -0,0 +1,41 @@
+/*
+ * author: Max Kellermann <mk@cm4all.com>
+ */
+
+#ifndef BENG_PROXY_DELAYED_TRIGGER_HXX
+#define BENG_PROXY_DELAYED_TRIGGER_HXX
+
+#include "Event.hxx"
+
+/**
+ * Invoke a callback after a certain delay.
+ */
+class DelayedTrigger {
+    Event event;
+
+    struct timeval tv;
+
+public:
+    DelayedTrigger(void (*callback)(evutil_socket_t, short, void *), void *ctx,
+                   unsigned delay_s) {
+        event.SetTimer(callback, ctx);
+
+        tv.tv_sec = delay_s;
+        tv.tv_usec = 0;
+    }
+
+    ~DelayedTrigger() {
+        event.Delete();
+    }
+
+    void Trigger() {
+        if (!event.IsTimerPending())
+            event.Add(&tv);
+    }
+
+    void Cancel() {
+        event.Delete();
+    }
+};
+
+#endif
