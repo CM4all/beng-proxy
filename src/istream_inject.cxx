3dd0e189ce20c6e2d2c045be6577737a1010cef8
Max Kellermann <mk@cm4all.com>
2015-06-26 14:37:07 +0200

    istream_inject: use the OO API

--- src/istream_inject.cxx
+++ src/istream_inject.cxx
@@ -13,99 +13,53 @@
 
 #include <assert.h>
 
-struct InjectIstream {
-    struct istream output;
-    struct istream *input;
+class InjectIstream final : public ForwardIstream {
+public:
+    InjectIstream(struct pool &p, struct istream &_input)
+        :ForwardIstream(p, _input,
+                        MakeIstreamHandler<InjectIstream>::handler, this) {}
+
+    static constexpr InjectIstream &Cast2(struct istream &i) {
+        return (InjectIstream &)Istream::Cast(i);
+    }
+
+    void InjectFault(GError *error) {
+        if (HasInput())
+            input.Close();
+
+        DestroyError(error);
+    }
+
+    /* virtual methods from class Istream */
+
+    off_t GetAvailable(bool partial) override {
+        /* never return the total length, because the caller may then
+           make assumptions on when this stream ends */
+        return partial && HasInput()
+            ? ForwardIstream::GetAvailable(partial)
+            : -1;
+    }
+
+    void Read() override {
+        if (HasInput())
+            ForwardIstream::Read();
+    }
+
+    int AsFd() override {
+        return -1;
+    }
+
+    /* handler */
+
+    void OnEof() {
+        ClearInput();
+    }
+
+    void OnError(GError *error) {
+        g_error_free(error);
+        ClearInput();
+    }
 };
-
-/*
- * istream handler
- *
- */
-
-static void
-inject_input_eof(void *ctx)
-{
-    auto *inject = (struct InjectIstream *)ctx;
-
-    assert(inject->input != nullptr);
-
-    inject->input = nullptr;
-}
-
-static void
-inject_input_abort(GError *error, void *ctx)
-{
-    auto *inject = (struct InjectIstream *)ctx;
-
-    g_error_free(error);
-
-    assert(inject->input != nullptr);
-
-    inject->input = nullptr;
-}
-
-static const struct istream_handler inject_input_handler = {
-    .data = istream_forward_data,
-    .direct = istream_forward_direct,
-    .eof = inject_input_eof,
-    .abort = inject_input_abort,
-};
-
-
-/*
- * istream implementation
- *
- */
-
-static inline struct InjectIstream *
-istream_to_inject(struct istream *istream)
-{
-    return &ContainerCast2(*istream, &InjectIstream::output);
-}
-
-static off_t
-istream_inject_available(struct istream *istream, bool partial)
-{
-    struct InjectIstream *inject = istream_to_inject(istream);
-
-    /* never return the total length, because the caller may then make
-       assumptions on when this stream ends */
-
-    return partial && inject->input != nullptr
-        ? istream_available(inject->input, partial)
-        : -1;
-}
-
-static void
-istream_inject_read(struct istream *istream)
-{
-    struct InjectIstream *inject = istream_to_inject(istream);
-
-    if (inject->input == nullptr)
-        return;
-
-    istream_handler_set_direct(inject->input, inject->output.handler_direct);
-    istream_read(inject->input);
-}
-
-static void
-istream_inject_close(struct istream *istream)
-{
-    struct InjectIstream *inject = istream_to_inject(istream);
-
-    assert(inject->input != nullptr);
-
-    istream_close_handler(inject->input);
-    istream_deinit(&inject->output);
-}
-
-static const struct istream_class istream_inject = {
-    .available = istream_inject_available,
-    .read = istream_inject_read,
-    .close = istream_inject_close,
-};
-
 
 /*
  * constructor
@@ -115,27 +69,12 @@ static const struct istream_class istream_inject = {
 struct istream *
 istream_inject_new(struct pool *pool, struct istream *input)
 {
-    assert(pool != nullptr);
-    assert(input != nullptr);
-    assert(!istream_has_handler(input));
-
-    auto inject = NewFromPool<struct InjectIstream>(*pool);
-    istream_init(&inject->output, &istream_inject, pool);
-
-    istream_assign_handler(&inject->input, input,
-                           &inject_input_handler, inject,
-                           0);
-
-    return &inject->output;
+    return NewIstream<InjectIstream>(*pool, *input);
 }
 
 void
 istream_inject_fault(struct istream *i_inject, GError *error)
 {
-    struct InjectIstream *inject = (struct InjectIstream *)i_inject;
-
-    if (inject->input != nullptr)
-        istream_close_handler(inject->input);
-
-    istream_deinit_abort(&inject->output, error);
+    auto &inject = InjectIstream::Cast2(*i_inject);
+    inject.InjectFault(error);
 }
