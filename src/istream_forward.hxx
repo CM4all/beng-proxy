a0942a7c5f3904c59351780ef4d47251a2ab8a57
Max Kellermann <mk@cm4all.com>
2015-06-29 10:28:42 +0200

    istream_forward: remove obsolete code

--- src/istream_forward.hxx
+++ src/istream_forward.hxx
@@ -71,19 +71,4 @@ public:
     }
 };
 
-size_t
-istream_forward_data(const void *data, size_t length, void *ctx);
-
-ssize_t
-istream_forward_direct(enum istream_direct type, int fd, size_t max_length,
-                       void *ctx);
-
-void
-istream_forward_eof(void *ctx);
-
-void
-istream_forward_abort(GError *error, void *ctx);
-
-extern const struct istream_handler istream_forward_handler;
-
 #endif
