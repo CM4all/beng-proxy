4f52205a7a5ffd0c7c5e09af944b282133e54a14
Max Kellermann <mk@cm4all.com>
2015-08-17 11:06:08 +0200

    puri_escape: use uri_unescape() instead of uri_unescape_inplace()
    Eliminate one memcpy().

--- src/puri_escape.cxx
+++ src/puri_escape.cxx
@@ -8,8 +8,6 @@
 #include "uri_escape.hxx"
 #include "pool.hxx"
 
-#include <string.h>
-
 const char *
 uri_escape_dup(struct pool *pool, const char *src, size_t src_length,
                char escape_char)
@@ -25,8 +23,7 @@ uri_unescape_dup(struct pool *pool, const char *src, size_t length,
                  char escape_char)
 {
     char *dest = (char *)p_malloc(pool, length + 1);
-    memcpy(dest, src, length);
-    size_t dest_length = uri_unescape_inplace(dest, length, escape_char);
-    dest[dest_length] = 0;
+    char *end = uri_unescape(dest, src, length, escape_char);
+    *end = 0;
     return dest;
 }
