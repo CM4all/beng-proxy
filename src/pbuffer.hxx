3f34628835049baed60ea2d994fb0d9e8424e99a
Max Kellermann <mk@cm4all.com>
2015-09-25 10:53:41 +0200

    pbuffer: add DupBuffer() overload with StringView parameter

--- src/pbuffer.hxx
+++ src/pbuffer.hxx
@@ -9,6 +9,7 @@
 
 #include "pool.hxx"
 #include "util/ConstBuffer.hxx"
+#include "util/StringView.hxx"
 
 template<typename T>
 static inline ConstBuffer<T>
@@ -23,6 +24,19 @@ DupBuffer(pool &p, ConstBuffer<T> src)
     ConstBuffer<void> src_v = src.ToVoid();
     ConstBuffer<void> dest_v(p_memdup(&p, src_v.data, src_v.size), src_v.size);
     return ConstBuffer<T>::FromVoid(dest_v);
+}
+
+static inline StringView
+DupBuffer(pool &p, StringView src)
+{
+    if (src.IsNull())
+        return nullptr;
+
+    if (src.IsEmpty())
+        return StringView::Empty();
+
+    return StringView((const char *)p_memdup(&p, src.data, src.size),
+                      src.size);
 }
 
 /**
