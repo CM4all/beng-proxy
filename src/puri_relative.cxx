a163ddd93b335294b2fb4554174065493ffea9c9
Max Kellermann <mk@cm4all.com>
2015-09-28 17:20:45 +0200

    puri_relative: fix self-include

--- src/puri_relative.cxx
+++ src/puri_relative.cxx
@@ -4,7 +4,7 @@
  * author: Max Kellermann <mk@cm4all.com>
  */
 
-#include "uri/uri_relative.hxx"
+#include "puri_relative.hxx"
 #include "uri/uri_extract.hxx"
 #include "pool.hxx"
 #include "util/StringView.hxx"
