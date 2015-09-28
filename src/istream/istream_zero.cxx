01f01db67bcd8b06f3dc377b6d53507ff9ff2326
Max Kellermann <mk@cm4all.com>
2015-09-28 22:09:43 +0200

    istream_zero: use class Istream

--- src/istream/istream_zero.cxx
+++ src/istream/istream_zero.cxx
@@ -3,64 +3,35 @@
  */
 
 #include "istream_zero.hxx"
-#include "istream_internal.hxx"
-#include "pool.hxx"
-#include "util/Cast.hxx"
+#include "istream_oo.hxx"
 
 #include <limits.h>
 
-struct ZeroIstream {
-    struct istream stream;
-};
+class ZeroIstream final : public Istream {
+public:
+    explicit ZeroIstream(struct pool &_pool):Istream(_pool) {}
 
-static inline ZeroIstream *
-istream_to_zero(struct istream *istream)
-{
-    return &ContainerCast2(*istream, &ZeroIstream::stream);
-}
+    /* virtual methods from class Istream */
 
-static off_t
-istream_zero_available(gcc_unused struct istream *istream, bool partial)
-{
-    return partial
-        ? INT_MAX
-        : -1;
-}
+    off_t GetAvailable(bool partial) override {
+        return partial
+            ? INT_MAX
+            : -1;
+    }
 
-static off_t
-istream_zero_skip(struct istream *istream gcc_unused, off_t length)
-{
-    return length;
-}
+    off_t Skip(off_t length) override {
+        return length;
+    }
 
-static void
-istream_zero_read(struct istream *istream)
-{
-    ZeroIstream *zero = istream_to_zero(istream);
-    static char buffer[1024];
+    void Read() override {
+        static char buffer[1024];
 
-    istream_invoke_data(&zero->stream, buffer, sizeof(buffer));
-}
-
-static void
-istream_zero_close(struct istream *istream)
-{
-    ZeroIstream *zero = istream_to_zero(istream);
-
-    istream_deinit(&zero->stream);
-}
-
-static const struct istream_class istream_zero = {
-    .available = istream_zero_available,
-    .skip = istream_zero_skip,
-    .read = istream_zero_read,
-    .close = istream_zero_close,
+        InvokeData(buffer, sizeof(buffer));
+    }
 };
 
 struct istream *
 istream_zero_new(struct pool *pool)
 {
-    auto zero = NewFromPool<ZeroIstream>(*pool);
-    istream_init(&zero->stream, &istream_zero, pool);
-    return &zero->stream;
+    return NewIstream(*pool);
 }
