c281d31e45ebdd4cd3058a9b8f7c7956b82bf9ad
Max Kellermann <mk@cm4all.com>
2015-10-15 10:33:21 +0200

    gcc5

--- autogen.sh
+++ autogen.sh
@@ -7,9 +7,17 @@ mkdir build
 aclocal -I m4 $ACLOCAL_FLAGS
 automake --add-missing --foreign
 autoconf
+
+unset CLANG_CXXFLAGS
+if gcc -dumpversion 2>/dev/null |grep '^[0-4]'; then
+    # Use clang if gcc is older than version 5.
+    export CC=clang
+    export CLANG=clang++
+    CLANG_CXXFLAGS="-D__STRICT_ANSI__"
+fi
+
 ./configure \
-	CC=clang CXX=clang++ \
-	CFLAGS="-O0 -ggdb" CXXFLAGS="-O0 -ggdb -D__STRICT_ANSI__" \
+	CFLAGS="-O0 -ggdb" CXXFLAGS="-O0 -ggdb $CLANG_CXXFLAGS" \
 	--prefix=/usr/local/stow/cm4all-beng-proxy \
 	--enable-debug \
 	--enable-silent-rules \
