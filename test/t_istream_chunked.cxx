bfec6a53caeea5cb9365d3dfa1f907b83d58a531
Max Kellermann <mk@cm4all.com>
2015-10-19 17:58:16 +0200

    test/t_istream_chunked: use the OO API

--- test/t_istream_chunked.cxx
+++ test/t_istream_chunked.cxx
@@ -17,15 +17,23 @@ create_test(struct pool *pool, struct istream *input)
 
 #define CUSTOM_TEST
 
-struct Custom {
-    struct istream output;
-
+struct Custom final : Istream {
     bool eof;
     GError *error;
 
+    explicit Custom(struct pool &p):Istream(p) {}
+
+    /* virtual methods from class Istream */
+
+    off_t GetAvailable(gcc_unused bool partial) override {
+        return 1;
+    }
+
+    void Read() override {}
+
     /* istream handler */
     size_t OnData(gcc_unused const void *data, gcc_unused size_t length) {
-        istream_invoke_data(&output, " ", 1);
+        InvokeData(" ", 1);
         return 0;
     }
 
@@ -44,45 +52,13 @@ struct Custom {
 
 };
 
-/*
- * istream class
- *
- */
-
-static off_t
-istream_custom_available(gcc_unused struct istream *istream,
-                         gcc_unused bool partial)
-{
-    return 1;
-}
-
-static void
-istream_custom_read(gcc_unused struct istream *istream)
-{
-}
-
-static void
-istream_custom_close(struct istream *istream)
-{
-    istream_deinit(istream);
-}
-
-static const struct istream_class istream_custom = {
-    .available = istream_custom_available,
-    .skip = nullptr,
-    .read = istream_custom_read,
-    .as_fd = nullptr,
-    .close = istream_custom_close,
-};
-
 static void
 test_custom(struct pool *pool)
 {
     pool = pool_new_linear(pool, "test", 8192);
-    auto *ctx = NewFromPool<Custom>(*pool);
-    istream_init(&ctx->output, &istream_custom, pool);
+    auto *ctx = NewFromPool<Custom>(*pool, *pool);
 
-    auto *chunked = istream_chunked_new(pool, &ctx->output);
+    auto *chunked = istream_chunked_new(pool, ctx->Cast());
     istream_handler_set(chunked, &MakeIstreamHandler<Custom>::handler, ctx, 0);
     pool_unref(pool);
 
