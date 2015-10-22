7fee887e2d88a934ad073b00a2eab18d635812d6
Max Kellermann <mk@cm4all.com>
2015-10-22 12:23:24 +0200

    configure.ac: make shadow warnings fatal

--- test/PoolTest.hxx
+++ test/PoolTest.hxx
@@ -5,22 +5,22 @@
 #include <cstddef>
 
 class PoolTest : public CppUnit::TestFixture {
-    struct pool *root_pool, *pool;
+    struct pool *root_pool, *the_pool;
 
 protected:
     struct pool *GetPool() {
-        return pool;
+        return the_pool;
     }
 
 public:
     virtual void setUp() {
         root_pool = pool_new_libc(NULL, "root");
-        pool = pool_new_libc(root_pool, "test");
+        the_pool = pool_new_libc(root_pool, "test");
     }
 
     virtual void tearDown() {
         pool_unref(root_pool);
-        pool_unref(pool);
+        pool_unref(the_pool);
         pool_commit();
         pool_recycler_clear();
     }
