005e07c97951aadf15ab74f20c2d590b7216dfde
Max Kellermann <mk@cm4all.com>
2015-11-18 11:42:49 +0100

    test/t_ajp_client: move AJP protocol code to ajp_server.cxx

---
+++ test/ajp_server.hxx
@@ -0,0 +1,39 @@
+#include "ajp/ajp_protocol.hxx"
+
+#include <http/status.h>
+
+#include <stddef.h>
+
+struct ajp_request {
+    enum ajp_code code;
+    enum ajp_method method;
+    const char *uri;
+    struct strmap *headers;
+
+    uint8_t *body;
+    size_t length, requested, received;
+};
+
+void
+read_ajp_header(struct ajp_header *header);
+
+void
+read_ajp_request(struct pool *pool, struct ajp_request *r);
+
+void
+read_ajp_request_body_chunk(struct ajp_request *r);
+
+void
+read_ajp_end_request_body_chunk(struct ajp_request *r);
+
+void
+discard_ajp_request_body(struct ajp_request *r);
+
+void
+write_headers(http_status_t status, const struct strmap *headers);
+
+void
+write_body_chunk(const void *value, size_t length, size_t junk);
+
+void
+write_end();
