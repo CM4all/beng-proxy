e1b7af50b5461d005100e98c36057af7151043f5
Max Kellermann <mk@cm4all.com>
2015-11-18 10:36:28 +0100

    test/t_fcgi_client: move FastCGI protocol code to fcgi_server.cxx

---
+++ test/fcgi_server.hxx
@@ -0,0 +1,48 @@
+#include "fcgi/Protocol.hxx"
+
+#include <http/method.h>
+#include <http/status.h>
+
+#include <sys/types.h>
+#include <string.h>
+
+struct pool;
+struct strmap;
+struct fcgi_record_header;
+
+struct FcgiRequest {
+    uint16_t id;
+
+    http_method_t method;
+    const char *uri;
+    struct strmap *headers;
+
+    off_t length;
+};
+
+void
+read_fcgi_header(struct fcgi_record_header *header);
+
+void
+read_fcgi_request(struct pool *pool, FcgiRequest *r);
+
+void
+discard_fcgi_request_body(FcgiRequest *r);
+
+void
+write_fcgi_stdout(const FcgiRequest *r,
+                  const void *data, size_t length);
+
+static inline void
+write_fcgi_stdout_string(const FcgiRequest *r,
+                         const char *data)
+{
+    write_fcgi_stdout(r, data, strlen(data));
+}
+
+void
+write_fcgi_headers(const FcgiRequest *r, http_status_t status,
+                   struct strmap *headers);
+
+void
+write_fcgi_end(const FcgiRequest *r);
