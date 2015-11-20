db7179b85cc41ca6d951c3e55c52f1f5e399995d
Max Kellermann <mk@cm4all.com>
2015-11-20 11:55:24 +0100

    event/Event: move EventBase to new header

---
+++ src/event/Base.hxx
@@ -0,0 +1,41 @@
+/*
+ * C++ wrappers for libevent.
+ *
+ * author: Max Kellermann <mk@cm4all.com>
+ */
+
+#ifndef EVENT_BASE_HXX
+#define EVENT_BASE_HXX
+
+#include <event.h>
+
+class EventBase {
+    struct event_base *event_base;
+
+public:
+    EventBase():event_base(::event_init()) {}
+    ~EventBase() {
+        ::event_base_free(event_base);
+    }
+
+    EventBase(const EventBase &other) = delete;
+    EventBase &operator=(const EventBase &other) = delete;
+
+    struct event_base *Get() {
+        return event_base;
+    }
+
+    void Reinit() {
+        event_reinit(event_base);
+    }
+
+    void Dispatch() {
+        ::event_base_dispatch(event_base);
+    }
+
+    void Break() {
+        ::event_base_loopbreak(event_base);
+    }
+};
+
+#endif
--- src/event/Event.hxx
+++ src/event/Event.hxx
@@ -7,38 +7,11 @@
 #ifndef EVENT_HXX
 #define EVENT_HXX
 
+#include "Base.hxx"
+
 #include <inline/compiler.h>
 
 #include <event.h>
-
-class EventBase {
-    struct event_base *event_base;
-
-public:
-    EventBase():event_base(::event_init()) {}
-    ~EventBase() {
-        ::event_base_free(event_base);
-    }
-
-    EventBase(const EventBase &other) = delete;
-    EventBase &operator=(const EventBase &other) = delete;
-
-    struct event_base *Get() {
-        return event_base;
-    }
-
-    void Reinit() {
-        event_reinit(event_base);
-    }
-
-    void Dispatch() {
-        ::event_base_dispatch(event_base);
-    }
-
-    void Break() {
-        ::event_base_loopbreak(event_base);
-    }
-};
 
 class Event {
     struct event event;
