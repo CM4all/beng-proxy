63361a2907c1e42edb951e9a01d6773ec7f64017
Max Kellermann <mk@cm4all.com>
2016-01-08 17:31:04 +0100

    ssl/init: move ERR_remove_thread_state() to the end

--- src/ssl/ssl_init.cxx
+++ src/ssl/ssl_init.cxx
@@ -59,18 +59,18 @@ ssl_global_deinit()
     EVP_cleanup();
     CRYPTO_cleanup_all_ex_data();
 
+    CRYPTO_set_id_callback(nullptr);
+    CRYPTO_set_locking_callback(nullptr);
+
+    delete[] ssl_mutexes;
+
+    ERR_free_strings();
+
 #if OPENSSL_VERSION_NUMBER >= 0x10000000L
     ERR_remove_thread_state(nullptr);
 #else
     ERR_remove_state(0);
 #endif
-
-    ERR_free_strings();
-
-    CRYPTO_set_id_callback(nullptr);
-    CRYPTO_set_locking_callback(nullptr);
-
-    delete[] ssl_mutexes;
 }
 
 void
