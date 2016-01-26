aa1da611903014910089ef08233c414f01f15507
Max Kellermann <mk@cm4all.com>
2016-01-26 14:27:47 +0100

    autogen.sh: move flags to DEBUG_FLAGS

--- autogen.sh
+++ autogen.sh
@@ -8,6 +8,8 @@ aclocal -I m4 $ACLOCAL_FLAGS
 automake --add-missing --foreign
 autoconf
 
+DEBUG_FLAGS="-O0 -ggdb"
+
 unset CLANG_CXXFLAGS
 if gcc -dumpversion 2>/dev/null |grep -q '^[0-4]'; then
     # Use clang if gcc is older than version 5.
@@ -17,7 +19,7 @@ if gcc -dumpversion 2>/dev/null |grep -q '^[0-4]'; then
 fi
 
 ./configure \
-	CFLAGS="-O0 -ggdb" CXXFLAGS="-O0 -ggdb $CLANG_CXXFLAGS" \
+	CFLAGS="$DEBUG_FLAGS" CXXFLAGS="$DEBUG_FLAGS $CLANG_CXXFLAGS" \
 	--prefix=/usr/local/stow/cm4all-beng-proxy \
 	--enable-debug \
 	--enable-silent-rules \
