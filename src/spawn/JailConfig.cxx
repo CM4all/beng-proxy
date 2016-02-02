CM4all/beng-proxy

commit 03f08ca4cc3543b29aec0e238b9a5c084f545159

util/StringUtil: import string library

src/spawn/JailConfig.cxx
@@ -6,7 +6,7 @@
 
 #include "JailConfig.hxx"
 #include "pool.hxx"
-#include "util/CharUtil.hxx"
+#include "util/StringUtil.hxx"
 
 #include <stdio.h>
 #include <string.h>
@@ -14,16 +14,14 @@
 static char *
 next_word(char *p)
 {
-    while (!IsWhitespaceOrNull(*p))
-        ++p;
+    p = StripLeft(p);
 
     if (*p == 0)
         return nullptr;
 
     *p++ = 0;
 
-    while (IsWhitespaceNotNull(*p))
-        ++p;
+    p = StripLeft(p);
 
     if (*p == 0)
         return nullptr;
@@ -44,8 +42,7 @@ jail_config_load(struct jail_config *config, const char *path,
 
     char line[4096], *p, *q;
     while ((p = fgets(line, sizeof(line), file)) != nullptr) {
-        while (IsWhitespaceNotNull(*p))
-            ++p;
+        p = StripLeft(p);
 
         if (*p == 0 || *p == '#')
             /* ignore comments */

Makefile.am
@@ -570,6 +570,7 @@ libutil_a_SOURCES = \
 	src/util/CharUtil.hxx \
 	src/util/ConstBuffer.hxx \
 	src/util/StringAPI.hxx \
+	src/util/StringUtil.cxx src/util/StringUtil.hxx \
 	src/util/StringView.cxx src/util/StringView.hxx \
 	src/util/StringCompare.hxx \
 	src/util/StringPointer.hxx \
@@ -2024,6 +2025,7 @@ test_run_css_parser_LDADD = $(AM_LDADD) \
 	libistream.a libpool.a libmemory.a libevent.a \
 	libsystem.a \
 	libputil.a \
+	libutil.a \
 	$(LIBCM4ALL_DAEMON_LIBS) $(LIBEVENT_LIBS) $(GLIB_LIBS)
 
 endif

src/cgi/cgi_parser.cxx
@@ -9,7 +9,7 @@
 #include "strmap.hxx"
 #include "header_parser.hxx"
 #include "util/ForeignFifoBuffer.hxx"
-#include "util/CharUtil.hxx"
+#include "util/StringUtil.hxx"
 #include "util/StringView.hxx"
 
 #include <string.h>
@@ -85,11 +85,10 @@ CGIParser::FeedHeaders(struct pool &pool, ForeignFifoBuffer<uint8_t> &buffer,
     while ((end = (const char *)memchr(start, '\n',
                                        data_end - start)) != nullptr) {
         next = end + 1;
-        --end;
-        while (end >= start && IsWhitespaceOrNull(*end))
-            --end;
 
-        const size_t line_length = end - start + 1;
+        end = StripRight(start, end);
+
+        const size_t line_length = end - start;
         if (line_length == 0) {
             /* found an empty line, which is the separator between
                headers and body */

src/css_parser.cxx
@@ -9,7 +9,7 @@
 #include "pool.hxx"
 #include "istream/istream_oo.hxx"
 #include "istream/istream_pointer.hxx"
-#include "util/CharUtil.hxx"
+#include "util/StringUtil.hxx"
 #include "util/TrivialArray.hxx"
 
 enum CssParserState {
@@ -120,15 +120,6 @@ struct CssParser final : IstreamHandler {
         pool_unref(pool);
     }
 };
-
-static const char *
-skip_whitespace(const char *p, const char *end)
-{
-    while (p < end && IsWhitespaceOrNull(*p))
-        ++p;
-
-    return p;
-}
 
 gcc_pure
 static bool
@@ -338,7 +329,7 @@ CssParser::Feed(const char *start, size_t length)
             break;
 
         case CSS_PARSER_PRE_VALUE:
-            buffer = skip_whitespace(buffer, end);
+            buffer = StripLeft(buffer, end);
             if (buffer < end) {
                 switch (*buffer) {
                 case '}':
@@ -413,7 +404,7 @@ CssParser::Feed(const char *start, size_t length)
             break;
 
         case CSS_PARSER_PRE_URL:
-            buffer = skip_whitespace(buffer, end);
+            buffer = StripLeft(buffer, end);
             if (buffer < end) {
                 switch (*buffer) {
                 case '}':

src/fcgi/Client.cxx
@@ -27,7 +27,7 @@
 #include "pool.hxx"
 #include "util/Cast.hxx"
 #include "util/ConstBuffer.hxx"
-#include "util/CharUtil.hxx"
+#include "util/StringUtil.hxx"
 #include "util/ByteOrder.hxx"
 #include "util/StringView.hxx"
 
@@ -386,11 +386,10 @@ FcgiClient::ParseHeaders(const char *data, size_t length)
     const char *eol;
     while ((eol = (const char *)memchr(p, '\n', data_end - p)) != nullptr) {
         next = eol + 1;
-        --eol;
-        while (eol >= p && IsWhitespaceOrNull(*eol))
-            --eol;
 
-        finished = HandleLine(p, eol - p + 1);
+        eol = StripRight(p, eol);
+
+        finished = HandleLine(p, eol - p);
         if (finished)
             break;
 

src/header_parser.cxx
@@ -11,7 +11,7 @@
 #include "util/StringView.hxx"
 #include "util/ConstBuffer.hxx"
 #include "util/StaticFifoBuffer.hxx"
-#include "util/CharUtil.hxx"
+#include "util/StringUtil.hxx"
 
 #include <algorithm>
 
@@ -31,8 +31,7 @@ header_parse_line(struct pool &pool, struct strmap *headers,
     ++colon;
     if (likely(colon < line.end() && *colon == ' '))
         ++colon;
-    while (colon < line.end() && IsWhitespaceOrNull(*colon))
-        ++colon;
+    colon = StripLeft(colon, line.end());
 
     char *key = p_strdup_lower(pool, StringView(line.begin(), key_end));
     char *value = p_strndup(&pool, colon, line.end() - colon);
@@ -80,8 +79,7 @@ header_parse_buffer(struct pool *pool, struct strmap *headers,
         const size_t length = r.size;
 
         while (true) {
-            while (p < src + length && IsWhitespaceOrNull(*p))
-                ++p;
+            p = StripLeft(p, src + length);
 
             const char *eol = (const char *)memchr(p, '\n', src + length - p);
             if (eol == nullptr) {

src/http_client.cxx
@@ -33,6 +33,7 @@
 #include "pool.hxx"
 #include "util/Cast.hxx"
 #include "util/CharUtil.hxx"
+#include "util/StringUtil.hxx"
 #include "util/StringView.hxx"
 #include "util/StaticArray.hxx"
 
@@ -750,12 +751,10 @@ HttpClient::ParseHeaders(const void *_data, size_t length)
         const char *const next = end + 1;
 
         /* strip the line */
-        --end;
-        while (end >= start && IsWhitespaceOrNull(*end))
-            --end;
+        end = StripRight(start, end);
 
         /* handle this line */
-        if (!HandleLine(start, end - start + 1))
+        if (!HandleLine(start, end - start))
             return BufferedResult::CLOSED;
 
         if (response.read_state != response::READ_HEADERS) {

src/http_server/http_server_read.cxx
@@ -15,7 +15,7 @@
 #include "strmap.hxx"
 #include "header_parser.hxx"
 #include "istream/istream_null.hxx"
-#include "util/CharUtil.hxx"
+#include "util/StringUtil.hxx"
 #include "util/StringView.hxx"
 
 #include <inline/poison.h>
@@ -303,11 +303,10 @@ HttpServerConnection::FeedHeaders(const void *_data, size_t length)
     while ((end = (const char *)memchr(start, '\n',
                                        buffer_end - start)) != nullptr) {
         next = end + 1;
-        --end;
-        while (end >= start && IsWhitespaceOrNull(*end))
-            --end;
 
-        if (!HandleLine(start, end - start + 1))
+        end = StripRight(start, end);
+
+        if (!HandleLine(start, end - start))
             return BufferedResult::CLOSED;
 
         if (request.read_state != Request::HEADERS)

commit bb27d5ad6288817343cda073e5828cb84bb9f6bb

spawn/JailConfig: fix inverted check
Regression by commit 03f08ca4

src/spawn/JailConfig.cxx
@@ -6,6 +6,7 @@
 
 #include "JailConfig.hxx"
 #include "pool.hxx"
+#include "util/CharUtil.hxx"
 #include "util/StringUtil.hxx"
 
 #include <stdio.h>
@@ -14,7 +15,8 @@
 static char *
 next_word(char *p)
 {
-    p = StripLeft(p);
+    while (!IsWhitespaceOrNull(*p))
+        ++p;
 
     if (*p == 0)
         return nullptr;