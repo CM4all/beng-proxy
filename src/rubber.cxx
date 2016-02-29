1b8436f3ce11b95e0f88ed052d3fbc9f30848d5d
Max Kellermann <mk@cm4all.com>
2016-02-29 15:53:51 +0100

    rubber: remove excessive debugging code to speed up cache flush
    Rubber::Remove() was called many many times after SIGHUP, which made
    flushing the cache scale quadratically.

--- src/rubber.cxx
+++ src/rubber.cxx
@@ -1104,7 +1104,6 @@ Rubber::ReplaceWithHole(RubberObject &o,
 inline void
 Rubber::Remove(unsigned id)
 {
-    assert(netto_size + GetTotalHoleSize() == GetBruttoSize());
     assert(id > 0);
 
     auto &o = table->entries[id];
@@ -1119,8 +1118,6 @@ Rubber::Remove(unsigned id)
     netto_size -= size;
 
     ReplaceWithHole(o, previous_id, next_id);
-
-    assert(netto_size + GetTotalHoleSize() == GetBruttoSize());
 }
 
 void
