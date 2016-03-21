65d04f438af55840e3634b2c1de7d1c73f0a261d
Max Kellermann <mk@cm4all.com>
2016-03-21 19:34:18 +0100

    odbus/ReadIter: add more methods

--- src/odbus/ReadIter.hxx
+++ src/odbus/ReadIter.hxx
@@ -15,8 +15,24 @@ namespace ODBus {
 			dbus_message_iter_init(&msg, &iter);
 		}
 
+		bool HasNext() {
+			return dbus_message_iter_has_next(&iter);
+		}
+
+		bool Next() {
+			return dbus_message_iter_next(&iter);
+		}
+
+		int GetArgType() {
+			return dbus_message_iter_get_arg_type(&iter);
+		}
+
 		const char *GetSignature() {
 			return dbus_message_iter_get_signature(&iter);
+		}
+
+		void GetBasic(void *value) {
+			dbus_message_iter_get_basic(&iter, value);
 		}
 	};
 }
