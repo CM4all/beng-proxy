f658b079b418a127d4c41e75d8e7e0fdeb261660
Max Kellermann <mk@cm4all.com>
2016-03-22 12:51:48 +0100

    odbus/Message: add more getter methods

--- src/odbus/Message.hxx
+++ src/odbus/Message.hxx
@@ -81,6 +81,40 @@ namespace ODBus {
 			return dbus_message_has_member(msg, member);
 		}
 
+		bool IsError(const char *error_name) const {
+			return dbus_message_is_error(msg, error_name);
+		}
+
+		const char *GetErrorName() const {
+			return dbus_message_get_error_name(msg);
+		}
+
+		const char *GetDestination() const {
+			return dbus_message_get_destination(msg);
+		}
+
+		const char *GetSender() const {
+			return dbus_message_get_sender(msg);
+		}
+
+		const char *GetSignature() const {
+			return dbus_message_get_signature(msg);
+		}
+
+		bool GetNoReply() const {
+			return dbus_message_get_no_reply(msg);
+		}
+
+		bool IsMethodCall(const char *iface,
+				  const char *method) const {
+			return dbus_message_is_method_call(msg, iface, method);
+		}
+
+		bool IsSignal(const char *iface,
+			      const char *signal_name) const {
+			return dbus_message_is_signal(msg, iface, signal_name);
+		}
+
 		void CheckThrowError();
 
 		template<typename... Args>
