3f51c4955a93d113b45738910788881947866204
Max Kellermann <mk@cm4all.com>
2016-04-15 13:50:38 +0200

    thread_job: add method IsIdle()

--- src/thread_job.hxx
+++ src/thread_job.hxx
@@ -44,6 +44,18 @@ public:
      */
     bool again = false;
 
+    /**
+     * Is this job currently idle, i.e. not being worked on by a
+     * worker thread?  This method may be called only from the main
+     * thread.  A "true" return value guarantees that no worker thread
+     * is and will be working on it, and its internal data structures
+     * may be accessed without mutex protection.  Use this method with
+     * caution.
+     */
+    bool IsIdle() const {
+        return state == State::INITIAL;
+    }
+
     virtual void Run() = 0;
     virtual void Done() = 0;
 };
