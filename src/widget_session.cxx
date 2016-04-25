a73632cf0d39fb6b87b5da978083579db7c2b8c9
Max Kellermann <mk@cm4all.com>
2016-04-25 16:10:40 +0200

    widget_session: use class AutoRewindPool

--- src/widget_session.cxx
+++ src/widget_session.cxx
@@ -14,8 +14,6 @@ WidgetSession *
 widget_get_session(struct widget *widget, Session *session,
                    bool create)
 {
-    struct pool_mark_state mark;
-
     assert(widget != NULL);
     assert(session != NULL);
     assert(lock_is_locked(&session->lock));
@@ -27,30 +25,29 @@ widget_get_session(struct widget *widget, Session *session,
         return session_get_widget(session, widget->id, create);
 
     switch (widget->session) {
-        WidgetSession *parent, *ws;
-
     case widget::WIDGET_SESSION_RESOURCE:
         /* the session is bound to the resource: determine
            widget_session from the parent's session */
 
-        parent = widget_get_session(widget->parent, session, create);
-        if (parent == NULL)
-            return NULL;
+        {
+            WidgetSession *parent =
+                widget_get_session(widget->parent, session, create);
+            if (parent == nullptr)
+                return nullptr;
 
-        pool_mark(tpool, &mark);
-        ws = widget_session_get_child(parent, widget->id, create);
-        pool_rewind(tpool, &mark);
-        return ws;
+            const AutoRewindPool auto_rewind(*tpool);
+            return widget_session_get_child(parent, widget->id, create);
+        }
 
     case widget::WIDGET_SESSION_SITE:
         /* this is a site-global widget: get the widget_session
            directly from the session struct (which is site
            specific) */
 
-        pool_mark(tpool, &mark);
-        ws = session_get_widget(session, widget->id, create);
-        pool_rewind(tpool, &mark);
-        return ws;
+        {
+            const AutoRewindPool auto_rewind(*tpool);
+            return session_get_widget(session, widget->id, create);
+        }
     }
 
     assert(0);
