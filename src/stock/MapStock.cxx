2a9dc1d7180d86d36715d4011bdc50bd94dfb542
Max Kellermann <mk@cm4all.com>
2016-05-24 15:32:04 +0200

    stock/map: expose the struct

--- src/stock/MapStock.cxx
+++ src/stock/MapStock.cxx
@@ -6,137 +6,32 @@
  */
 
 #include "MapStock.hxx"
-#include "Stock.hxx"
-#include "Item.hxx"
-#include "pool.hxx"
 #include "util/djbhash.h"
 #include "util/DeleteDisposer.hxx"
-#include "util/Cast.hxx"
 
 #include <daemon/log.h>
 
 #include <assert.h>
-#include <string.h>
 
-#include <boost/intrusive/unordered_set.hpp>
+inline size_t
+StockMap::Item::KeyHasher(const char *key)
+{
+    assert(key != nullptr);
 
-struct StockMap final : StockHandler {
-    struct Item
-        : boost::intrusive::unordered_set_base_hook<boost::intrusive::link_mode<boost::intrusive::normal_link>> {
-        Stock stock;
+    return djb_hash_string(key);
+}
 
-        template<typename... Args>
-        explicit Item(Args&&... args):stock(std::forward<Args>(args)...) {}
+StockMap::~StockMap()
+{
+    map.clear_and_dispose(DeleteDisposer());
+}
 
-        static Item &Cast(Stock &s) {
-            return ContainerCast2(s, &Item::stock);
-        }
-
-        const char *GetKey() const {
-            return stock.GetName();
-        }
-
-        gcc_pure
-        static size_t KeyHasher(const char *key) {
-            assert(key != nullptr);
-
-            return djb_hash_string(key);
-        }
-
-        gcc_pure
-        static size_t ValueHasher(const Item &value) {
-            return KeyHasher(value.GetKey());
-        }
-
-        gcc_pure
-        static bool KeyValueEqual(const char *a, const Item &b) {
-            assert(a != nullptr);
-
-            return strcmp(a, b.GetKey()) == 0;
-        }
-
-        struct Hash {
-            gcc_pure
-            size_t operator()(const Item &value) const {
-                return ValueHasher(value);
-            }
-        };
-
-        struct Equal {
-            gcc_pure
-            bool operator()(const Item &a, const Item &b) const {
-                return KeyValueEqual(a.GetKey(), b);
-            }
-        };
-    };
-
-    typedef boost::intrusive::unordered_set<Item,
-                                            boost::intrusive::hash<Item::Hash>,
-                                            boost::intrusive::equal<Item::Equal>,
-                                            boost::intrusive::constant_time_size<false>> Map;
-
-    const StockClass &cls;
-    void *const class_ctx;
-
-    /**
-     * The maximum number of items in each stock.
-     */
-    const unsigned limit;
-
-    /**
-     * The maximum number of permanent idle items in each stock.
-     */
-    const unsigned max_idle;
-
-    Map map;
-
-    static constexpr size_t N_BUCKETS = 251;
-    Map::bucket_type buckets[N_BUCKETS];
-
-    StockMap(const StockClass &_cls, void *_class_ctx,
-             unsigned _limit, unsigned _max_idle)
-        :cls(_cls), class_ctx(_class_ctx),
-         limit(_limit), max_idle(_max_idle),
-         map(Map::bucket_traits(buckets, N_BUCKETS)) {}
-
-    ~StockMap() {
-        map.clear_and_dispose(DeleteDisposer());
-    }
-
-    void Erase(Item &item) {
-        auto i = map.iterator_to(item);
-        map.erase_and_dispose(i, DeleteDisposer());
-    }
-
-    void FadeAll() {
-        for (auto &i : map)
-            i.stock.FadeAll();
-    }
-
-    void AddStats(StockStats &data) const {
-        for (const auto &i : map)
-            i.stock.AddStats(data);
-    }
-
-    Stock &GetStock(const char *uri);
-
-    void Get(struct pool &caller_pool,
-             const char *uri, void *info,
-             StockGetHandler &handler,
-             struct async_operation_ref &async_ref) {
-        Stock &stock = GetStock(uri);
-        stock.Get(caller_pool, info, handler, async_ref);
-    }
-
-    StockItem *GetNow(struct pool &caller_pool, const char *uri, void *info,
-                      GError **error_r) {
-        Stock &stock = GetStock(uri);
-        return stock.GetNow(caller_pool, info, error_r);
-    }
-
-    /* virtual methods from class StockHandler */
-    void OnStockEmpty(Stock &stock) override;
-};
+void
+StockMap::Erase(Item &item)
+{
+    auto i = map.iterator_to(item);
+    map.erase_and_dispose(i, DeleteDisposer());
+}
 
 void
 StockMap::OnStockEmpty(Stock &stock)
--- src/stock/MapStock.hxx
+++ src/stock/MapStock.hxx
@@ -8,17 +8,120 @@
 #ifndef BENG_PROXY_MAP_STOCK_HXX
 #define BENG_PROXY_MAP_STOCK_HXX
 
-#include "glibfwd.hxx"
+#include "Stock.hxx"
 
 #include <inline/compiler.h>
 
-struct pool;
-struct async_operation_ref;
-struct StockClass;
-struct StockItem;
-struct StockStats;
-class StockGetHandler;
-struct StockMap;
+#include <boost/intrusive/unordered_set.hpp>
+
+struct StockMap final : StockHandler {
+    struct Item
+        : boost::intrusive::unordered_set_base_hook<boost::intrusive::link_mode<boost::intrusive::normal_link>> {
+        Stock stock;
+
+        template<typename... Args>
+        explicit Item(Args&&... args):stock(std::forward<Args>(args)...) {}
+
+        static Item &Cast(Stock &s) {
+            return ContainerCast2(s, &Item::stock);
+        }
+
+        const char *GetKey() const {
+            return stock.GetName();
+        }
+
+        gcc_pure
+        static size_t KeyHasher(const char *key);
+
+        gcc_pure
+        static size_t ValueHasher(const Item &value) {
+            return KeyHasher(value.GetKey());
+        }
+
+        gcc_pure
+        static bool KeyValueEqual(const char *a, const Item &b) {
+            assert(a != nullptr);
+
+            return strcmp(a, b.GetKey()) == 0;
+        }
+
+        struct Hash {
+            gcc_pure
+            size_t operator()(const Item &value) const {
+                return ValueHasher(value);
+            }
+        };
+
+        struct Equal {
+            gcc_pure
+            bool operator()(const Item &a, const Item &b) const {
+                return KeyValueEqual(a.GetKey(), b);
+            }
+        };
+    };
+
+    typedef boost::intrusive::unordered_set<Item,
+                                            boost::intrusive::hash<Item::Hash>,
+                                            boost::intrusive::equal<Item::Equal>,
+                                            boost::intrusive::constant_time_size<false>> Map;
+
+    const StockClass &cls;
+    void *const class_ctx;
+
+    /**
+     * The maximum number of items in each stock.
+     */
+    const unsigned limit;
+
+    /**
+     * The maximum number of permanent idle items in each stock.
+     */
+    const unsigned max_idle;
+
+    Map map;
+
+    static constexpr size_t N_BUCKETS = 251;
+    Map::bucket_type buckets[N_BUCKETS];
+
+    StockMap(const StockClass &_cls, void *_class_ctx,
+             unsigned _limit, unsigned _max_idle)
+        :cls(_cls), class_ctx(_class_ctx),
+         limit(_limit), max_idle(_max_idle),
+         map(Map::bucket_traits(buckets, N_BUCKETS)) {}
+
+    ~StockMap();
+
+    void Erase(Item &item);
+
+    void FadeAll() {
+        for (auto &i : map)
+            i.stock.FadeAll();
+    }
+
+    void AddStats(StockStats &data) const {
+        for (const auto &i : map)
+            i.stock.AddStats(data);
+    }
+
+    Stock &GetStock(const char *uri);
+
+    void Get(struct pool &caller_pool,
+             const char *uri, void *info,
+             StockGetHandler &handler,
+             struct async_operation_ref &async_ref) {
+        Stock &stock = GetStock(uri);
+        stock.Get(caller_pool, info, handler, async_ref);
+    }
+
+    StockItem *GetNow(struct pool &caller_pool, const char *uri, void *info,
+                      GError **error_r) {
+        Stock &stock = GetStock(uri);
+        return stock.GetNow(caller_pool, info, error_r);
+    }
+
+    /* virtual methods from class StockHandler */
+    void OnStockEmpty(Stock &stock) override;
+};
 
 gcc_malloc
 StockMap *
