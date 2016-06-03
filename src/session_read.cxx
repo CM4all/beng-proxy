8aed369dd1d6f1b52ff200f460250b1e32500960
Max Kellermann <mk@cm4all.com>
2016-06-03 15:48:05 +0200

    session_read: handle errors with C++ exception

--- src/session_read.cxx
+++ src/session_read.cxx
@@ -12,318 +12,239 @@
 
 #include <stdint.h>
 
-static bool
-read_8(FILE *file, uint8_t *value_r)
-{
-    return fread(value_r, sizeof(*value_r), 1, file) == 1;
-}
+namespace {
 
-static bool
-read_bool(FILE *file, bool *value_r)
-{
-    uint8_t t;
-    if (!read_8(file, &t))
-        return false;
-    *value_r = t != 0;
-    return true;
-}
+class SessionDeserializerError {};
 
-static bool
-read_16(FILE *file, uint16_t *value_r)
-{
-    return fread(value_r, sizeof(*value_r), 1, file) == 1;
-}
+class FileReader {
+    FILE *const file;
 
-static bool
-read_32(FILE *file, uint32_t *value_r)
-{
-    return fread(value_r, sizeof(*value_r), 1, file) == 1;
-}
+public:
+    explicit FileReader(FILE *_file):file(_file) {}
 
-static bool
-read_64(FILE *file, uint64_t *value_r)
-{
-    return fread(value_r, sizeof(*value_r), 1, file) == 1;
-}
-
-static bool
-read_time(FILE *file, time_t *value_r)
-{
-    uint64_t t;
-    if (!read_64(file, &t))
-        return false;
-
-    *value_r = (time_t)t;
-    return true;
-}
-
-static bool
-read_buffer(FILE *file, void *data, size_t length)
-{
-    return fread(data, 1, length, file) == length;
-}
-
-static bool
-read_session_id(FILE *file, SessionId *id)
-{
-    return read_buffer(file, id, sizeof(*id));
-}
-
-static bool
-read_string(FILE *file, struct dpool *pool, char **s_r)
-{
-    assert(pool != nullptr);
-    assert(s_r != nullptr);
-
-    uint16_t length;
-    if (!read_16(file, &length))
-        return false;
-
-    if (length == (uint16_t)-1) {
-        *s_r = nullptr;
-        return true;
+    void ReadBuffer(void *buffer, size_t size) {
+        if (fread(buffer, 1, size, file) != size)
+            throw SessionDeserializerError();
     }
 
-    char *s = (char *)d_malloc(pool, length + 1);
-    if (s == nullptr)
-        return false;
-
-    if (!read_buffer(file, s, length))
-        return false;
-
-    s[length] = 0;
-    *s_r = s;
-    return true;
-}
-
-static bool
-read_string_const(FILE *file, struct dpool *pool, const char **s_r)
-{
-    char *s;
-    if (!read_string(file, pool, &s))
-        return false;
-
-    *s_r = s;
-    return true;
-}
-
-static bool
-read_string(FILE *file, struct dpool *pool, StringView &s)
-{
-    assert(pool != nullptr);
-
-    uint16_t length;
-    if (!read_16(file, &length))
-        return false;
-
-    if (length == (uint16_t)-1) {
-        s = nullptr;
-        return true;
+    template<typename T>
+    void ReadT(T &value) {
+        ReadBuffer(&value, sizeof(value));
     }
 
-    if (length == 0) {
-        s.SetEmpty();
-        return true;
+    template<typename T>
+    T ReadT() {
+        T value;
+        ReadT<T>(value);
+        return value;
     }
 
-    char *p = (char *)d_malloc(pool, length);
-    if (p == nullptr)
-        return false;
+    uint8_t ReadByte() {
+        return ReadT<uint8_t>();
+    }
 
-    if (!read_buffer(file, p, length))
-        return false;
+    bool ReadBool() {
+        return ReadByte() != 0;
+    }
 
-    s = {p, length};
-    return true;
+    uint32_t Read32() {
+        return ReadT<uint32_t>();
+    }
+
+    uint64_t Read64() {
+        return ReadT<uint64_t>();
+    }
+
+    char *ReadString(struct dpool &pool) {
+        uint16_t length;
+        ReadT(length);
+
+        if (length == (uint16_t)-1)
+            return nullptr;
+
+        char *s = (char *)d_malloc(&pool, length + 1);
+        ReadBuffer(s, length);
+        s[length] = 0;
+        return s;
+    }
+
+    StringView ReadStringView(struct dpool &pool) {
+        uint16_t length;
+        ReadT(length);
+
+        if (length == (uint16_t)-1)
+            return nullptr;
+
+        if (length == 0)
+            return StringView::Empty();
+
+        char *s = (char *)d_malloc(&pool, length);
+        ReadBuffer(s, length);
+        return {s, length};
+    }
+
+    ConstBuffer<void> ReadConstBuffer(struct dpool &pool) {
+        uint16_t size;
+        ReadT(size);
+
+        if (size == (uint16_t)-1)
+            return nullptr;
+
+        if (size == 0)
+            return { "", 0 };
+
+        void *p = d_malloc(&pool, size);
+        ReadBuffer(p, size);
+        return { p, size };
+    }
+};
+
 }
 
-static bool
-read_buffer(FILE *file, struct dpool *pool, ConstBuffer<void> &buffer)
+static time_t
+ReadTime(FileReader &file)
 {
-    assert(pool != nullptr);
-
-    uint16_t size;
-    if (!read_16(file, &size))
-        return false;
-
-    if (size == (uint16_t)-1) {
-        buffer = ConstBuffer<void>::Null();
-        return true;
-    }
-
-    if (size == 0) {
-        buffer = { "", 0 };
-        return true;
-    }
-
-    void *p = d_malloc(pool, size);
-    if (p == nullptr)
-        return false;
-
-    if (!read_buffer(file, p, size))
-        return false;
-
-    buffer = { p, size };
-    return true;
+    return file.Read64();
 }
 
-static bool
-expect_32(FILE *file, uint32_t expected)
+static void
+Expect32(FileReader &file, uint32_t expected)
 {
-    uint32_t value;
-
-    return read_32(file, &value) && value == expected;
+    if (file.Read32() != expected)
+        throw SessionDeserializerError();
 }
 
 uint32_t
 session_read_magic(FILE *file)
-{
-    uint32_t magic;
-    return read_32(file, &magic) ? magic : 0;
+try {
+    return FileReader(file).Read32();
+} catch (SessionDeserializerError) {
+    return 0;
 }
 
 bool
-session_read_file_header(FILE *file)
-{
-    const Session *session = nullptr;
-
-    return expect_32(file, MAGIC_FILE) && expect_32(file, sizeof(*session));
+session_read_file_header(FILE *_file)
+try {
+    FileReader file(_file);
+    Expect32(file, MAGIC_FILE);
+    Expect32(file, sizeof(Session));
+    return true;
+} catch (SessionDeserializerError) {
+    return false;
 }
 
-static bool
-read_widget_sessions(FILE *file, Session *session,
-                     WidgetSession *parent,
-                     WidgetSession::Set &widgets)
-    throw(std::bad_alloc);
+static void
+ReadWidgetSessions(FileReader &file, Session &session,
+                   WidgetSession *parent,
+                   WidgetSession::Set &widgets)
+    throw(std::bad_alloc, SessionDeserializerError);
 
-static bool
-do_read_widget_session(FILE *file, Session *session,
-                       WidgetSession *ws)
+static void
+DoReadWidgetSession(FileReader &file, Session &session, WidgetSession &ws)
+    throw(std::bad_alloc, SessionDeserializerError)
 {
-    struct dpool *pool = &session->pool;
+    struct dpool &pool = session.pool;
 
-    return read_string_const(file, pool, &ws->id) &&
-        read_widget_sessions(file, session, ws, ws->children) &&
-        read_string(file, pool, &ws->path_info) &&
-        read_string(file, pool, &ws->query_string) &&
-        expect_32(file, MAGIC_END_OF_RECORD);
+    ws.id = file.ReadString(pool);
+    ReadWidgetSessions(file, session, &ws, ws.children);
+    ws.path_info = file.ReadString(pool);
+    ws.query_string = file.ReadString(pool);
+    Expect32(file, MAGIC_END_OF_RECORD);
 }
 
 static WidgetSession *
-read_widget_session(FILE *file, Session *session, WidgetSession *parent)
-    throw(std::bad_alloc)
+ReadWidgetSession(FileReader &file, Session &session, WidgetSession *parent)
+    throw(std::bad_alloc, SessionDeserializerError)
 {
-    auto *ws = widget_session_allocate(session, parent);
-
-    if (!do_read_widget_session(file, session, ws))
-        return nullptr;
-
+    auto *ws = widget_session_allocate(&session, parent);
+    DoReadWidgetSession(file, session, *ws);
     return ws;
 }
 
-static bool
-read_widget_sessions(FILE *file, Session *session,
-                     WidgetSession *parent,
-                     WidgetSession::Set &widgets)
-    throw(std::bad_alloc)
+static void
+ReadWidgetSessions(FileReader &file, Session &session,
+                   WidgetSession *parent,
+                   WidgetSession::Set &widgets)
+    throw(std::bad_alloc, SessionDeserializerError)
 {
     while (true) {
-        uint32_t magic;
-        if (!read_32(file, &magic))
-            return false;
-
+        uint32_t magic = file.Read32();
         if (magic == MAGIC_END_OF_LIST) {
-            return true;
+            break;
         } else if (magic != MAGIC_WIDGET_SESSION)
-            return false;
+            throw SessionDeserializerError();
 
-        auto *ws = read_widget_session(file, session, parent);
-        if (ws == nullptr)
-            return false;
-
+        auto *ws = ReadWidgetSession(file, session, parent);
         widgets.insert(*ws);
     }
 }
 
-static bool
-do_read_cookie(FILE *file, struct dpool *pool, Cookie *cookie)
+static void
+DoReadCookie(FileReader &file, struct dpool &pool, Cookie &cookie)
+    throw(std::bad_alloc, SessionDeserializerError)
 {
-    assert(cookie != nullptr);
-
-    return read_string(file, pool, cookie->name) &&
-        read_string(file, pool, cookie->value) &&
-        read_string_const(file, pool, &cookie->domain) &&
-        read_string_const(file, pool, &cookie->path) &&
-        read_time(file, &cookie->expires) &&
-        expect_32(file, MAGIC_END_OF_RECORD);
+    cookie.name = file.ReadStringView(pool);
+    cookie.value = file.ReadStringView(pool);
+    cookie.domain = file.ReadString(pool);
+    cookie.path = file.ReadString(pool);
+    cookie.expires = ReadTime(file);
+    Expect32(file, MAGIC_END_OF_RECORD);
 }
 
 static Cookie *
-read_cookie(FILE *file, struct dpool *pool)
-    throw(std::bad_alloc)
+ReadCookie(FileReader &file, struct dpool &pool)
+    throw(std::bad_alloc, SessionDeserializerError)
 {
-    auto *cookie = NewFromPool<Cookie>(pool, *pool, nullptr, nullptr);
-    if (!do_read_cookie(file, pool, cookie))
-        return nullptr;
-
+    auto *cookie = NewFromPool<Cookie>(&pool, pool, nullptr, nullptr);
+    DoReadCookie(file, pool, *cookie);
     return cookie;
 }
 
-static bool
-read_cookie_jar(FILE *file, struct dpool *pool, CookieJar *jar)
-    throw(std::bad_alloc)
+static void
+ReadCookieJar(FileReader &file, struct dpool &pool, CookieJar &jar)
+    throw(std::bad_alloc, SessionDeserializerError)
 {
     while (true) {
-        uint32_t magic;
-        if (!read_32(file, &magic))
-            return false;
-
+        uint32_t magic = file.Read32();
         if (magic == MAGIC_END_OF_LIST)
-            return true;
+            break;
         else if (magic != MAGIC_COOKIE)
-            return false;
+            throw SessionDeserializerError();
 
-        auto *cookie = read_cookie(file, pool);
-        if (cookie == nullptr)
-            return false;
-
-        jar->Add(*cookie);
+        auto *cookie = ReadCookie(file, pool);
+        jar.Add(*cookie);
     }
 }
 
-static bool
-do_read_session(FILE *file, struct dpool *pool, Session *session)
+static void
+DoReadSession(FileReader &file, struct dpool &pool, Session &session)
     throw(std::bad_alloc)
 {
-    assert(session != nullptr);
-
-    session->cookies = cookie_jar_new(*pool);
-    if (session->cookies == nullptr)
-        return false;
-
-    return read_session_id(file, &session->id) &&
-        read_time(file, &session->expires) &&
-        read_32(file, &session->counter) &&
-        read_bool(file, &session->is_new) &&
-        read_bool(file, &session->cookie_sent) &&
-        read_bool(file, &session->cookie_received) &&
-        read_string_const(file, pool, &session->realm) &&
-        read_buffer(file, pool, session->translate) &&
-        read_string_const(file, pool, &session->user) &&
-        read_time(file, &session->user_expires) &&
-        read_string_const(file, pool, &session->language) &&
-        read_widget_sessions(file, session, nullptr, session->widgets) &&
-        read_cookie_jar(file, pool, session->cookies) &&
-        expect_32(file, MAGIC_END_OF_RECORD);
+    file.ReadT(session.id);
+    session.expires = ReadTime(file);
+    file.ReadT(session.counter);
+    session.is_new = file.ReadBool();
+    session.cookie_sent = file.ReadBool();
+    session.cookie_received = file.ReadBool();
+    session.realm = file.ReadString(pool);
+    session.translate = file.ReadConstBuffer(pool);
+    session.realm = file.ReadString(pool);
+    session.user_expires = ReadTime(file);
+    session.language = file.ReadString(pool);
+    ReadWidgetSessions(file, session, nullptr, session.widgets);
+    session.cookies = cookie_jar_new(pool);
+    ReadCookieJar(file, pool, *session.cookies);
+    Expect32(file, MAGIC_END_OF_RECORD);
 }
 
 Session *
-session_read(FILE *file, struct dpool *pool)
+session_read(FILE *_file, struct dpool *pool)
     throw(std::bad_alloc)
-{
+try {
+    FileReader file(_file);
     Session *session = session_allocate(pool, nullptr);
-    if (session == nullptr || !do_read_session(file, pool, session))
-        return nullptr;
-
+    DoReadSession(file, *pool, *session);
     return session;
+} catch (SessionDeserializerError) {
+    return nullptr;
 }
