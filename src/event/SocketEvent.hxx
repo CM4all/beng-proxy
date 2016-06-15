708eb6943012eaf6a29fcc7bf55488cc270defd0
Max Kellermann <mk@cm4all.com>
2016-06-15 12:46:11 +0200

    event/SocketEvent: new Event wrapper using BoundMethod<>

---
+++ src/event/SocketEvent.hxx
@@ -0,0 +1,79 @@
+/*
+ * C++ wrappers for libevent.
+ *
+ * author: Max Kellermann <mk@cm4all.com>
+ */
+
+#ifndef SOCKET_EVENT_HXX
+#define SOCKET_EVENT_HXX
+
+#include "Event.hxx"
+#include "util/BindMethod.hxx"
+
+class SocketEvent {
+    EventLoop &event_loop;
+
+    Event event;
+
+    typedef BoundMethod<void(short events)> Callback;
+    const Callback callback;
+
+public:
+    SocketEvent(EventLoop &_event_loop, Callback _callback)
+        :event_loop(_event_loop), callback(_callback) {}
+
+    SocketEvent(EventLoop &_event_loop, evutil_socket_t fd, short events,
+                Callback _callback)
+        :SocketEvent(_event_loop, _callback) {
+        Set(fd, events);
+    }
+
+    gcc_pure
+    evutil_socket_t GetFd() const {
+        return event.GetFd();
+    }
+
+    gcc_pure
+    short GetEvents() const {
+        return event.GetEvents();
+    }
+
+    void Set(evutil_socket_t fd, short events) {
+        event.Set(event_loop, fd, events, EventCallback, this);
+    }
+
+    bool Add(const struct timeval *timeout=nullptr) {
+        return event.Add(timeout);
+    }
+
+    bool Add(const struct timeval &timeout) {
+        return event.Add(timeout);
+    }
+
+    void Delete() {
+        event.Delete();
+    }
+
+    void MakeActive(short events) {
+        event.MakeActive(events);
+    }
+
+    gcc_pure
+    bool IsPending(short events) const {
+        return event.IsPending(events);
+    }
+
+    gcc_pure
+    bool IsTimerPending() const {
+        return event.IsTimerPending();
+    }
+
+private:
+    static void EventCallback(gcc_unused evutil_socket_t fd, short events,
+                              void *ctx) {
+        auto &event = *(SocketEvent *)ctx;
+        event.callback(events);
+    }
+};
+
+#endif
