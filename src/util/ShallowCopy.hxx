714955cb78fdfc21ec11c8395720c88564fa72b4
Max Kellermann <mk@cm4all.com>
2016-06-21 12:20:33 +0200

    util/ShallowCopy: new tag type for shallow copy constructors

---
+++ src/util/ShallowCopy.hxx
@@ -0,0 +1,15 @@
+/*
+ * author: Max Kellermann <mk@cm4all.com>
+ */
+
+#ifndef SHALLOW_COPY_HXX
+#define SHALLOW_COPY_HXX
+
+/**
+ * A tag for overloading copying constructors, telling them to make
+ * shallow copies of source data (e.g. copy pointers instead of
+ * duplicating the referenced objects).
+ */
+struct ShallowCopy {};
+
+#endif
