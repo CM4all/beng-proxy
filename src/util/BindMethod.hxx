8e253e651c07cb155deb8db0bfeb2d29232cb31e
Max Kellermann <mk@cm4all.com>
2016-06-23 15:23:03 +0200

    util/BindMethod: add nullptr constructor and bool operator

--- src/util/BindMethod.hxx
+++ src/util/BindMethod.hxx
@@ -60,6 +60,19 @@ public:
 	BoundMethod(void *_instance, function_pointer _function)
 		:instance(_instance), function(_function) {}
 
+	/**
+	 * Construct an "undefined" object.  It must not be called,
+	 * and its "bool" operator returns false.
+	 */
+	BoundMethod(std::nullptr_t):function(nullptr) {}
+
+	/**
+	 * Was this object initialized with a valid function pointer?
+	 */
+	operator bool() const {
+		return function != nullptr;
+	}
+
 	R operator()(Args... args) const {
 		return function(instance, std::forward<Args>(args)...);
 	}
