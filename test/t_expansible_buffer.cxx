854fc65610ab243f922572188a96ea36afc7c49b
Max Kellermann <mk@cm4all.com>
2016-06-23 23:16:40 +0200

    text/t_expansible_buffer: allocate ExpansibleBuffer on the stack

--- test/t_expansible_buffer.cxx
+++ test/t_expansible_buffer.cxx
@@ -14,48 +14,48 @@ main(gcc_unused int argc, gcc_unused char **argv)
 
     RootPool pool;
 
-    auto *eb = expansible_buffer_new(pool, 4, 1024);
-    assert(expansible_buffer_is_empty(eb));
+    ExpansibleBuffer eb(pool, 4, 1024);
+    assert(expansible_buffer_is_empty(&eb));
 
-    p = expansible_buffer_read(eb, &size);
+    p = expansible_buffer_read(&eb, &size);
     assert(p != nullptr);
     assert(size == 0);
 
-    expansible_buffer_write_string(eb, "01");
-    assert(!expansible_buffer_is_empty(eb));
+    expansible_buffer_write_string(&eb, "01");
+    assert(!expansible_buffer_is_empty(&eb));
 
-    q = expansible_buffer_read(eb, &size);
+    q = expansible_buffer_read(&eb, &size);
     assert(q == p);
     assert(size == 2);
     assert(memcmp(q, "01", 2) == 0);
 
-    expansible_buffer_write_string(eb, "234");
-    assert(!expansible_buffer_is_empty(eb));
+    expansible_buffer_write_string(&eb, "234");
+    assert(!expansible_buffer_is_empty(&eb));
 
-    q = expansible_buffer_read(eb, &size);
+    q = expansible_buffer_read(&eb, &size);
     assert(q != p);
     assert(size == 5);
     assert(memcmp(q, "01234", 5) == 0);
 
-    expansible_buffer_reset(eb);
-    assert(expansible_buffer_is_empty(eb));
+    expansible_buffer_reset(&eb);
+    assert(expansible_buffer_is_empty(&eb));
 
-    p = expansible_buffer_read(eb, &size);
+    p = expansible_buffer_read(&eb, &size);
     assert(p == q);
     assert(size == 0);
 
-    expansible_buffer_write_string(eb, "abcdef");
-    assert(!expansible_buffer_is_empty(eb));
+    expansible_buffer_write_string(&eb, "abcdef");
+    assert(!expansible_buffer_is_empty(&eb));
 
-    p = expansible_buffer_read(eb, &size);
+    p = expansible_buffer_read(&eb, &size);
     assert(p == q);
     assert(size == 6);
     assert(memcmp(q, "abcdef", 6) == 0);
 
-    p = expansible_buffer_write(eb, 512);
+    p = expansible_buffer_write(&eb, 512);
     assert(p != nullptr);
 
     /* this call hits the hard limit */
-    p = expansible_buffer_write(eb, 512);
+    p = expansible_buffer_write(&eb, 512);
     assert(p == nullptr);
 }
