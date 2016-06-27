bef62ac8ec5a515ab55344d03f9877541562be2a
Max Kellermann <mk@cm4all.com>
2016-06-27 22:07:03 +0200

    test/t_cookie_client: allocate StringMap on the stack

--- test/t_cookie_client.cxx
+++ test/t_cookie_client.cxx
@@ -16,79 +16,78 @@ static void
 Test1(struct dpool *dpool)
 {
     RootPool pool;
-    auto *headers = strmap_new(pool);
+    StringMap headers(pool);
 
     CookieJar jar(*dpool);
 
     /* empty cookie jar */
-    cookie_jar_http_header(jar, "foo.bar", "/", *headers, pool);
-    assert(headers->Get("cookie") == nullptr);
-    assert(headers->Get("cookie2") == nullptr);
+    cookie_jar_http_header(jar, "foo.bar", "/", headers, pool);
+    assert(headers.Get("cookie") == nullptr);
+    assert(headers.Get("cookie2") == nullptr);
 
     /* wrong domain */
     cookie_jar_set_cookie2(jar, "a=b", "other.domain", nullptr);
-    cookie_jar_http_header(jar, "foo.bar", "/", *headers, pool);
-    assert(headers->Get("cookie") == nullptr);
-    assert(headers->Get("cookie2") == nullptr);
+    cookie_jar_http_header(jar, "foo.bar", "/", headers, pool);
+    assert(headers.Get("cookie") == nullptr);
+    assert(headers.Get("cookie2") == nullptr);
 
     /* correct domain */
     cookie_jar_set_cookie2(jar, "a=b", "foo.bar", nullptr);
-    cookie_jar_http_header(jar, "foo.bar", "/", *headers, pool);
-    assert(strcmp(headers->Get("cookie"), "a=b") == 0);
+    cookie_jar_http_header(jar, "foo.bar", "/", headers, pool);
+    assert(strcmp(headers.Get("cookie"), "a=b") == 0);
 
     /* another cookie */
-    headers = strmap_new(pool);
+    headers.Clear();
     cookie_jar_set_cookie2(jar, "c=d", "foo.bar", nullptr);
-    cookie_jar_http_header(jar, "foo.bar", "/", *headers, pool);
-    assert(strcmp(headers->Get("cookie"), "c=d; a=b") == 0);
+    cookie_jar_http_header(jar, "foo.bar", "/", headers, pool);
+    assert(strcmp(headers.Get("cookie"), "c=d; a=b") == 0);
 
     /* delete a cookie */
-    headers = strmap_new(pool);
+    headers.Clear();
     cookie_jar_set_cookie2(jar, "c=xyz;max-age=0", "foo.bar", nullptr);
-    cookie_jar_http_header(jar, "foo.bar", "/", *headers, pool);
-    assert(strcmp(headers->Get("cookie"), "a=b") == 0);
+    cookie_jar_http_header(jar, "foo.bar", "/", headers, pool);
+    assert(strcmp(headers.Get("cookie"), "a=b") == 0);
 
     /* other domain */
-    headers = strmap_new(pool);
-    cookie_jar_http_header(jar, "other.domain", "/some_path", *headers, pool);
-    assert(strcmp(headers->Get("cookie"), "a=b") == 0);
+    headers.Clear();
+    cookie_jar_http_header(jar, "other.domain", "/some_path", headers, pool);
+    assert(strcmp(headers.Get("cookie"), "a=b") == 0);
 }
 
 static void
 Test2(struct dpool *dpool)
 {
     RootPool pool;
-    auto *headers = strmap_new(pool);
+    StringMap headers(pool);
 
     /* wrong path */
     CookieJar jar(*dpool);
 
-    headers = strmap_new(pool);
     cookie_jar_set_cookie2(jar, "a=b;path=\"/foo\"", "foo.bar", "/bar/x");
-    cookie_jar_http_header(jar, "foo.bar", "/", *headers, pool);
-    assert(headers->Get("cookie") == nullptr);
-    assert(headers->Get("cookie2") == nullptr);
+    cookie_jar_http_header(jar, "foo.bar", "/", headers, pool);
+    assert(headers.Get("cookie") == nullptr);
+    assert(headers.Get("cookie2") == nullptr);
 
     /* correct path */
-    headers = strmap_new(pool);
+    headers.Clear();
     cookie_jar_set_cookie2(jar, "a=b;path=\"/bar\"", "foo.bar", "/bar/x");
-    cookie_jar_http_header(jar, "foo.bar", "/bar", *headers, pool);
-    assert(strcmp(headers->Get("cookie"), "a=b") == 0);
+    cookie_jar_http_header(jar, "foo.bar", "/bar", headers, pool);
+    assert(strcmp(headers.Get("cookie"), "a=b") == 0);
 
     /* delete: path mismatch */
-    headers = strmap_new(pool);
+    headers.Clear();
     cookie_jar_set_cookie2(jar, "a=b;path=\"/foo\";max-age=0",
                            "foo.bar", "/foo/x");
-    cookie_jar_http_header(jar, "foo.bar", "/bar", *headers, pool);
-    assert(strcmp(headers->Get("cookie"), "a=b") == 0);
+    cookie_jar_http_header(jar, "foo.bar", "/bar", headers, pool);
+    assert(strcmp(headers.Get("cookie"), "a=b") == 0);
 
     /* delete: path match */
-    headers = strmap_new(pool);
+    headers.Clear();
     cookie_jar_set_cookie2(jar, "a=b;path=\"/bar\";max-age=0",
                            "foo.bar", "/bar/x");
-    cookie_jar_http_header(jar, "foo.bar", "/bar", *headers, pool);
-    assert(headers->Get("cookie") == nullptr);
-    assert(headers->Get("cookie2") == nullptr);
+    cookie_jar_http_header(jar, "foo.bar", "/bar", headers, pool);
+    assert(headers.Get("cookie") == nullptr);
+    assert(headers.Get("cookie2") == nullptr);
 }
 
 int main(int argc gcc_unused, char **argv gcc_unused) {
