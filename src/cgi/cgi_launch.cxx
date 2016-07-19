a2b2d24195dc384370cd37ec09f004813480c4db
Max Kellermann <mk@cm4all.com>
2016-07-19 10:54:46 +0200

    cgi/launch: ignore the "Proxy" request header

--- src/cgi/cgi_launch.cxx
+++ src/cgi/cgi_launch.cxx
@@ -105,6 +105,13 @@ cgi_run(const JailParams *jail,
                 continue;
             }
 
+            if (strcmp(pair.key, "proxy") == 0)
+                /* work around vulnerability in several CGI programs
+                   which take the environment variable HTTP_PROXY as
+                   proxy specification for their internal HTTP
+                   clients; see CVE-2016-5385 and others */
+                continue;
+
             char buffer[512] = "HTTP_";
             size_t i;
             for (i = 0; 5 + i < sizeof(buffer) - 1 && pair.key[i] != 0; ++i) {
