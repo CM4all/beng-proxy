5ac4324fd9d0110b08631603c551bfb3bf0653ec
Max Kellermann <mk@cm4all.com>
2016-07-28 17:47:20 +0200

    pipe_filter: fix double close() bug
    ~PreparedChildProcess() will do this for us.

--- src/pipe_filter.cxx
+++ src/pipe_filter.cxx
@@ -112,8 +112,6 @@ pipe_filter(SpawnService &spawn_service,
     pid_t pid = SpawnChildProcess(pool, path, body, &response,
                                   std::move(p),
                                   spawn_service, &error);
-    if (prefix_logger.second >= 0)
-        close(prefix_logger.second);
     if (pid < 0) {
         DeletePrefixLogger(prefix_logger.first);
         handler->InvokeAbort(handler_ctx, error);
