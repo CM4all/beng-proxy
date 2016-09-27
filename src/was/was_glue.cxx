bd5d729c9706c3898669655da0fb5f624f6c75c5
Max Kellermann <mk@cm4all.com>
2016-09-27 20:49:57 +0200

    was/glue: close the request body before submitting spawn error
    By the time the error was submitted, the "this" pointer may be
    invalidated, leading to a crash.  We need to free the request body
    first.

--- src/was/was_glue.cxx
+++ src/was/was_glue.cxx
@@ -121,10 +121,10 @@ WasRequest::OnStockItemReady(StockItem &item)
 void
 WasRequest::OnStockItemError(GError *error)
 {
-    handler.InvokeAbort(error);
-
     if (body != nullptr)
         body->CloseUnused();
+
+    handler.InvokeAbort(error);
 }
 
 /*
