CM4all/beng-proxy

commit d16bd8d209252a1ccced7d6c9d5063ae066ee71c

resource_loader: fix memory leak after failure
Fixes assertion failures due to request body leak.

src/resource_loader.cxx
@@ -198,6 +198,9 @@ resource_loader_request(struct resource_loader *rl, struct pool *pool,
         file = address->u.file;
         if (file->delegate != nullptr) {
             if (rl->delegate_stock == nullptr) {
+                if (body != nullptr)
+                    body->CloseUnused();
+
                 GError *error = g_error_new_literal(resource_loader_quark(), 0,
                                                     "No delegate stock");
                 handler->InvokeAbort(handler_ctx, error);
@@ -220,11 +223,11 @@ resource_loader_request(struct resource_loader *rl, struct pool *pool,
         return;
 
     case ResourceAddress::Type::NFS:
-#ifdef HAVE_LIBNFS
         if (body != nullptr)
             /* NFS files cannot receive a request body, close it */
             body->CloseUnused();
 
+#ifdef HAVE_LIBNFS
         nfs_request(*pool, *rl->nfs_cache,
                     address->u.nfs->server, address->u.nfs->export_name,
                     address->u.nfs->path,
@@ -261,6 +264,10 @@ resource_loader_request(struct resource_loader *rl, struct pool *pool,
             stderr_fd = cgi->options.OpenStderrPath();
             if (stderr_fd < 0) {
                 int code = errno;
+
+                if (body != nullptr)
+                    body->CloseUnused();
+
                 GError *error =
                     g_error_new(errno_quark(), code, "open('%s') failed: %s",
                                 cgi->options.stderr_path,

commit 437d977b600290e1aa59a5052889a5edfa654168

resource_loader: fix double free bug
Don't close the request body twice.  Caused by commit d16bd8d209

src/resource_loader.cxx
@@ -198,9 +198,6 @@ resource_loader_request(struct resource_loader *rl, struct pool *pool,
         file = address->u.file;
         if (file->delegate != nullptr) {
             if (rl->delegate_stock == nullptr) {
-                if (body != nullptr)
-                    body->CloseUnused();
-
                 GError *error = g_error_new_literal(resource_loader_quark(), 0,
                                                     "No delegate stock");
                 handler->InvokeAbort(handler_ctx, error);