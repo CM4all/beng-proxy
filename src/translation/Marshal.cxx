92eb249a259f8b7ef36d0c244ca28a5ef22760dd
Max Kellermann <mk@cm4all.com>
2016-11-10 15:05:17 +0100

    translation/Marshal: convert pointers to references

--- src/translation/Marshal.cxx
+++ src/translation/Marshal.cxx
@@ -14,7 +14,7 @@
 #include <string.h>
 
 static void
-write_packet_n(GrowingBuffer *gb, uint16_t command,
+write_packet_n(GrowingBuffer &gb, uint16_t command,
                const void *payload, size_t length)
 {
     if (length >= 0xffff)
@@ -25,13 +25,13 @@ write_packet_n(GrowingBuffer *gb, uint16_t command,
     header.length = (uint16_t)length;
     header.command = command;
 
-    gb->Write(&header, sizeof(header));
+    gb.Write(&header, sizeof(header));
     if (length > 0)
-        gb->Write(payload, length);
+        gb.Write(payload, length);
 }
 
 static void
-write_packet(GrowingBuffer *gb, uint16_t command, const char *payload)
+write_packet(GrowingBuffer &gb, uint16_t command, const char *payload)
 {
     write_packet_n(gb, command, payload,
                    payload != nullptr ? strlen(payload) : 0);
@@ -39,7 +39,7 @@ write_packet(GrowingBuffer *gb, uint16_t command, const char *payload)
 
 template<typename T>
 static void
-write_buffer(GrowingBuffer *gb, uint16_t command,
+write_buffer(GrowingBuffer &gb, uint16_t command,
              ConstBuffer<T> buffer)
 {
     auto b = buffer.ToVoid();
@@ -50,7 +50,7 @@ write_buffer(GrowingBuffer *gb, uint16_t command,
  * Forward the command to write_packet() only if #payload is not nullptr.
  */
 static void
-write_optional_packet(GrowingBuffer *gb, uint16_t command, const char *payload)
+write_optional_packet(GrowingBuffer &gb, uint16_t command, const char *payload)
 {
     if (payload != nullptr)
         write_packet(gb, command, payload);
@@ -58,7 +58,7 @@ write_optional_packet(GrowingBuffer *gb, uint16_t command, const char *payload)
 
 template<typename T>
 static void
-write_optional_buffer(GrowingBuffer *gb, uint16_t command,
+write_optional_buffer(GrowingBuffer &gb, uint16_t command,
                       ConstBuffer<T> buffer)
 {
     if (!buffer.IsNull())
@@ -66,13 +66,13 @@ write_optional_buffer(GrowingBuffer *gb, uint16_t command,
 }
 
 static void
-write_short(GrowingBuffer *gb, uint16_t command, uint16_t payload)
+write_short(GrowingBuffer &gb, uint16_t command, uint16_t payload)
 {
     write_packet_n(gb, command, &payload, sizeof(payload));
 }
 
 static void
-write_sockaddr(GrowingBuffer *gb,
+write_sockaddr(GrowingBuffer &gb,
                uint16_t command, uint16_t command_string,
                SocketAddress address)
 {
@@ -88,7 +88,7 @@ write_sockaddr(GrowingBuffer *gb,
 }
 
 static void
-write_optional_sockaddr(GrowingBuffer *gb,
+write_optional_sockaddr(GrowingBuffer &gb,
                         uint16_t command, uint16_t command_string,
                         SocketAddress address)
 {
@@ -102,55 +102,55 @@ MarshalTranslateRequest(struct pool &pool, uint8_t PROTOCOL_VERSION,
 {
     GrowingBuffer gb(pool, 512);
 
-    write_packet_n(&gb, TRANSLATE_BEGIN,
+    write_packet_n(gb, TRANSLATE_BEGIN,
                    &PROTOCOL_VERSION, sizeof(PROTOCOL_VERSION));
-    write_optional_buffer(&gb, TRANSLATE_ERROR_DOCUMENT,
+    write_optional_buffer(gb, TRANSLATE_ERROR_DOCUMENT,
                           request.error_document);
 
     if (request.error_document_status != 0)
-        write_short(&gb, TRANSLATE_STATUS,
+        write_short(gb, TRANSLATE_STATUS,
                     request.error_document_status);
 
-    write_optional_packet(&gb, TRANSLATE_LISTENER_TAG,
+    write_optional_packet(gb, TRANSLATE_LISTENER_TAG,
                           request.listener_tag);
-    write_optional_sockaddr(&gb, TRANSLATE_LOCAL_ADDRESS,
+    write_optional_sockaddr(gb, TRANSLATE_LOCAL_ADDRESS,
                             TRANSLATE_LOCAL_ADDRESS_STRING,
                             request.local_address);
-    write_optional_packet(&gb, TRANSLATE_REMOTE_HOST,
+    write_optional_packet(gb, TRANSLATE_REMOTE_HOST,
                           request.remote_host);
-    write_optional_packet(&gb, TRANSLATE_HOST, request.host);
-    write_optional_packet(&gb, TRANSLATE_USER_AGENT, request.user_agent);
-    write_optional_packet(&gb, TRANSLATE_UA_CLASS, request.ua_class);
-    write_optional_packet(&gb, TRANSLATE_LANGUAGE, request.accept_language);
-    write_optional_packet(&gb, TRANSLATE_AUTHORIZATION, request.authorization);
-    write_optional_packet(&gb, TRANSLATE_URI, request.uri);
-    write_optional_packet(&gb, TRANSLATE_ARGS, request.args);
-    write_optional_packet(&gb, TRANSLATE_QUERY_STRING, request.query_string);
-    write_optional_packet(&gb, TRANSLATE_WIDGET_TYPE, request.widget_type);
-    write_optional_buffer(&gb, TRANSLATE_SESSION, request.session);
-    write_optional_buffer(&gb, TRANSLATE_INTERNAL_REDIRECT,
+    write_optional_packet(gb, TRANSLATE_HOST, request.host);
+    write_optional_packet(gb, TRANSLATE_USER_AGENT, request.user_agent);
+    write_optional_packet(gb, TRANSLATE_UA_CLASS, request.ua_class);
+    write_optional_packet(gb, TRANSLATE_LANGUAGE, request.accept_language);
+    write_optional_packet(gb, TRANSLATE_AUTHORIZATION, request.authorization);
+    write_optional_packet(gb, TRANSLATE_URI, request.uri);
+    write_optional_packet(gb, TRANSLATE_ARGS, request.args);
+    write_optional_packet(gb, TRANSLATE_QUERY_STRING, request.query_string);
+    write_optional_packet(gb, TRANSLATE_WIDGET_TYPE, request.widget_type);
+    write_optional_buffer(gb, TRANSLATE_SESSION, request.session);
+    write_optional_buffer(gb, TRANSLATE_INTERNAL_REDIRECT,
                           request.internal_redirect);
-    write_optional_buffer(&gb, TRANSLATE_CHECK, request.check);
-    write_optional_buffer(&gb, TRANSLATE_AUTH, request.auth);
-    write_optional_buffer(&gb, TRANSLATE_WANT_FULL_URI, request.want_full_uri);
-    write_optional_buffer(&gb, TRANSLATE_WANT, request.want);
-    write_optional_buffer(&gb, TRANSLATE_FILE_NOT_FOUND,
+    write_optional_buffer(gb, TRANSLATE_CHECK, request.check);
+    write_optional_buffer(gb, TRANSLATE_AUTH, request.auth);
+    write_optional_buffer(gb, TRANSLATE_WANT_FULL_URI, request.want_full_uri);
+    write_optional_buffer(gb, TRANSLATE_WANT, request.want);
+    write_optional_buffer(gb, TRANSLATE_FILE_NOT_FOUND,
                           request.file_not_found);
-    write_optional_buffer(&gb, TRANSLATE_CONTENT_TYPE_LOOKUP,
+    write_optional_buffer(gb, TRANSLATE_CONTENT_TYPE_LOOKUP,
                           request.content_type_lookup);
-    write_optional_packet(&gb, TRANSLATE_SUFFIX, request.suffix);
-    write_optional_buffer(&gb, TRANSLATE_ENOTDIR, request.enotdir);
-    write_optional_buffer(&gb, TRANSLATE_DIRECTORY_INDEX,
+    write_optional_packet(gb, TRANSLATE_SUFFIX, request.suffix);
+    write_optional_buffer(gb, TRANSLATE_ENOTDIR, request.enotdir);
+    write_optional_buffer(gb, TRANSLATE_DIRECTORY_INDEX,
                           request.directory_index);
-    write_optional_packet(&gb, TRANSLATE_PARAM, request.param);
-    write_optional_buffer(&gb, TRANSLATE_PROBE_PATH_SUFFIXES,
+    write_optional_packet(gb, TRANSLATE_PARAM, request.param);
+    write_optional_buffer(gb, TRANSLATE_PROBE_PATH_SUFFIXES,
                           request.probe_path_suffixes);
-    write_optional_packet(&gb, TRANSLATE_PROBE_SUFFIX,
+    write_optional_packet(gb, TRANSLATE_PROBE_SUFFIX,
                           request.probe_suffix);
-    write_optional_buffer(&gb, TRANSLATE_READ_FILE,
+    write_optional_buffer(gb, TRANSLATE_READ_FILE,
                           request.read_file);
-    write_optional_packet(&gb, TRANSLATE_USER, request.user);
-    write_packet(&gb, TRANSLATE_END, nullptr);
+    write_optional_packet(gb, TRANSLATE_USER, request.user);
+    write_packet(gb, TRANSLATE_END, nullptr);
 
     return gb;
 }
