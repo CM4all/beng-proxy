8f636030549145a87ad0fa8a256198552ddd551a
Max Kellermann <mk@cm4all.com>
2017-03-09 12:24:02 +0100

    system/fd-util: remove fd_set_nonblock()

--- src/system/fd-util.c
+++ src/system/fd-util.c
@@ -6,29 +6,7 @@
 
 #include "fd-util.h"
 
-#include <assert.h>
-#include <fcntl.h>
 #include <poll.h>
-
-int
-fd_mask_status_flags(int fd, int and_mask, int xor_mask)
-{
-    int ret;
-
-    assert(fd >= 0);
-
-    ret = fcntl(fd, F_GETFL, 0);
-    if (ret < 0)
-        return ret;
-
-    return fcntl(fd, F_SETFL, (ret & and_mask) ^ xor_mask);
-}
-
-int
-fd_set_nonblock(int fd, bool value)
-{
-    return fd_mask_status_flags(fd, ~O_NONBLOCK, value ? O_NONBLOCK : 0);
-}
 
 bool
 fd_ready_for_writing(int fd)
--- src/system/fd-util.h
+++ src/system/fd-util.h
@@ -13,12 +13,6 @@
 extern "C" {
 #endif
 
-int
-fd_mask_status_flags(int fd, int and_mask, int xor_mask);
-
-int
-fd_set_nonblock(int fd, bool value);
-
 bool
 fd_ready_for_writing(int fd);
 
