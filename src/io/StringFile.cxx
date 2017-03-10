c7feea6b21806af64b5b31c34c8306eced63ac93
Max Kellermann <mk@cm4all.com>
2017-03-10 10:42:58 +0100

    io/StringFile: use class UniqueFileDescriptor

--- src/io/StringFile.cxx
+++ src/io/StringFile.cxx
@@ -4,23 +4,18 @@
 
 #include "StringFile.hxx"
 #include "system/Error.hxx"
+#include "io/UniqueFileDescriptor.hxx"
 #include "util/StringUtil.hxx"
-#include "util/ScopeExit.hxx"
-
-#include <unistd.h>
-#include <fcntl.h>
 
 std::string
 LoadStringFile(const char *path)
 {
-    int fd = open(path, O_RDONLY|O_CLOEXEC|O_NOCTTY);
-    if (fd < 0)
+    UniqueFileDescriptor fd;
+    if (!fd.OpenReadOnly(path))
         throw FormatErrno("Failed to open %s", path);
 
-    AtScopeExit(fd) { close(fd); };
-
     char buffer[1024];
-    ssize_t nbytes = read(fd, buffer, sizeof(buffer));
+    ssize_t nbytes = fd.Read(buffer, sizeof(buffer));
     if (nbytes < 0)
         throw FormatErrno("Failed to read %s", path);
 
