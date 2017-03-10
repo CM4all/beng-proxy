86562720c405027ff48cc6e5dd2e6f5e9680d4dc
Max Kellermann <mk@cm4all.com>
2017-03-10 10:09:27 +0100

    load_file: use class UniqueFileDescriptor

--- src/load_file.cxx
+++ src/load_file.cxx
@@ -6,9 +6,9 @@
 
 #include "load_file.hxx"
 #include "gerrno.h"
-#include "system/fd_util.h"
 #include "http_quark.h"
 #include "pool.hxx"
+#include "io/UniqueFileDescriptor.hxx"
 #include "util/ConstBuffer.hxx"
 
 #include <http/status.h>
@@ -22,58 +22,48 @@
 ConstBuffer<void>
 LoadFile(struct pool &pool, const char *path, off_t max_size, GError **error_r)
 {
-    int fd = open_cloexec(path, O_RDONLY, 0);
-    if (fd < 0) {
+    UniqueFileDescriptor fd;
+    if (!fd.OpenReadOnly(path)) {
         int code = errno;
         g_set_error(error_r, errno_quark(), code,
                     "Failed to open %s: %s", path, strerror(code));
         return nullptr;
     }
 
-    struct stat st;
-    if (fstat(fd, &st) < 0) {
+    off_t size = fd.GetSize();
+    if (size < 0) {
         int code = errno;
         g_set_error(error_r, errno_quark(), code,
                     "Failed to stat %s: %s", path, strerror(code));
-        close(fd);
         return nullptr;
     }
 
-    if (st.st_size > max_size) {
+    if (size > max_size) {
         g_set_error(error_r, http_response_quark(),
                     HTTP_STATUS_INTERNAL_SERVER_ERROR,
                     "File is too large: %s", path);
-        close(fd);
         return nullptr;
     }
 
-    const size_t size = st.st_size;
-
-    if (size == 0) {
-        close(fd);
+    if (size == 0)
         return { "", 0 };
-    }
 
     void *p = p_malloc(&pool, size);
     if (p == nullptr) {
         set_error_errno(error_r);
-        close(fd);
         return nullptr;
     }
 
-    ssize_t nbytes = read(fd, p, size);
+    ssize_t nbytes = fd.Read(p, size);
     if (nbytes < 0) {
         int code = errno;
         g_set_error(error_r, errno_quark(), code,
                     "Failed to read from %s: %s", path, strerror(code));
-        close(fd);
         free(p);
         return nullptr;
     }
 
-    close(fd);
-
-    if (size_t(nbytes) != size) {
+    if (size_t(nbytes) != size_t(size)) {
         g_set_error(error_r, http_response_quark(),
                     HTTP_STATUS_INTERNAL_SERVER_ERROR,
                     "Short read from %s", path);
@@ -81,5 +71,5 @@ LoadFile(struct pool &pool, const char *path, off_t max_size, GError **error_r)
         return nullptr;
     }
 
-    return { p, size };
+    return { p, size_t(size) };
 }
