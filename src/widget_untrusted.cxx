bac84dedcb9a564debc10a873b7f822fcc6a8388
Max Kellermann <mk@cm4all.com>
2017-03-15 14:01:13 +0100

    widget_untrusted: pass only strings to check functions

--- src/widget_untrusted.cxx
+++ src/widget_untrusted.cxx
@@ -9,9 +9,9 @@
 #include <string.h>
 
 static bool
-widget_check_untrusted_host(const WidgetClass &cls, const char *host)
+widget_check_untrusted_host(const char *untrusted_host, const char *host)
 {
-    assert(cls.untrusted_host != nullptr);
+    assert(untrusted_host != nullptr);
 
     if (host == nullptr)
         /* untrusted widget not allowed on trusted host name */
@@ -19,13 +19,13 @@ widget_check_untrusted_host(const WidgetClass &cls, const char *host)
 
     /* untrusted widget only allowed on matching untrusted host
        name */
-    return strcmp(host, cls.untrusted_host) == 0;
+    return strcmp(host, untrusted_host) == 0;
 }
 
 static bool
-widget_check_untrusted_prefix(const WidgetClass &cls, const char *host)
+widget_check_untrusted_prefix(const char *untrusted_prefix, const char *host)
 {
-    assert(cls.untrusted_prefix != nullptr);
+    assert(untrusted_prefix != nullptr);
 
     if (host == nullptr)
         /* untrusted widget not allowed on trusted host name */
@@ -33,16 +33,16 @@ widget_check_untrusted_prefix(const WidgetClass &cls, const char *host)
 
     /* untrusted widget only allowed on matching untrusted host
        name */
-    size_t length = strlen(cls.untrusted_prefix);
-    return memcmp(host, cls.untrusted_prefix, length) == 0 &&
+    size_t length = strlen(untrusted_prefix);
+    return memcmp(host, untrusted_prefix, length) == 0 &&
         host[length] == '.';
 }
 
 static bool
-widget_check_untrusted_site_suffix(const WidgetClass &cls,
+widget_check_untrusted_site_suffix(const char *untrusted_site_suffix,
                                    const char *host, const char *site_name)
 {
-    assert(cls.untrusted_site_suffix != nullptr);
+    assert(untrusted_site_suffix != nullptr);
 
     if (host == nullptr || site_name == nullptr)
         /* untrusted widget not allowed on trusted host name */
@@ -52,14 +52,14 @@ widget_check_untrusted_site_suffix(const WidgetClass &cls,
     return memcmp(host, site_name, site_name_length) == 0 &&
         host[site_name_length] == '.' &&
         strcmp(host + site_name_length + 1,
-               cls.untrusted_site_suffix) == 0;
+               untrusted_site_suffix) == 0;
 }
 
 static bool
-widget_check_untrusted_raw_site_suffix(const WidgetClass &cls,
+widget_check_untrusted_raw_site_suffix(const char *untrusted_raw_site_suffix,
                                        const char *host, const char *site_name)
 {
-    assert(cls.untrusted_raw_site_suffix != nullptr);
+    assert(untrusted_raw_site_suffix != nullptr);
 
     if (host == nullptr || site_name == nullptr)
         /* untrusted widget not allowed on trusted host name */
@@ -68,20 +68,22 @@ widget_check_untrusted_raw_site_suffix(const WidgetClass &cls,
     size_t site_name_length = strlen(site_name);
     return memcmp(host, site_name, site_name_length) == 0 &&
         strcmp(host + site_name_length,
-               cls.untrusted_raw_site_suffix) == 0;
+               untrusted_raw_site_suffix) == 0;
 }
 
 bool
 WidgetClass::CheckHost(const char *host, const char *site_name) const
 {
     if (untrusted_host != nullptr)
-        return widget_check_untrusted_host(*this, host);
+        return widget_check_untrusted_host(untrusted_host, host);
     else if (untrusted_prefix != nullptr)
-        return widget_check_untrusted_prefix(*this, host);
+        return widget_check_untrusted_prefix(untrusted_prefix, host);
     else if (untrusted_site_suffix != nullptr)
-        return widget_check_untrusted_site_suffix(*this, host, site_name);
+        return widget_check_untrusted_site_suffix(untrusted_site_suffix,
+                                                  host, site_name);
     else if (untrusted_raw_site_suffix != nullptr)
-        return widget_check_untrusted_raw_site_suffix(*this, host, site_name);
+        return widget_check_untrusted_raw_site_suffix(untrusted_raw_site_suffix,
+                                                      host, site_name);
     else
         /* trusted widget is only allowed on a trusted host name
            (host==nullptr) */
