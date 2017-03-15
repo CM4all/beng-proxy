6a746ebe196110667c4994dc9d29cb17120e8dc1
Max Kellermann <mk@cm4all.com>
2017-03-15 21:38:34 +0100

    autogen.sh, benchmark.sh: pass parameters to meson

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -e
 rm -rf build
-exec meson . build -Dprefix=/usr/local/stow/cm4all-beng-proxy --werror -Db_sanitize=address
+exec meson . build -Dprefix=/usr/local/stow/cm4all-beng-proxy --werror -Db_sanitize=address "$@"
--- benchmark.sh
+++ benchmark.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -e
 rm -rf build/release
-exec meson . build/release --buildtype=release -Dprefix=/usr/local/stow/cm4all-beng-proxy -Dwerror=true -Db_ndebug=true
+exec meson . build/release --buildtype=release -Dprefix=/usr/local/stow/cm4all-beng-proxy -Dwerror=true -Db_ndebug=true "$@"
