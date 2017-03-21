e169985fb81d1fb87e7ce64f8f068d830e776a30
Max Kellermann <mk@cm4all.com>
2017-03-21 12:21:20 +0100

    util/djbhash: use uint_fast32_t internally
    uint_fast32_t may be faster than uint32_t on some architectures.

--- src/util/djbhash.c
+++ src/util/djbhash.c
@@ -14,7 +14,7 @@ djb_hash(const void *p0, size_t size)
 {
     assert(p0 != NULL);
 
-    uint32_t hash = 5381;
+    uint_fast32_t hash = 5381;
 
     const uint8_t *p = p0;
     for (size_t i = 0; i != size; ++i)
@@ -28,7 +28,7 @@ djb_hash_string(const char *p0)
 {
     assert(p0 != NULL);
 
-    uint32_t hash = 5381;
+    uint_fast32_t hash = 5381;
 
     for (const uint8_t *p = (const uint8_t *)p0; *p != 0; ++p)
         hash = (hash << 5) + hash + *p;
