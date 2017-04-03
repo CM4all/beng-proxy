0f20baf0fa80c3d399cf482b8fb2f80269267c40
Max Kellermann <mk@cm4all.com>
2017-04-03 13:47:42 +0200

    certdb: update the Let's Encrypt agreement URL

--- src/certdb/AcmeClient.cxx
+++ src/certdb/AcmeClient.cxx
@@ -353,7 +353,7 @@ AcmeClient::NewReg(EVP_PKEY &key, const char *email)
         payload += "\" ], ";
     }
 
-    payload += "\"agreement\": \"https://letsencrypt.org/documents/LE-SA-v1.0.1-July-27-2015.pdf\"}";
+    payload += "\"agreement\": \"https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf\"}";
 
     auto response = SignedRequest(key,
                                   HTTP_METHOD_POST, "/acme/new-reg",
