c714f2297a674553fdb09b8b7de4d3c3ca0e4f67
Max Kellermann <mk@cm4all.com>
2017-05-09 09:05:49 +0200

    lb_tcp: expose the struct

--- src/lb_tcp.cxx
+++ src/lb_tcp.cxx
@@ -7,70 +7,16 @@
 #include "lb_tcp.hxx"
 #include "lb_config.hxx"
 #include "lb_cluster.hxx"
-#include "filtered_socket.hxx"
 #include "client_balancer.hxx"
 #include "address_sticky.hxx"
 #include "direct.hxx"
 #include "pool.hxx"
 #include "GException.hxx"
-#include "net/ConnectSocket.hxx"
 #include "net/UniqueSocketDescriptor.hxx"
 #include "net/SocketAddress.hxx"
-#include "net/StaticSocketAddress.hxx"
-#include "util/Cancellable.hxx"
 
 #include <unistd.h>
 #include <errno.h>
-
-struct LbTcpConnection final : ConnectSocketHandler {
-    struct pool &pool;
-    Stock *pipe_stock;
-
-    const LbTcpConnectionHandler *const handler;
-    void *const handler_ctx;
-
-    FilteredSocket inbound;
-
-    BufferedSocket outbound;
-
-    StaticSocketAddress bind_address;
-    const LbClusterConfig &cluster;
-    LbClusterMap &clusters;
-    Balancer &balancer;
-    const sticky_hash_t session_sticky;
-
-    CancellablePointer cancel_connect;
-
-    bool got_inbound_data, got_outbound_data;
-
-    LbTcpConnection(struct pool &_pool, EventLoop &event_loop,
-                    Stock *_pipe_stock,
-                    UniqueSocketDescriptor &&fd, FdType fd_type,
-                    const SocketFilter *filter, void *filter_ctx,
-                    SocketAddress remote_address,
-                    const LbClusterConfig &cluster,
-                    LbClusterMap &clusters,
-                    Balancer &balancer,
-                    const LbTcpConnectionHandler &_handler, void *ctx);
-
-    void ScheduleHandshakeCallback() {
-        inbound.ScheduleReadNoTimeout(false);
-        inbound.SetHandshakeCallback(BIND_THIS_METHOD(OnHandshake));
-    }
-
-    void ConnectOutbound();
-
-    void DestroyInbound();
-    void DestroyOutbound();
-    void Destroy();
-
-    void OnHandshake();
-
-    /* virtual methods from class ConnectSocketHandler */
-    void OnSocketConnectSuccess(UniqueSocketDescriptor &&fd) override;
-    void OnSocketConnectTimeout() override;
-    void OnSocketConnectError(std::exception_ptr ep) override;
-};
 
 static constexpr timeval write_timeout = { 30, 0 };
 
--- src/lb_tcp.hxx
+++ src/lb_tcp.hxx
@@ -7,20 +7,23 @@
 #ifndef BENG_PROXY_LB_TCP_H
 #define BENG_PROXY_LB_TCP_H
 
+#include "filtered_socket.hxx"
+#include "StickyHash.hxx"
+#include "net/StaticSocketAddress.hxx"
+#include "net/ConnectSocket.hxx"
 #include "io/FdType.hxx"
+#include "util/Cancellable.hxx"
 
 #include <exception>
 
 struct pool;
 class EventLoop;
 class Stock;
-struct SocketFilter;
 struct AddressList;
 struct Balancer;
 struct LbClusterConfig;
 struct LbTcpConnection;
 class UniqueSocketDescriptor;
-class SocketAddress;
 class LbClusterMap;
 
 struct LbTcpConnectionHandler {
@@ -28,6 +31,56 @@ struct LbTcpConnectionHandler {
     void (*error)(const char *prefix, const char *error, void *ctx);
     void (*_errno)(const char *prefix, int error, void *ctx);
     void (*exception)(const char *prefix, std::exception_ptr ep, void *ctx);
+};
+
+struct LbTcpConnection final : ConnectSocketHandler {
+    struct pool &pool;
+    Stock *pipe_stock;
+
+    const LbTcpConnectionHandler *const handler;
+    void *const handler_ctx;
+
+    FilteredSocket inbound;
+
+    BufferedSocket outbound;
+
+    StaticSocketAddress bind_address;
+    const LbClusterConfig &cluster;
+    LbClusterMap &clusters;
+    Balancer &balancer;
+    const sticky_hash_t session_sticky;
+
+    CancellablePointer cancel_connect;
+
+    bool got_inbound_data, got_outbound_data;
+
+    LbTcpConnection(struct pool &_pool, EventLoop &event_loop,
+                    Stock *_pipe_stock,
+                    UniqueSocketDescriptor &&fd, FdType fd_type,
+                    const SocketFilter *filter, void *filter_ctx,
+                    SocketAddress remote_address,
+                    const LbClusterConfig &cluster,
+                    LbClusterMap &clusters,
+                    Balancer &balancer,
+                    const LbTcpConnectionHandler &_handler, void *ctx);
+
+    void ScheduleHandshakeCallback() {
+        inbound.ScheduleReadNoTimeout(false);
+        inbound.SetHandshakeCallback(BIND_THIS_METHOD(OnHandshake));
+    }
+
+    void ConnectOutbound();
+
+    void DestroyInbound();
+    void DestroyOutbound();
+    void Destroy();
+
+    void OnHandshake();
+
+    /* virtual methods from class ConnectSocketHandler */
+    void OnSocketConnectSuccess(UniqueSocketDescriptor &&fd) override;
+    void OnSocketConnectTimeout() override;
+    void OnSocketConnectError(std::exception_ptr ep) override;
 };
 
 /**
