1f08a2facebcf3a6f025638975bf8674e302d437
Max Kellermann <mk@cm4all.com>
2017-05-29 19:42:53 +0200

    certdb/Wildcard: add API documentation

--- src/certdb/Wildcard.hxx
+++ src/certdb/Wildcard.hxx
@@ -7,6 +7,11 @@
 
 #include <string>
 
+/**
+ * Attempt to convert the given "common name" (i.e. host name) to a
+ * wild card by replacing the first segment with an asterisk.  Returns
+ * an empty string if this is not possible.
+ */
 std::string
 MakeCommonNameWildcard(const char *s);
 
