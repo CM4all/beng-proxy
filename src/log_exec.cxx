fc34e2ef9c4dcb61b2725611313a0fde6527b96c
Max Kellermann <mk@cm4all.com>
2017-06-07 19:23:07 +0200

    log_exec: use class UniqueSocketDescriptor

--- src/log_exec.cxx
+++ src/log_exec.cxx
@@ -7,6 +7,8 @@
 
 #include "log-server.h"
 #include "system/Error.hxx"
+#include "net/UniqueSocketDescriptor.hxx"
+#include "net/SocketAddress.hxx"
 #include "util/PrintException.hxx"
 #include "util/RuntimeError.hxx"
 
@@ -20,7 +22,7 @@
 #include <unistd.h>
 #include <sys/socket.h>
 
-static int
+static UniqueSocketDescriptor
 open_udp(const char *host, int default_port)
 {
     struct addrinfo hints;
@@ -34,17 +36,15 @@ open_udp(const char *host, int default_port)
         throw FormatRuntimeError("Failed to resolve '%s': %s",
                                  host, gai_strerror(ret));
 
-    int fd = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol);
-    if (fd < 0) {
+    UniqueSocketDescriptor fd;
+    if (!fd.Create(ai->ai_family, ai->ai_socktype, ai->ai_protocol)) {
         const int e = errno;
         freeaddrinfo(ai);
         throw MakeErrno(e, "socket() failed");
     }
 
-    ret = bind(fd, ai->ai_addr, ai->ai_addrlen);
-    if (ret < 0) {
+    if (!fd.Bind({ai->ai_addr, ai->ai_addrlen})) {
         const int e = errno;
-        close(fd);
         freeaddrinfo(ai);
         throw MakeErrno(e, "bind() failed");
     }
@@ -60,11 +60,8 @@ try {
         return EXIT_FAILURE;
     }
 
-    int fd = open_udp(argv[1], 5479);
-    if (fd > 0) {
-        dup2(fd, 0);
-        close(fd);
-    }
+    auto fd = open_udp(argv[1], 5479);
+    fd.CheckDuplicate(FileDescriptor(STDIN_FILENO));
 
     execv(argv[2], &argv[2]);
 
