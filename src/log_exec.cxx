f8139fa1df0cdc68f4bf9e34f173c1dd64a0c14e
Max Kellermann <mk@cm4all.com>
2017-06-07 19:23:40 +0200

    log_exec: throw exception on error

--- src/log_exec.cxx
+++ src/log_exec.cxx
@@ -6,6 +6,9 @@
  */
 
 #include "log-server.h"
+#include "system/Error.hxx"
+#include "util/PrintException.hxx"
+#include "util/RuntimeError.hxx"
 
 #include <socket/resolver.h>
 
@@ -27,25 +30,23 @@ open_udp(const char *host, int default_port)
 
     struct addrinfo *ai;
     int ret = socket_resolve_host_port(host, default_port, &hints, &ai);
-    if (ret != 0) {
-        fprintf(stderr, "Failed to resolve '%s': %s\n",
-                host, gai_strerror(ret));
-        return -1;
-    }
+    if (ret != 0)
+        throw FormatRuntimeError("Failed to resolve '%s': %s",
+                                 host, gai_strerror(ret));
 
     int fd = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol);
     if (fd < 0) {
-        fprintf(stderr, "socket() failed: %s\n", strerror(errno));
+        const int e = errno;
         freeaddrinfo(ai);
-        return -1;
+        throw MakeErrno(e, "socket() failed");
     }
 
     ret = bind(fd, ai->ai_addr, ai->ai_addrlen);
     if (ret < 0) {
-        fprintf(stderr, "bind() failed: %s\n", strerror(errno));
+        const int e = errno;
         close(fd);
         freeaddrinfo(ai);
-        return -1;
+        throw MakeErrno(e, "bind() failed");
     }
 
     freeaddrinfo(ai);
@@ -53,16 +54,13 @@ open_udp(const char *host, int default_port)
 }
 
 int main(int argc, char **argv)
-{
+try {
     if (argc < 3) {
         fprintf(stderr, "Usage: log-exec IP PROGRAM ...\n");
         return EXIT_FAILURE;
     }
 
     int fd = open_udp(argv[1], 5479);
-    if (fd < 0)
-        return EXIT_FAILURE;
-
     if (fd > 0) {
         dup2(fd, 0);
         close(fd);
@@ -72,5 +70,8 @@ int main(int argc, char **argv)
 
     fprintf(stderr, "Failed to execute %s: %s\n",
             argv[2], strerror(errno));
+    return EXIT_FAILURE;
+} catch (const std::exception &e) {
+    PrintException(e);
     return EXIT_FAILURE;
 }
