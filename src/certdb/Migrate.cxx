e2e69d62f1ccc8479e2fe0ade3627dd5b8fb287f
Max Kellermann <mk@cm4all.com>
2017-06-12 13:05:10 +0200

    certdb/Migrate: move code to libcommon

--- src/certdb/Migrate.cxx
+++ src/certdb/Migrate.cxx
@@ -5,35 +5,11 @@
 #include "CertDatabase.hxx"
 #include "FromResult.hxx"
 #include "Config.hxx"
+#include "pg/Reflection.hxx"
 #include "pg/CheckError.hxx"
 #include "ssl/Name.hxx"
 #include "util/ByteOrder.hxx"
 #include "util/PrintException.hxx"
-
-static bool
-ColumnExists(Pg::Connection &c, const char *schema,
-             const char *table_name, const char *column_name)
-{
-    if (schema == nullptr || *schema == 0)
-        schema = "public";
-
-    return CheckError(c.ExecuteParams("SELECT data_type FROM INFORMATION_SCHEMA.COLUMNS "
-                                      "WHERE table_schema=$1 AND table_name=$2 AND column_name=$3",
-                                      schema, table_name, column_name)).GetRowCount() > 0;
-
-}
-
-static bool
-IndexExists(Pg::Connection &c, const char *schema,
-            const char *table_name, const char *index_name)
-{
-    if (schema == nullptr || *schema == 0)
-        schema = "public";
-
-    return CheckError(c.ExecuteParams("SELECT indexdef FROM pg_indexes "
-                                      "WHERE schemaname=$1 AND tablename=$2 AND indexname=$3",
-                                      schema, table_name, index_name)).GetRowCount() > 0;
-}
 
 static void
 FillIssuerCommonName(Pg::Connection &c)
@@ -69,10 +45,14 @@ FillIssuerCommonName(Pg::Connection &c)
 void
 CertDatabase::Migrate()
 {
+    const char *schema = config.schema.empty()
+        ? "public"
+        : config.schema.c_str();
+
     /* server_certificate.issuer_common_name added in version 12.0.14 */
 
-    if (!ColumnExists(conn, config.schema.c_str(), "server_certificate",
-                      "issuer_common_name"))
+    if (!Pg::ColumnExists(conn, schema, "server_certificate",
+                          "issuer_common_name"))
         CheckError(conn.Execute("ALTER TABLE server_certificate "
                                 "ADD COLUMN issuer_common_name varchar(256) NULL"));
 
@@ -80,14 +60,14 @@ CertDatabase::Migrate()
 
     /* server_certificate.handle added in version 12.0.15 */
 
-    if (!ColumnExists(conn, config.schema.c_str(), "server_certificate",
-                      "handle"))
+    if (!Pg::ColumnExists(conn, schema, "server_certificate",
+                          "handle"))
         CheckError(conn.Execute("ALTER TABLE server_certificate "
                                 "ADD COLUMN handle varchar(256) NULL"));
 
 
-    if (!IndexExists(conn, config.schema.c_str(), "server_certificate",
-                     "server_certificate_handle"))
+    if (!Pg::IndexExists(conn, schema, "server_certificate",
+                         "server_certificate_handle"))
         CheckError(conn.Execute("CREATE UNIQUE INDEX server_certificate_handle "
                                 "ON server_certificate(handle);"));
 }
