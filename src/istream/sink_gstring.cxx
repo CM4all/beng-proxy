2a23db29a0da2afa5d35db4be093404011521885
Max Kellermann <mk@cm4all.com>
2017-06-29 08:57:05 +0200

    istream/sink_gstring: call destructor

--- src/istream/sink_gstring.cxx
+++ src/istream/sink_gstring.cxx
@@ -28,12 +28,17 @@ struct GStringSink final : IstreamSink, Cancellable {
         cancel_ptr = *this;
     }
 
+    void Destroy() {
+        this->~GStringSink();
+    }
+
     /* virtual methods from class Cancellable */
     void Cancel() override {
         g_string_free(value, true);
 
         const ScopePoolRef ref(pool TRACE_ARGS);
         input.Close();
+        Destroy();
     }
 
     /* virtual methods from class IstreamHandler */
@@ -45,11 +50,13 @@ struct GStringSink final : IstreamSink, Cancellable {
 
     void OnEof() override {
         callback(value, nullptr, callback_ctx);
+        Destroy();
     }
 
     void OnError(std::exception_ptr ep) override {
         g_string_free(value, true);
         callback(nullptr, ep, callback_ctx);
+        Destroy();
     }
 };
 
