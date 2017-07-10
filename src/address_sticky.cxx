d85d413f9fd1d0e4d7046427028aabdcc7e51f6a
Max Kellermann <mk@cm4all.com>
2017-07-10 22:11:00 +0200

    net/SocketAddress: add method GetSteadyPart()

--- src/address_sticky.cxx
+++ src/address_sticky.cxx
@@ -5,38 +5,14 @@
 #include "address_sticky.hxx"
 #include "net/SocketAddress.hxx"
 #include "util/djbhash.h"
-
-#include <sys/socket.h>
-#include <netinet/in.h>
-
-gcc_pure
-static sticky_hash_t
-ipv4_sticky(const struct sockaddr_in *address)
-{
-    return djb_hash(&address->sin_addr, sizeof(address->sin_addr));
-}
-
-gcc_pure
-static sticky_hash_t
-ipv6_sticky(const struct sockaddr_in6 *address)
-{
-    return djb_hash(&address->sin6_addr, sizeof(address->sin6_addr));
-}
+#include "util/ConstBuffer.hxx"
 
 sticky_hash_t
 socket_address_sticky(SocketAddress address)
 {
-    if (address.IsNull())
+    const auto p = address.GetSteadyPart();
+    if (p.IsNull())
         return 0;
 
-    switch (address.GetFamily()) {
-    case AF_INET:
-        return ipv4_sticky((const struct sockaddr_in *)(const void *)address.GetAddress());
-
-    case AF_INET6:
-        return ipv6_sticky((const struct sockaddr_in6 *)(const void *)address.GetAddress());
-
-    default:
-        return 0;
-    }
+    return djb_hash(p.data, p.size);
 }
