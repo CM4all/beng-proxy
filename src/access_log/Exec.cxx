59a5cd82b37d84509ca6be9cebee4945eb11ac6c
Max Kellermann <mk@cm4all.com>
2017-07-11 17:21:07 +0200

    access_log/Exec: move code to net/RBindSocket.cxx

--- src/access_log/Exec.cxx
+++ src/access_log/Exec.cxx
@@ -6,38 +6,14 @@
  */
 
 #include "system/Error.hxx"
-#include "net/Resolver.hxx"
-#include "net/AddressInfo.hxx"
 #include "net/UniqueSocketDescriptor.hxx"
-#include "net/SocketAddress.hxx"
+#include "net/RBindSocket.hxx"
 #include "util/PrintException.hxx"
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 #include <errno.h>
 #include <unistd.h>
-
-static UniqueSocketDescriptor
-open_udp(const char *host, int default_port)
-{
-    struct addrinfo hints;
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_flags = AI_ADDRCONFIG|AI_PASSIVE;
-    hints.ai_socktype = SOCK_DGRAM;
-
-    const auto ail = Resolve(host, default_port, &hints);
-    const auto &ai = ail.front();
-
-    UniqueSocketDescriptor fd;
-    if (!fd.Create(ai.GetFamily(), ai.GetType(), ai.GetProtocol()))
-        throw MakeErrno("socket() failed");
-
-    if (!fd.Bind(ai))
-        throw MakeErrno("bind() failed");
-
-    return fd;
-}
 
 int main(int argc, char **argv)
 try {
@@ -46,7 +22,8 @@ try {
         return EXIT_FAILURE;
     }
 
-    auto fd = open_udp(argv[1], 5479);
+    auto fd = ResolveBindDatagramSocket(argv[1], 5479);
+    fd.SetBlocking();
     fd.CheckDuplicate(FileDescriptor(STDIN_FILENO));
 
     execv(argv[2], &argv[2]);
