e16bcd75549d0bf4027337e6c8e7b39a14029fc2
Max Kellermann <mk@cm4all.com>
2017-07-17 14:59:26 +0200

    access_log/Datagram: add initializing constructor

--- src/access_log/Datagram.hxx
+++ src/access_log/Datagram.hxx
@@ -30,6 +30,29 @@ struct AccessLogDatagram {
     bool valid_timestamp, valid_http_method, valid_http_status;
     bool valid_length, valid_traffic;
     bool valid_duration;
+
+    AccessLogDatagram() = default;
+
+    AccessLogDatagram(uint64_t _timestamp,
+                      http_method_t _method, const char *_uri,
+                      const char *_remote_host,
+                      const char *_host, const char *_site,
+                      const char *_referer, const char *_user_agent,
+                      http_status_t _status, int64_t _length,
+                      uint64_t _traffic_received, uint64_t _traffic_sent,
+                      uint64_t _duration)
+        :timestamp(_timestamp),
+         remote_host(_remote_host), host(_host), site(_site),
+         http_method(_method),
+         http_uri(_uri), http_referer(_referer), user_agent(_user_agent),
+         http_status(_status),
+         length(_length),
+         traffic_received(_traffic_received), traffic_sent(_traffic_sent),
+         duration(_duration),
+         valid_timestamp(true),
+         valid_http_method(true), valid_http_status(true),
+         valid_length(_length >= 0), valid_traffic(true),
+         valid_duration(true) {}
 };
 
 #endif
