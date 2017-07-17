7b41fa71e66f288c44546d1901ae8f44df66f290
Max Kellermann <mk@cm4all.com>
2017-07-17 16:19:10 +0200

    access_log: use LogOneLine(), eliminate duplicate code

--- src/access_log.cxx
+++ src/access_log.cxx
@@ -9,20 +9,9 @@
 #ifndef NO_ACCESS_LOG
 
 #include "http_server/Request.hxx"
+#include "access_log/OneLine.hxx"
 #include "access_log/Glue.hxx"
 #include "access_log/Datagram.hxx"
-
-#include <stdio.h>
-#include <time.h>
-
-static const char *
-optional_string(const char *p)
-{
-    if (p == nullptr)
-        return "-";
-
-    return p;
-}
 
 void
 access_log(HttpServerRequest *request, const char *site,
@@ -50,35 +39,7 @@ access_log(HttpServerRequest *request, const char *site,
         return;
     }
 
-    time_t now = time(nullptr);
-    char stamp[32];
-
-    if (site == nullptr)
-        site = "-";
-
-    strftime(stamp, sizeof(stamp),
-             "%d/%b/%Y:%H:%M:%S %z", localtime(&now));
-
-    char length_buffer[32];
-    const char *length;
-    if (content_length < 0)
-        length = "-";
-    else {
-        snprintf(length_buffer, sizeof(length_buffer), "%llu",
-                 (unsigned long long)content_length);
-        length = length_buffer;
-    }
-
-    const auto duration_us = std::chrono::duration_cast<std::chrono::microseconds>(duration);
-
-    printf("%s %s - - [%s] \"%s %s HTTP/1.1\" %u %s \"%s\" \"%s\" %llu\n",
-           site, optional_string(request->remote_host), stamp,
-           http_method_to_string(request->method),
-           request->uri,
-           status, length,
-           optional_string(referer),
-           optional_string(user_agent),
-           (unsigned long long)duration_us.count());
+    LogOneLine(d);
 }
 
 #endif
