6020b1edff46ef9a03bb390b1538f0f3739072dd
Max Kellermann <mk@cm4all.com>
2017-07-19 12:32:30 +0200

    lb_headers: set the "X-CM4all-HTTPS" header

--- src/lb_headers.cxx
+++ src/lb_headers.cxx
@@ -57,10 +57,13 @@ forward_identity(struct pool &pool, StringMap &headers,
 void
 lb_forward_request_headers(struct pool &pool, StringMap &headers,
                            const char *local_host, const char *remote_host,
+                           bool https,
                            const char *peer_subject,
                            const char *peer_issuer_subject,
                            bool mangle_via)
 {
+    headers.SecureSet("x-cm4all-https", https ? "on" : nullptr);
+
     headers.SecureSet("x-cm4all-beng-peer-subject", peer_subject);
     headers.SecureSet("x-cm4all-beng-peer-issuer-subject",
                       peer_issuer_subject);
--- src/lb_headers.hxx
+++ src/lb_headers.hxx
@@ -14,6 +14,7 @@ class HttpHeaders;
 void
 lb_forward_request_headers(struct pool &pool, StringMap &headers,
                            const char *local_host, const char *remote_host,
+                           bool https,
                            const char *peer_subject,
                            const char *peer_issuer_subject,
                            bool mangle_via);
