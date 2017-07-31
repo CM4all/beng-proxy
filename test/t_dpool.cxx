6029bca032ee645acf44ade9ecdffbc7b3d31d42
Max Kellermann <mk@cm4all.com>
2017-07-31 14:29:11 +0200

    test/t_dpool: migrate to GTest

--- test/t_dpool.cxx
+++ test/t_dpool.cxx
@@ -3,64 +3,66 @@
 
 #include "util/Compiler.h"
 
-#include <assert.h>
+#include <gtest/gtest.h>
+
 #include <string.h>
 
-int main(int argc gcc_unused, char **argv gcc_unused) {
+TEST(ShmTest, Dpool)
+{
     void *a, *b, *c, *d;
 
     auto *shm = shm_new(1024, 2);
-    assert(shm != nullptr);
+    ASSERT_NE(shm, nullptr);
 
     auto *pool = dpool_new(*shm);
-    assert(pool != nullptr);
+    ASSERT_NE(pool, nullptr);
 
     a = shm_alloc(shm, 1);
-    assert(a != nullptr);
-    assert(a != pool);
+    ASSERT_NE(a, nullptr);
+    ASSERT_NE(a, pool);
 
     b = shm_alloc(shm, 1);
-    assert(b == nullptr);
+    ASSERT_EQ(b, nullptr);
 
     shm_free(shm, a);
 
     a = d_malloc(*pool, 512);
-    assert(a != nullptr);
+    ASSERT_NE(a, nullptr);
     memset(a, 0, 512);
 
     b = d_malloc(*pool, 800);
-    assert(b != nullptr);
+    ASSERT_NE(b, nullptr);
     memset(b, 0, 800);
 
     try {
         c = d_malloc(*pool, 512);
-        assert(c == nullptr);
+        ASSERT_EQ(c, nullptr);
     } catch (std::bad_alloc) {
     }
 
     d = d_malloc(*pool, 220);
-    assert(d != nullptr);
+    ASSERT_NE(d, nullptr);
 
     d_free(*pool, a);
 
     a = d_malloc(*pool, 240);
-    assert(a != nullptr);
+    ASSERT_NE(a, nullptr);
 
     try {
         c = d_malloc(*pool, 270);
-        assert(c == nullptr);
+        ASSERT_EQ(c, nullptr);
     } catch (std::bad_alloc) {
     }
 
     /* no free SHM page */
     c = shm_alloc(shm, 1);
-    assert(c == nullptr);
+    ASSERT_EQ(c, nullptr);
 
     /* free "b" which should release one SHM page */
     d_free(*pool, b);
 
     c = shm_alloc(shm, 1);
-    assert(c != nullptr);
+    ASSERT_NE(c, nullptr);
 
     dpool_destroy(pool);
     shm_close(shm);
