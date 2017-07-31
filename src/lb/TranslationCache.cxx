CM4all/beng-proxy

commit 439956f4a6177f2764e4eaf6eff1f49b8f1ca8d4

lb/TranslationCache: add prefix to VARY values
Previously, a nonexistent value which was marked as "VARY" produced
the same cache key as the wildcard (i.e. no VARY).  This caused wrong
wildcard matches in following lookups.  By prepending an arbitrary
character (here: the underscore) we can make those different again.

src/lb/TranslationCache.cxx
@@ -29,7 +29,8 @@ gcc_pure
 static size_t
 CalculateKeyIteratorBufferSize(StringView host, StringView listener_tag)
 {
-    return host.size + 1 + listener_tag.size + 1;
+    /* the ones are: underscore, separator, underscore, null terminator */
+    return 1 + host.size + 1 + 1 + listener_tag.size + 1;
 }
 
 /**
@@ -100,13 +101,21 @@ private:
 
         char *result = buffer.get(), *p = result;
 
-        if (HasHost(i))
+        if (HasHost(i)) {
+            /* the underscore is just here to make a difference
+               between "wildcard" (nothing) and "empty value"
+               (underscore) */
+            *p++ = '_';
             p = (char *)mempcpy(p, host.data, host.size);
+        }
 
         *p++ = '|';
 
-        if (HasListenerTag(i))
+        if (HasListenerTag(i)) {
+            /* see above for the underscore explanation */
+            *p++ = '_';
             p = (char *)mempcpy(p, listener_tag.data, listener_tag.size);
+        }
 
         *p = 0;
         return result;

commit 49733c09be093b84d4e6ace2574a6bd0a751327e

lb/TranslationCache: use conditional mask in FullKey()
Fixes false cache misses because an underscore was emitted instead of
nothing (regression by commit 439956f4a6177f276).

src/lb/TranslationCache.cxx
@@ -72,7 +72,8 @@ public:
      * Generates a key for storing into the cache.
      */
     const char *FullKey() const {
-        return MakeKey(HOST|LISTENER_TAG);
+        return MakeKey((!host.IsNull() * HOST) |
+                       (!listener_tag.IsNull() * LISTENER_TAG));
     }
 
 private: