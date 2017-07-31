232228b8da387981efbcf9c69f58e1cc281dd1a4
Max Kellermann <mk@cm4all.com>
2017-08-01 00:53:26 +0200

    test/t_pool: migrate to GTest

--- test/t_pool.cxx
+++ test/t_pool.cxx
@@ -1,112 +1,81 @@
 #include "pool.hxx"
 #include "RootPool.hxx"
-
 #include "util/Compiler.h"
 
-#include <cppunit/CompilerOutputter.h>
-#include <cppunit/extensions/TestFactoryRegistry.h>
-#include <cppunit/ui/text/TestRunner.h>
-#include <cppunit/extensions/HelperMacros.h>
+#include <gtest/gtest.h>
 
-#include <assert.h>
 #include <stdint.h>
 #include <stdlib.h>
 
-class PoolTest : public CppUnit::TestFixture {
-    CPPUNIT_TEST_SUITE(PoolTest);
-    CPPUNIT_TEST(TestLibc);
-    CPPUNIT_TEST(TestLinear);
-    CPPUNIT_TEST_SUITE_END();
-
-public:
-    void TestLibc() {
-        RootPool pool;
-        CPPUNIT_ASSERT(pool != NULL);
-        CPPUNIT_ASSERT_EQUAL(size_t(0), pool_brutto_size(pool));
-        CPPUNIT_ASSERT_EQUAL(size_t(0), pool_netto_size(pool));
-
-        const void *q = p_malloc(pool, 64);
-        CPPUNIT_ASSERT(q != NULL);
-        CPPUNIT_ASSERT_EQUAL(size_t(64), pool_brutto_size(pool));
-        CPPUNIT_ASSERT_EQUAL(size_t(64), pool_netto_size(pool));
-
-        const void *r = p_malloc(pool, 256);
-        CPPUNIT_ASSERT(r != NULL);
-        CPPUNIT_ASSERT_EQUAL(size_t(256 + 64), pool_brutto_size(pool));
-        CPPUNIT_ASSERT_EQUAL(size_t(256 + 64), pool_netto_size(pool));
-
-        /* freeing doesn't reduce the size counters, because the pool
-           doesn't know the size of the allocations that are being
-           freed; these tests must be adjusted once this is
-           implemented */
-        p_free(pool, q);
-        CPPUNIT_ASSERT_EQUAL(size_t(256 + 64), pool_brutto_size(pool));
-        CPPUNIT_ASSERT_EQUAL(size_t(256 + 64), pool_netto_size(pool));
-        p_free(pool, r);
-        CPPUNIT_ASSERT_EQUAL(size_t(256 + 64), pool_brutto_size(pool));
-        CPPUNIT_ASSERT_EQUAL(size_t(256 + 64), pool_netto_size(pool));
-    }
-
-    void TestLinear() {
-        RootPool root_pool;
-        LinearPool pool(root_pool, "foo", 64);
-        CPPUNIT_ASSERT(pool != NULL);
-#ifdef NDEBUG
-        CPPUNIT_ASSERT_EQUAL(size_t(0), pool_brutto_size(pool));
-#endif
-        CPPUNIT_ASSERT_EQUAL(size_t(0), pool_netto_size(pool));
-
-        const void *q = p_malloc(pool, 1024);
-        CPPUNIT_ASSERT(q != NULL);
-#ifdef NDEBUG
-        CPPUNIT_ASSERT_EQUAL(size_t(1024), pool_brutto_size(pool));
-#endif
-        CPPUNIT_ASSERT_EQUAL(size_t(1024), pool_netto_size(pool));
-
-        q = p_malloc(pool, 32);
-        CPPUNIT_ASSERT(q != NULL);
-#ifdef NDEBUG
-        CPPUNIT_ASSERT_EQUAL(size_t(1024 + 64), pool_brutto_size(pool));
-#endif
-        CPPUNIT_ASSERT_EQUAL(size_t(1024 + 32), pool_netto_size(pool));
-
-        q = p_malloc(pool, 16);
-        CPPUNIT_ASSERT(q != NULL);
-#ifdef NDEBUG
-        CPPUNIT_ASSERT_EQUAL(size_t(1024 + 64), pool_brutto_size(pool));
-#endif
-        CPPUNIT_ASSERT_EQUAL(size_t(1024 + 32 + 16), pool_netto_size(pool));
-
-        q = p_malloc(pool, 32);
-        CPPUNIT_ASSERT(q != NULL);
-#ifdef NDEBUG
-        CPPUNIT_ASSERT_EQUAL(size_t(1024 + 2 * 64), pool_brutto_size(pool));
-#endif
-        CPPUNIT_ASSERT_EQUAL(size_t(1024 + 32 + 16 + 32), pool_netto_size(pool));
-
-        q = p_malloc(pool, 1024);
-        CPPUNIT_ASSERT(q != NULL);
-#ifdef NDEBUG
-        CPPUNIT_ASSERT_EQUAL(size_t(2 * 1024 + 2 * 64), pool_brutto_size(pool));
-#endif
-        CPPUNIT_ASSERT_EQUAL(size_t(2 * 1024 + 32 + 16 + 32), pool_netto_size(pool));
-    }
-};
-
-CPPUNIT_TEST_SUITE_REGISTRATION(PoolTest);
-
-int
-main(gcc_unused int argc, gcc_unused char **argv)
+TEST(PoolTest, Libc)
 {
-    CppUnit::Test *suite =
-        CppUnit::TestFactoryRegistry::getRegistry().makeTest();
+    RootPool pool;
+    ASSERT_EQ(size_t(0), pool_brutto_size(pool));
+    ASSERT_EQ(size_t(0), pool_netto_size(pool));
 
-    CppUnit::TextUi::TestRunner runner;
-    runner.addTest(suite);
+    const void *q = p_malloc(pool, 64);
+    ASSERT_NE(q, nullptr);
+    ASSERT_EQ(size_t(64), pool_brutto_size(pool));
+    ASSERT_EQ(size_t(64), pool_netto_size(pool));
 
-    runner.setOutputter(new CppUnit::CompilerOutputter(&runner.result(),
-                                                       std::cerr));
-    bool success = runner.run();
+    const void *r = p_malloc(pool, 256);
+    ASSERT_NE(r, nullptr);
+    ASSERT_EQ(size_t(256 + 64), pool_brutto_size(pool));
+    ASSERT_EQ(size_t(256 + 64), pool_netto_size(pool));
 
-    return success ? EXIT_SUCCESS : EXIT_FAILURE;
+    /* freeing doesn't reduce the size counters, because the pool
+       doesn't know the size of the allocations that are being
+       freed; these tests must be adjusted once this is
+       implemented */
+    p_free(pool, q);
+    ASSERT_EQ(size_t(256 + 64), pool_brutto_size(pool));
+    ASSERT_EQ(size_t(256 + 64), pool_netto_size(pool));
+    p_free(pool, r);
+    ASSERT_EQ(size_t(256 + 64), pool_brutto_size(pool));
+    ASSERT_EQ(size_t(256 + 64), pool_netto_size(pool));
+}
+
+TEST(PoolTest, Linear)
+{
+    RootPool root_pool;
+    LinearPool pool(root_pool, "foo", 64);
+#ifdef NDEBUG
+    ASSERT_EQ(size_t(0), pool_brutto_size(pool));
+#endif
+    ASSERT_EQ(size_t(0), pool_netto_size(pool));
+
+    const void *q = p_malloc(pool, 1024);
+    ASSERT_NE(q, nullptr);
+#ifdef NDEBUG
+    ASSERT_EQ(size_t(1024), pool_brutto_size(pool));
+#endif
+    ASSERT_EQ(size_t(1024), pool_netto_size(pool));
+
+    q = p_malloc(pool, 32);
+    ASSERT_NE(q, nullptr);
+#ifdef NDEBUG
+    ASSERT_EQ(size_t(1024 + 64), pool_brutto_size(pool));
+#endif
+    ASSERT_EQ(size_t(1024 + 32), pool_netto_size(pool));
+
+    q = p_malloc(pool, 16);
+    ASSERT_NE(q, nullptr);
+#ifdef NDEBUG
+    ASSERT_EQ(size_t(1024 + 64), pool_brutto_size(pool));
+#endif
+    ASSERT_EQ(size_t(1024 + 32 + 16), pool_netto_size(pool));
+
+    q = p_malloc(pool, 32);
+    ASSERT_NE(q, nullptr);
+#ifdef NDEBUG
+    ASSERT_EQ(size_t(1024 + 2 * 64), pool_brutto_size(pool));
+#endif
+    ASSERT_EQ(size_t(1024 + 32 + 16 + 32), pool_netto_size(pool));
+
+    q = p_malloc(pool, 1024);
+    ASSERT_NE(q, nullptr);
+#ifdef NDEBUG
+    ASSERT_EQ(size_t(2 * 1024 + 2 * 64), pool_brutto_size(pool));
+#endif
+    ASSERT_EQ(size_t(2 * 1024 + 32 + 16 + 32), pool_netto_size(pool));
 }
