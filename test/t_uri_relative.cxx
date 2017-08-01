82fafcbbf2cbb9256509779c75003fda8742ccc3
Max Kellermann <mk@cm4all.com>
2017-08-01 07:38:12 +0200

    test/t_uri_relative: migrate to GTest

--- test/t_uri_relative.cxx
+++ test/t_uri_relative.cxx
@@ -3,81 +3,82 @@
 #include "TestPool.hxx"
 #include "AllocatorPtr.hxx"
 #include "util/StringView.hxx"
-
 #include "util/Compiler.h"
 
-#include <assert.h>
+#include <gtest/gtest.h>
+
 #include <string.h>
 
-int
-main(gcc_unused int argc, gcc_unused char **argv)
+TEST(UriRelativeTest, Compress)
 {
     TestPool pool;
     AllocatorPtr alloc(pool);
 
-    assert(strcmp(uri_compress(alloc, "/foo/bar"), "/foo/bar") == 0);
-    assert(strcmp(uri_compress(alloc, "/foo/./bar"), "/foo/bar") == 0);
-    assert(strcmp(uri_compress(alloc, "/./foo/bar"), "/foo/bar") == 0);
-    assert(strcmp(uri_compress(alloc, "/foo/bar/./"), "/foo/bar/") == 0);
-    assert(strcmp(uri_compress(alloc, "./foo/bar/"), "foo/bar/") == 0);
-    assert(strcmp(uri_compress(alloc, "/foo//bar/"), "/foo/bar/") == 0);
-    assert(strcmp(uri_compress(alloc, "/foo///bar/"), "/foo/bar/") == 0);
-    assert(strcmp(uri_compress(alloc, "/1/2/../3/"), "/1/3/") == 0);
-    assert(strcmp(uri_compress(alloc, "/1/2/../../3/"), "/3/") == 0);
-    assert(strcmp(uri_compress(alloc, "foo/../bar"), "bar") == 0);
-    assert(strcmp(uri_compress(alloc, "foo//../bar"), "bar") == 0);
-    assert(strcmp(uri_compress(alloc, "foo/.."), "") == 0);
-    assert(strcmp(uri_compress(alloc, "foo/../."), "") == 0);
+    ASSERT_STREQ(uri_compress(alloc, "/foo/bar"), "/foo/bar");
+    ASSERT_STREQ(uri_compress(alloc, "/foo/./bar"), "/foo/bar");
+    ASSERT_STREQ(uri_compress(alloc, "/./foo/bar"), "/foo/bar");
+    ASSERT_STREQ(uri_compress(alloc, "/foo/bar/./"), "/foo/bar/");
+    ASSERT_STREQ(uri_compress(alloc, "./foo/bar/"), "foo/bar/");
+    ASSERT_STREQ(uri_compress(alloc, "/foo//bar/"), "/foo/bar/");
+    ASSERT_STREQ(uri_compress(alloc, "/foo///bar/"), "/foo/bar/");
+    ASSERT_STREQ(uri_compress(alloc, "/1/2/../3/"), "/1/3/");
+    ASSERT_STREQ(uri_compress(alloc, "/1/2/../../3/"), "/3/");
+    ASSERT_STREQ(uri_compress(alloc, "foo/../bar"), "bar");
+    ASSERT_STREQ(uri_compress(alloc, "foo//../bar"), "bar");
+    ASSERT_STREQ(uri_compress(alloc, "foo/.."), "");
+    ASSERT_STREQ(uri_compress(alloc, "foo/../."), "");
 
-    assert(uri_compress(alloc, "/1/2/../../../3/") == nullptr);
-    assert(uri_compress(alloc, "/../") == nullptr);
-    assert(uri_compress(alloc, "/a/../../") == nullptr);
-    assert(uri_compress(alloc, "/..") == nullptr);
-    assert(uri_compress(alloc, "..") == nullptr);
-    assert(strcmp(uri_compress(alloc, "/1/2/.."), "/1/") == 0);
+    ASSERT_EQ(uri_compress(alloc, "/1/2/../../../3/"), nullptr);
+    ASSERT_EQ(uri_compress(alloc, "/../"), nullptr);
+    ASSERT_EQ(uri_compress(alloc, "/a/../../"), nullptr);
+    ASSERT_EQ(uri_compress(alloc, "/.."), nullptr);
+    ASSERT_EQ(uri_compress(alloc, ".."), nullptr);
+    ASSERT_STREQ(uri_compress(alloc, "/1/2/.."), "/1/");
+}
 
-    assert(strcmp(uri_absolute(alloc, "http://localhost/", "foo"),
-                  "http://localhost/foo") == 0);
-    assert(strcmp(uri_absolute(alloc, "http://localhost/bar", "foo"),
-                  "http://localhost/foo") == 0);
-    assert(strcmp(uri_absolute(alloc, "http://localhost/bar/", "foo"),
-                  "http://localhost/bar/foo") == 0);
-    assert(strcmp(uri_absolute(alloc, "http://localhost/bar/", "/foo"),
-                  "http://localhost/foo") == 0);
-    assert(strcmp(uri_absolute(alloc, "http://localhost/bar/",
-                               "http://localhost/bar/foo"),
-                  "http://localhost/bar/foo") == 0);
-    assert(strcmp(uri_absolute(alloc, "http://localhost/bar/",
-                               "http://localhost/foo"),
-                  "http://localhost/foo") == 0);
-    assert(strcmp(uri_absolute(alloc, "http://localhost", "foo"),
-                  "http://localhost/foo") == 0);
-    assert(strcmp(uri_absolute(alloc, "/", "foo"), "/foo") == 0);
-    assert(strcmp(uri_absolute(alloc, "/bar", "foo"), "/foo") == 0);
-    assert(strcmp(uri_absolute(alloc, "/bar/", "foo"), "/bar/foo") == 0);
-    assert(strcmp(uri_absolute(alloc, "/bar/", "/foo"), "/foo") == 0);
-    assert(strcmp(uri_absolute(alloc, "/bar", "?foo"), "/bar?foo") == 0);
+TEST(UriRelativeTest, Absolute)
+{
+    TestPool pool;
+    AllocatorPtr alloc(pool);
 
-    assert(strcmp(uri_absolute(alloc, "http://localhost/foo/",
-                               "//example.com/bar"),
-                  "http://example.com/bar") == 0);
+    ASSERT_STREQ(uri_absolute(alloc, "http://localhost/", "foo"), "http://localhost/foo");
+    ASSERT_STREQ(uri_absolute(alloc, "http://localhost/bar", "foo"), "http://localhost/foo");
+    ASSERT_STREQ(uri_absolute(alloc, "http://localhost/bar/", "foo"), "http://localhost/bar/foo");
+    ASSERT_STREQ(uri_absolute(alloc, "http://localhost/bar/", "/foo"), "http://localhost/foo");
+    ASSERT_STREQ(uri_absolute(alloc, "http://localhost/bar/",
+                              "http://localhost/bar/foo"),
+                 "http://localhost/bar/foo");
+    ASSERT_STREQ(uri_absolute(alloc, "http://localhost/bar/",
+                              "http://localhost/foo"),
+                 "http://localhost/foo");
+    ASSERT_STREQ(uri_absolute(alloc, "http://localhost", "foo"),
+                 "http://localhost/foo");
+    ASSERT_STREQ(uri_absolute(alloc, "/", "foo"), "/foo");
+    ASSERT_STREQ(uri_absolute(alloc, "/bar", "foo"), "/foo");
+    ASSERT_STREQ(uri_absolute(alloc, "/bar/", "foo"), "/bar/foo");
+    ASSERT_STREQ(uri_absolute(alloc, "/bar/", "/foo"), "/foo");
+    ASSERT_STREQ(uri_absolute(alloc, "/bar", "?foo"), "/bar?foo");
 
-    assert(strcmp(uri_absolute(alloc, "ftp://localhost/foo/",
-                               "//example.com/bar"),
-                  "ftp://example.com/bar") == 0);
+    ASSERT_STREQ(uri_absolute(alloc, "http://localhost/foo/",
+                              "//example.com/bar"),
+                 "http://example.com/bar");
 
-    assert(strcmp(uri_absolute(alloc, "/foo/", "//example.com/bar"),
-                  "//example.com/bar") == 0);
+    ASSERT_STREQ(uri_absolute(alloc, "ftp://localhost/foo/",
+                              "//example.com/bar"),
+                 "ftp://example.com/bar");
 
-    assert(strcmp(uri_absolute(alloc, "//example.com/foo/", "bar"),
-                  "//example.com/foo/bar") == 0);
+    ASSERT_STREQ(uri_absolute(alloc, "/foo/", "//example.com/bar"),
+                 "//example.com/bar");
 
-    assert(strcmp(uri_absolute(alloc, "//example.com/foo/", "/bar"),
-                  "//example.com/bar") == 0);
+    ASSERT_STREQ(uri_absolute(alloc, "//example.com/foo/", "bar"),
+                 "//example.com/foo/bar");
 
-    assert(strcmp(uri_absolute(alloc, "//example.com", "bar"),
-                  "//example.com/bar") == 0);
+    ASSERT_STREQ(uri_absolute(alloc, "//example.com/foo/", "/bar"),
+                 "//example.com/bar");
 
-    assert(strcmp(uri_absolute(alloc, "//example.com", "/bar"),
-                  "//example.com/bar") == 0);
+    ASSERT_STREQ(uri_absolute(alloc, "//example.com", "bar"),
+                 "//example.com/bar");
+
+    ASSERT_STREQ(uri_absolute(alloc, "//example.com", "/bar"),
+                 "//example.com/bar");
 }
