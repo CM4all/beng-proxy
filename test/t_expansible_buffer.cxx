87c114aad24ff174bbe34a5166004352b31bb5ce
Max Kellermann <mk@cm4all.com>
2017-08-01 07:48:58 +0200

    test/t_expansible_buffer: migrate to GTest

--- test/t_expansible_buffer.cxx
+++ test/t_expansible_buffer.cxx
@@ -2,58 +2,56 @@
 #include "TestPool.hxx"
 #include "util/ConstBuffer.hxx"
 
-#include "util/Compiler.h"
+#include <gtest/gtest.h>
 
-#include <assert.h>
 #include <string.h>
 
-int
-main(gcc_unused int argc, gcc_unused char **argv)
+TEST(ExpansibleBufferTest, Basic)
 {
     TestPool pool;
 
     ExpansibleBuffer eb(pool, 4, 1024);
-    assert(eb.IsEmpty());
+    ASSERT_TRUE(eb.IsEmpty());
 
     auto p = eb.Read();
-    assert(p.data != nullptr);
-    assert(p.size == 0);
+    ASSERT_NE(p.data, nullptr);
+    ASSERT_EQ(p.size, 0);
 
     eb.Write("01");
-    assert(!eb.IsEmpty());
+    ASSERT_FALSE(eb.IsEmpty());
 
     auto q = eb.Read();
-    assert(q.data == p.data);
-    assert(q.size == 2);
-    assert(memcmp(q.data, "01", 2) == 0);
+    ASSERT_EQ(q.data, p.data);
+    ASSERT_EQ(q.size, 2);
+    ASSERT_EQ(memcmp(q.data, "01", 2), 0);
 
     eb.Write("234");
-    assert(!eb.IsEmpty());
+    ASSERT_FALSE(eb.IsEmpty());
 
     q = eb.Read();
-    assert(q.data != p.data);
-    assert(q.size == 5);
-    assert(memcmp(q.data, "01234", 5) == 0);
+    ASSERT_NE(q.data, p.data);
+    ASSERT_EQ(q.size, 5);
+    ASSERT_EQ(memcmp(q.data, "01234", 5), 0);
 
     eb.Clear();
-    assert(eb.IsEmpty());
+    ASSERT_TRUE(eb.IsEmpty());
 
     p = eb.Read();
-    assert(p.data == q.data);
-    assert(p.size == 0);
+    ASSERT_EQ(p.data, q.data);
+    ASSERT_EQ(p.size, 0);
 
     eb.Write("abcdef");
-    assert(!eb.IsEmpty());
+    ASSERT_FALSE(eb.IsEmpty());
 
     p = eb.Read();
-    assert(p.data == q.data);
-    assert(p.size == 6);
-    assert(memcmp(q.data, "abcdef", 6) == 0);
+    ASSERT_EQ(p.data, q.data);
+    ASSERT_EQ(p.size, 6);
+    ASSERT_EQ(memcmp(q.data, "abcdef", 6), 0);
 
     void *r = eb.Write(512);
-    assert(r != nullptr);
+    ASSERT_NE(r, nullptr);
 
     /* this call hits the hard limit */
     r = eb.Write(512);
-    assert(r == nullptr);
+    ASSERT_EQ(r, nullptr);
 }
