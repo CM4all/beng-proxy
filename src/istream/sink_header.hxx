e7e981936f4f336cfc709dc8297d56db0e98683a
Max Kellermann <mk@cm4all.com>
2017-08-17 20:34:01 +0200

    apply the Simplified BSD License (BSD-2-Clause) to all code owned by CM-AG

--- src/istream/sink_header.hxx
+++ src/istream/sink_header.hxx
@@ -1,9 +1,33 @@
 /*
- * This istream filter reads a 32 bit header size from the stream,
- * reads it into a buffer and invokes a callback with the tail of the
- * stream.
+ * Copyright 2007-2017 Content Management AG
+ * All rights reserved.
  *
  * author: Max Kellermann <mk@cm4all.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the
+ * distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * FOUNDATION OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #ifndef BENG_PROXY_SINK_HEADER_HXX
@@ -22,6 +46,11 @@ struct sink_header_handler {
     void (*error)(std::exception_ptr ep, void *ctx);
 };
 
+/*
+ * This istream filter reads a 32 bit header size from the stream,
+ * reads it into a buffer and invokes a callback with the tail of the
+ * stream.
+ */
 void
 sink_header_new(struct pool &pool, Istream &input,
                 const struct sink_header_handler &handler, void *ctx,
--- src/nfs/Cache.hxx
+++ src/nfs/Cache.hxx
@@ -1,7 +1,33 @@
 /*
- * A cache for NFS files.
+ * Copyright 2007-2017 Content Management AG
+ * All rights reserved.
  *
  * author: Max Kellermann <mk@cm4all.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the
+ * distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * FOUNDATION OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #ifndef BENG_PROXY_NFS_CACHE_HXX
@@ -31,6 +57,9 @@ public:
     virtual void OnNfsCacheError(std::exception_ptr ep) = 0;
 };
 
+/**
+ * A cache for NFS files.
+ */
 NfsCache *
 nfs_cache_new(struct pool &pool, size_t max_size, NfsStock &stock,
               EventLoop &event_loop);
