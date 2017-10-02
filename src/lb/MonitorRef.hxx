c1f733eec7fe825332665510e7a8e31d04801a10
Max Kellermann <mk@cm4all.com>
2017-10-02 20:47:54 +0200

    lb/MonitorRef: helper class for holding monitor references

---
+++ src/lb/MonitorRef.hxx
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2007-2017 Content Management AG
+ * All rights reserved.
+ *
+ * author: Max Kellermann <mk@cm4all.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the
+ * distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * FOUNDATION OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include <utility>
+
+class LbMonitorStock;
+class LbMonitorController;
+
+class LbMonitorRef final {
+    LbMonitorStock &stock;
+    LbMonitorController *controller;
+
+public:
+    LbMonitorRef(LbMonitorStock &_stock,
+                 LbMonitorController &_controller) noexcept;
+
+    LbMonitorRef(LbMonitorRef &&src) noexcept
+        :stock(src.stock), controller(std::exchange(src.controller, nullptr)) {}
+
+    ~LbMonitorRef() noexcept {
+        if (controller != nullptr)
+            Release();
+    }
+
+private:
+    void Release() noexcept;
+};
