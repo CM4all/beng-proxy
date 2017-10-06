CM4all/beng-proxy

commit 9e00061959180289e83d7a875a62459be5a8d17b

serialize: merge duplicate code with template function

src/serialize.cxx
@@ -119,46 +119,41 @@ SkipFront(ConstBuffer<void> &input, size_t n)
     input.size -= n;
 }
 
+template<typename T>
+static void
+DeserializeT(ConstBuffer<void> &input, T &dest)
+{
+    static_assert(std::is_trivial<T>::value, "type is not trivial");
+
+    if (gcc_unlikely(input.size < sizeof(dest)))
+        throw DeserializeError();
+
+    memcpy(&dest, input.data, sizeof(dest));
+    SkipFront(input, sizeof(dest));
+}
+
 uint16_t
 deserialize_uint16(ConstBuffer<void> &input)
 {
     uint16_t value;
-
-    if (input.size < sizeof(value))
-        throw DeserializeError();
-
-    value = FromBE16(*(const uint16_t *)input.data);
-    SkipFront(input, sizeof(value));
-
-    return value;
+    DeserializeT(input, value);
+    return FromBE16(*(const uint16_t *)input.data);
 }
 
 uint32_t
 deserialize_uint32(ConstBuffer<void> &input)
 {
     uint32_t value;
-
-    if (input.size < sizeof(value))
-        throw DeserializeError();
-
-    value = FromBE32(*(const uint32_t *)input.data);
-    SkipFront(input, sizeof(value));
-
-    return value;
+    DeserializeT(input, value);
+    return FromBE32(*(const uint32_t *)input.data);
 }
 
 uint64_t
 deserialize_uint64(ConstBuffer<void> &input)
 {
     uint64_t value;
-
-    if (input.size < sizeof(value))
-        throw DeserializeError();
-
-    value = FromBE64(*(const uint64_t *)input.data);
-    SkipFront(input, sizeof(value));
-
-    return value;
+    DeserializeT(input, value);
+    return FromBE64(*(const uint64_t *)input.data);
 }
 
 const char *

commit 6b551718cd4e925eed026fcfd2a62b27ee93fdcc

serialize: fix FromBE*() parameters
The result from DeserializeT() must be converted, not the current
buffer.  This fixes a major breakage from commit 9e00061959180

src/serialize.cxx
@@ -137,7 +137,7 @@ deserialize_uint16(ConstBuffer<void> &input)
 {
     uint16_t value;
     DeserializeT(input, value);
-    return FromBE16(*(const uint16_t *)input.data);
+    return FromBE16(value);
 }
 
 uint32_t
@@ -145,7 +145,7 @@ deserialize_uint32(ConstBuffer<void> &input)
 {
     uint32_t value;
     DeserializeT(input, value);
-    return FromBE32(*(const uint32_t *)input.data);
+    return FromBE32(value);
 }
 
 uint64_t
@@ -153,7 +153,7 @@ deserialize_uint64(ConstBuffer<void> &input)
 {
     uint64_t value;
     DeserializeT(input, value);
-    return FromBE64(*(const uint64_t *)input.data);
+    return FromBE64(value);
 }
 
 const char *