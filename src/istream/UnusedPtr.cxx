18b69551ab412e8c25d17e184737eb686ab17c20
Max Kellermann <mk@cm4all.com>
2017-10-11 11:50:13 +0200

    istream/UnusedPtr: reduce includes

--- src/istream/UnusedPtr.cxx
+++ src/istream/UnusedPtr.cxx
@@ -30,53 +30,11 @@
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef BENG_PROXY_UNUSED_ISTREAM_PTR_HXX
-#define BENG_PROXY_UNUSED_ISTREAM_PTR_HXX
+#include "UnusedPtr.hxx"
+#include "istream.hxx"
 
-#include <utility>
-
-class Istream;
-
-/**
- * This class holds a pointer to an unused #Istream and auto-closes
- * it.  It can be moved to other instances, until it is finally
- * "stolen" using Steal() to actually use it.
- */
-class UnusedIstreamPtr {
-    Istream *stream = nullptr;
-
-public:
-    UnusedIstreamPtr() = default;
-
-    explicit UnusedIstreamPtr(Istream *_stream)
-        :stream(_stream) {}
-
-    UnusedIstreamPtr(UnusedIstreamPtr &&src)
-        :stream(std::exchange(src.stream, nullptr)) {}
-
-    ~UnusedIstreamPtr() {
-        if (stream != nullptr)
-            Close(*stream);
-    }
-
-    UnusedIstreamPtr &operator=(UnusedIstreamPtr &&src) {
-        using std::swap;
-        swap(stream, src.stream);
-        return *this;
-    }
-
-    Istream *Steal() {
-        return std::exchange(stream, nullptr);
-    }
-
-    void Clear() {
-        auto *s = Steal();
-        if (s != nullptr)
-            Close(*s);
-    }
-
-private:
-    static void Close(Istream &i);
-};
-
-#endif
+void
+UnusedIstreamPtr::Close(Istream &i)
+{
+    i.Close();
+}
