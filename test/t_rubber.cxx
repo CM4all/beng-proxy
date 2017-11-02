1cf0504b32b077ea28647e49cb7fb2f2b60eba84
Max Kellermann <mk@cm4all.com>
2017-11-02 16:35:36 +0100

    test/t_rubber: call Rubber methods directly

--- test/t_rubber.cxx
+++ test/t_rubber.cxx
@@ -59,15 +59,15 @@ Check(const void *_p, size_t length, unsigned seed)
 }
 
 static void
-FillRubber(Rubber *r, unsigned id, size_t length)
+FillRubber(Rubber &r, unsigned id, size_t length)
 {
-    Fill(rubber_write(r, id), length, id);
+    Fill(r.Write(id), length, id);
 }
 
 static unsigned
-AddFillRubber(Rubber *r, size_t length)
+AddFillRubber(Rubber &r, size_t length)
 {
-    unsigned id = rubber_add(r, length);
+    unsigned id = r.Add(length);
     if (id != 0)
         FillRubber(r, id, length);
 
@@ -76,43 +76,42 @@ AddFillRubber(Rubber *r, size_t length)
 
 gcc_pure
 static bool
-CheckRubber(Rubber *r, unsigned id, size_t length)
+CheckRubber(Rubber &r, unsigned id, size_t length)
 {
-    return Check(rubber_read(r, id), length, id);
+    return Check(r.Read(id), length, id);
 }
 
 TEST(RubberTest, Basic)
 {
     size_t total = 4 * 1024 * 1024;
 
-    Rubber *r = rubber_new(total);
-    ASSERT_NE(r, nullptr);
+    Rubber r(total);
 
-    total = rubber_get_max_size(r);
+    total = r.GetMaxSize();
 
     /* fill the whole "rubber" object with four quarters */
 
     unsigned a = AddFillRubber(r, total / 4);
     ASSERT_GT(a, 0);
-    ASSERT_EQ(rubber_size_of(r, a), total / 4);
+    ASSERT_EQ(r.GetSizeOf(a), total / 4);
 
     unsigned b = AddFillRubber(r, total / 4);
     ASSERT_GT(b, 0);
-    ASSERT_EQ(rubber_size_of(r, b), total / 4);
+    ASSERT_EQ(r.GetSizeOf(b), total / 4);
 
-    ASSERT_EQ(rubber_get_netto_size(r), total / 2);
-    ASSERT_EQ(rubber_get_brutto_size(r), total / 2);
+    ASSERT_EQ(r.GetNettoSize(), total / 2);
+    ASSERT_EQ(r.GetBruttoSize(), total / 2);
 
     unsigned c = AddFillRubber(r, total / 4);
     ASSERT_GT(c, 0);
-    ASSERT_EQ(rubber_size_of(r, c), total / 4);
+    ASSERT_EQ(r.GetSizeOf(c), total / 4);
 
     unsigned d = AddFillRubber(r, total / 4);
     ASSERT_GT(d, 0);
-    ASSERT_EQ(rubber_size_of(r, d), total / 4);
+    ASSERT_EQ(r.GetSizeOf(d), total / 4);
 
-    ASSERT_EQ(rubber_get_netto_size(r), total);
-    ASSERT_EQ(rubber_get_brutto_size(r), total);
+    ASSERT_EQ(r.GetNettoSize(), total);
+    ASSERT_EQ(r.GetBruttoSize(), total);
 
     /* another allocation must fail */
 
@@ -127,17 +126,17 @@ TEST(RubberTest, Basic)
        rubber_add() call must automatically compress the "rubber"
        object, and the allocation succeeds */
 
-    rubber_remove(r, b);
-    rubber_remove(r, d);
+    r.Remove(b);
+    r.Remove(d);
 
-    ASSERT_EQ(rubber_get_netto_size(r), total / 2);
-    ASSERT_EQ(rubber_get_brutto_size(r), total * 3 / 4);
+    ASSERT_EQ(r.GetNettoSize(), total / 2);
+    ASSERT_EQ(r.GetBruttoSize(), total * 3 / 4);
 
     unsigned e = AddFillRubber(r, total / 2);
     ASSERT_GT(e, 0);
 
-    ASSERT_EQ(rubber_get_netto_size(r), total);
-    ASSERT_EQ(rubber_get_brutto_size(r), total);
+    ASSERT_EQ(r.GetNettoSize(), total);
+    ASSERT_EQ(r.GetBruttoSize(), total);
 
     ASSERT_TRUE(CheckRubber(r, a, total / 4));
     ASSERT_TRUE(CheckRubber(r, c, total / 4));
@@ -146,63 +145,60 @@ TEST(RubberTest, Basic)
     /* remove one after another, and see if rubber results are
        correct */
 
-    rubber_remove(r, a);
+    r.Remove(a);
 
-    ASSERT_EQ(rubber_get_netto_size(r), total * 3 / 4);
-    ASSERT_EQ(rubber_get_brutto_size(r), total);
+    ASSERT_EQ(r.GetNettoSize(), total * 3 / 4);
+    ASSERT_EQ(r.GetBruttoSize(), total);
 
-    rubber_compress(r);
+    r.Compress();
 
-    ASSERT_EQ(rubber_get_netto_size(r), total * 3 / 4);
-    ASSERT_EQ(rubber_get_brutto_size(r), total * 3 / 4);
+    ASSERT_EQ(r.GetNettoSize(), total * 3 / 4);
+    ASSERT_EQ(r.GetBruttoSize(), total * 3 / 4);
     ASSERT_TRUE(CheckRubber(r, c, total / 4));
     ASSERT_TRUE(CheckRubber(r, e, total / 2));
 
-    rubber_remove(r, c);
+    r.Remove(c);
 
-    ASSERT_EQ(rubber_get_netto_size(r), total / 2);
-    ASSERT_EQ(rubber_get_brutto_size(r), total * 3 / 4);
+    ASSERT_EQ(r.GetNettoSize(), total / 2);
+    ASSERT_EQ(r.GetBruttoSize(), total * 3 / 4);
     ASSERT_TRUE(CheckRubber(r, e, total / 2));
 
-    rubber_compress(r);
+    r.Compress();
 
-    ASSERT_EQ(rubber_get_netto_size(r), total / 2);
-    ASSERT_EQ(rubber_get_brutto_size(r), total / 2);
+    ASSERT_EQ(r.GetNettoSize(), total / 2);
+    ASSERT_EQ(r.GetBruttoSize(), total / 2);
     ASSERT_TRUE(CheckRubber(r, e, total / 2));
 
-    rubber_remove(r, e);
+    r.Remove(e);
 
-    ASSERT_EQ(rubber_get_netto_size(r), size_t(0u));
-    ASSERT_EQ(rubber_get_brutto_size(r), size_t(0u));
+    ASSERT_EQ(r.GetNettoSize(), size_t(0u));
+    ASSERT_EQ(r.GetBruttoSize(), size_t(0u));
 
-    rubber_compress(r);
+    r.Compress();
 
-    ASSERT_EQ(rubber_get_netto_size(r), size_t(0u));
-    ASSERT_EQ(rubber_get_brutto_size(r), size_t(0u));
-
-    rubber_free(r);
+    ASSERT_EQ(r.GetNettoSize(), size_t(0u));
+    ASSERT_EQ(r.GetBruttoSize(), size_t(0u));
 }
 
 TEST(RubberTest, Shrink)
 {
     size_t total = 4 * 1024 * 1024;
 
-    Rubber *r = rubber_new(total);
-    ASSERT_NE(r, nullptr);
+    Rubber r(total);
 
-    total = rubber_get_max_size(r);
+    total = r.GetMaxSize();
 
     /* fill the whole "rubber" object */
 
     unsigned a = AddFillRubber(r, total * 3 / 4);
     ASSERT_GT(a, 0);
-    ASSERT_EQ(rubber_size_of(r, a), total * 3 / 4);
+    ASSERT_EQ(r.GetSizeOf(a), total * 3 / 4);
 
     unsigned b = AddFillRubber(r, total / 4);
     ASSERT_GT(b, 0);
 
-    ASSERT_EQ(rubber_get_netto_size(r), total);
-    ASSERT_EQ(rubber_get_brutto_size(r), total);
+    ASSERT_EQ(r.GetNettoSize(), total);
+    ASSERT_EQ(r.GetBruttoSize(), total);
 
     /* another allocation must fail */
 
@@ -213,17 +209,17 @@ TEST(RubberTest, Shrink)
 
     /* shrink the first allocation, try again */
 
-    rubber_shrink(r, a, total / 4);
-    ASSERT_EQ(rubber_size_of(r, a), total / 4);
+    r.Shrink(a, total / 4);
+    ASSERT_EQ(r.GetSizeOf(a), total / 4);
 
-    ASSERT_EQ(rubber_get_netto_size(r), total / 2);
-    ASSERT_EQ(rubber_get_brutto_size(r), total);
+    ASSERT_EQ(r.GetNettoSize(), total / 2);
+    ASSERT_EQ(r.GetBruttoSize(), total);
 
     unsigned c = AddFillRubber(r, total / 2);
     ASSERT_GT(c, 0);
 
-    ASSERT_EQ(rubber_get_netto_size(r), total);
-    ASSERT_EQ(rubber_get_brutto_size(r), total);
+    ASSERT_EQ(r.GetNettoSize(), total);
+    ASSERT_EQ(r.GetBruttoSize(), total);
 
     ASSERT_TRUE(CheckRubber(r, a, total / 4));
     ASSERT_TRUE(CheckRubber(r, b, total / 4));
@@ -231,19 +227,19 @@ TEST(RubberTest, Shrink)
 
     /* shrink the third allocation, verify rubber_compress() */
 
-    rubber_shrink(r, c, total / 4);
+    r.Shrink(c, total / 4);
 
-    ASSERT_EQ(rubber_get_netto_size(r), total * 3 / 4);
-    ASSERT_EQ(rubber_get_brutto_size(r), total);
+    ASSERT_EQ(r.GetNettoSize(), total * 3 / 4);
+    ASSERT_EQ(r.GetBruttoSize(), total);
 
     ASSERT_TRUE(CheckRubber(r, a, total / 4));
     ASSERT_TRUE(CheckRubber(r, b, total / 4));
     ASSERT_TRUE(CheckRubber(r, c, total / 4));
 
-    rubber_compress(r);
+    r.Compress();
 
-    ASSERT_EQ(rubber_get_netto_size(r), total * 3 / 4);
-    ASSERT_EQ(rubber_get_brutto_size(r), total * 3 / 4);
+    ASSERT_EQ(r.GetNettoSize(), total * 3 / 4);
+    ASSERT_EQ(r.GetBruttoSize(), total * 3 / 4);
 
     ASSERT_TRUE(CheckRubber(r, a, total / 4));
     ASSERT_TRUE(CheckRubber(r, b, total / 4));
@@ -251,10 +247,9 @@ TEST(RubberTest, Shrink)
 
     /* clean up */
 
-    rubber_remove(r, a);
-    rubber_remove(r, b);
-    rubber_remove(r, c);
-    rubber_free(r);
+    r.Remove(a);
+    r.Remove(b);
+    r.Remove(c);
 }
 
 /**
@@ -265,19 +260,18 @@ TEST(RubberTest, FullTable)
 {
     size_t total = 64 * 1024 * 1024;
 
-    Rubber *r = rubber_new(total);
-    ASSERT_NE(r, nullptr);
+    Rubber r(total);
 
-    total = rubber_get_max_size(r);
+    total = r.GetMaxSize();
 
     static const size_t max = 300000;
     static unsigned ids[max], n = 0;
     while (n < max) {
-        unsigned id = rubber_add(r, 1);
+        unsigned id = r.Add(1);
         if (id == 0)
             break;
 
-        ASSERT_EQ((size_t)rubber_read(r, id) % 0x10, size_t(0));
+        ASSERT_EQ((size_t)r.Read(id) % 0x10, size_t(0));
 
         ids[n++] = id;
     }
@@ -287,20 +281,18 @@ TEST(RubberTest, FullTable)
 
     /* just to be sure: try again, must still fail */
 
-    ASSERT_EQ(rubber_add(r, 1024 * 1024), 0u);
+    ASSERT_EQ(r.Add(1024 * 1024), 0u);
 
     /* remove one item; now a large allocation must succeed */
 
-    rubber_remove(r, ids[n / 2]);
+    r.Remove(ids[n / 2]);
 
-    unsigned id = rubber_add(r, 1024 * 1024);
+    unsigned id = r.Add(1024 * 1024);
     ASSERT_GT(id, 0);
     ASSERT_EQ(id, ids[n / 2]);
 
     /* cleanup */
 
     for (unsigned i = 0; i < n; ++i)
-        rubber_remove(r, ids[i]);
-
-    rubber_free(r);
+        r.Remove(ids[i]);
 }
