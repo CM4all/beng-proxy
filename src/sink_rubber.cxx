38ed671731d51f7c08f7d162903e73b655c8607b
Max Kellermann <mk@cm4all.com>
2017-11-02 16:27:19 +0100

    sink_rubber: call Rubber methods directly

--- src/sink_rubber.cxx
+++ src/sink_rubber.cxx
@@ -87,7 +87,7 @@ fd_read(FdType type, int fd, void *p, size_t size)
 void
 RubberSink::FailTooLarge()
 {
-    rubber_remove(&rubber, rubber_id);
+    rubber.Remove(rubber_id);
 
     if (input.IsDefined())
         input.ClearAndClose();
@@ -104,10 +104,10 @@ RubberSink::InvokeEof()
     if (position == 0) {
         /* the stream was empty; remove the object from the rubber
            allocator */
-        rubber_remove(&rubber, rubber_id);
+        rubber.Remove(rubber_id);
         rubber_id = 0;
     } else
-        rubber_shrink(&rubber, rubber_id, position);
+        rubber.Shrink(rubber_id, position);
 
     handler.RubberDone(rubber_id, position);
 }
@@ -129,7 +129,7 @@ RubberSink::OnData(const void *data, size_t length)
         return 0;
     }
 
-    uint8_t *p = (uint8_t *)rubber_write(&rubber, rubber_id);
+    uint8_t *p = (uint8_t *)rubber.Write(rubber_id);
     memcpy(p + position, data, length);
     position += length;
 
@@ -163,7 +163,7 @@ RubberSink::OnDirect(FdType type, int fd, size_t max_length)
     if (length > max_length)
         length = max_length;
 
-    uint8_t *p = (uint8_t *)rubber_write(&rubber, rubber_id);
+    uint8_t *p = (uint8_t *)rubber.Write(rubber_id);
     p += position;
 
     ssize_t nbytes = fd_read(type, fd, p, length);
@@ -188,7 +188,7 @@ RubberSink::OnError(std::exception_ptr ep) noexcept
     assert(input.IsDefined());
     input.Clear();
 
-    rubber_remove(&rubber, rubber_id);
+    rubber.Remove(rubber_id);
     handler.RubberError(ep);
 }
 
@@ -200,7 +200,7 @@ RubberSink::OnError(std::exception_ptr ep) noexcept
 void
 RubberSink::Cancel()
 {
-    rubber_remove(&rubber, rubber_id);
+    rubber.Remove(rubber_id);
 
     if (input.IsDefined())
         input.ClearAndClose();
@@ -237,7 +237,7 @@ sink_rubber_new(struct pool &pool, Istream &input,
         ? max_size
         : (size_t)size;
 
-    unsigned rubber_id = rubber_add(&rubber, allocate);
+    unsigned rubber_id = rubber.Add(allocate);
     if (rubber_id == 0) {
         input.CloseUnused();
         handler.RubberOutOfMemory();
