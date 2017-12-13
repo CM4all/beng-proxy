ae95c41985c801a6037e332f85eceb66e7694c77
Max Kellermann <mk@cm4all.com>
2017-12-13 16:54:21 +0100

    access_log/Json: generate JSONL (JSON Lines)
    Because libjsoncpp is unable to omit the newlines after each value, I
    implemented a tiny JSON writing library tailored for this use case.
    
    Implements ticket 0014360.

--- src/access_log/Json.cxx
+++ src/access_log/Json.cxx
@@ -34,144 +34,86 @@
  * An access logger which emits JSON.
  */
 
+#include "JsonWriter.hxx"
 #include "Server.hxx"
 #include "net/ToString.hxx"
-#include "util/StringFormat.hxx"
 
-#include <json/json.h>
-
-#include <iostream>
 #include <functional>
-#include <memory>
-#include <cinttypes>
 
 #include <time.h>
 
-static Json::Value
-ToJson(const ReceivedAccessLogDatagram &d)
+static void
+Dump(JsonWriter::Sink sink, const ReceivedAccessLogDatagram &d)
 {
-    Json::Value root;
+    JsonWriter::Object o(sink);
 
     if (!d.logger_client_address.IsNull() &&
         d.logger_client_address.IsDefined()) {
         char buffer[1024];
         if (ToString(buffer, sizeof(buffer), d.logger_client_address))
-            root["logger_client"] = buffer;
+            o.AddMember("logger_client", buffer);
     }
 
     if (d.valid_timestamp) {
         time_t t = d.timestamp / 1000000;
         char buffer[64];
         strftime(buffer, sizeof(buffer), "%FT%TZ", gmtime(&t));
-        root["time"] = buffer;
+        o.AddMember("time", buffer);
     }
 
     if (d.remote_host != nullptr)
-        root["remote_host"] = d.remote_host;
+        o.AddMember("remote_host", d.remote_host);
 
     if (d.host != nullptr)
-        root["host"] = d.host;
+        o.AddMember("host", d.host);
 
     if (d.site != nullptr)
-        root["site"] = d.site;
+        o.AddMember("site", d.site);
 
     if (d.forwarded_to != nullptr)
-        root["forwarded_to"] = d.forwarded_to;
+        o.AddMember("forwarded_to", d.forwarded_to);
 
     if (d.valid_http_method &&
         http_method_is_valid(d.http_method))
-        root["method"] = http_method_to_string(d.http_method);
+        o.AddMember("method", http_method_to_string(d.http_method));
 
     if (d.http_uri != nullptr)
-        root["uri"] = d.http_uri;
+        o.AddMember("uri", d.http_uri);
 
     if (d.http_referer != nullptr)
-        root["referer"] = d.http_referer;
+        o.AddMember("referer", d.http_referer);
 
     if (d.user_agent != nullptr)
-        root["user_agent"] = d.user_agent;
+        o.AddMember("user_agent", d.user_agent);
 
     if (d.message != nullptr)
-        root["message"] = std::string(d.message.data, d.message.size);
+        o.AddMember("message", d.message);
 
     if (d.valid_http_status)
-        root["status"] = http_status_to_string(d.http_status);
+        o.AddMember("status", http_status_to_string(d.http_status));
 
     if (d.valid_length)
-        root["length"] = StringFormat<64>("%" PRIu64, d.length).c_str();
+        o.AddMember("length", d.length);
 
     if (d.valid_traffic) {
-        root["traffic_received"] = StringFormat<64>("%" PRIu64, d.traffic_received).c_str();
-        root["traffic_sent"] = StringFormat<64>("%" PRIu64, d.traffic_sent).c_str();
+        o.AddMember("traffic_received", d.traffic_received);
+        o.AddMember("traffic_sent", d.traffic_sent);
     }
 
     if (d.valid_duration)
-        root["duration"] = StringFormat<64>("%f", d.duration * 1e-6).c_str();
+        o.AddMember("duration", d.duration * 1e-6);
 
-    return root;
+    o.Flush();
+
+    sink.NewLine();
 }
-
-/**
- * Write a comma, but not if this is the first call.
- *
- * This is used to make the whole process output a valid JSON array.
- * It begins with "[" and ends with "]", and between all request log
- * objects, there is a comma, but not after the last one.
- */
-static void
-MaybeWriteComma()
-{
-    static bool not_first;
-    if (not_first)
-        std::cout << ", ";
-    else
-        not_first = true;
-}
-
-#ifdef JSONCPP_VERSION_MAJOR
-
-static void
-Dump(Json::StreamWriter &writer, const ReceivedAccessLogDatagram &d)
-{
-    MaybeWriteComma();
-    writer.write(ToJson(d), &std::cout);
-    std::cout << std::endl;
-}
-
-#else
-// libjsoncpp 0.6 (Debian Jessie)
-
-static void
-Dump(Json::StyledStreamWriter &writer, const ReceivedAccessLogDatagram &d)
-{
-    MaybeWriteComma();
-    writer.write(std::cout, ToJson(d));
-    std::cout << std::endl;
-}
-
-#endif
 
 int main(int argc, char **argv)
 {
     (void)argc;
     (void)argv;
 
-#ifdef JSONCPP_VERSION_MAJOR
-    Json::StreamWriterBuilder builder;
-    builder["commentStyle"] = "None";
-    builder["indentation"] = "   ";
-
-    std::unique_ptr<Json::StreamWriter> writer(builder.newStreamWriter());
-#else
-    // libjsoncpp 0.6 (Debian Jessie)
-    auto writer = std::make_unique<Json::StyledStreamWriter>();
-#endif
-
-    std::cout << "[" << std::endl;
-
-    AccessLogServer().Run(std::bind(Dump, std::ref(*writer),
-                                     std::placeholders::_1));
-
-    std::cout << "]" << std::endl;
+    AccessLogServer().Run(std::bind(Dump, JsonWriter::Sink(stdout),
+                                    std::placeholders::_1));
     return 0;
 }
---
+++ src/access_log/JsonWriter.hxx
@@ -0,0 +1,185 @@
+/*
+ * Copyright 2007-2017 Content Management AG
+ * All rights reserved.
+ *
+ * author: Max Kellermann <mk@cm4all.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the
+ * distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * FOUNDATION OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include "util/StringView.hxx"
+
+#include <utility>
+#include <cinttypes>
+
+#include <stdio.h>
+
+/**
+ * A very simple JSON writing library.  Everything is written to a
+ * FILE* in one single line, which makes it easy to generate JSONL
+ * (JSON Lines).
+ */
+namespace JsonWriter {
+
+/**
+ * The sink which will receive JSON data.  This is a wrapper for a
+ * FILE* which knows how to write some simple values.
+ */
+class Sink {
+    FILE *file;
+
+public:
+    explicit Sink(FILE *_file) noexcept:file(_file) {}
+
+    void WriteRaw(char ch) noexcept {
+        fputc(ch, file);
+    }
+
+    void WriteRaw(const char *s) noexcept {
+        fputs(s, file);
+    }
+
+    void NewLine() noexcept {
+        WriteRaw('\n');
+        fflush(file);
+    }
+
+private:
+    void WriteStringChar(char ch) noexcept {
+        switch (ch) {
+        case '\\':
+        case '"':
+            WriteRaw('\\');
+            WriteRaw(ch);
+            break;
+
+        case '\n':
+            WriteRaw('\\');
+            WriteRaw('n');
+            break;
+
+        case '\r':
+            WriteRaw('\\');
+            WriteRaw('r');
+            break;
+
+        default:
+            WriteRaw(ch);
+            break;
+        }
+    }
+
+public:
+    void WriteValue(StringView value) noexcept {
+        WriteRaw('"');
+
+        for (const char ch : value)
+            WriteStringChar(ch);
+
+        WriteRaw('"');
+    }
+
+    void WriteValue(const char *value) noexcept {
+        WriteRaw('"');
+
+        while (*value != 0)
+            WriteStringChar(*value++);
+
+        WriteRaw('"');
+    }
+
+    void WriteValue(std::nullptr_t) noexcept {
+        WriteRaw("null");
+    }
+
+    void WriteValue(bool value) noexcept {
+        WriteRaw(value ? "true" : "false");
+    }
+
+    void WriteValue(int value) noexcept {
+        fprintf(file, "%i", value);
+    }
+
+    void WriteValue(unsigned value) noexcept {
+        fprintf(file, "%u", value);
+    }
+
+    void WriteValue(int64_t value) noexcept {
+        fprintf(file, "%" PRIi64, value);
+    }
+
+    void WriteValue(uint64_t value) noexcept {
+        fprintf(file, "%" PRIu64, value);
+    }
+
+    void WriteValue(double value) noexcept {
+        fprintf(file, "%f", value);
+    }
+};
+
+/**
+ * Write an object (dictionary, map).  Call AddMember() for each
+ * member, and call Flush() once to finish this object.
+ */
+class Object {
+    Sink sink;
+
+    bool pending_comma = false;
+
+public:
+    explicit Object(Sink _sink) noexcept:sink(_sink) {
+        sink.WriteRaw('{');
+    }
+
+    Object(const Object &) = delete;
+    Object &operator=(const Object &) = delete;
+
+    Sink AddMember(const char *name) noexcept {
+        if (pending_comma) {
+            sink.WriteRaw(',');
+            pending_comma = false;
+        }
+
+        sink.WriteValue(name);
+        sink.WriteRaw(':');
+        pending_comma = true;
+
+        return sink;
+    }
+
+    template<typename T>
+    void AddMember(const char *name, T &&value) noexcept {
+        AddMember(name).WriteValue(std::forward<T>(value));
+    }
+
+    void Flush() noexcept {
+        sink.WriteRaw('}');
+    }
+};
+
+} // namespace JsonWriter
