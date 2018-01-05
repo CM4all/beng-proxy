fb077627cf7d6b2205865f5b9f0090fd687caa43
Max Kellermann <mk@cm4all.com>
2018-01-05 12:35:20 +0100

    certdb/Progress: un-inline operator()()

--- src/certdb/Progress.cxx
+++ src/certdb/Progress.cxx
@@ -30,67 +30,16 @@
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef PROGRESS_HXX
-#define PROGRESS_HXX
+#include "Progress.hxx"
 
-#include "util/Compiler.h"
+#include <stdio.h>
 
-#include <algorithm>
+void
+WorkshopProgress::operator()(int _value) noexcept
+{
+    if (!IsEnabled())
+        return;
 
-/**
- * An interface to Workshop job progress reporting.
- */
-class WorkshopProgress {
-    unsigned min = 0, max = 0;
-
-public:
-    WorkshopProgress() = default;
-
-    constexpr WorkshopProgress(unsigned _min, unsigned _max)
-        :min(_min), max(_max) {}
-
-    constexpr WorkshopProgress(WorkshopProgress parent,
-                               unsigned _min, unsigned _max)
-        :min(parent.Scale(_min)), max(parent.Scale(_max)) {}
-
-    bool IsEnabled() const {
-        return min < max;
-    }
-
-    void operator()(int value) noexcept;
-
-private:
-#if !GCC_OLDER_THAN(5,0)
-    constexpr
-#endif
-    static unsigned Clamp(int x) {
-        return std::min(100u, (unsigned)std::max(0, x));
-    }
-
-    constexpr unsigned Scale(unsigned x) const {
-        return (min * (100u - x) + max * x) / 100u;
-    }
-};
-
-/**
- * A simple wrapper for #WorkshopProgress which counts up to a
- * predefined number of steps.
- */
-class StepProgress {
-    WorkshopProgress parent;
-
-    const unsigned n;
-
-    unsigned i = 0;
-
-public:
-    StepProgress(WorkshopProgress _parent, unsigned _n)
-        :parent(_parent), n(_n) {}
-
-    void operator()() {
-        ++i;
-        parent(i * 100u / n);
-    }
-};
-
-#endif
+    const unsigned value = Scale(Clamp(_value));
+    printf("%u\n", value);
+}
