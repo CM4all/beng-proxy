CM4all/beng-proxy

commit 872fd6f07fb22c96f342dbc178f36a620ea5d406

bp/{Xml,Css}Processor: call destructor

src/bp/XmlProcessor.cxx
@@ -266,6 +266,16 @@ private:
         return (options & PROCESSOR_STYLE) != 0;
     }
 
+    void Destroy() noexcept {
+        DeleteFromPool(pool, this);
+    }
+
+    void Close() noexcept {
+        auto *parser2 = parser;
+        Destroy();
+        parser_close(parser2);
+    }
+
     void Replace(off_t start, off_t end, UnusedIstreamPtr istream) noexcept {
         istream_replace_add(*replace, start, end, std::move(istream));
     }
@@ -375,7 +385,7 @@ XmlProcessor::Cancel() noexcept
     pool_unref(&container.pool);
     pool_unref(&caller_pool);
 
-    parser_close(parser);
+    Close();
 }
 
 /*
@@ -1261,7 +1271,7 @@ XmlProcessor::EmbedWidget(Widget &child_widget) noexcept
         auto &widget_pool = container.pool;
         auto &handler2 = *handler;
 
-        parser_close(parser);
+        Close();
 
         try {
             child_widget.CopyFromRequest();
@@ -1503,6 +1513,8 @@ XmlProcessor::OnXmlEof(gcc_unused off_t length) noexcept
     }
 
     pool_unref(&widget_pool);
+
+    Destroy();
 }
 
 void
@@ -1524,4 +1536,6 @@ XmlProcessor::OnXmlError(std::exception_ptr ep) noexcept
     }
 
     pool_unref(&widget_pool);
+
+    Destroy();
 }

src/bp/CssProcessor.cxx
@@ -74,6 +74,10 @@ struct CssProcessor {
                  Widget &_container,
                  struct processor_env &_env,
                  unsigned _options);
+
+    void Destroy() noexcept {
+        DeleteFromPool(pool, this);
+    }
 };
 
 static inline bool
@@ -258,6 +262,8 @@ css_processor_parser_eof(void *ctx, off_t length gcc_unused)
     processor->parser = nullptr;
 
     istream_replace_finish(processor->replace);
+
+    processor->Destroy();
 }
 
 static void
@@ -268,6 +274,8 @@ css_processor_parser_error(std::exception_ptr, void *ctx)
     assert(processor->parser != nullptr);
 
     processor->parser = nullptr;
+
+    processor->Destroy();
 }
 
 static constexpr CssParserHandler css_processor_parser_handler = {

commit 280da0ed51acc74fb649888232d2ead165efe0cf

bp/XmlProcessor: copy caller_pool reference before destroying the XmlProcessor
Fixes use-after-free bug caused by
872fd6f07fb22c96f342dbc178f36a620ea5d406

src/bp/XmlProcessor.cxx
@@ -1267,7 +1267,7 @@ XmlProcessor::EmbedWidget(Widget &child_widget) noexcept
         return istream;
     } else if (child_widget.id != nullptr &&
                strcmp(lookup_id, child_widget.id) == 0) {
-        auto &widget_pool = container.pool;
+        auto &_caller_pool = caller_pool, &widget_pool = container.pool;
         auto &handler2 = *handler;
 
         Close();
@@ -1280,7 +1280,7 @@ XmlProcessor::EmbedWidget(Widget &child_widget) noexcept
             handler2.WidgetLookupError(std::current_exception());
         }
 
-        pool_unref(&caller_pool);
+        pool_unref(&_caller_pool);
         pool_unref(&widget_pool);
 
         return nullptr;