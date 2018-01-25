CM4all/beng-proxy

commit 3a7a737892172814580f6365648065a943c00ee2

ForwardRequest: store request body in a UnusedIstreamPtr

src/bp/ForwardRequest.cxx
@@ -39,14 +39,14 @@ ForwardRequest
 request_forward(Request &request2,
                 const struct header_forward_settings &header_forward,
                 const char *host_and_port, const char *uri,
-                bool exclude_host)
+                bool exclude_host) noexcept
 {
     const auto &request = request2.request;
 
     assert(!request.HasBody() || request2.request_body);
 
     http_method_t method;
-    Istream *body;
+    UnusedIstreamPtr body;
 
     /* send a request body? */
 
@@ -55,12 +55,11 @@ request_forward(Request &request2,
            convert this request to a GET */
 
         method = HTTP_METHOD_GET;
-        body = nullptr;
     } else {
         /* forward body (if any) to the real server */
 
         method = request.method;
-        body = request2.request_body.Steal();
+        body = std::move(request2.request_body);
     }
 
     /* generate request headers */
@@ -70,7 +69,7 @@ request_forward(Request &request2,
                                                   request.local_host_and_port,
                                                   request.remote_host,
                                                   exclude_host,
-                                                  body != nullptr,
+                                                  body,
                                                   !request2.IsProcessorEnabled(),
                                                   !request2.IsTransformationEnabled(),
                                                   !request2.IsTransformationEnabled(),
@@ -78,5 +77,5 @@ request_forward(Request &request2,
                                                   request2.session_cookie,
                                                   request2.GetRealmSession().get(),
                                                   host_and_port, uri),
-                          body);
+                          std::move(body));
 }

src/bp/ForwardRequest.hxx
@@ -39,6 +39,7 @@
 
 #include "strmap.hxx"
 #include "http/Method.h"
+#include "istream/UnusedPtr.hxx"
 
 class StringMap;
 struct header_forward_settings;
@@ -50,17 +51,18 @@ struct ForwardRequest {
 
     StringMap headers;
 
-    Istream *body;
+    UnusedIstreamPtr body;
 
     ForwardRequest(http_method_t _method, StringMap &&_headers,
-                   Istream *_body)
-        :method(_method), headers(std::move(_headers)), body(_body) {}
+                   UnusedIstreamPtr _body) noexcept
+        :method(_method), headers(std::move(_headers)),
+         body(std::move(_body)) {}
 };
 
 ForwardRequest
 request_forward(Request &src,
                 const struct header_forward_settings &header_forward,
                 const char *host_and_port, const char *uri,
-                bool exclude_host);
+                bool exclude_host) noexcept;
 
 #endif

src/bp/ProxyHandler.cxx
@@ -125,9 +125,9 @@ proxy_handler(Request &request2)
                                    address.IsAnyHttp());
 
 #ifdef SPLICE
-    if (forward.body != nullptr)
-        forward.body = istream_pipe_new(&pool, *forward.body,
-                                        request2.instance.pipe_stock);
+    if (forward.body)
+        forward.body = UnusedIstreamPtr(istream_pipe_new(&pool, *forward.body.Steal(),
+                                                         request2.instance.pipe_stock));
 #endif
 
     for (const auto &i : tr.request_headers)
@@ -139,6 +139,8 @@ proxy_handler(Request &request2)
         ->SendRequest(pool,
                       request2.session_id.GetClusterHash(),
                       forward.method, address, HTTP_STATUS_OK,
-                      std::move(forward.headers), forward.body, nullptr,
+                      std::move(forward.headers),
+                      forward.body.Steal(),
+                      nullptr,
                       request2, request2.cancel_ptr);
 }

commit af0c371b68ab72bb132203c1f16e186ed8797edc

bp/ForwardRequest: check request body presence before moving it
Commit 3a7a7378921 broke forwarding headers for the request body by
always passing "false" to forward_request_headers().  That is due to
C++'s parameter evaluation order (right to left) - the std::move(body)
to ForwardRequest() happens before the "body" gets evaluated for the
call thi forward_request_headers().

This commit makes the evaluation order explicit by moving the check
out of the expression into a local variable.

src/bp/ForwardRequest.cxx
@@ -64,12 +64,14 @@ request_forward(Request &request2,
 
     /* generate request headers */
 
+    const bool has_body = body;
+
     return ForwardRequest(method,
                           forward_request_headers(request2.pool, request.headers,
                                                   request.local_host_and_port,
                                                   request.remote_host,
                                                   exclude_host,
-                                                  body,
+                                                  has_body,
                                                   !request2.IsProcessorEnabled(),
                                                   !request2.IsTransformationEnabled(),
                                                   !request2.IsTransformationEnabled(),