b1d2657696d56a8e1b261672164b024367dd7783
Max Kellermann <mk@cm4all.com>
2018-01-31 10:03:54 +0100

    istream/delayed: implement _FillBucketList()

--- src/istream/DelayedIstream.cxx
+++ src/istream/DelayedIstream.cxx
@@ -33,6 +33,7 @@
 #include "DelayedIstream.hxx"
 #include "ForwardIstream.hxx"
 #include "UnusedPtr.hxx"
+#include "Bucket.hxx"
 #include "event/DeferEvent.hxx"
 
 #include <assert.h>
@@ -91,6 +92,18 @@ public:
     void _Read() noexcept override {
         if (HasInput())
             ForwardIstream::_Read();
+    }
+
+    void _FillBucketList(IstreamBucketList &list) override {
+        if (HasInput()) {
+            try {
+                input.FillBucketList(list);
+            } catch (...) {
+                Destroy();
+                throw;
+            }
+        } else
+            list.SetMore();
     }
 
     int _AsFd() noexcept override {
