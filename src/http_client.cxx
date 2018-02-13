CM4all/beng-proxy

commit 9fead555f85d9ada9588367a3761b806b764864e

http_client: use class PoolNotify instead of accessing destructed instances

src/http_client.cxx
@@ -238,14 +238,6 @@ struct HttpClient final : BufferedSocketHandler, IstreamHandler, Cancellable {
     }
 
     /**
-     * @return false if the #HttpClient has been destructed
-     */
-    gcc_pure
-    bool IsValid() const {
-        return socket.IsValid();
-    }
-
-    /**
      * @return false if the #HttpClient has released the socket
      */
     gcc_pure
@@ -859,12 +851,12 @@ HttpClient::FeedBody(const void *data, size_t length)
     size_t nbytes;
 
     {
-        const ScopePoolRef ref(GetPool() TRACE_ARGS);
+        const PoolNotify notify(GetPool());
         nbytes = response_body_reader.FeedBody(data, length);
         if (nbytes == 0)
-            return IsValid()
-                ? BufferedResult::BLOCKING
-                : BufferedResult::CLOSED;
+            return notify.IsDestroyed()
+                ? BufferedResult::CLOSED
+                : BufferedResult::BLOCKING;
     }
 
     socket.Consumed(nbytes);
@@ -950,7 +942,7 @@ HttpClient::FeedHeaders(const void *data, size_t length)
            need in the input buffer */
         ReleaseSocket(keep_alive);
 
-    const ScopePoolRef ref(GetPool() TRACE_ARGS);
+    const PoolNotify notify(GetPool());
     const ScopePoolRef caller_ref(caller_pool TRACE_ARGS);
 
     if (!response.body && !response.no_body)
@@ -960,10 +952,10 @@ HttpClient::FeedHeaders(const void *data, size_t length)
     request.handler.InvokeResponse(response.status,
                                    std::move(response.headers),
                                    std::move(response.body));
-    response.in_handler = false;
-
-    if (!IsValid())
+    if (notify.IsDestroyed())
         return BufferedResult::CLOSED;
+
+    response.in_handler = false;
 
     if (response.state == Response::State::END) {
         http_client_response_finished(this);
@@ -1114,11 +1106,11 @@ HttpClient::OnBufferedWrite()
         return false;
     }
 
-    const ScopePoolRef ref(GetPool() TRACE_ARGS);
+    const PoolNotify notify(GetPool());
 
     request.istream.Read();
 
-    const bool result = IsValid() && IsConnected();
+    const bool result = !notify.IsDestroyed() && IsConnected();
     if (result && request.istream.IsDefined()) {
         if (request.got_data)
             ScheduleWrite();

commit 239dd0d85e966957164952dabcd3f83295000af8

http_client: use DestructObserver instead of PoolNotify
Fixes a use-after-free bug caused by commit
9fead555f85d9ada9588367a3761b806b764864e because the PoolNotify
doesn't check whether the HttpClient is destroyed; the pool may live
longer than the HttpClient.

src/http_client.cxx
@@ -60,6 +60,7 @@
 #include "util/Cancellable.hxx"
 #include "util/Cast.hxx"
 #include "util/CharUtil.hxx"
+#include "util/DestructObserver.hxx"
 #include "util/StringUtil.hxx"
 #include "util/StringView.hxx"
 #include "util/StringFormat.hxx"
@@ -98,7 +99,7 @@ static constexpr struct timeval http_client_timeout = {
     .tv_usec = 0,
 };
 
-struct HttpClient final : BufferedSocketHandler, IstreamHandler, Cancellable {
+struct HttpClient final : BufferedSocketHandler, IstreamHandler, Cancellable, DestructAnchor {
     enum class BucketResult {
         MORE,
         BLOCKING,
@@ -851,10 +852,10 @@ HttpClient::FeedBody(const void *data, size_t length)
     size_t nbytes;
 
     {
-        const PoolNotify notify(GetPool());
+        const DestructObserver destructed(*this);
         nbytes = response_body_reader.FeedBody(data, length);
         if (nbytes == 0)
-            return notify.IsDestroyed()
+            return destructed
                 ? BufferedResult::CLOSED
                 : BufferedResult::BLOCKING;
     }
@@ -942,7 +943,7 @@ HttpClient::FeedHeaders(const void *data, size_t length)
            need in the input buffer */
         ReleaseSocket(keep_alive);
 
-    const PoolNotify notify(GetPool());
+    const DestructObserver destructed(*this);
     const ScopePoolRef caller_ref(caller_pool TRACE_ARGS);
 
     if (!response.body && !response.no_body)
@@ -952,7 +953,7 @@ HttpClient::FeedHeaders(const void *data, size_t length)
     request.handler.InvokeResponse(response.status,
                                    std::move(response.headers),
                                    std::move(response.body));
-    if (notify.IsDestroyed())
+    if (destructed)
         return BufferedResult::CLOSED;
 
     response.in_handler = false;
@@ -1106,11 +1107,11 @@ HttpClient::OnBufferedWrite()
         return false;
     }
 
-    const PoolNotify notify(GetPool());
+    const DestructObserver destructed(*this);
 
     request.istream.Read();
 
-    const bool result = !notify.IsDestroyed() && IsConnected();
+    const bool result = !destructed && IsConnected();
     if (result && request.istream.IsDefined()) {
         if (request.got_data)
             ScheduleWrite();