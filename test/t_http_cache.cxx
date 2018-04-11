d63e9578664c8515b2b092a8cf1773adb333b8d9
Max Kellermann <mk@cm4all.com>
2018-04-11 13:07:23 +0200

    test/t_http_cache: make requests static

--- test/t_http_cache.cxx
+++ test/t_http_cache.cxx
@@ -100,7 +100,7 @@ struct Request final : IstreamHandler {
 #define STAMP2 "Fri, 20 Jan 2009 08:53:30 GMT"
 #define EXPIRES "Fri, 20 Jan 2029 08:53:30 GMT"
 
-Request requests[] = {
+static Request requests[] = {
     { "/foo", nullptr,
       "date: " DATE "\n"
       "last-modified: " STAMP1 "\n"
