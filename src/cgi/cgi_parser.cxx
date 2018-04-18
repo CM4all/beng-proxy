CM4all/beng-proxy

commit 16a029ec8d2544b2e33b55e6fc4c1100f626ef07

cgi/parser: embed StringMap, no pointer

src/cgi/cgi_parser.cxx
@@ -6,7 +6,6 @@
 
 #include "cgi_parser.hxx"
 #include "cgi_quark.h"
-#include "strmap.hxx"
 #include "header_parser.hxx"
 #include "util/ForeignFifoBuffer.hxx"
 #include "util/StringUtil.hxx"
@@ -16,7 +15,7 @@
 #include <stdlib.h>
 
 CGIParser::CGIParser(struct pool &pool)
-    :headers(strmap_new(&pool))
+    :headers(pool)
 {
 }
 
@@ -28,7 +27,7 @@ inline enum completion
 CGIParser::Finish(ForeignFifoBuffer<uint8_t> &buffer, GError **error_r)
 {
     /* parse the status */
-    const char *p = headers->Remove("status");
+    const char *p = headers.Remove("status");
     if (p != nullptr) {
         int i = atoi(p);
         if (http_status_is_valid((http_status_t)i))
@@ -39,7 +38,7 @@ CGIParser::Finish(ForeignFifoBuffer<uint8_t> &buffer, GError **error_r)
         /* there cannot be a response body */
         remaining = 0;
     } else {
-        p = headers->Remove("content-length");
+        p = headers.Remove("content-length");
         if (p != nullptr) {
             /* parse the Content-Length response header */
             char *endptr;
@@ -91,7 +90,7 @@ CGIParser::FeedHeaders(struct pool &pool, ForeignFifoBuffer<uint8_t> &buffer,
             return Finish(buffer, error_r);
         }
 
-        header_parse_line(pool, headers, {start, line_length});
+        header_parse_line(pool, &headers, {start, line_length});
 
         start = next;
     }

src/cgi/cgi_parser.hxx
@@ -7,6 +7,7 @@
 #ifndef BENG_PROXY_CGI_PARSER_HXX
 #define BENG_PROXY_CGI_PARSER_HXX
 
+#include "strmap.hxx"
 #include "completion.h"
 #include "glibfwd.hxx"
 
@@ -44,11 +45,9 @@ struct CGIParser {
      */
     off_t remaining = -1;
 
-    StringMap *headers;
+    StringMap headers;
 
-#ifndef NDEBUG
     bool finished = false;
-#endif
 
     CGIParser(struct pool &pool);
 
@@ -56,9 +55,7 @@ struct CGIParser {
      * Did the parser finish reading the response headers?
      */
     bool AreHeadersFinished() const {
-        assert(finished == (headers == nullptr));
-
-        return headers == nullptr;
+        return finished;
     }
 
     /**
@@ -82,12 +79,9 @@ struct CGIParser {
     }
 
     StringMap &GetHeaders() {
-        assert(headers != nullptr);
         assert(finished);
 
-        StringMap *_headers = headers;
-        headers = nullptr;
-        return *_headers;
+        return headers;
     }
 
     bool KnownLength() const {

commit f989b638f1f2ef56f1ea1de3f951e6349b9e298c

cgi/parser: set "finished" flag in NDEBUG build
Fixes crash bug, regression from commit
16a029ec8d2544b2e33b55e6fc4c1100f626ef07

src/cgi/cgi_parser.cxx
@@ -75,9 +75,7 @@ CGIParser::Finish(ForeignFifoBuffer<uint8_t> &buffer)
     if (IsTooMuch(buffer.GetAvailable()))
         throw CgiError("too much data from CGI script");
 
-#ifndef NDEBUG
     finished = true;
-#endif
     return Completion::DONE;
 }
 