CM4all/beng-proxy

commit 7aed1ad61fc97037b101854a0ed9775d2d99e547

istream/Pointer: convert Replace() to template

src/istream/Pointer.hxx
@@ -114,13 +114,13 @@ public:
         stream->SetHandler(handler, direct);
     }
 
-    void Replace(Istream &_stream,
+    template<typename I>
+    void Replace(I &&_stream,
                  IstreamHandler &handler,
                  FdTypeMask direct=0) noexcept {
         Close();
 
-        stream = &_stream;
-        stream->SetHandler(handler, direct);
+        Set(std::forward<I>(_stream), handler, direct);
     }
 
     void SetDirect(FdTypeMask direct) noexcept {

src/istream/FacadeIstream.hxx
@@ -50,10 +50,11 @@ protected:
         input.SetDirect(GetHandlerDirect());
     }
 
-    void ReplaceInputDirect(Istream &_input) noexcept {
+    template<typename I>
+    void ReplaceInputDirect(I &&_input) noexcept {
         assert(input.IsDefined());
 
-        input.Replace(_input, *this, GetHandlerDirect());
+        input.Replace(std::forward<I>(_input), *this, GetHandlerDirect());
     }
 };
 

commit b547d013a6bae95dccf13161a204781351150c47

istream/Pointer: work around assertion failure in Replace()
This assertion failure can be triggered in the HTTP client when the
server starts sending the response without "100 Continue".

Regression from commit 7aed1ad61fc97037b101854a0ed9775d2d99e547

src/istream/Pointer.hxx
@@ -105,6 +105,12 @@ public:
                  FdTypeMask direct=0) noexcept {
         Close();
 
+#ifndef NDEBUG
+        /* must clear the pointer in the debug build to avoid
+           assertion failure in Set() */
+        Clear();
+#endif
+
         Set(std::forward<I>(_stream), handler, direct);
     }
 