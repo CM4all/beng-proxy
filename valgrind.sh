5b0be948eadea54ee86002da7700d77e45a683e9
Max Kellermann <mk@cm4all.com>
2018-06-02 12:06:39 +0200

    valgrind.sh: forward parameters

--- valgrind.sh
+++ valgrind.sh
@@ -8,4 +8,4 @@ VALGRIND_FLAGS="--leak-check=yes --show-reachable=yes"
 VALGRIND_FLAGS="$VALGRIND_FLAGS --track-fds=yes --track-origins=yes"
 VALGRIND_FLAGS="$VALGRIND_FLAGS --suppressions=valgrind.suppressions"
 
-exec valgrind $VALGRIND_FLAGS ./output/debug/cm4all-beng-proxy -vvvvv
+exec valgrind $VALGRIND_FLAGS ./output/debug/cm4all-beng-proxy -vvvvv "$@"
