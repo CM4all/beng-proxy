07bbaaa08d1405b08a45a6632a6f6ae37eda26cc
Max Kellermann <mk@cm4all.com>
2018-09-19 18:14:03 +0200

    pool/StringBuilder: new class

---
+++ src/pool/StringBuilder.hxx
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2007-2018 Content Management AG
+ * All rights reserved.
+ *
+ * author: Max Kellermann <mk@cm4all.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the
+ * distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * FOUNDATION OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include "pool.hxx"
+#include "util/StaticArray.hxx"
+#include "util/StringView.hxx"
+
+template<size_t MAX>
+class PoolStringBuilder {
+    StaticArray<StringView, MAX> items;
+
+public:
+    void push_back(StringView s) noexcept {
+        items.push_back(s);
+    }
+
+    template<typename... Args>
+    void emplace_back(Args&&... args) noexcept {
+        items.emplace_back(std::forward<Args>(args)...);
+    }
+
+    char *operator()(struct pool &p) const noexcept {
+        return StringConcat(p, {items.raw(), items.size()});
+    }
+};
