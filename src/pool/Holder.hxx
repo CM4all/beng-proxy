d0165da31b55cefd847018f911760b1adcb42ee0
Max Kellermann <mk@cm4all.com>
2018-10-18 15:49:35 +0200

    pool/Holder: more API documentation

--- src/pool/Holder.hxx
+++ src/pool/Holder.hxx
@@ -36,6 +36,14 @@
 
 /**
  * Base class for classes which hold a reference to a #pool.
+ *
+ * This works around an ordering problem with class #PoolPtr: if you
+ * instead use #PoolPtr as an attribute inside a class allocated with
+ * this pool, its destructor will be called before destruction of the
+ * whole object has finished, leading to use-after-free bugs.  By
+ * deriving from this class, you are able to call its destructor at
+ * the very end.  For this to happen, this must be the innermost and
+ * first subclass.
  */
 class PoolHolder {
 protected:
