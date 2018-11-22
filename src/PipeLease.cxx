CM4all/beng-proxy

commit 80c7d4464cdc7acdfcd9e2d5cb91c89933e859e3

PipeLease: don't use array for pipe descriptors
An array is difficult to initialize, and using separate variables
makes the code more readable.

src/PipeLease.cxx
@@ -48,12 +48,13 @@ PipeLease::Release(bool reuse) noexcept
         assert(item != nullptr);
         item->Put(!reuse);
 
-        for (auto &fd : fds)
-            fd.SetUndefined();
+        read_fd.SetUndefined();
+        write_fd.SetUndefined();
     } else {
-        for (auto &fd : fds)
-            if (fd.IsDefined())
-                fd.Close();
+        if (read_fd.IsDefined())
+            read_fd.Close();
+        if (write_fd.IsDefined())
+            write_fd.Close();
     }
 }
 
@@ -66,9 +67,13 @@ PipeLease::Create(struct pool &pool)
         assert(item == nullptr);
 
         item = stock->GetNow(pool, nullptr);
+
+        FileDescriptor fds[2];
         pipe_stock_item_get(item, fds);
+        read_fd = fds[0];
+        write_fd = fds[0];
     } else {
-        if (!FileDescriptor::CreatePipeNonBlock(fds[0], fds[1]))
+        if (!FileDescriptor::CreatePipeNonBlock(read_fd, write_fd))
             throw MakeErrno("pipe() failed");
     }
 }

src/PipeLease.hxx
@@ -42,7 +42,8 @@ struct StockItem;
 class PipeLease {
     Stock *stock;
     StockItem *item = nullptr;
-    FileDescriptor fds[2]{FileDescriptor::Undefined(), FileDescriptor::Undefined()};
+    FileDescriptor read_fd = FileDescriptor::Undefined();
+    FileDescriptor write_fd = FileDescriptor::Undefined();
 
 public:
     explicit PipeLease(Stock *_stock) noexcept
@@ -50,21 +51,20 @@ public:
 
     PipeLease(PipeLease &&src) noexcept
         :stock(src.stock), item(std::exchange(src.item, nullptr)),
-         fds(src.fds) {
-        for (auto &fd : src.fds)
-            fd.SetUndefined();
-    }
+         read_fd(std::exchange(src.read_fd, FileDescriptor::Undefined())),
+         write_fd(std::exchange(src.write_fd, FileDescriptor::Undefined())) {}
 
     PipeLease &operator=(PipeLease &&src) noexcept {
         using std::swap;
         swap(stock, src.stock);
         swap(item, src.item);
-        swap(fds, src.fds);
+        swap(read_fd, src.read_fd);
+        swap(write_fd, src.write_fd);
         return *this;
     }
 
     bool IsDefined() const noexcept {
-        return fds[0].IsDefined();
+        return read_fd.IsDefined();
     }
 
     /**
@@ -90,15 +90,15 @@ public:
     }
 
     void CloseWriteIfNotStock() noexcept {
-        if (item == nullptr && fds[1].IsDefined())
-            fds[1].Close();
+        if (item == nullptr && write_fd.IsDefined())
+            write_fd.Close();
     }
 
     FileDescriptor GetReadFd() noexcept {
-        return fds[0];
+        return read_fd;
     }
 
     FileDescriptor GetWriteFd() noexcept {
-        return fds[1];
+        return write_fd;
     }
 };

commit c524145a06cfe39e44f1fc6b3cdf5ef30779aa2f

PipeLease: fix EBADFD due to typo
Regression by commit 80c7d4464cdc7acdfcd9e2d5cb91c89933e859e3, d'oh!

src/PipeLease.cxx
@@ -71,7 +71,7 @@ PipeLease::Create(struct pool &pool)
         FileDescriptor fds[2];
         pipe_stock_item_get(item, fds);
         read_fd = fds[0];
-        write_fd = fds[0];
+        write_fd = fds[1];
     } else {
         if (!FileDescriptor::CreatePipeNonBlock(read_fd, write_fd))
             throw MakeErrno("pipe() failed");