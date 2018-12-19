f5befc618b81f8c300547b289ecbfd1cce347713
Max Kellermann <mk@cm4all.com>
2018-12-19 12:42:02 +0100

    pipe_stock: disable expensive assert()s
    These were added 8 years ago in commit 105313e5d726aa4 but havn't
    caught anything in a long time.  They're expensive because they invoke
    the fcntl() system call.

--- src/pipe_stock.cxx
+++ src/pipe_stock.cxx
@@ -96,18 +96,12 @@ PipeStock::Create(CreateStockItem c,
 bool
 PipeStockItem::Borrow() noexcept
 {
-    assert(fds[0].IsValid());
-    assert(fds[1].IsValid());
-
     return true;
 }
 
 bool
 PipeStockItem::Release() noexcept
 {
-    assert(fds[0].IsValid());
-    assert(fds[1].IsValid());
-
     return true;
 }
 
