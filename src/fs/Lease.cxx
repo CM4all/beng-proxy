a0c12b92873a69d4a66f93d14816e1a12e9d1ec5
Max Kellermann <mk@cm4all.com>
2018-12-28 22:55:32 +0100

    fs/Lease: read from remaining buffer after socket was closed&released
    This fixes a stall bug: if the socket is closed by the peer and our
    client releases the socket lease in the OnBufferedClosed() callback,
    nobody will ever request reading again.
    
    This commit adds a ReadReleased() call right after
    OnBufferedRemaining().

--- src/fs/Lease.cxx
+++ src/fs/Lease.cxx
@@ -219,6 +219,7 @@ FilteredSocketLease::OnBufferedClosed() noexcept
         result = false;
 
         if (handler.OnBufferedRemaining(GetAvailable()) &&
+            ReadReleased() &&
             IsReleasedEmpty() &&
             !handler.OnBufferedEnd())
             handler.OnBufferedError(std::make_exception_ptr(SocketClosedPrematurelyError()));
