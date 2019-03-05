93a4ebb269d4b3ad6e7c00a6cb47bbab2cca32a9
Max Kellermann <mk@cm4all.com>
2019-03-05 22:02:29 +0100

    istream/zero: larger buffer

--- src/istream/ZeroIstream.cxx
+++ src/istream/ZeroIstream.cxx
@@ -38,7 +38,7 @@
 #include <limits.h>
 #include <stdint.h>
 
-static constexpr uint8_t zero_buffer[1024]{};
+static constexpr uint8_t zero_buffer[4096]{};
 
 class ZeroIstream final : public Istream {
 public:
