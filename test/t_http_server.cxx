f038e8a5a4f2551b27505fb6a6d61ca73e08beb7
Max Kellermann <mk@cm4all.com>
2019-03-05 16:56:07 +0100

    test/t_http_server: use the HTTP client library

--- test/t_http_server.cxx
+++ test/t_http_server.cxx
@@ -33,26 +33,38 @@
 #include "http_server/http_server.hxx"
 #include "http_server/Request.hxx"
 #include "http_server/Handler.hxx"
+#include "http_client.hxx"
 #include "http_headers.hxx"
+#include "HttpResponseHandler.hxx"
+#include "lease.hxx"
 #include "direct.hxx"
 #include "PInstance.hxx"
 #include "pool/pool.hxx"
 #include "istream/UnusedPtr.hxx"
+#include "istream/HeadIstream.hxx"
+#include "istream/BlockIstream.hxx"
 #include "istream/istream_catch.hxx"
 #include "fb_pool.hxx"
+#include "fs/FilteredSocket.hxx"
 #include "net/UniqueSocketDescriptor.hxx"
 #include "system/Error.hxx"
+#include "util/Cancellable.hxx"
 #include "util/PrintException.hxx"
 
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Instance final : HttpServerConnectionHandler {
+struct Instance final
+    : HttpServerConnectionHandler, Lease, HttpResponseHandler, BufferedSocketHandler
+{
     struct pool *pool;
 
     HttpServerConnection *connection = nullptr;
 
-    UniqueSocketDescriptor client_socket;
+    FilteredSocket client_fs;
+    CancellablePointer client_cancel_ptr;
+
+    bool client_fs_released = false;
 
     Instance(struct pool &_pool, EventLoop &event_loop);
 
@@ -70,6 +82,23 @@ struct Instance final : HttpServerConnectionHandler {
             CloseConnection();
     }
 
+    void SendRequest(http_method_t method, const char *uri,
+                     HttpHeaders &&headers,
+                     UnusedIstreamPtr body, bool expect_100=false) noexcept {
+        http_client_request(*pool, client_fs, *this,
+                            "foo",
+                            method, uri, std::move(headers),
+                            std::move(body), expect_100,
+                            *this, client_cancel_ptr);
+    }
+
+    void CloseClientSocket() noexcept {
+        if (client_fs.IsValid() && client_fs.IsConnected()) {
+            client_fs.Close();
+            client_fs.Destroy();
+        }
+    }
+
     /* virtual methods from class HttpServerConnectionHandler */
     void HandleHttpRequest(HttpServerRequest &request,
                            CancellablePointer &cancel_ptr) noexcept override;
@@ -80,12 +109,61 @@ struct Instance final : HttpServerConnectionHandler {
 
     void HttpConnectionError(std::exception_ptr e) noexcept override;
     void HttpConnectionClosed() noexcept override;
+
+    /* virtual methods from class HttpResponseHandler */
+    void ReleaseLease(bool reuse) noexcept override {
+        client_fs_released = true;
+
+        if (reuse && client_fs.IsValid() && client_fs.IsConnected()) {
+            client_fs.Reinit(Event::Duration(-1), Event::Duration(-1), *this);
+            client_fs.UnscheduleWrite();
+        } else {
+            CloseClientSocket();
+        }
+    }
+
+    /* virtual methods from class HttpResponseHandler */
+    void OnHttpResponse(http_status_t status, StringMap &&headers,
+                        UnusedIstreamPtr body) noexcept override {
+        (void)status;
+        (void)headers;
+        (void)body;
+    }
+
+    void OnHttpError(std::exception_ptr ep) noexcept override {
+        PrintException(ep);
+    }
+
+    /* virtual methods from class BufferedSocketHandler */
+    BufferedResult OnBufferedData() override {
+        fprintf(stderr, "unexpected data in idle TCP connection");
+        CloseClientSocket();
+        return BufferedResult::CLOSED;
+    }
+
+    bool OnBufferedClosed() noexcept override {
+        CloseClientSocket();
+        return false;
+    }
+
+    gcc_noreturn
+    bool OnBufferedWrite() override {
+        /* should never be reached because we never schedule
+           writing */
+        gcc_unreachable();
+    }
+
+    void OnBufferedError(std::exception_ptr e) noexcept override {
+        PrintException(e);
+        CloseClientSocket();
+    }
 };
 
 Instance::Instance(struct pool &_pool, EventLoop &event_loop)
-    :pool(pool_new_libc(&_pool, "catch"))
+    :pool(pool_new_libc(&_pool, "catch")),
+     client_fs(event_loop)
 {
-    UniqueSocketDescriptor server_socket;
+    UniqueSocketDescriptor client_socket, server_socket;
     if (!UniqueSocketDescriptor::CreateSocketPair(AF_LOCAL, SOCK_STREAM, 0,
                                                   client_socket, server_socket))
         throw MakeErrno("socketpair() failed");
@@ -96,6 +174,8 @@ Instance::Instance(struct pool &_pool, EventLoop &event_loop)
                                             nullptr,
                                             nullptr, nullptr,
                                             true, *this);
+
+    client_fs.InitDummy(client_socket.Release(), FdType::FD_SOCKET);
 }
 
 static std::exception_ptr
@@ -137,9 +217,10 @@ test_catch(EventLoop &event_loop, struct pool *_pool)
     Instance instance(*_pool, event_loop);
     pool_unref(instance.pool);
 
-    static constexpr char request[] =
-        "POST / HTTP/1.1\r\nContent-Length: 1024\r\n\r\nfoo";
-    send(instance.client_socket.Get(), request, sizeof(request) - 1, 0);
+    instance.SendRequest(HTTP_METHOD_POST, "/", HttpHeaders(*instance.pool),
+                         istream_head_new(*instance.pool,
+                                          istream_block_new(*instance.pool),
+                                          1024, true));
 
     event_loop.Dispatch();
 }
