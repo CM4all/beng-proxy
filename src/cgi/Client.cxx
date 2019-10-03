29c74ad152f8ec65b2909d93589d46b3e358249e
Max Kellermann <mk@cm4all.com>
2019-10-03 23:01:48 +0200

    cgi/Client: use class PoolLeakDetector

--- src/cgi/Client.cxx
+++ src/cgi/Client.cxx
@@ -34,6 +34,7 @@
 #include "Error.hxx"
 #include "Parser.hxx"
 #include "pool/pool.hxx"
+#include "pool/LeakDetector.hxx"
 #include "istream/Handler.hxx"
 #include "istream/UnusedPtr.hxx"
 #include "istream/Pointer.hxx"
@@ -52,7 +53,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-class CGIClient final : Istream, IstreamHandler, Cancellable, DestructAnchor {
+class CGIClient final : PoolLeakDetector, Istream, IstreamHandler, Cancellable, DestructAnchor {
     const StopwatchPtr stopwatch;
 
     IstreamPointer input;
@@ -478,7 +479,7 @@ CGIClient::CGIClient(struct pool &_pool, StopwatchPtr &&_stopwatch,
                      UnusedIstreamPtr _input,
                      HttpResponseHandler &_handler,
                      CancellablePointer &cancel_ptr)
-    :Istream(_pool),
+    :PoolLeakDetector(_pool), Istream(_pool),
      stopwatch(std::move(_stopwatch)),
      input(std::move(_input), *this),
      buffer(fb_pool_get()),
