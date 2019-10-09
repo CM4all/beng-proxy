CM4all/beng-proxy

commit 702db16e760f73bff276ddfa20d5d26a0ae094b3

istream/istream: use DestructObserver instead of PoolNotify
We really need to check whether the Istream has been destructed, not
whether the pool has been freed.

src/istream/istream.hxx
@@ -34,8 +34,8 @@
 #define ISTREAM_HXX
 
 #include "pool/Holder.hxx"
-#include "pool/Notify.hxx"
 #include "io/FdType.hxx"
+#include "util/DestructObserver.hxx"
 #include "util/LeakDetector.hxx"
 
 #include <algorithm>
@@ -44,6 +44,21 @@
 
 class IstreamHandler;
 class IstreamBucketList;
+
+/**
+ * This trick avoids the "DestructAnchor is an ambiguous base of ..."
+ * error.
+ */
+class IstreamDestructAnchor {
+#ifndef NDEBUG
+    DestructAnchor destruct_anchor;
+#endif
+
+public:
+    operator DestructAnchor &() noexcept {
+        return destruct_anchor;
+    }
+};
 
 /**
  * An asynchronous input stream.
@@ -56,7 +71,7 @@ class IstreamBucketList;
  * - it has reached end-of-file
  * - an error has occurred
  */
-class Istream : PoolHolder, LeakDetector {
+class Istream : PoolHolder, LeakDetector, IstreamDestructAnchor {
     /** data sink */
     IstreamHandler *handler = nullptr;
 
@@ -228,7 +243,7 @@ public:
         assert(!eof);
         assert(!reading);
 
-        PoolNotify notify(pool);
+        const DestructObserver destructed(*this);
         reading = true;
 #endif
 
@@ -236,7 +251,7 @@ public:
 
 #ifndef NDEBUG
         assert(available >= -1);
-        assert(!notify.IsDestroyed());
+        assert(!destructed);
         assert(!destroyed);
         assert(reading);
 
@@ -273,7 +288,7 @@ public:
         assert(!eof);
         assert(!reading);
 
-        PoolNotify notify(pool);
+        const DestructObserver destructed(*this);
         reading = true;
 #endif
 
@@ -281,7 +296,7 @@ public:
         assert(nbytes <= length);
 
 #ifndef NDEBUG
-        if (notify.IsDestroyed() || destroyed)
+        if (destructed || destroyed)
             return nbytes;
 
         reading = false;
@@ -324,14 +339,14 @@ public:
         assert(!reading);
         assert(!in_data);
 
-        PoolNotify notify(pool);
+        const DestructObserver destructed(*this);
         reading = true;
 #endif
 
         _Read();
 
 #ifndef NDEBUG
-        if (notify.IsDestroyed() || destroyed)
+        if (destructed || destroyed)
             return;
 
         reading = false;
@@ -355,7 +370,7 @@ public:
         assert(!reading);
         assert(!in_data);
 
-        PoolNotify notify(pool);
+        const DestructObserver destructed(*this);
         reading = true;
 
         try {
@@ -365,14 +380,14 @@ public:
 
 #ifndef NDEBUG
         } catch (...) {
-            if (!notify.IsDestroyed()) {
+            if (!destructed) {
                 assert(destroyed);
             }
 
             throw;
         }
 
-        assert(!notify.IsDestroyed());
+        assert(!destructed);
         assert(!destroyed);
         assert(reading);
 
@@ -438,14 +453,14 @@ public:
         assert(!reading);
         assert(!in_data);
 
-        PoolNotify notify(pool);
+        const DestructObserver destructed(*this);
         reading = true;
 #endif
 
         int fd = _AsFd();
 
 #ifndef NDEBUG
-        assert((notify.IsDestroyed() || destroyed) == (fd >= 0));
+        assert((destructed || destroyed) == (fd >= 0));
 
         if (fd < 0)
             reading = false;

src/istream/Invoke.cxx
@@ -32,7 +32,6 @@
 
 #include "istream.hxx"
 #include "Handler.hxx"
-#include "pool/Notify.hxx"
 
 #include <assert.h>
 
@@ -46,13 +45,13 @@ Istream::InvokeReady() noexcept
     assert(!closing);
 
 #ifndef NDEBUG
-    PoolNotify notify(pool);
+    const DestructObserver destructed(*this);
 #endif
 
     bool result = handler->OnIstreamReady();
 
 #ifndef NDEBUG
-    if (notify.IsDestroyed() || destroyed) {
+    if (destructed || destroyed) {
         assert(!result);
     }
 #endif
@@ -75,7 +74,7 @@ Istream::InvokeData(const void *data, size_t length) noexcept
            (off_t)length <= available_full);
 
 #ifndef NDEBUG
-    PoolNotify notify(pool);
+    const DestructObserver destructed(*this);
     in_data = true;
 #endif
 
@@ -84,7 +83,7 @@ Istream::InvokeData(const void *data, size_t length) noexcept
     assert(nbytes == 0 || !eof);
 
 #ifndef NDEBUG
-    if (notify.IsDestroyed() || destroyed) {
+    if (destructed || destroyed) {
         assert(nbytes == 0);
         return nbytes;
     }
@@ -113,7 +112,7 @@ Istream::InvokeDirect(FdType type, int fd, size_t max_length) noexcept
     assert(!closing);
 
 #ifndef NDEBUG
-    PoolNotify notify(pool);
+    const DestructObserver destructed(*this);
     in_data = true;
 #endif
 
@@ -123,7 +122,7 @@ Istream::InvokeDirect(FdType type, int fd, size_t max_length) noexcept
     assert(nbytes == ISTREAM_RESULT_CLOSED || !eof);
 
 #ifndef NDEBUG
-    if (notify.IsDestroyed() || destroyed) {
+    if (destructed || destroyed) {
         assert(nbytes == ISTREAM_RESULT_CLOSED);
         return nbytes;
     }

src/istream/ReplaceIstream.cxx
@@ -38,7 +38,6 @@
 #include "GrowingBuffer.hxx"
 #include "event/DeferEvent.hxx"
 #include "pool/pool.hxx"
-#include "pool/Notify.hxx"
 #include "util/ConstBuffer.hxx"
 #include "util/DestructObserver.hxx"
 

commit 430500e84b3b2214aa98db68b671a143749d376b

istream/istream: fix NDEBUG build failure
Regression by commit 702db16e760f73bff276ddfa20d5d26a0ae094b3

src/istream/istream.hxx
@@ -52,12 +52,12 @@ class IstreamBucketList;
 class IstreamDestructAnchor {
 #ifndef NDEBUG
     DestructAnchor destruct_anchor;
-#endif
 
 public:
     operator DestructAnchor &() noexcept {
         return destruct_anchor;
     }
+#endif
 };
 
 /**