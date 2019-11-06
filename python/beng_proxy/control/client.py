CM4all/beng-proxy

commit 42841d05504d61b66986c12d7ba2f84304e0f10d

python: use future.utils.iteritems() for Python 3 compatibility

python/beng_proxy/control/client.py
@@ -4,6 +4,7 @@
 # Author: Max Kellermann <mk@cm4all.com>
 #
 
+from future.utils import iteritems
 import socket
 import struct
 from beng_proxy.control.protocol import *
@@ -69,7 +70,7 @@ class Client:
 
     def send_tcache_invalidate(self, vary):
         payload = ''
-        for command, value in vary.iteritems():
+        for command, value in iteritems(iteritems):
             assert isinstance(command, int)
             assert isinstance(value, str)
 

commit d85d83a14f39c9ca938f3401e7cc29dc0c0aedbf

python/control/client: fix typo
Introduced by commit 42841d05504

python/beng_proxy/control/client.py
@@ -70,7 +70,7 @@ class Client:
 
     def send_tcache_invalidate(self, vary):
         payload = ''
-        for command, value in iteritems(iteritems):
+        for command, value in iteritems(vary):
             assert isinstance(command, int)
             assert isinstance(value, str)
 