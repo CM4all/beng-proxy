9174d9f6bfd8bc679394eecfdd5bf3ef12f6d1a6
Max Kellermann <mk@cm4all.com>
2019-12-07 21:09:37 +0100

    istream/FifoBufferIstream: add API documentation

--- src/istream/FifoBufferIstream.hxx
+++ src/istream/FifoBufferIstream.hxx
@@ -35,13 +35,32 @@
 #include "istream.hxx"
 #include "SliceFifoBuffer.hxx"
 
+/**
+ * Callbacks for #FifoBufferIstream.
+ */
 class FifoBufferIstreamHandler {
 public:
+	/**
+	 * Called whenever some data has been consumed from the
+	 * buffer.
+	 */
 	virtual void OnFifoBufferIstreamConsumed(size_t nbytes) noexcept = 0;
+
+	/**
+	 * Called after the buffer has become empty.
+	 */
 	virtual void OnFifoBufferIstreamDrained() noexcept = 0;
+
+	/**
+	 * Called while the #FifoBufferIstream is being closed.
+	 */
 	virtual void OnFifoBufferIstreamClosed() noexcept = 0;
 };
 
+/**
+ * An #Istream implementation which reads data from a FIFO buffer
+ * which may be filled by somebody using the Push() method.
+ */
 class FifoBufferIstream final : public Istream {
 	FifoBufferIstreamHandler &handler;
 
@@ -59,12 +78,34 @@ public:
 		return buffer;
 	}
 
+	/**
+	 * Copy data into the FIFO buffer.  This will not invoke the
+	 * #IstreamHandler and thus will never destroy the object.  To
+	 * actually invoke the #IstreamHandler, call SubmitBuffer().
+	 *
+	 * @return the number of bytes copied into the buffer
+	 */
 	size_t Push(ConstBuffer<void> src) noexcept;
 
+	/**
+	 * Indicate that this #Istream will end after all remaining
+	 * data in the buffer has been consumed.  This will suppress
+	 * any further #FifoBufferIstreamHandler calls.  This method
+	 * may invoke the #IstreamHandler and destroy this object.
+	 */
 	void SetEof() noexcept;
 
+	/**
+	 * Pass the given error to the #IstreamHandler and destroy
+	 * this object.
+	 */
 	using Istream::DestroyError;
 
+	/**
+	 * Submit data from the buffer to the #IstreamHandler.  After
+	 * returning, this object may have been destroyed by the
+	 * #IstreamHandler.
+	 */
 	void SubmitBuffer() noexcept;
 
 	/* virtual methods from class Istream */
