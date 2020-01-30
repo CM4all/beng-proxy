cca6b74c6bc5d2d53d22d65ceade14ddea31c65f
Max Kellermann <mk@cm4all.com>
2020-01-30 16:28:05 +0100

    test/t_istream_processor: add more entity escaping tests

--- test/t_istream_processor.cxx
+++ test/t_istream_processor.cxx
@@ -48,7 +48,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define EXPECTED_RESULT "foo &c:url; <script><c:widget id=\"foo\" type=\"bar\"/></script> bar<b>http://localhost:8080/beng.html?%27</b>"
+#define EXPECTED_RESULT "foo &c:url; <script><c:widget id=\"foo\" type=\"bar\"/></script> bar<b>http://localhost:8080/beng.html?%27%%22%3c%3e</b>"
 
 class EventLoop;
 
@@ -99,7 +99,7 @@ create_test(EventLoop &event_loop, struct pool &pool, UnusedIstreamPtr input)
                         "localhost:8080",
                         "localhost:8080",
                         "/beng.html?'",
-                        "http://localhost:8080/beng.html?'",
+                        "http://localhost:8080/beng.html?'%\"<>",
                         "/beng.html?'",
                         nullptr,
                         "bp_session", session->id, "foo",
