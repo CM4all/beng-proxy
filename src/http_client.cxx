a45a21b2ca0a636db4a25ad75c172f7ad5dc639f
Max Kellermann <mk@cm4all.com>
2020-02-29 19:32:52 +0100

    http_client: handle WRITE_BROKEN properly in TryWriteBuckets2()
    Don't abort the whole request, just stop sending the request body.

--- src/http_client.cxx
+++ src/http_client.cxx
@@ -521,6 +521,10 @@ HttpClient::Close()
 	Destroy();
 }
 
+namespace {
+struct RequestBodyCanceled {};
+}
+
 inline HttpClient::BucketResult
 HttpClient::TryWriteBuckets2()
 {
@@ -562,8 +566,7 @@ HttpClient::TryWriteBuckets2()
 		if (nbytes == WRITE_BROKEN)
 			/* request.istream has already been closed by
 			   OnBufferedBroken() */
-			throw HttpClientError(HttpClientErrorCode::IO,
-					      "write error");
+			throw RequestBodyCanceled{};
 
 		request.istream.ClearAndClose();
 
@@ -587,6 +590,10 @@ HttpClient::TryWriteBuckets()
 
 	try {
 		result = TryWriteBuckets2();
+	} catch (RequestBodyCanceled) {
+		assert(!request.istream.IsDefined());
+		stopwatch.RecordEvent("request_canceled");
+		return BucketResult::DEPLETED;
 	} catch (...) {
 		assert(!request.istream.IsDefined());
 		stopwatch.RecordEvent("send_error");
