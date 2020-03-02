67a7f3f295bc051063a068d81eff6f4ce5a9d956
Max Kellermann <mk@cm4all.com>
2020-03-02 13:18:44 +0100

    test/t_istream_replace: indent with tabs

--- test/t_istream_replace.cxx
+++ test/t_istream_replace.cxx
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007-2018 Content Management AG
+ * Copyright 2007-2020 CM4all GmbH
  * All rights reserved.
  *
  * author: Max Kellermann <mk@cm4all.com>
@@ -37,56 +37,56 @@
 
 class IstreamReplaceTestTraits {
 public:
-    static constexpr const char *expected_result = "foo";
+	static constexpr const char *expected_result = "foo";
 
-    static constexpr bool call_available = true;
-    static constexpr bool got_data_assert = true;
-    static constexpr bool enable_blocking = true;
-    static constexpr bool enable_abort_istream = true;
+	static constexpr bool call_available = true;
+	static constexpr bool got_data_assert = true;
+	static constexpr bool enable_blocking = true;
+	static constexpr bool enable_abort_istream = true;
 
-    UnusedIstreamPtr CreateInput(struct pool &pool) const noexcept {
-        return istream_string_new(pool, "foo");
-    }
+	UnusedIstreamPtr CreateInput(struct pool &pool) const noexcept {
+		return istream_string_new(pool, "foo");
+	}
 
-    UnusedIstreamPtr CreateTest(EventLoop &event_loop, struct pool &pool,
-                                UnusedIstreamPtr input) const noexcept {
-        auto replace = istream_replace_new(event_loop, pool, std::move(input));
-        replace.second->Add(0, 0, nullptr);
-        replace.second->Add(3, 3, nullptr);
-        replace.second->Finish();
-        return std::move(replace.first);
-    }
+	UnusedIstreamPtr CreateTest(EventLoop &event_loop, struct pool &pool,
+				    UnusedIstreamPtr input) const noexcept {
+		auto replace = istream_replace_new(event_loop, pool, std::move(input));
+		replace.second->Add(0, 0, nullptr);
+		replace.second->Add(3, 3, nullptr);
+		replace.second->Finish();
+		return std::move(replace.first);
+	}
 };
 
 INSTANTIATE_TYPED_TEST_CASE_P(Replace, IstreamFilterTest,
-                              IstreamReplaceTestTraits);
+			      IstreamReplaceTestTraits);
 
 class IstreamReplace2TestTraits {
 public:
-    static constexpr const char *expected_result =
-        "abcfoofghijklmnopqrstuvwxyz";
+	static constexpr const char *expected_result =
+		"abcfoofghijklmnopqrstuvwxyz";
 
-    static constexpr bool call_available = true;
-    static constexpr bool got_data_assert = true;
-    static constexpr bool enable_blocking = true;
-    static constexpr bool enable_abort_istream = true;
+	static constexpr bool call_available = true;
+	static constexpr bool got_data_assert = true;
+	static constexpr bool enable_blocking = true;
+	static constexpr bool enable_abort_istream = true;
 
-    UnusedIstreamPtr CreateInput(struct pool &pool) const noexcept {
-        return istream_string_new(pool, "foo");
-    }
+	UnusedIstreamPtr CreateInput(struct pool &pool) const noexcept {
+		return istream_string_new(pool, "foo");
+	}
 
-    UnusedIstreamPtr CreateTest(EventLoop &event_loop, struct pool &pool,
-                                UnusedIstreamPtr input) const noexcept {
-        auto istream =
-            istream_string_new(pool, "abcdefghijklmnopqrstuvwxyz");
-        auto replace = istream_replace_new(event_loop, pool, std::move(istream));
-        replace.second->Add(3, 3, std::move(input));
-        replace.second->Extend(3, 4);
-        replace.second->Extend(3, 5);
-        replace.second->Finish();
-        return std::move(replace.first);
-    }
+	UnusedIstreamPtr CreateTest(EventLoop &event_loop, struct pool &pool,
+				    UnusedIstreamPtr input) const noexcept {
+		auto istream =
+			istream_string_new(pool, "abcdefghijklmnopqrstuvwxyz");
+		auto replace = istream_replace_new(event_loop, pool, std::move(istream));
+		replace.second->Add(3, 3, std::move(input));
+		replace.second->Extend(3, 4);
+		replace.second->Extend(3, 5);
+		replace.second->Finish();
+		return std::move(replace.first);
+	}
 };
 
 INSTANTIATE_TYPED_TEST_CASE_P(Replace2, IstreamFilterTest,
-                              IstreamReplace2TestTraits);
+			      IstreamReplace2TestTraits);
