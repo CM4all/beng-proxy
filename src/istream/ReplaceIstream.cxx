CM4all/beng-proxy

commit 640309508d95ef23a461662c1d3edfc8b2579f7e

istream/replace: call OnIstreamReady() on source EOF

src/istream/ReplaceIstream.cxx
@@ -339,7 +339,7 @@ ReplaceIstream::OnEof() noexcept
 
 	if (IsEOF())
 		DestroyEof();
-	else
+	else if (!InvokeReady())
 		TryRead();
 }
 

commit 4c31ea5ba41f1a2f8a13980a8ea26aaae9f13139

istream/replace: defer read on EOF to fix assertion failure
Caused by 640309508d95ef23a461662c1d3edfc8b2579f7e

src/istream/ReplaceIstream.cxx
@@ -339,8 +339,8 @@ ReplaceIstream::OnEof() noexcept
 
 	if (IsEOF())
 		DestroyEof();
-	else if (InvokeReady())
-		TryRead();
+	else
+		defer_read.Schedule();
 }
 
 void