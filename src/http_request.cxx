c56dd0b3a30af62718821d66e45dc9b017709103
Max Kellermann <mk@cm4all.com>
2020-03-31 18:28:19 +0200

    http_request: use class StockItemLease
    Eliminate some code complexity by removing this part of the
    HttpRequest class.

--- src/http_request.cxx
+++ src/http_request.cxx
@@ -38,7 +38,7 @@
 #include "http/HeaderWriter.hxx"
 #include "stock/GetHandler.hxx"
 #include "stock/Item.hxx"
-#include "lease.hxx"
+#include "stock/Lease.hxx"
 #include "istream/istream.hxx"
 #include "istream/UnusedHoldPtr.hxx"
 #include "fs/Stock.hxx"
@@ -64,7 +64,7 @@ static constexpr Event::Duration HTTP_CONNECT_TIMEOUT =
 	std::chrono::seconds(30);
 
 class HttpRequest final
-	: Cancellable, StockGetHandler, Lease, HttpResponseHandler, PoolLeakDetector {
+	: Cancellable, StockGetHandler, HttpResponseHandler, PoolLeakDetector {
 
 	struct pool &pool;
 
@@ -76,14 +76,11 @@ class HttpRequest final
 
 	SocketFilterFactory *const filter_factory;
 
-	StockItem *stock_item = nullptr;
 	FailurePtr failure;
 
 	const sticky_hash_t session_sticky;
 
 	unsigned retries;
-
-	bool response_sent = false;
 
 	const http_method_t method;
 	const HttpAddress &address;
@@ -131,30 +128,18 @@ public:
 
 private:
 	void Destroy() noexcept {
-		assert(stock_item == nullptr);
-
 		DeleteFromPool(pool, this);
-	}
-
-	void ResponseSent() {
-		assert(!response_sent);
-		response_sent = true;
-
-		if (stock_item == nullptr)
-			Destroy();
 	}
 
 	void Failed(std::exception_ptr ep) {
 		body.Clear();
 		auto &_handler = handler;
-		ResponseSent();
+		Destroy();
 		_handler.InvokeError(ep);
 	}
 
 	/* virtual methods from class Cancellable */
 	void Cancel() noexcept override {
-		assert(!response_sent);
-
 		cancel_ptr.Cancel();
 		Destroy();
 	}
@@ -162,9 +147,6 @@ private:
 	/* virtual methods from class StockGetHandler */
 	void OnStockItemReady(StockItem &item) noexcept override;
 	void OnStockItemError(std::exception_ptr ep) noexcept override;
-
-	/* virtual methods from class Lease */
-	void ReleaseLease(bool reuse) noexcept override;
 
 	/* virtual methods from class HttpResponseHandler */
 	void OnHttpResponse(http_status_t status, StringMap &&headers,
@@ -181,20 +163,16 @@ void
 HttpRequest::OnHttpResponse(http_status_t status, StringMap &&_headers,
 			    UnusedIstreamPtr _body) noexcept
 {
-	assert(!response_sent);
-
 	failure->UnsetProtocol();
 
 	auto &_handler = handler;
-	ResponseSent();
+	Destroy();
 	_handler.InvokeResponse(status, std::move(_headers), std::move(_body));
 }
 
 void
 HttpRequest::OnHttpError(std::exception_ptr ep) noexcept
 {
-	assert(!response_sent);
-
 	if (retries > 0 && IsHttpClientRetryFailure(ep)) {
 		/* the server has closed the connection prematurely, maybe
 		   because it didn't want to get any further requests on that
@@ -220,15 +198,10 @@ HttpRequest::OnHttpError(std::exception_ptr ep) noexcept
 void
 HttpRequest::OnStockItemReady(StockItem &item) noexcept
 {
-	assert(stock_item == nullptr);
-	assert(!response_sent);
-
 	stopwatch.RecordEvent("connect");
 
-	stock_item = &item;
-
 	failure = fs_balancer.GetFailureManager()
-		.Make(fs_stock_item_get_address(*stock_item));
+		.Make(fs_stock_item_get_address(item));
 
 	GrowingBuffer more_headers;
 	if (address.host_and_port != nullptr)
@@ -236,7 +209,7 @@ HttpRequest::OnStockItemReady(StockItem &item) noexcept
 
 	http_client_request(pool, std::move(stopwatch),
 			    fs_stock_item_get(item),
-			    *this,
+			    *NewFromPool<StockItemLease>(pool, item),
 			    item.GetStockName(),
 			    method, address.path,
 			    headers, std::move(more_headers),
@@ -247,30 +220,9 @@ HttpRequest::OnStockItemReady(StockItem &item) noexcept
 void
 HttpRequest::OnStockItemError(std::exception_ptr ep) noexcept
 {
-	assert(stock_item == nullptr);
-	assert(!response_sent);
-
 	stopwatch.RecordEvent("connect_error");
 
 	Failed(ep);
-}
-
-/*
- * Lease
- *
- */
-
-void
-HttpRequest::ReleaseLease(bool reuse) noexcept
-{
-	assert(stock_item != nullptr);
-
-	stock_item->Put(!reuse);
-	stock_item = nullptr;
-
-	if (response_sent) {
-		Destroy();
-	}
 }
 
 /*
