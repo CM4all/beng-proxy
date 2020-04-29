CM4all/beng-proxy

commit 33cc95991fca5017382bfad9361665bd8c1330a1

http_client: split the "headers" parameter
Instead of passing an `HttpHeaders&&`, pass a `const StringMap&` and
a `GrowingBuffer&&`.  By leaving the main headers `const`, we can fix
a (theoretical) problem with retries, by not "consuming" the
`StringMap` with the rvalue reference.

src/http_request.cxx
@@ -87,7 +87,7 @@ class HttpRequest final
 
 	const http_method_t method;
 	const HttpAddress &address;
-	HttpHeaders headers;
+	const StringMap &headers;
 	UnusedHoldIstreamPtr body;
 
 	HttpResponseHandler &handler;
@@ -101,7 +101,7 @@ public:
 		    SocketFilterFactory *_filter_factory,
 		    http_method_t _method,
 		    const HttpAddress &_address,
-		    StringMap &&_headers,
+		    const StringMap &_headers,
 		    UnusedIstreamPtr _body,
 		    HttpResponseHandler &_handler,
 		    CancellablePointer &_cancel_ptr)
@@ -113,13 +113,10 @@ public:
 		 /* can only retry if there is no request body */
 		 retries(_body ? 0 : 2),
 		 method(_method), address(_address),
-		 headers(std::move(_headers)), body(pool, std::move(_body)),
+		 headers(_headers), body(pool, std::move(_body)),
 		 handler(_handler)
 	{
 		_cancel_ptr = *this;
-
-		if (address.host_and_port != nullptr)
-			headers.Write("host", address.host_and_port);
 	}
 
 	void BeginConnect() {
@@ -233,11 +230,16 @@ HttpRequest::OnStockItemReady(StockItem &item) noexcept
 	failure = fs_balancer.GetFailureManager()
 		.Make(fs_stock_item_get_address(*stock_item));
 
+	GrowingBuffer more_headers;
+	if (address.host_and_port != nullptr)
+		header_write(more_headers, "host", address.host_and_port);
+
 	http_client_request(pool, std::move(stopwatch),
 			    fs_stock_item_get(item),
 			    *this,
 			    item.GetStockName(),
-			    method, address.path, std::move(headers),
+			    method, address.path,
+			    headers, std::move(more_headers),
 			    std::move(body), true,
 			    *this, cancel_ptr);
 }
@@ -284,7 +286,7 @@ http_request(struct pool &pool, EventLoop &event_loop,
 	     SocketFilterFactory *filter_factory,
 	     http_method_t method,
 	     const HttpAddress &uwa,
-	     StringMap &&headers,
+	     const StringMap &headers,
 	     UnusedIstreamPtr body,
 	     HttpResponseHandler &handler,
 	     CancellablePointer &_cancel_ptr)
@@ -297,7 +299,7 @@ http_request(struct pool &pool, EventLoop &event_loop,
 					   session_sticky,
 					   filter_factory,
 					   method, uwa,
-					   std::move(headers), std::move(body),
+					   headers, std::move(body),
 					   handler, _cancel_ptr);
 
 	hr->BeginConnect();

src/http_request.hxx
@@ -60,6 +60,6 @@ http_request(struct pool &pool, EventLoop &event_loop,
 	     SocketFilterFactory *filter_factory,
 	     http_method_t method,
 	     const HttpAddress &address,
-	     StringMap &&headers, UnusedIstreamPtr body,
+	     const StringMap &headers, UnusedIstreamPtr body,
 	     HttpResponseHandler &handler,
 	     CancellablePointer &cancel_ptr);

src/lhttp_request.cxx
@@ -34,8 +34,8 @@
 #include "lhttp_stock.hxx"
 #include "lhttp_address.hxx"
 #include "http_client.hxx"
-#include "http/Headers.hxx"
 #include "HttpResponseHandler.hxx"
+#include "GrowingBuffer.hxx"
 #include "fs/FilteredSocket.hxx"
 #include "stock/Item.hxx"
 #include "lease.hxx"
@@ -66,15 +66,22 @@ public:
 	void Start(struct pool &pool,
 		   StopwatchPtr &&stopwatch,
 		   const LhttpAddress &address,
-		   http_method_t method, HttpHeaders &&headers,
+		   http_method_t method,
+		   const StringMap &headers,
 		   UnusedIstreamPtr body,
 		   HttpResponseHandler &handler,
 		   CancellablePointer &cancel_ptr) noexcept {
+		GrowingBuffer more_headers;
+		if (address.host_and_port != nullptr)
+			header_write(more_headers, "host",
+				     address.host_and_port);
+
 		http_client_request(pool, std::move(stopwatch),
 				    socket,
 				    *this,
 				    stock_item.GetStockName(),
-				    method, address.uri, std::move(headers),
+				    method, address.uri,
+				    headers, std::move(more_headers),
 				    std::move(body), true,
 				    handler, cancel_ptr);
 	}
@@ -106,7 +113,7 @@ lhttp_request(struct pool &pool, EventLoop &event_loop,
 	      const StopwatchPtr &parent_stopwatch,
 	      const char *site_name,
 	      const LhttpAddress &address,
-	      http_method_t method, StringMap &&_headers,
+	      http_method_t method, const StringMap &headers,
 	      UnusedIstreamPtr body,
 	      HttpResponseHandler &handler,
 	      CancellablePointer &cancel_ptr) noexcept
@@ -145,12 +152,8 @@ lhttp_request(struct pool &pool, EventLoop &event_loop,
 	auto request = NewFromPool<LhttpRequest>(pool, pool,
 						 event_loop, *stock_item);
 
-	HttpHeaders headers(std::move(_headers));
-	if (address.host_and_port != nullptr)
-		headers.Write("host", address.host_and_port);
-
 	request->Start(pool, std::move(stopwatch), address,
-		       method, std::move(headers),
+		       method, headers,
 		       std::move(body),
 		       handler, cancel_ptr);
 }

src/lhttp_request.hxx
@@ -55,7 +55,7 @@ lhttp_request(struct pool &pool, EventLoop &event_loop,
 	      const char *site_name,
 	      const LhttpAddress &address,
 	      http_method_t method,
-	      StringMap &&headers, UnusedIstreamPtr body,
+	      const StringMap &headers, UnusedIstreamPtr body,
 	      HttpResponseHandler &handler,
 	      CancellablePointer &cancel_ptr) noexcept;
 

src/http_client.cxx
@@ -31,7 +31,6 @@
  */
 
 #include "http_client.hxx"
-#include "http/Headers.hxx"
 #include "http/Upgrade.hxx"
 #include "http/List.hxx"
 #include "HttpResponseHandler.hxx"
@@ -246,7 +245,8 @@ public:
 		   FilteredSocket &_socket, Lease &lease,
 		   const char *_peer_name,
 		   http_method_t method, const char *uri,
-		   HttpHeaders &&headers,
+		   const StringMap &headers,
+		   GrowingBuffer &&more_headers,
 		   UnusedIstreamPtr body, bool expect_100,
 		   HttpResponseHandler &handler,
 		   CancellablePointer &cancel_ptr) noexcept;
@@ -1323,7 +1323,8 @@ HttpClient::HttpClient(struct pool &_pool, struct pool &_caller_pool,
 		       FilteredSocket &_socket, Lease &lease,
 		       const char *_peer_name,
 		       http_method_t method, const char *uri,
-		       HttpHeaders &&headers,
+		       const StringMap &headers,
+		       GrowingBuffer &&headers2,
 		       UnusedIstreamPtr body, bool expect_100,
 		       HttpResponseHandler &handler,
 		       CancellablePointer &cancel_ptr) noexcept
@@ -1352,14 +1353,15 @@ HttpClient::HttpClient(struct pool &_pool, struct pool &_caller_pool,
 
 	/* headers */
 
-	GrowingBuffer &headers2 = headers.GetBuffer();
-
 	const bool upgrade = body && http_is_upgrade(headers);
 	if (upgrade) {
 		/* forward hop-by-hop headers requesting the protocol
 		   upgrade */
-		headers.Write("connection", "upgrade");
-		headers.MoveToBuffer("upgrade");
+		header_write(headers2, "connection", "upgrade");
+
+		const char *value = headers.Get("upgrade");
+		if (value != nullptr)
+			header_write(headers2, "upgrade", value);
 	} else if (body) {
 		off_t content_length = body.GetAvailable(false);
 		if (content_length == (off_t)-1) {
@@ -1392,10 +1394,9 @@ HttpClient::HttpClient(struct pool &_pool, struct pool &_caller_pool,
 		}
 	}
 
-	GrowingBuffer headers3 = headers.ToBuffer();
-	headers3.Write("\r\n", 2);
+	headers2.Write("\r\n", 2);
 
-	auto header_stream = istream_gb_new(GetPool(), std::move(headers3));
+	auto header_stream = istream_gb_new(GetPool(), std::move(headers2));
 
 	/* request istream */
 
@@ -1426,7 +1427,8 @@ http_client_request(struct pool &caller_pool,
 		    FilteredSocket &socket, Lease &lease,
 		    const char *peer_name,
 		    http_method_t method, const char *uri,
-		    HttpHeaders &&headers,
+		    const StringMap &headers,
+		    GrowingBuffer &&more_headers,
 		    UnusedIstreamPtr body, bool expect_100,
 		    HttpResponseHandler &handler,
 		    CancellablePointer &cancel_ptr) noexcept
@@ -1448,6 +1450,7 @@ http_client_request(struct pool &caller_pool,
 				lease,
 				peer_name,
 				method, uri,
-				std::move(headers), std::move(body), expect_100,
+				headers, std::move(more_headers),
+				std::move(body), expect_100,
 				handler, cancel_ptr);
 }

src/http_client.hxx
@@ -48,7 +48,8 @@ class FilteredSocket;
 class Lease;
 class HttpResponseHandler;
 class CancellablePointer;
-class HttpHeaders;
+class StringMap;
+class GrowingBuffer;
 
 /**
  * Error codes for #HttpClientError.
@@ -132,7 +133,8 @@ http_client_request(struct pool &pool,
 		    FilteredSocket &socket, Lease &lease,
 		    const char *peer_name,
 		    http_method_t method, const char *uri,
-		    HttpHeaders &&headers,
+		    const StringMap &headers,
+		    GrowingBuffer &&more_headers,
 		    UnusedIstreamPtr body, bool expect_100,
 		    HttpResponseHandler &handler,
 		    CancellablePointer &cancel_ptr) noexcept;

src/lb/ForwardHttpRequest.cxx
@@ -442,7 +442,7 @@ LbRequest::OnStockItemReady(StockItem &item) noexcept
 			    *this,
 			    item.GetStockName(),
 			    request.method, request.uri,
-			    HttpHeaders(std::move(headers)),
+			    headers, {},
 			    std::move(body), true,
 			    *this, cancel_ptr);
 }

src/lb/ResolveConnect.cxx
@@ -171,7 +171,7 @@ LbResolveConnectRequest::OnStockItemReady(StockItem &item) noexcept
 			    *this,
 			    item.GetStockName(),
 			    request.method, request.uri,
-			    HttpHeaders(std::move(headers)),
+			    headers, {},
 			    std::move(body), true,
 			    *this, cancel_ptr);
 }

commit cf5951ad84f4abe739c03dbf7690c1528b042ad4

http_request: move the StringMap
Another regression from commit
33cc95991fca5017382bfad9361665bd8c1330a1: by keeping a reference to
the `StringMap`, we run into a use-after-free bug, because some
callers (such as `WidgetRequest::SendRequest()`) pass a temporary
`StringMap` to `ResourceLoader::SendRequest()` which goes out of scope
quickly, but the reference is used later, when the connection to the
HTTP server is established.

So instead of holding a reference, move the StringMap to a local copy.

src/http_request.cxx
@@ -57,6 +57,7 @@
 #include "util/Compiler.h"
 #include "AllocatorPtr.hxx"
 #include "stopwatch.hxx"
+#include "strmap.hxx"
 
 #include <string.h>
 
@@ -84,7 +85,7 @@ class HttpRequest final
 
 	const http_method_t method;
 	const HttpAddress &address;
-	const StringMap &headers;
+	const StringMap headers;
 	UnusedHoldIstreamPtr body;
 
 	HttpResponseHandler &handler;
@@ -98,7 +99,7 @@ public:
 		    SocketFilterFactory *_filter_factory,
 		    http_method_t _method,
 		    const HttpAddress &_address,
-		    const StringMap &_headers,
+		    StringMap &&_headers,
 		    UnusedIstreamPtr _body,
 		    HttpResponseHandler &_handler,
 		    CancellablePointer &_cancel_ptr)
@@ -110,7 +111,7 @@ public:
 		 /* can only retry if there is no request body */
 		 retries(_body ? 0 : 2),
 		 method(_method), address(_address),
-		 headers(_headers), body(pool, std::move(_body)),
+		 headers(std::move(_headers)), body(pool, std::move(_body)),
 		 handler(_handler)
 	{
 		_cancel_ptr = *this;
@@ -238,7 +239,7 @@ http_request(struct pool &pool, EventLoop &event_loop,
 	     SocketFilterFactory *filter_factory,
 	     http_method_t method,
 	     const HttpAddress &uwa,
-	     const StringMap &headers,
+	     StringMap &&headers,
 	     UnusedIstreamPtr body,
 	     HttpResponseHandler &handler,
 	     CancellablePointer &_cancel_ptr)
@@ -251,7 +252,7 @@ http_request(struct pool &pool, EventLoop &event_loop,
 					   session_sticky,
 					   filter_factory,
 					   method, uwa,
-					   headers, std::move(body),
+					   std::move(headers), std::move(body),
 					   handler, _cancel_ptr);
 
 	hr->BeginConnect();

src/http_request.hxx
@@ -60,6 +60,6 @@ http_request(struct pool &pool, EventLoop &event_loop,
 	     SocketFilterFactory *filter_factory,
 	     http_method_t method,
 	     const HttpAddress &address,
-	     const StringMap &headers, UnusedIstreamPtr body,
+	     StringMap &&headers, UnusedIstreamPtr body,
 	     HttpResponseHandler &handler,
 	     CancellablePointer &cancel_ptr);

src/lhttp_request.cxx
@@ -46,6 +46,7 @@
 #include "net/SocketDescriptor.hxx"
 #include "util/Cancellable.hxx"
 #include "stopwatch.hxx"
+#include "strmap.hxx"
 
 #include <stdexcept>
 
@@ -97,7 +98,7 @@ class LhttpRequest final : Cancellable, HttpResponseHandler, PoolLeakDetector {
 
 	const http_method_t method;
 	const LhttpAddress &address;
-	const StringMap &headers;
+	const StringMap headers;
 
 	HttpResponseHandler &handler;
 	CancellablePointer cancel_ptr;
@@ -110,7 +111,7 @@ public:
 			      const char *_site_name,
 			      http_method_t _method,
 			      const LhttpAddress &_address,
-			      const StringMap &_headers,
+			      StringMap &&_headers,
 			      const UnusedIstreamPtr &body,
 			      HttpResponseHandler &_handler,
 			      CancellablePointer &_cancel_ptr) noexcept
@@ -121,7 +122,7 @@ public:
 		 /* can only retry if there is no request body */
 		 retries(body ? 0 : 1),
 		 method(_method), address(_address),
-		 headers(_headers),
+		 headers(std::move(_headers)),
 		 handler(_handler)
 	{
 		_cancel_ptr = *this;
@@ -226,7 +227,7 @@ lhttp_request(struct pool &pool, EventLoop &event_loop,
 	      const StopwatchPtr &parent_stopwatch,
 	      const char *site_name,
 	      const LhttpAddress &address,
-	      http_method_t method, const StringMap &headers,
+	      http_method_t method, StringMap &&headers,
 	      UnusedIstreamPtr body,
 	      HttpResponseHandler &handler,
 	      CancellablePointer &cancel_ptr) noexcept
@@ -248,7 +249,7 @@ lhttp_request(struct pool &pool, EventLoop &event_loop,
 						 lhttp_stock,
 						 std::move(stopwatch),
 						 site_name, method, address,
-						 headers, body,
+						 std::move(headers), body,
 						 handler, cancel_ptr);
 
 	request->Start(std::move(body));

src/lhttp_request.hxx
@@ -55,7 +55,7 @@ lhttp_request(struct pool &pool, EventLoop &event_loop,
 	      const char *site_name,
 	      const LhttpAddress &address,
 	      http_method_t method,
-	      const StringMap &headers, UnusedIstreamPtr body,
+	      StringMap &&headers, UnusedIstreamPtr body,
 	      HttpResponseHandler &handler,
 	      CancellablePointer &cancel_ptr) noexcept;
 