c3d2c2b65728c353f8f5a48c168397f16d0ee739
Max Kellermann <mk@cm4all.com>
2020-06-03 10:25:05 +0200

    increment version number to 16.17

--- doc/conf.py
+++ doc/conf.py
@@ -58,7 +58,7 @@ author = 'Max Kellermann'
 # built documents.
 #
 # The short X.Y version.
-version = '16.16'
+version = '16.17'
 # The full version, including alpha/beta/rc tags.
 release = version
 
