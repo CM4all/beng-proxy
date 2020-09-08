8452e0daad8e859d43bd9ceb6c1ad158f4cbad86
Max Kellermann <mk@cm4all.com>
2020-09-08 12:33:07 +0200

    istream/hold: clear input in OnEof() and OnError()
    This makes the code more consistent and allows eliminating the
    `input_eof` field, which shrinks the class by 8 bytes.

--- src/istream/istream_hold.cxx
+++ src/istream/istream_hold.cxx
@@ -38,7 +38,6 @@
 #include <assert.h>
 
 class HoldIstream final : public ForwardIstream {
-	bool input_eof = false;
 	std::exception_ptr input_error;
 
 public:
@@ -47,30 +46,28 @@ public:
 
 private:
 	bool Check() {
-		if (gcc_unlikely(input_eof)) {
-			DestroyEof();
-			return false;
-		} else if (gcc_unlikely(input_error)) {
-			DestroyError(input_error);
-			return false;
-		} else
+		if (gcc_likely(HasInput()))
 			return true;
+
+		if (gcc_unlikely(input_error))
+			DestroyError(input_error);
+		else
+			DestroyEof();
+		return false;
 	}
 
 public:
 	/* virtual methods from class Istream */
 
 	off_t _GetAvailable(bool partial) noexcept override {
-		if (gcc_unlikely(input_eof))
-			return 0;
-		else if (gcc_unlikely(input_error))
-			return -1;
+		if (gcc_likely(HasInput()))
+			return ForwardIstream::_GetAvailable(partial);
 
-		return ForwardIstream::_GetAvailable(partial);
+		return input_error ? -1 : 0;
 	}
 
 	off_t _Skip(off_t length) noexcept override {
-		return gcc_likely(!input_eof && !input_error)
+		return gcc_likely(HasInput())
 			? ForwardIstream::_Skip(length)
 			: -1;
 	}
@@ -81,29 +78,27 @@ public:
 	}
 
 	void _FillBucketList(IstreamBucketList &list) override {
-		if (input_eof)
-			return;
-		else if (input_error) {
-			auto copy = input_error;
-			Destroy();
-			std::rethrow_exception(copy);
-		} else {
+		if (gcc_likely(HasInput())) {
 			try {
 				input.FillBucketList(list);
 			} catch (...) {
 				Destroy();
 				throw;
 			}
+		} else if (gcc_unlikely(input_error)) {
+			auto copy = input_error;
+			Destroy();
+			std::rethrow_exception(copy);
 		}
 	}
 
 	size_t _ConsumeBucketList(size_t nbytes) noexcept override {
 		assert(!input_error);
 
-		if (input_eof)
-			return 0;
-		else
+		if (gcc_likely(HasInput()))
 			return ForwardIstream::_ConsumeBucketList(nbytes);
+		else
+			return 0;
 	}
 
 	int _AsFd() noexcept override {
@@ -113,15 +108,12 @@ public:
 	}
 
 	void _Close() noexcept override {
-		if (input_eof)
-			Destroy();
-		else if (input_error) {
-			/* the handler is not interested in the error */
-			Destroy();
-		} else {
+		if (gcc_likely(HasInput()))
 			/* the input object is still there */
 			ForwardIstream::_Close();
-		}
+		else
+			/* EOF or the handler is not interested in the error */
+			Destroy();
 	}
 
 	/* virtual methods from class IstreamHandler */
@@ -138,19 +130,23 @@ public:
 	}
 
 	void OnEof() noexcept override {
-		assert(!input_eof);
+		assert(HasInput());
 		assert(!input_error);
+
+		ClearInput();
 
 		if (HasHandler())
 			ForwardIstream::OnEof();
-		else
+		else {
 			/* queue the eof() call */
-			input_eof = true;
+		}
 	}
 
 	void OnError(std::exception_ptr ep) noexcept override {
-		assert(!input_eof);
+		assert(HasInput());
 		assert(!input_error);
+
+		ClearInput();
 
 		if (HasHandler())
 			ForwardIstream::OnError(ep);
