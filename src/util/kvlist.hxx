9b2376651b20ee731f6fd31030b924b663febc3b
Max Kellermann <mk@cm4all.com>
2020-09-25 15:49:31 +0200

    util/kvlist: add `noexcept`

--- src/util/kvlist.hxx
+++ src/util/kvlist.hxx
@@ -44,7 +44,7 @@ public:
 	struct Item : boost::intrusive::slist_base_hook<boost::intrusive::link_mode<boost::intrusive::normal_link>> {
 		const char *key, *value;
 
-		Item(const char *_key, const char *_value)
+		Item(const char *_key, const char *_value) noexcept
 			:key(_key), value(_value) {}
 	};
 
@@ -59,45 +59,45 @@ private:
 public:
 	KeyValueList() = default;
 	KeyValueList(const KeyValueList &) = delete;
-	KeyValueList(KeyValueList &&src) {
+	KeyValueList(KeyValueList &&src) noexcept {
 		list.swap(src.list);
 	}
 
 	template<typename Alloc>
-	KeyValueList(Alloc &&alloc, const KeyValueList &src) {
+	KeyValueList(Alloc &&alloc, const KeyValueList &src) noexcept {
 		for (const auto &i : src)
 			Add(alloc, alloc.Dup(i.key), alloc.Dup(i.value));
 	}
 
-	KeyValueList &operator=(KeyValueList &&src) {
+	KeyValueList &operator=(KeyValueList &&src) noexcept {
 		list.swap(src.list);
 		return *this;
 	}
 
-	const_iterator begin() const {
+	const_iterator begin() const noexcept {
 		return list.begin();
 	}
 
-	const_iterator end() const {
+	const_iterator end() const noexcept {
 		return list.end();
 	}
 
 	gcc_pure
-	bool IsEmpty() const {
+	bool IsEmpty() const noexcept {
 		return list.empty();
 	}
 
-	void Clear() {
+	void Clear() noexcept {
 		list.clear();
 	}
 
 	template<typename Alloc>
-	void Add(Alloc &&alloc, const char *key, const char *value) {
+	void Add(Alloc &&alloc, const char *key, const char *value) noexcept {
 		auto item = alloc.template New<Item>(key, value);
 		list.push_front(*item);
 	}
 
-	void Reverse() {
+	void Reverse() noexcept {
 		list.reverse();
 	}
 };
