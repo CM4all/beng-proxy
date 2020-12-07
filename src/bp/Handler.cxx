CM4all/beng-proxy

commit f996ff0ac30cd53057858484b4c053e4d275f88c

uri/Recompose: move code from GetBounceUri()

src/bp/Handler.cxx
@@ -48,6 +48,7 @@
 #include "AllocatorPtr.hxx"
 #include "puri_edit.hxx"
 #include "puri_escape.hxx"
+#include "uri/Recompose.hxx"
 #include "uri/Verify.hxx"
 #include "RedirectHttps.hxx"
 #include "strmap.hxx"
@@ -76,13 +77,14 @@ GetBounceUri(AllocatorPtr alloc, const IncomingHttpRequest &request,
 	if (host == nullptr)
 		host = "localhost";
 
-	const char *uri_path = response.uri != nullptr
-		? alloc.Concat(response.uri,
-			       StringView{";", dissected_uri.args == nullptr ? (size_t)0 : 1},
-			       dissected_uri.args,
-			       StringView{"?", dissected_uri.query == nullptr ? (size_t)0 : 1},
-			       dissected_uri.query)
-		: request.uri;
+	const char *uri_path = request.uri;
+
+	if (response.uri != nullptr) {
+		auto new_du = dissected_uri;
+		new_du.base = response.uri;
+		new_du.path_info = nullptr;
+		uri_path = RecomposeUri(alloc, dissected_uri);
+	}
 
 	const char *current_uri = alloc.Concat(scheme, "://", host, uri_path);
 	const char *escaped_uri = uri_escape_dup(alloc, current_uri,

meson.build
@@ -163,6 +163,7 @@ util2 = static_library(
   'src/uri/Base.cxx',
   'src/uri/Compare.cxx',
   'src/uri/Dissect.cxx',
+  'src/uri/Recompose.cxx',
   'src/uri/Extract.cxx',
   'src/uri/Relative.cxx',
   'src/escape_html.cxx',

src/bp/ProxyHandler.cxx
@@ -43,6 +43,7 @@
 #include "http/IncomingRequest.hxx"
 #include "cgi/Address.hxx"
 #include "istream/AutoPipeIstream.hxx"
+#include "uri/Recompose.hxx"
 #include "AllocatorPtr.hxx"
 
 /**
@@ -51,12 +52,10 @@
  */
 gcc_pure
 static const char *
-ForwardURI(AllocatorPtr alloc, const DissectedUri &uri) noexcept
+ForwardURI(AllocatorPtr alloc, DissectedUri uri) noexcept
 {
-	if (uri.query.empty())
-		return alloc.DupZ(uri.base);
-	else
-		return alloc.Concat(uri.base, '?', uri.query);
+	uri.args = nullptr;
+	return RecomposeUri(alloc, uri);
 }
 
 inline const char *

src/uri/Recompose.cxx
@@ -1,0 +1,46 @@
+/*
+ * Copyright 2007-2020 CM4all GmbH
+ * All rights reserved.
+ *
+ * author: Max Kellermann <mk@cm4all.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the
+ * distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * FOUNDATION OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "Recompose.hxx"
+#include "Dissect.hxx"
+#include "AllocatorPtr.hxx"
+
+char *
+RecomposeUri(AllocatorPtr alloc, const DissectedUri &uri) noexcept
+{
+	return alloc.Concat(uri.base,
+			    StringView{";", uri.args == nullptr ? (size_t)0 : 1},
+			    uri.args,
+			    uri.path_info,
+			    StringView{"?", uri.query == nullptr ? (size_t)0 : 1},
+			    uri.query);
+}

src/uri/Recompose.hxx
@@ -1,0 +1,39 @@
+/*
+ * Copyright 2007-2020 CM4all GmbH
+ * All rights reserved.
+ *
+ * author: Max Kellermann <mk@cm4all.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the
+ * distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * FOUNDATION OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+class AllocatorPtr;
+struct DissectedUri;
+
+char *
+RecomposeUri(AllocatorPtr alloc, const DissectedUri &uri) noexcept;

commit a2eef83b89c47668f7d72da822b87a56e73eddca

bp/Handler: fix RecomposeUri() parameter
Regression from commit f996ff0ac30cd5

src/bp/Handler.cxx
@@ -83,7 +83,7 @@ GetBounceUri(AllocatorPtr alloc, const IncomingHttpRequest &request,
 		auto new_du = dissected_uri;
 		new_du.base = response.uri;
 		new_du.path_info = nullptr;
-		uri_path = RecomposeUri(alloc, dissected_uri);
+		uri_path = RecomposeUri(alloc, new_du);
 	}
 
 	const char *current_uri = alloc.Concat(scheme, "://", host, uri_path);