fa3e11d03d29d58b5caa05b10be9125f02b83885
Max Kellermann <mk@cm4all.com>
2021-02-09 20:46:47 +0100

    bp/CoLoadResource: Coroutine wrapper for class ResourceLoader

---
+++ src/bp/CoLoadResource.hxx
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2007-2021 CM4all GmbH
+ * All rights reserved.
+ *
+ * author: Max Kellermann <mk@cm4all.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the
+ * distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * FOUNDATION OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include "http/CoResponseHandler.hxx"
+#include "ResourceLoader.hxx"
+
+class CoLoadResource final : public CoHttpResponseHandler {
+public:
+	CoLoadResource(ResourceLoader &rl, struct pool &_pool,
+		       const StopwatchPtr &parent_stopwatch,
+		       sticky_hash_t sticky_hash,
+		       const char *cache_tag,
+		       const char *site_name,
+		       http_method_t method,
+		       const ResourceAddress &address,
+		       http_status_t status, StringMap &&headers,
+		       UnusedIstreamPtr &&body, const char *body_etag) noexcept
+		:CoHttpResponseHandler(_pool)
+	{
+		rl.SendRequest(_pool, parent_stopwatch, sticky_hash,
+			       cache_tag, site_name,
+			       method, address, status, std::move(headers),
+			       std::move(body), body_etag,
+			       *this, cancel_ptr);
+	}
+};
