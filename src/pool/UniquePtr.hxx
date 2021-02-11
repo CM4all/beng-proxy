153f5e5b89b5808aa97fe832e0a44ee2dc61c97a
Max Kellermann <mk@cm4all.com>
2021-02-11 18:09:27 +0100

    pool/UniquePtr: add nullptr_t constructor

--- src/pool/UniquePtr.hxx
+++ src/pool/UniquePtr.hxx
@@ -35,6 +35,7 @@
 #include "pool.hxx"
 #include "util/LeakDetector.hxx"
 
+#include <cstddef>
 #include <utility>
 
 /**
@@ -47,6 +48,7 @@ class UniquePoolPtr : LeakDetector {
 
 public:
 	UniquePoolPtr() = default;
+	UniquePoolPtr(std::nullptr_t) noexcept {};
 	explicit UniquePoolPtr(T *_value) noexcept:value(_value) {}
 
 	UniquePoolPtr(UniquePoolPtr &&src) noexcept
@@ -60,6 +62,13 @@ public:
 	UniquePoolPtr &operator=(UniquePoolPtr &&src) noexcept {
 		using std::swap;
 		swap(value, src.value);
+		return *this;
+	}
+
+	UniquePoolPtr &operator=(std::nullptr_t n) noexcept {
+		if (value != nullptr)
+			value->~T();
+		value = n;
 		return *this;
 	}
 
