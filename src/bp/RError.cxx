CM4all/beng-proxy

commit b250f7a7db202eda54fe33784ff4c112c9afdf60

bp/RError: deep search for HttpMessageResponse

src/bp/RError.cxx
@@ -57,12 +57,9 @@
 bool
 Request::DispatchHttpMessageResponse(std::exception_ptr e) noexcept
 {
-	try {
-		std::rethrow_exception(e);
-	} catch (const HttpMessageResponse &response) {
-		DispatchError(response.GetStatus(), response.what());
+	if (const auto *r = FindNested<HttpMessageResponse>(e)) {
+		DispatchError(r->GetStatus(), r->what());
 		return true;
-	} catch (...) {
 	}
 
 	return false;

commit 8bd1641685caea2250d3e55f3e0641bfae0533bb

bp/RError, lb/HttpConnection: duplicate the HttpMessageResponse message
Fixes use-after-free bug because SendMessage() requires the pointed-to
string to persist until the response has been submitted completely.

The beng-proxy bug is a regression by the recent commit b250f7a7db20,
but beng-lb has been broken for a long time.

src/bp/RError.cxx
@@ -58,7 +58,8 @@ bool
 Request::DispatchHttpMessageResponse(std::exception_ptr e) noexcept
 {
 	if (const auto *r = FindNested<HttpMessageResponse>(e)) {
-		DispatchError(r->GetStatus(), r->what());
+		DispatchError(r->GetStatus(),
+			      p_strdup(request.pool, r->what()));
 		return true;
 	}
 

src/lb/HttpConnection.cxx
@@ -186,7 +186,8 @@ void
 LbHttpConnection::SendError(IncomingHttpRequest &request, std::exception_ptr ep)
 {
 	if (const auto *r = FindNested<HttpMessageResponse>(ep)) {
-		request.SendMessage(r->GetStatus(), r->what());
+		request.SendMessage(r->GetStatus(),
+				    p_strdup(request.pool, r->what()));
 		return;
 	}
 