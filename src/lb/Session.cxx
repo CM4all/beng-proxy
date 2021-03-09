afe6b90d804cef92ee6506a82a2a3fd46af9dd1c
Max Kellermann <mk@cm4all.com>
2021-03-09 17:06:50 +0100

    lb/Session: compare only the endptr value

--- src/lb/Session.cxx
+++ src/lb/Session.cxx
@@ -54,13 +54,15 @@ lb_session_get(const StringMap &request_headers, const char *cookie_name)
 		return 0;
 
 	size_t length = strlen(session);
+	const char *end = session + length;
+
 	if (length > 8)
 		/* only parse the upper 32 bits */
 		session += length - 8;
 
 	char *endptr;
 	unsigned long id = strtoul(session, &endptr, 16);
-	if (endptr == session || *endptr != 0)
+	if (endptr != end)
 		return 0;
 
 	return (unsigned)id;
