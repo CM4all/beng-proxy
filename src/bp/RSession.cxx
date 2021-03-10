CM4all/beng-proxy

commit d7cd333f1574437b6840b686351cd43401faed33

translation/Protocol: add packet RECOVER_SESSION

src/bp/RSession.cxx
@@ -81,7 +81,9 @@ Request::LoadSession(const char *_session_id) noexcept
 	assert(!session_id.IsDefined());
 	assert(_session_id != nullptr);
 
-	if (!session_id.Parse(_session_id))
+	auto [sid, recover] = StringView(_session_id).Split('/');
+
+	if (!session_id.Parse(sid))
 		return nullptr;
 
 	const AllocatorPtr alloc(pool);
@@ -96,6 +98,11 @@ Request::LoadSession(const char *_session_id) noexcept
 		session->cookie_received = true;
 
 		session->Expire(instance.event_loop.SteadyNow());
+	} else {
+		/* note: "recover" is a StringView, but this part of
+		   the buffer is null-terminated, so this is legal */
+		recover_session_from_cookie = recover.data;
+		// TODO: do we need to unescape the string?
 	}
 
 	return session;
@@ -185,6 +192,7 @@ Request::MakeSession() noexcept
 
 	session_id = session->id;
 	send_session_cookie = true;
+	recover_session_to_cookie = nullptr;
 
 	return session;
 }
@@ -320,6 +328,17 @@ Request::ApplyTranslateSession(const TranslateResponse &response) noexcept
 
 			if (session)
 				session->parent.SetTranslate(response.session);
+		}
+	}
+
+	if (response.recover_session != nullptr) {
+		if (!session)
+			session = MakeRealmSession();
+
+		if (session &&
+		    session->parent.SetRecover(response.recover_session)) {
+			send_session_cookie = true;
+			recover_session_to_cookie = response.recover_session;
 		}
 	}
 

prototypes/translate.py
@@ -170,14 +170,19 @@ class Translation(Protocol):
 
         return response
 
-    def _handle_token_auth(self, token_auth, auth_token):
-        log.msg(f"token_auth {token_auth!r} auth_token={auth_token!r}")
+    def _handle_token_auth(self, token_auth, auth_token, recover_session):
+        log.msg(f"token_auth {token_auth!r} auth_token={auth_token!r} recover_session={recover_session!r}")
 
         response = Response(protocol_version=2)
         response.max_age(0)
 
         if auth_token == b'foo' or auth_token == b'bar':
+            response.packet(TRANSLATE_RECOVER_SESSION, b'recover:' + auth_token)
             response.packet(TRANSLATE_USER, auth_token)
+            response.max_age(20)
+        elif recover_session is not None and recover_session[:8] == b'recover:':
+            response.packet(TRANSLATE_RECOVER_SESSION, recover_session)
+            response.packet(TRANSLATE_USER, recover_session[8:])
             response.max_age(20)
 
         return response
@@ -1053,7 +1058,8 @@ class Translation(Protocol):
 
         if request.token_auth is not None:
             return self._handle_token_auth(request.token_auth,
-                                           request.auth_token)
+                                           request.auth_token,
+                                           request.recover_session)
 
         if request.pool is not None:
             return self._handle_pool(request.pool, request.listener_tag, request.host)

python/beng_proxy/translation/protocol.py
@@ -231,6 +231,7 @@ TRANSLATE_ATTACH_SESSION = 224
 TRANSLATE_DISCARD_REALM_SESSION = 225
 TRANSLATE_LIKE_HOST = 226
 TRANSLATE_LAYOUT = 227
+TRANSLATE_RECOVER_SESSION = 228
 
 TRANSLATE_PROXY = TRANSLATE_HTTP # deprecated
 TRANSLATE_LHTTP_EXPAND_URI = TRANSLATE_EXPAND_LHTTP_URI # deprecated

python/beng_proxy/translation/request.py
@@ -54,6 +54,7 @@ class Request:
         self.http_auth = None
         self.token_auth = None
         self.auth_token = None
+        self.recover_session = None
         self.want_full_uri = None
         self.chain = None
         self.chain_header = None
@@ -130,6 +131,8 @@ class Request:
             self.token_auth = packet.payload
         elif packet.command == TRANSLATE_AUTH_TOKEN:
             self.auth_token = packet.payload
+        elif packet.command == TRANSLATE_RECOVER_SESSION:
+            self.recover_session = packet.payload
         elif packet.command == TRANSLATE_WANT_FULL_URI:
             self.want_full_uri = packet.payload
         elif packet.command == TRANSLATE_CHAIN:

src/bp/Request.hxx
@@ -297,6 +297,25 @@ private:
 	 */
 	const char *resource_tag;
 
+	/**
+	 * The RECOVER_COOKIE value parsed from the session cookie.
+	 * This value will be sent to the translation server in a
+	 * TOKEN_AUTH request if there is no session.
+	 *
+	 * This field is set by LoadSession(), but only if a valid
+	 * session was not found.
+	 */
+	const char *recover_session_from_cookie = nullptr;
+
+	/**
+	 * The RECOVER_COOKIE value to be included in the session
+	 * cookie value.
+	 *
+	 * This field is initialized by ApplyTranslateSession().  It
+	 * is only initialized if #send_session_cookie is true.
+	 */
+	const char *recover_session_to_cookie;
+
 	SharedPoolPtr<WidgetContext> widget_context;
 
 public:

src/bp/Response.cxx
@@ -516,6 +516,13 @@ Request::GenerateSetCookie(GrowingBuffer &headers) noexcept
 		headers.Write(session_cookie);
 		headers.Write("=", 1);
 		headers.Write(session_id.Format());
+
+		if (recover_session_to_cookie != nullptr) {
+			headers.Write("/", 1);
+			// TODO: escape the string?
+			headers.Write(recover_session_to_cookie);
+		}
+
 		headers.Write("; HttpOnly; Path=");
 
 		const char *cookie_path = tr.cookie_path;

src/bp/TokenAuth.cxx
@@ -241,6 +241,8 @@ Request::HandleTokenAuth(const TranslateResponse &response) noexcept
 	auto t = NewFromPool<TranslateRequest>(pool);
 	t->token_auth = response.token_auth;
 	t->auth_token = auth_token;
+	if (auth_token == nullptr)
+		t->recover_session = recover_session_from_cookie;
 	t->uri = auth_token != nullptr
 		? RecomposeUri(pool, dissected_uri)
 		: request.uri;

src/bp/session/Session.cxx
@@ -185,6 +185,22 @@ Session::SetTranslate(ConstBuffer<void> _translate) noexcept
 	translate = ConstBuffer<std::byte>::FromVoid(_translate);
 }
 
+bool
+Session::SetRecover(const char *_recover) noexcept
+{
+	assert(_recover != nullptr);
+
+	if (recover != nullptr && StringIsEqual(recover.c_str(), _recover))
+		return false;
+
+	recover = _recover;
+
+	/* TODO: re-send session cookie with modified "recover" value
+	   in all realms? */
+
+	return true;
+}
+
 void
 RealmSession::SetUser(const char *_user, std::chrono::seconds max_age) noexcept
 {

commit e10772cce1c95f41ee1cc87436f6d8e6ec74ae9e

bp/RSession: add missing recover_session_to_cookie initalizer
Fixes ATTACH_SESSION crash caused by commit d7cd333f1574

src/bp/RSession.cxx
@@ -311,6 +311,7 @@ Request::ApplyTranslateSession(const TranslateResponse &response) noexcept
 			   send a new session cookie */
 			session_id = session->parent.id;
 			send_session_cookie = true;
+			recover_session_to_cookie = nullptr;
 		}
 	}
 