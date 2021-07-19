CM4all/beng-proxy

commit 41316df06d7afbad6b2334b36be307136e0a61c6

bp/session/Id: use 64 bit integers
Contemporary computers are easier with 64 bit integers than 32 bit.
Let's switch.

src/bp/session/Id.cxx
@@ -41,13 +41,13 @@ void
 SessionId::Generate()
 {
     for (auto &i : data)
-        i = random_uint32();
+        i = random_uint64();
 }
 
 static auto
-ToClusterNode(uint32_t id, unsigned cluster_size, unsigned cluster_node)
+ToClusterNode(uint64_t id, unsigned cluster_size, unsigned cluster_node)
 {
-    uint32_t remainder = id % (uint32_t)cluster_size;
+    uint64_t remainder = id % (uint64_t)cluster_size;
     assert(remainder < cluster_size);
 
     id -= remainder;
@@ -92,7 +92,7 @@ SessionId::Format(struct session_id_string &string) const
 {
     char *p = string.buffer;
     for (const auto i : data) {
-        format_uint32_hex_fixed(p, i);
+        format_uint64_hex_fixed(p, i);
         p += sizeof(i) * 2;
     }
 

src/bp/session/Id.hxx
@@ -45,7 +45,7 @@
  * The session id data structure.
  */
 class SessionId {
-    std::array<uint32_t, 4> data;
+    std::array<uint64_t, 2> data;
 
 public:
     gcc_pure

commit b7a89759c08d85285bbb480227766ca9f0e480a5

bp/session/Id: store the cluster modulo only in the lower 32 bit
Fixes a regression caused by commit
41316df06d7afbad6b2334b36be307136e0a61c6

There, the SessionId members were switched to 64 bit integers, and the
modulo was inserted into those 64 bits.  However, beng-lb (function
lb_session_get()) uses only the lower 32 bit.  This can cause
confusion with clusters whose size is not a power of two.

src/bp/session/Id.cxx
@@ -49,7 +49,9 @@ static auto
 ToClusterNode(uint64_t id,
 	      unsigned cluster_size, unsigned cluster_node) noexcept
 {
-	uint64_t remainder = id % (uint64_t)cluster_size;
+	/* use only the lower 32 bit because that is what beng-lb's
+	   lb_session_get() function uses */
+	uint32_t remainder = uint32_t(id) % uint32_t(cluster_size);
 	assert(remainder < cluster_size);
 
 	id -= remainder;

src/bp/session/Id.hxx
@@ -88,7 +88,9 @@ public:
 	 */
 	gcc_pure
 	auto GetClusterHash() const noexcept {
-		return data.back();
+		/* truncating to 32 bit because that is what beng-lb's
+		   lb_session_get() function uses */
+		return (uint32_t)data.back();
 	}
 
 	/**