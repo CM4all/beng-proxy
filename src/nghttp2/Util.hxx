450d3e7241a4edd546500b855963f9b666505d0c
Max Kellermann <mk@cm4all.com>
2021-10-12 20:23:12 +0200

    nghttp2/Util: switch to std::string_view

--- src/nghttp2/Util.hxx
+++ src/nghttp2/Util.hxx
@@ -32,9 +32,9 @@
 
 #pragma once
 
-#include "util/StringView.hxx"
-
 #include <nghttp2/nghttp2.h>
+
+#include <string_view>
 
 namespace NgHttp2 {
 
@@ -42,14 +42,14 @@ namespace NgHttp2 {
  * Construct a #nghttp2_nv from two string views.
  */
 constexpr auto
-MakeNv(StringView name, StringView value,
+MakeNv(std::string_view name, std::string_view value,
        uint8_t flags=NGHTTP2_NV_FLAG_NONE) noexcept
 {
 	nghttp2_nv nv{};
-	nv.name = const_cast<uint8_t *>((const uint8_t *)(const void *)name.data);
-	nv.value = const_cast<uint8_t *>((const uint8_t *)(const void *)value.data);
-	nv.namelen = name.size;
-	nv.valuelen = value.size;
+	nv.name = const_cast<uint8_t *>((const uint8_t *)(const void *)name.data());
+	nv.value = const_cast<uint8_t *>((const uint8_t *)(const void *)value.data());
+	nv.namelen = name.size();
+	nv.valuelen = value.size();
 	nv.flags = flags;
 	return nv;
 }
