9dc6bd4b122c5ce20a1cbee1025d6f90393a5836
Max Kellermann <mk@cm4all.com>
2021-10-15 21:22:16 +0200

    system/SetupProcess: reduce timer slack to 10ms
    Some very short timers (e.g. the WAS client response timer) may
    benefit from a shorter timer slack, and the usefulness of
    PR_SET_TIMERSLACK is probably not so large.  Now that our EventLoop
    has a CoarseTimerEvent, most timers have a 1s resolution anyway.

--- src/system/SetupProcess.cxx
+++ src/system/SetupProcess.cxx
@@ -41,8 +41,8 @@ SetupProcess()
 {
     signal(SIGPIPE, SIG_IGN);
 
-    /* timer slack 100ms - we don't care for timer correctness */
-    prctl(PR_SET_TIMERSLACK, 100000000, 0, 0, 0);
+    /* timer slack 10ms - we don't care for timer correctness */
+    prctl(PR_SET_TIMERSLACK, 10000000, 0, 0, 0);
 
     /* reduce glibc's thread cancellation overhead */
     pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, nullptr);
