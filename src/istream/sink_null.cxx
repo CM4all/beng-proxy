961ed65e97b3298c44ec4b59a09021cdb892aade
Max Kellermann <mk@cm4all.com>
2021-11-03 13:46:59 +0100

    istream/sink_null: implement OnDirect() (to /dev/null)

--- src/istream/sink_null.cxx
+++ src/istream/sink_null.cxx
@@ -34,16 +34,35 @@
 #include "Sink.hxx"
 #include "UnusedPtr.hxx"
 #include "pool/pool.hxx"
+#include "io/SpliceSupport.hxx"
+#include "io/UniqueFileDescriptor.hxx"
+
+#include <fcntl.h> // for splice()
 
 class SinkNull final : IstreamSink {
+	UniqueFileDescriptor dev_null;
+
 public:
 	explicit SinkNull(UnusedIstreamPtr &&_input)
-		:IstreamSink(std::move(_input)) {}
+		:IstreamSink(std::move(_input))
+	{
+		input.SetDirect(ISTREAM_TO_CHARDEV);
+	}
 
 	/* virtual methods from class IstreamHandler */
 
 	size_t OnData(const void *, size_t length) noexcept override {
 		return length;
+	}
+
+	ssize_t OnDirect(FdType, int fd, size_t max_length) noexcept override
+	{
+		if (!dev_null.IsDefined())
+			if (!dev_null.Open("/dev/null", O_WRONLY))
+				return ISTREAM_RESULT_ERRNO;
+
+		return splice(fd, nullptr, dev_null.Get(), nullptr, max_length,
+			      SPLICE_F_NONBLOCK | SPLICE_F_MOVE);
 	}
 
 	void OnEof() noexcept override {
