8c575eee23844237b5764ff940c28e0c1f6abaf2
Max Kellermann <mk@cm4all.com>
2021-11-22 15:10:17 +0100

    expand: indent with tabs

--- src/expand.hxx
+++ src/expand.hxx
@@ -47,35 +47,35 @@ template<typename Result, typename MatchInfo>
 void
 ExpandString(Result &result, const char *src, MatchInfo &&match_info)
 {
-    assert(src != nullptr);
+	assert(src != nullptr);
 
-    while (true) {
-        const char *backslash = strchr(src, '\\');
-        if (backslash == nullptr) {
-            /* append the remaining input string and return */
-            result.Append(src);
-            return;
-        }
+	while (true) {
+		const char *backslash = strchr(src, '\\');
+		if (backslash == nullptr) {
+			/* append the remaining input string and return */
+			result.Append(src);
+			return;
+		}
 
-        /* copy everything up to the backslash */
-        result.Append(src, backslash - src);
+		/* copy everything up to the backslash */
+		result.Append(src, backslash - src);
 
-        /* now evaluate the escape */
-        src = backslash + 1;
-        const char ch = *src++;
-        if (ch == '\\')
-            result.Append(ch);
-        else if (ch >= '0' && ch <= '9') {
-            auto c = match_info.GetCapture(ch - '0');
-            if (c.IsNull())
-                throw std::runtime_error("Invalid regex capture");
+		/* now evaluate the escape */
+		src = backslash + 1;
+		const char ch = *src++;
+		if (ch == '\\')
+			result.Append(ch);
+		else if (ch >= '0' && ch <= '9') {
+			auto c = match_info.GetCapture(ch - '0');
+			if (c.IsNull())
+				throw std::runtime_error("Invalid regex capture");
 
-            if (!c.empty())
-                result.AppendValue(c.data, c.size);
-        } else {
-            throw FormatRuntimeError("Invalid backslash escape (0x%02x)", ch);
-        }
-    }
+			if (!c.empty())
+				result.AppendValue(c.data, c.size);
+		} else {
+			throw FormatRuntimeError("Invalid backslash escape (0x%02x)", ch);
+		}
+	}
 }
 
 #endif
--- src/pexpand.cxx
+++ src/pexpand.cxx
@@ -43,84 +43,84 @@
 
 const char *
 expand_string(AllocatorPtr alloc, const char *src,
-              const MatchInfo &match_info)
+	      const MatchInfo &match_info)
 {
-    assert(src != nullptr);
-    assert(match_info.IsDefined());
+	assert(src != nullptr);
+	assert(match_info.IsDefined());
 
-    const size_t length = ExpandStringLength(src, match_info);
-    const auto buffer = alloc.NewArray<char>(length + 1);
+	const size_t length = ExpandStringLength(src, match_info);
+	const auto buffer = alloc.NewArray<char>(length + 1);
 
-    struct Result {
-        char *q;
+	struct Result {
+		char *q;
 
-        explicit Result(char *_q):q(_q) {}
+		explicit Result(char *_q):q(_q) {}
 
-        void Append(char ch) {
-            *q++ = ch;
-        }
+		void Append(char ch) {
+			*q++ = ch;
+		}
 
-        void Append(const char *p) {
-            q = stpcpy(q, p);
-        }
+		void Append(const char *p) {
+			q = stpcpy(q, p);
+		}
 
-        void Append(const char *p, size_t _length) {
-            q = (char *)mempcpy(q, p, _length);
-        }
+		void Append(const char *p, size_t _length) {
+			q = (char *)mempcpy(q, p, _length);
+		}
 
-        void AppendValue(const char *p, size_t _length) {
-            Append(p, _length);
-        }
-    };
+		void AppendValue(const char *p, size_t _length) {
+			Append(p, _length);
+		}
+	};
 
-    Result result(buffer);
-    ExpandString(result, src, match_info);
+	Result result(buffer);
+	ExpandString(result, src, match_info);
 
-    assert(result.q == buffer + length);
-    *result.q = 0;
+	assert(result.q == buffer + length);
+	*result.q = 0;
 
-    return buffer;
+	return buffer;
 }
 
 const char *
 expand_string_unescaped(AllocatorPtr alloc, const char *src,
-                        const MatchInfo &match_info)
+			const MatchInfo &match_info)
 {
-    assert(src != nullptr);
-    assert(match_info.IsDefined());
+	assert(src != nullptr);
+	assert(match_info.IsDefined());
 
-    const size_t length = ExpandStringLength(src, match_info);
-    const auto buffer = alloc.NewArray<char>(length + 1);
+	const size_t length = ExpandStringLength(src, match_info);
+	const auto buffer = alloc.NewArray<char>(length + 1);
 
-    struct Result {
-        char *q;
+	struct Result {
+		char *q;
 
-        explicit Result(char *_q):q(_q) {}
+		explicit Result(char *_q):q(_q) {}
 
-        void Append(char ch) {
-            *q++ = ch;
-        }
+		void Append(char ch) {
+			*q++ = ch;
+		}
 
-        void Append(const char *p) {
-            q = stpcpy(q, p);
-        }
+		void Append(const char *p) {
+			q = stpcpy(q, p);
+		}
 
-        void Append(const char *p, size_t _length) {
-            q = (char *)mempcpy(q, p, _length);
-        }
+		void Append(const char *p, size_t _length) {
+			q = (char *)mempcpy(q, p, _length);
+		}
 
-        void AppendValue(const char *p, size_t _length) {
-            q = UriUnescape(q, {p, _length});
-            if (q == nullptr)
-                throw std::runtime_error("Malformed URI escape");
-        }
-    };
+		void AppendValue(const char *p, size_t _length) {
+			q = UriUnescape(q, {p, _length});
+			if (q == nullptr)
+				throw std::runtime_error("Malformed URI escape");
+		}
+	};
 
-    Result result(buffer);
-    ExpandString(result, src, match_info);
+	Result result(buffer);
+	ExpandString(result, src, match_info);
 
-    assert(result.q <= buffer + length);
-    *result.q = 0;
+	assert(result.q <= buffer + length);
+	*result.q = 0;
 
-    return buffer;
+	return buffer;
 }
