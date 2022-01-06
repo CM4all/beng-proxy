bbfd613be2985ce5fa0fbfbd8359241996d5bd5b
Max Kellermann <mk@cm4all.com>
2022-01-06 18:41:40 +0100

    ssl/AlpnIterator: new class

--- src/ssl/AlpnIterator.hxx
+++ src/ssl/AlpnIterator.hxx
@@ -30,24 +30,62 @@
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "AlpnSelect.hxx"
-#include "AlpnIterator.hxx"
+#pragma once
 
-#include <assert.h>
-#include <string.h>
+/**
+ * An iterable range of ALPN strrings.
+ */
+class AlpnRange {
+	using Span = std::span<const unsigned char>;
+	Span s;
 
-std::span<const unsigned char>
-FindAlpn(std::span<const unsigned char> haystack,
-	 std::span<const unsigned char> needle) noexcept
-{
-	assert(!needle.empty());
-	assert((size_t)needle.front() + 1 == needle.size());
+public:
+	constexpr AlpnRange(Span _s) noexcept
+		:s(_s) {}
 
-	for (const auto i : AlpnRange{haystack}) {
-		if (i.size() == needle.size() &&
-		    memcmp(i.data(), needle.data(), needle.size()) == 0)
-			return i;
+	class const_iterator {
+		Span current, rest;
+
+	public:
+		const_iterator(Span _current, Span _rest) noexcept
+			:current(_current), rest(_rest) {}
+
+		const_iterator(std::pair<Span, Span> p) noexcept
+			:const_iterator(p.first, p.second) {}
+
+		constexpr Span operator*() const noexcept {
+			return current;
+		}
+
+		constexpr const_iterator &operator++() noexcept {
+			auto p = Split(rest);
+			current = p.first;
+			rest = p.second;
+			return *this;
+		}
+
+		bool operator==(const_iterator other) const noexcept {
+			return current.data() == other.current.data();
+		}
+	};
+
+	const_iterator begin() const noexcept {
+		return Split(s);
 	}
 
-	return {};
-}
+	const_iterator end() const noexcept {
+		return {s.subspan(s.size()), s};
+	}
+
+private:
+	static constexpr std::pair<Span, Span> Split(Span s) noexcept {
+		if (s.empty())
+			return {s.subspan(s.size()), s};
+
+		std::size_t size = s.front() + 1;
+		if (s.size() < size)
+			return {s.subspan(s.size()), s};
+
+		return {s.subspan(0, size), s.subspan(size)};
+	}
+};
