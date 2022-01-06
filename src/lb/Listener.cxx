CM4all/beng-proxy

commit 7e7a13793b0043cf4342d7adf2758fe9cf904bee

ssl/Factory: move code to constructor

src/lb/Listener.cxx
@@ -61,8 +61,8 @@ MakeSslFactory(const LbListenerConfig &config,
 	(void)instance;
 #endif
 
-	auto ssl_factory = ssl_factory_new_server(config.ssl_config,
-						  std::move(sni_callback));
+	auto ssl_factory = std::make_unique<SslFactory>(config.ssl_config,
+							nullptr);
 
 	/* we use the listener name as OpenSSL session_id_context,
 	   because listener names are unique, so I hope this should be

src/bp/Listener.cxx
@@ -48,7 +48,7 @@ MakeSslFactory(const SslConfig *ssl_config)
 	if (ssl_config == nullptr)
 		return nullptr;
 
-	auto ssl_factory = ssl_factory_new_server(*ssl_config, nullptr);
+	auto ssl_factory = std::make_unique<SslFactory>(*ssl_config, nullptr);
 	// TODO: call SetSessionIdContext()
 
 #ifdef HAVE_NGHTTP2

src/lb/lb_check.cxx
@@ -60,8 +60,7 @@ static void
 lb_check(const LbListenerConfig &config)
 {
 	if (config.ssl) {
-		ssl_factory_new_server(config.ssl_config,
-				       std::unique_ptr<SslCertCallback>());
+		SslFactory(config.ssl_config, nullptr);
 	}
 }
 

src/ssl/Factory.cxx
@@ -123,22 +123,6 @@ struct SslFactoryCertKey {
 	}
 };
 
-void
-SslFactory::LoadCertsKeys(const SslConfig &config)
-{
-	assert(!ssl_ctx);
-
-	ssl_ctx = CreateBasicSslCtx(true);
-
-	ApplyServerConfig(*ssl_ctx, config);
-
-	cert_key.reserve(config.cert_key.size());
-
-	for (const auto &c : config.cert_key) {
-		cert_key.emplace_back(c);
-	}
-}
-
 inline bool
 SslFactoryCertKey::Name::Match(StringView host_name) const noexcept
 {
@@ -172,10 +156,23 @@ SslFactoryCertKey::MatchCommonName(StringView host_name) const noexcept
 	return false;
 }
 
-inline
-SslFactory::SslFactory(std::unique_ptr<SslCertCallback> &&_cert_callback) noexcept
-	:cert_callback(std::move(_cert_callback))
+SslFactory::SslFactory(const SslConfig &config,
+		       std::unique_ptr<SslCertCallback> _cert_callback)
+	:ssl_ctx(CreateBasicSslCtx(true)),
+	 cert_callback(std::move(_cert_callback))
 {
+	assert(!config.cert_key.empty());
+
+	ApplyServerConfig(*ssl_ctx, config);
+
+	cert_key.reserve(config.cert_key.size());
+	for (const auto &c : config.cert_key)
+		cert_key.emplace_back(c);
+
+	if (cert_key.size() > 1 || cert_callback)
+		SSL_CTX_set_cert_cb(ssl_ctx.get(), CertCallback, this);
+	else if (!cert_key.empty())
+		cert_key.front().Apply(*ssl_ctx);
 }
 
 SslFactory::~SslFactory() noexcept = default;
@@ -234,21 +231,6 @@ SslFactory::CertCallback(SSL *ssl, void *arg) noexcept
 	return f.CertCallback(*ssl);
 }
 
-inline void
-SslFactory::EnableSNI()
-{
-	SSL_CTX_set_cert_cb(ssl_ctx.get(), CertCallback, this);
-}
-
-inline void
-SslFactory::AutoEnableSNI()
-{
-	if (cert_key.size() > 1 || cert_callback)
-		EnableSNI();
-	else if (!cert_key.empty())
-		cert_key.front().Apply(*ssl_ctx);
-}
-
 void
 SslFactory::SetSessionIdContext(ConstBuffer<void> _sid_ctx)
 {
@@ -294,18 +276,4 @@ SslFactoryCertKey::SslFactoryCertKey(const SslCertKeyConfig &config)
 	key = std::move(ck.second);
 
 	names = GetCertificateNames<Name>(*cert);
-}
-
-std::unique_ptr<SslFactory>
-ssl_factory_new_server(const SslConfig &config,
-		       std::unique_ptr<SslCertCallback> &&sni)
-{
-	assert(!config.cert_key.empty());
-
-	std::unique_ptr<SslFactory> factory(new SslFactory(std::move(sni)));
-
-	factory->LoadCertsKeys(config);
-	factory->AutoEnableSNI();
-
-	return factory;
 }

src/ssl/Factory.hxx
@@ -53,16 +53,12 @@ class SslFactory {
 	const std::unique_ptr<SslCertCallback> cert_callback;
 
 public:
-	explicit SslFactory(std::unique_ptr<SslCertCallback> &&_cert_callback) noexcept;
+	SslFactory(const SslConfig &config,
+		   std::unique_ptr<SslCertCallback> _cert_callback);
 	~SslFactory() noexcept;
-
-	void LoadCertsKeys(const SslConfig &config);
 
 	[[gnu::pure]]
 	const SslFactoryCertKey *FindCommonName(StringView host_name) const noexcept;
-
-	void EnableSNI();
-	void AutoEnableSNI();
 
 	/**
 	 * Wrapper for SSL_CTX_set_session_id_context().
@@ -86,7 +82,3 @@ private:
 	int CertCallback(SSL &ssl) noexcept;
 	static int CertCallback(SSL *ssl, void *arg) noexcept;
 };
-
-std::unique_ptr<SslFactory>
-ssl_factory_new_server(const SslConfig &config,
-		       std::unique_ptr<SslCertCallback> &&cert_callback);

commit 7dc7f4dc5c78886d19db081af824b2466e21453b

lb/Listener: fix certdb support
Regression by commit 7e7a13793b0043cf4342d7adf2758fe9cf904bee

src/lb/Listener.cxx
@@ -62,7 +62,7 @@ MakeSslFactory(const LbListenerConfig &config,
 #endif
 
 	auto ssl_factory = std::make_unique<SslFactory>(config.ssl_config,
-							nullptr);
+							std::move(sni_callback));
 
 	/* we use the listener name as OpenSSL session_id_context,
 	   because listener names are unique, so I hope this should be