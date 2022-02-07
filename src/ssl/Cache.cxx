CM4all/beng-proxy

commit 514c0c83785f03c4aae7e0e9206ba72a7427a0de

ssl/Cache: switch to asynchronous mode
Maintain only one PostgreSQL connection, and use it only from the main
thread, asynchronously with a queue of certificate callbacks.  We can
defer OpenSSL's cert_cb with SSL_ERROR_WANT_X509_LOOKUP, and resume it
as soon as the PostgreSQL query returns.

src/ssl/Cache.cxx
@@ -32,11 +32,15 @@
 
 #include "Cache.hxx"
 #include "SessionCache.hxx"
+#include "CompletionHandler.hxx"
 #include "lib/openssl/Name.hxx"
 #include "lib/openssl/AltName.hxx"
 #include "lib/openssl/Error.hxx"
 #include "lib/openssl/LoadFile.hxx"
 #include "certdb/Wildcard.hxx"
+#include "certdb/CoCertDatabase.hxx"
+#include "co/InvokeTask.hxx"
+#include "co/Task.hxx"
 #include "event/Loop.hxx"
 #include "util/AllocatedString.hxx"
 
@@ -44,9 +48,150 @@
 
 #include <set>
 
+struct CertCache::Request final : AutoUnlinkIntrusiveListHook, Cancellable {
+	SSL &ssl;
+
+	explicit Request(SSL &_ssl) noexcept
+		:ssl(_ssl)
+	{
+		auto &handler = GetSslCompletionHandler(ssl);
+		assert(!handler.cancel_ptr);
+		handler.cancel_ptr = *this;
+	}
+
+	/* virtual methods from class Cancellable */
+	void Cancel() noexcept override {
+		delete this;
+	}
+};
+
+class CertCache::Query {
+	CertCache &cache;
+
+	std::string host, special;
+
+	IntrusiveList<Request> requests;
+
+	Co::InvokeTask invoke_task;
+
+public:
+	template<typename H, typename S>
+	Query(CertCache &_cache, H &&_host, S &&_special) noexcept
+		:cache(_cache),
+		 host(std::forward<H>(_host)),
+		 special(std::forward<S>(_special))
+	{
+	}
+
+	~Query() noexcept {
+		assert(requests.empty());
+	}
+
+	void AddRequest(Request &request) noexcept {
+		requests.push_back(request);
+	}
+
+	bool IsRunning() const noexcept {
+		return invoke_task;
+	}
+
+	bool IsCancelled() const noexcept {
+		return requests.empty();
+	}
+
+	void Start() noexcept {
+		assert(&cache.current_query->second == this);
+		assert(!IsRunning());
+
+		invoke_task = Run();
+		invoke_task.Start(BIND_THIS_METHOD(OnCompletion));
+	}
+
+	/**
+	 * Stop execution of the coroutine.  This is only supposed to
+	 * be called during shutdown, when it is expected that all
+	 * requests will be canceled.
+	 */
+	void Stop() noexcept {
+		assert(&cache.current_query->second == this);
+		assert(IsRunning());
+
+		invoke_task = {};
+	}
+
+private:
+	Co::InvokeTask Run();
+
+	void OnCompletion(std::exception_ptr error) noexcept;
+};
+
+Co::InvokeTask
+CertCache::Query::Run()
+{
+	assert(&cache.current_query->second == this);
+
+	const char *_special = special.empty() ? nullptr : special.c_str();
+
+	auto cert_key = co_await CoGetServerCertificateKey(cache.db,
+							   cache.config,
+							   host.c_str(),
+							   _special);
+	if (!cert_key.first)
+		/* certificate was not found; the
+		   SslCompletionHandlers will be invoked by
+		   OnCompletion() */
+		co_return;
+
+	cert_key = cache.Add(std::move(cert_key.first),
+			     std::move(cert_key.second),
+			     _special);
+
+	requests.clear_and_dispose([this, &cert_key](Request *request){
+		cache.state_idx.Set(request->ssl, State::COMPLETE);
+		cache.Apply(request->ssl, cert_key);
+		InvokeSslCompletionHandler(request->ssl);
+		delete request;
+	});
+}
+
+inline void
+CertCache::Query::OnCompletion(std::exception_ptr error) noexcept
+{
+	assert(&cache.current_query->second == this);
+
+	if (error)
+		cache.logger(1, error);
+
+	/* invoke all remaining SslCompletionHandlers; this is
+	   only relevant if Run() has not finished
+	   sucessfully */
+	requests.clear_and_dispose([this](Request *request){
+		cache.state_idx.Set(request->ssl, State::NOT_FOUND);
+		InvokeSslCompletionHandler(request->ssl);
+		delete request;
+	});
+
+	/* copy the "cache" reference to the stack because the
+	   following erase() call will delete this object */
+	auto &_cache = cache;
+
+	{
+		const std::unique_lock<std::mutex> lock{_cache.mutex};
+		_cache.queries.erase(_cache.current_query);
+	}
+
+	_cache.current_query = _cache.queries.end();
+
+	/* start the next query */
+	_cache.StartQuery();
+}
+
 CertCache::CertCache(EventLoop &event_loop,
 		     const CertDatabaseConfig &_config) noexcept
 	:logger("CertCache"), config(_config),
+	 query_added_notify(event_loop, BIND_THIS_METHOD(StartQuery)),
+	 db(event_loop, config.connect.c_str(), config.schema.c_str(),
+	    *this),
 	 name_cache(event_loop, _config, *this)
 {
 }
@@ -87,6 +232,7 @@ CertCache::LoadCaCertificate(const char *path)
 void
 CertCache::Connect() noexcept
 {
+	db.Connect();
 	name_cache.Connect();
 }
 
@@ -94,6 +240,15 @@ void
 CertCache::Disconnect() noexcept
 {
 	name_cache.Disconnect();
+
+	if (current_query != queries.end()) {
+		const std::unique_lock lock{mutex};
+		current_query->second.Stop();
+		current_query = queries.end();
+	}
+
+	db.Disconnect();
+	query_added_notify.Disable();
 }
 
 inline CertCache::CertKey
@@ -131,20 +286,6 @@ CertCache::Add(UniqueX509 &&cert, UniqueEVP_PKEY &&key, const char *special)
 	return i->second.UpRef();
 }
 
-std::optional<CertCache::CertKey>
-CertCache::Query(const char *host, const char *special)
-{
-	auto db = dbs.Get(config);
-	db->EnsureConnected();
-
-	auto cert_key = db->GetServerCertificateKey(host, special);
-	if (!cert_key.second)
-		return std::nullopt;
-
-	return Add(std::move(cert_key.first), std::move(cert_key.second),
-		   special);
-}
-
 inline std::optional<CertCache::CertKey>
 CertCache::GetNoWildCardCached(const char *host, const char *_special) noexcept
 {
@@ -166,32 +307,60 @@ CertCache::GetNoWildCardCached(const char *host, const char *_special) noexcept
 	return std::nullopt;
 }
 
-std::optional<CertCache::CertKey>
-CertCache::GetNoWildCard(const char *host, const char *special)
+void
+CertCache::StartQuery() noexcept
 {
-	if (auto item = GetNoWildCardCached(host, special))
-		return item;
+	if (current_query != queries.end())
+		/* already busy */
+		return;
 
-	if (name_cache.Lookup(host)) {
-		if (auto item = Query(host, special))
-			return item;
+	if (!db.IsReady())
+		/* database is (re)connecting */
+		return;
+
+	/* pick an arbitrary request and start the database query */
+	const std::unique_lock<std::mutex> lock{mutex};
+	while (!queries.empty()) {
+		auto i = queries.begin();
+		if (!i->second.IsCancelled()) {
+			/* found a candidate - start it */
+			current_query = i;
+			current_query->second.Start();
+			break;
+		}
+
+		/* this query was scheduled, but meanwhile all
+		   requests were cancelled, so don't bother */
+		queries.erase(i);
 	}
-
-	return std::nullopt;
 }
 
-std::optional<CertCache::CertKey>
-CertCache::Get(const char *host, const char *special)
+void
+CertCache::ScheduleQuery(SSL &ssl, const char *host,
+			 const char *special) noexcept
 {
-	auto item = GetNoWildCard(host, special);
-	if (!item) {
-		/* not found: try the wildcard */
-		const auto wildcard = MakeCommonNameWildcard(host);
-		if (!wildcard.empty())
-			item = GetNoWildCard(wildcard.c_str(), special);
+	std::string key(host);
+	if (special != nullptr) {
+		key.push_back(0);
+		key.append(special);
 	}
 
-	return item;
+	bool was_empty;
+
+	{
+		const std::unique_lock<std::mutex> lock{mutex};
+		was_empty = queries.empty();
+
+		const char *_special = special != nullptr ? special : "";
+		auto &query = queries.try_emplace(std::move(key),
+						  *this, host, _special).first->second;
+
+		auto *request = new Request(ssl);
+		query.AddRequest(*request);
+	}
+
+	if (was_empty)
+		query_added_notify.Signal();
 }
 
 inline void
@@ -220,15 +389,53 @@ CertCache::Apply(SSL &ssl, const CertKey &cert_key)
 	Apply(ssl, *cert_key.first, *cert_key.second);
 }
 
-bool
-CertCache::Apply(SSL &ssl, const char *host, const char *special)
+LookupCertResult
+CertCache::Apply(SSL &ssl, const char *host,
+		 const char *special) noexcept
 {
-	const auto item = Get(host, special);
-	if (!item)
-		return false;
+	assert(db.IsDefined());
 
-	Apply(ssl, *item);
-	return true;
+	switch (state_idx.Get(ssl)) {
+	case State::NONE:
+		break;
+
+	case State::IN_PROGRESS:
+		/* registered again, already in progress */
+		return LookupCertResult::IN_PROGRESS;
+
+	case State::COMPLETE:
+		/* registered again, but was already found */
+		return LookupCertResult::COMPLETE;
+
+	case State::NOT_FOUND:
+		/* registered again, but was not found */
+		return LookupCertResult::NOT_FOUND;
+	}
+
+	if (auto item = GetNoWildCardCached(host, special)) {
+		state_idx.Set(ssl, State::COMPLETE);
+		Apply(ssl, *item);
+		return LookupCertResult::COMPLETE;
+	}
+
+	const auto wildcard = MakeCommonNameWildcard(host);
+	if (!wildcard.empty()) {
+		if (auto item = GetNoWildCardCached(wildcard.c_str(), special)) {
+			state_idx.Set(ssl, State::COMPLETE);
+			Apply(ssl, *item);
+			return LookupCertResult::COMPLETE;
+		}
+	}
+
+	if (name_cache.Lookup(host) ||
+	    (!wildcard.empty() && name_cache.Lookup(wildcard.c_str()))) {
+		state_idx.Set(ssl, State::IN_PROGRESS);
+		ScheduleQuery(ssl, host, special);
+		return LookupCertResult::IN_PROGRESS;
+	}
+
+	state_idx.Set(ssl, State::NOT_FOUND);
+	return LookupCertResult::NOT_FOUND;
 }
 
 bool
@@ -269,4 +476,30 @@ CertCache::OnCertModified(const std::string &name, bool deleted) noexcept
 		logger.Format(5, "flushed %s certificate '%s'",
 			      deleted ? "deleted" : "modified",
 			      name.c_str());
+}
+
+void
+CertCache::OnConnect()
+{
+	logger(5, "connected to certificate database");
+
+	StartQuery();
+}
+
+void
+CertCache::OnDisconnect() noexcept
+{
+	logger(4, "disconnected from certificate database");
+}
+
+void
+CertCache::OnNotify(const char *name)
+{
+	logger(5, "received notify '", name, "'");
+}
+
+void
+CertCache::OnError(std::exception_ptr e) noexcept
+{
+	logger(1, e);
 }

meson.build
@@ -383,6 +383,7 @@ if get_option('certdb')
 
   ssl2_sources += [
     '../certdb/CertDatabase.cxx',
+    '../certdb/CoCertDatabase.cxx',
     '../certdb/FromResult.cxx',
     '../certdb/WrapKey.cxx',
     '../certdb/Wildcard.cxx',

src/certdb/CoCertDatabase.cxx
@@ -30,34 +30,41 @@
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#pragma once
-
-#include "LookupCertResult.hxx"
-
-#include <openssl/ossl_typ.h>
-
-class SslCompletionHandler;
-class CancellablePointer;
+#include "CoCertDatabase.hxx"
+#include "Queries.hxx"
+#include "FromResult.hxx"
+#include "pg/CoQuery.hxx"
+#include "co/Task.hxx"
 
 /**
- * C++ wrapper for the SSL_CTX_set_cert_cb() callback function.
+ * A callable which invokes Pg::CoQuery().
  */
-class SslCertCallback {
-public:
-	virtual ~SslCertCallback() noexcept = default;
+struct CoQueryWrapper {
+	Pg::AsyncConnection &connection;
 
-	/**
-	 * The actual certificate callback.  This method is supposed
-	 * to look up the given host name and then call
-	 * SSL_use_certificate() and SSL_use_PrivateKey().
-	 *
-	 * May throw on error.
-	 *
-	 * @param ssl a #SSL object which must have a
-	 * #SslCompletionHandler (via SetSslCompletionHandler()); this
-	 * handler will be invoked after this method has returned
-	 * #IN_PROGRESS; using its #CancellablePointer field, the
-	 * caller may cancel the operation
-	 */
-	virtual LookupCertResult OnCertCallback(SSL &ssl, const char *name) = 0;
+	template<typename... Params>
+	auto operator()(const Params&... params) const {
+		return Pg::CoQuery(connection,
+				   Pg::CoQuery::CancelType::DISCARD,
+				   params...);
+	}
 };
+
+Co::Task<std::pair<UniqueX509, UniqueEVP_PKEY>>
+CoGetServerCertificateKey(Pg::AsyncConnection &connection,
+			  const CertDatabaseConfig &config,
+			  const char *name, const char *special)
+{
+	const CoQueryWrapper q{connection};
+
+	auto result = co_await FindServerCertificateKeyByName(q, name, special);
+	if (result.GetRowCount() == 0) {
+		/* no matching common_name; check for an altName */
+		// TODO do both queries, use the most recent record
+		result = co_await FindServerCertificateKeyByAltName(q, name, special);
+		if (result.GetRowCount() == 0)
+			co_return std::make_pair(nullptr, nullptr);
+	}
+
+	co_return LoadCertificateKey(config, result, 0, 0);
+}

src/certdb/CoCertDatabase.hxx
@@ -32,32 +32,17 @@
 
 #pragma once
 
-#include "LookupCertResult.hxx"
+#include "lib/openssl/UniqueEVP.hxx"
+#include "lib/openssl/UniqueX509.hxx"
 
-#include <openssl/ossl_typ.h>
+#include <utility>
 
-class SslCompletionHandler;
-class CancellablePointer;
+struct CertDatabaseConfig;
 
-/**
- * C++ wrapper for the SSL_CTX_set_cert_cb() callback function.
- */
-class SslCertCallback {
-public:
-	virtual ~SslCertCallback() noexcept = default;
+namespace Pg { class AsyncConnection; }
+namespace Co { template<typename T> class Task; }
 
-	/**
-	 * The actual certificate callback.  This method is supposed
-	 * to look up the given host name and then call
-	 * SSL_use_certificate() and SSL_use_PrivateKey().
-	 *
-	 * May throw on error.
-	 *
-	 * @param ssl a #SSL object which must have a
-	 * #SslCompletionHandler (via SetSslCompletionHandler()); this
-	 * handler will be invoked after this method has returned
-	 * #IN_PROGRESS; using its #CancellablePointer field, the
-	 * caller may cancel the operation
-	 */
-	virtual LookupCertResult OnCertCallback(SSL &ssl, const char *name) = 0;
-};
+Co::Task<std::pair<UniqueX509, UniqueEVP_PKEY>>
+CoGetServerCertificateKey(Pg::AsyncConnection &connection,
+			  const CertDatabaseConfig &config,
+			  const char *name, const char *special);

src/ssl/Cache.hxx
@@ -33,13 +33,17 @@
 #pragma once
 
 #include "NameCache.hxx"
+#include "LookupCertResult.hxx"
 #include "lib/openssl/Hash.hxx"
 #include "lib/openssl/UniqueX509.hxx"
 #include "lib/openssl/UniqueEVP.hxx"
+#include "lib/openssl/IntegralExDataIndex.hxx"
 #include "certdb/Config.hxx"
-#include "certdb/CertDatabase.hxx"
-#include "stock/ThreadedStock.hxx"
+#include "pg/AsyncConnection.hxx"
+#include "thread/Notify.hxx"
 #include "io/Logger.hxx"
+#include "util/Cancellable.hxx"
+#include "util/IntrusiveList.hxx"
 
 #include <unordered_map>
 #include <map>
@@ -58,10 +62,29 @@ class CertDatabase;
  * instance.  It is thread-safe, designed to be called synchronously
  * by worker threads (via #SslFilter).
  */
-class CertCache final : CertNameCacheHandler {
+class CertCache final : Pg::AsyncConnectionHandler, CertNameCacheHandler {
 	const LLogger logger;
 
 	const CertDatabaseConfig config;
+
+	enum class State {
+		NONE = 0,
+		IN_PROGRESS,
+		COMPLETE,
+		NOT_FOUND,
+	};
+
+	const OpenSSL::IntegralExDataIndex<State> state_idx;
+
+	/**
+	 * Used to move the Apply() call from a worker thread to the
+	 * main thread.  The worker thread adds a #Request/#Query to
+	 * the #queries map and then signals this object, which
+	 * triggers a StartQuery() call in the main thread.
+	 */
+	Notify query_added_notify;
+
+	Pg::AsyncConnection db;
 
 	CertNameCache name_cache;
 
@@ -76,12 +99,7 @@ class CertCache final : CertNameCacheHandler {
 	std::map<SHA1Digest, std::forward_list<UniqueX509>, SHA1Compare> ca_certs;
 
 	/**
-	 * Database connections used by worker threads.
-	 */
-	ThreadedStock<CertDatabase> dbs;
-
-	/**
-	 * Protects #map.
+	 * Protects #map, #queries.
 	 */
 	std::mutex mutex;
 
@@ -125,6 +143,20 @@ class CertCache final : CertNameCacheHandler {
 	 */
 	std::unordered_multimap<std::string, Item> map;
 
+	struct Request;
+	class Query;
+
+	using QueryMap = std::map<std::string, Query>;
+	QueryMap queries;
+
+	/**
+	 * The query that is currently being executed.
+	 *
+	 * This field is not protected by #mutex because it is
+	 * accessed only from the main thread.
+	 */
+	QueryMap::iterator current_query = queries.end();
+
 public:
 	CertCache(EventLoop &event_loop,
 		  const CertDatabaseConfig &_config) noexcept;
@@ -144,22 +176,34 @@ public:
 
 	/**
 	 * Look up a certificate by host name, and set it in the given
-	 * #SSL.  Returns true on success, false if a certificate for
-	 * that name was not found, and throws an exception on error.
+	 * #SSL.
+	 *
+	 * @param ssl a #SSL object which must have a
+	 * #SslCompletionHandler (via SetSslCompletionHandler()); this
+	 * handler will be invoked after this method has returned
+	 * #IN_PROGRESS; using its #CancellablePointer field, the
+	 * caller may cancel the operation
 	 */
-	bool Apply(SSL &ssl, const char *host, const char *special);
+	LookupCertResult Apply(SSL &ssl, const char *host,
+			       const char *special) noexcept;
 
 private:
+	/**
+	 * Add the given certificate/key pair to the cache.
+	 *
+	 * This method locks the mutex when necessary.
+	 */
 	CertKey Add(UniqueX509 &&cert, UniqueEVP_PKEY &&key,
-			const char *special);
-	std::optional<CertKey> Query(const char *host, const char *special);
+		    const char *special);
 
 	[[gnu::pure]]
 	std::optional<CertKey> GetNoWildCardCached(const char *host,
 						   const char *special) noexcept;
 
-	std::optional<CertKey> GetNoWildCard(const char *host, const char *special);
-	std::optional<CertKey> Get(const char *host, const char *special);
+	void StartQuery() noexcept;
+
+	void ScheduleQuery(SSL &ssl, const char *host,
+			   const char *special) noexcept;
 
 	void Apply(SSL &ssl, X509 &cert, EVP_PKEY &key);
 	void Apply(SSL &ssl, const CertKey &cert_key);
@@ -172,6 +216,12 @@ private:
 	 * @return true if at least one item was found and deleted
 	 */
 	bool Flush(const std::string &name) noexcept;
+
+	/* virtual methods from Pg::AsyncConnectionHandler */
+	void OnConnect() override;
+	void OnDisconnect() noexcept override;
+	void OnNotify(const char *name) override;
+	void OnError(std::exception_ptr e) noexcept override;
 
 	/* virtual methods from class CertNameCacheHandler */
 	void OnCertModified(const std::string &name,

src/ssl/DbCertCallback.cxx
@@ -67,7 +67,5 @@ DbSslCertCallback::OnCertCallback(SSL &ssl, const char *name)
 	    Equals(alpn_selected, std::span{alpn_acme_tls1}.subspan(1)))
 		special = "acme-alpn-tls-01";
 
-	return cache.Apply(ssl, name, special)
-		? LookupCertResult::COMPLETE
-		: LookupCertResult::NOT_FOUND;
+	return cache.Apply(ssl, name, special);
 }

commit defa54be301a23186e02fcf8b0ebd49500ba2085

ssl/Cache: fix wildcard support
Was broken by commit 514c0c83785f03c4aae7e0e9206ba72a7427a0de

src/ssl/Cache.cxx
@@ -125,6 +125,25 @@ private:
 	void OnCompletion(std::exception_ptr error) noexcept;
 };
 
+static Co::Task<std::pair<UniqueX509, UniqueEVP_PKEY>>
+CoGetServerCertificateKeyMaybeWildcard(Pg::AsyncConnection &connection,
+				       const CertDatabaseConfig &config,
+				       const char *name, const char *special)
+{
+	auto cert_key = co_await CoGetServerCertificateKey(connection, config,
+							   name, special);
+	if (!cert_key.first) {
+		const auto wildcard = MakeCommonNameWildcard(name);
+		if (!wildcard.empty())
+			cert_key = co_await
+				CoGetServerCertificateKey(connection, config,
+							  wildcard.c_str(),
+							  special);
+	}
+
+	co_return cert_key;
+}
+
 Co::InvokeTask
 CertCache::Query::Run()
 {
@@ -132,10 +151,10 @@ CertCache::Query::Run()
 
 	const char *_special = special.empty() ? nullptr : special.c_str();
 
-	auto cert_key = co_await CoGetServerCertificateKey(cache.db,
-							   cache.config,
-							   host.c_str(),
-							   _special);
+	auto cert_key = co_await CoGetServerCertificateKeyMaybeWildcard(cache.db,
+									cache.config,
+									host.c_str(),
+									_special);
 	if (!cert_key.first)
 		/* certificate was not found; the
 		   SslCompletionHandlers will be invoked by