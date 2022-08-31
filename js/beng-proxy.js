CM4all/beng-proxy

commit 9b9bdf2ca8c56efd0ef971eaed8007e973f6a7a2

js: use strict (in)equality operators
Closes https://github.com/CM4all/beng-proxy/issues/3

js/beng-proxy.js
@@ -11,32 +11,32 @@ function beng_widget_uri(base_uri, session_id, frame, focus, mode,
         return encodeURIComponent(x).replace(/%/g, '$');
     }
 
-    if (base_uri == null ||
-        (mode != null && mode != "focus" && mode != "frame" &&
-         mode != "partial" && mode != "save"))
+    if (base_uri === null ||
+        (mode !== null && mode !== "focus" && mode !== "frame" &&
+         mode !== "partial" && mode !== "save"))
         return null;
 
     let params = "";
 
-    if (translate != null)
+    if (translate !== null)
         params += "&translate=" + _beng_proxy_escape(translate);
 
-    if (focus != null) {
-        if (mode == "frame")
+    if (focus !== null) {
+        if (mode === "frame")
             mode = "partial";
 
         params += "&focus=" + _beng_proxy_escape(focus);
-        if (mode == "partial" || mode == "save")
+        if (mode === "partial" || mode === "save")
             frame = focus;
 
-        if (frame != null) {
+        if (frame !== null) {
             params += "&frame=" + _beng_proxy_escape(frame);
 
-            if (view != null)
+            if (view !== null)
                 params += "&view=" + _beng_proxy_escape(view);
         }
 
-        if (path != null) {
+        if (path !== null) {
             let query_string = null;
             let qmark = path.indexOf("?");
             if (qmark >= 0) {
@@ -44,7 +44,7 @@ function beng_widget_uri(base_uri, session_id, frame, focus, mode,
                 path = path.substring(0, qmark);
             }
             params += "&path=" + _beng_proxy_escape(path);
-            if (query_string != null)
+            if (query_string !== null)
                 params += query_string;
         }
     }

commit ebae76a78362faedf081097e53348f503ea933f3

js: fix strict (in)equality comparison
Commit 9b9bdf2c has caused a change of behaviour of function beng_widget_uri
because of missing parameters translate and view resulting in undefined
instead of null.

js/beng-proxy.js
@@ -62,7 +62,7 @@ function beng_widget_uri(base_uri, session_id, frame, focus, mode,
         window.cm4all.widgets[path] = {
             url: function(pathInfo, options) {
                 options = options || {};
-                return beng_widget_uri(base, session, frame, path, options.mode || "partial", pathInfo || '');
+                return beng_widget_uri(base, session, frame, path, options.mode || "partial", pathInfo || '', null, null);
             }
         };
     };