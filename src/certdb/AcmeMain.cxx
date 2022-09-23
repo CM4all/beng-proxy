CM4all/beng-proxy

commit 8327fa66b23c6275db5532e29987e527bf454e3f

lib/openssl/UniqueCertKey: new struct

src/certdb/AcmeMain.cxx
@@ -53,6 +53,7 @@
 #include "lib/openssl/Name.hxx"
 #include "lib/openssl/GeneralName.hxx"
 #include "lib/openssl/UniqueX509.hxx"
+#include "lib/openssl/UniqueCertKey.hxx"
 #include "lib/openssl/Error.hxx"
 #include "util/AllocatedString.hxx"
 #include "util/ConstBuffer.hxx"
@@ -486,11 +487,11 @@ AcmeRenewCert(const CertDatabaseConfig &db_config, const AcmeConfig &config,
 		throw "Neither --alpn nor --challenge-directory nor --dns-txt-program specified";
 
 	const auto old_cert_key = db.GetServerCertificateKeyByHandle(handle);
-	if (!old_cert_key.second)
+	if (old_cert_key)
 		throw "Old certificate not found in database";
 
-	auto &old_cert = *old_cert_key.first;
-	auto &old_key = *old_cert_key.second;
+	auto &old_cert = *old_cert_key.cert;
+	auto &old_key = *old_cert_key.key;
 
 	UniqueEVP_PKEY generated_key;
 	if (!AcceptKey(old_key)) {

src/certdb/CertDatabase.cxx
@@ -39,6 +39,7 @@
 #include "lib/openssl/Time.hxx"
 #include "lib/openssl/Name.hxx"
 #include "lib/openssl/AltName.hxx"
+#include "lib/openssl/UniqueCertKey.hxx"
 #include "io/FileDescriptor.hxx"
 #include "util/AllocatedString.hxx"
 
@@ -243,17 +244,17 @@ CertDatabase::GetServerCertificateByHandle(const char *handle)
 	return LoadCertificate(result, 0, 0);
 }
 
-std::pair<UniqueX509, UniqueEVP_PKEY>
+UniqueCertKey
 CertDatabase::GetServerCertificateKeyByHandle(const char *handle)
 {
 	auto result = FindServerCertificateKeyByHandle(handle);
 	if (result.GetRowCount() == 0)
-		return std::make_pair(nullptr, nullptr);
+		return {};
 
 	return LoadCertificateKey(config, result, 0, 0);
 }
 
-std::pair<UniqueX509, UniqueEVP_PKEY>
+UniqueCertKey
 CertDatabase::GetServerCertificateKey(const char *name, const char *special)
 {
 	auto result = FindServerCertificateKeyByName(SyncQueryWrapper{conn},
@@ -264,18 +265,18 @@ CertDatabase::GetServerCertificateKey(const char *name, const char *special)
 		result = FindServerCertificateKeyByAltName(SyncQueryWrapper{conn},
 							   name, special);
 		if (result.GetRowCount() == 0)
-			return std::make_pair(nullptr, nullptr);
+			return {};
 	}
 
 	return LoadCertificateKey(config, result, 0, 0);
 }
 
-std::pair<UniqueX509, UniqueEVP_PKEY>
+UniqueCertKey
 CertDatabase::GetServerCertificateKey(Pg::Serial id)
 {
 	auto result = FindServerCertificateKeyById(id);
 	if (result.GetRowCount() == 0)
-		return std::make_pair(nullptr, nullptr);
+		return {};
 
 	return LoadCertificateKey(config, result, 0, 0);
 }

src/certdb/CertDatabase.hxx
@@ -42,6 +42,7 @@
 
 typedef struct aes_key_st AES_KEY;
 struct CertDatabaseConfig;
+struct UniqueCertKey;
 
 class CertDatabase {
 	const CertDatabaseConfig &config;
@@ -150,10 +151,10 @@ public:
 	 * @return a pair of certificate and key, or {nullptr, nullptr} if
 	 * no matching certificate was found
 	 */
-	std::pair<UniqueX509, UniqueEVP_PKEY> GetServerCertificateKeyByHandle(const char *handle);
-	std::pair<UniqueX509, UniqueEVP_PKEY> GetServerCertificateKey(const char *name,
-								      const char *special);
-	std::pair<UniqueX509, UniqueEVP_PKEY> GetServerCertificateKey(Pg::Serial id);
+	UniqueCertKey GetServerCertificateKeyByHandle(const char *handle);
+	UniqueCertKey GetServerCertificateKey(const char *name,
+					      const char *special);
+	UniqueCertKey GetServerCertificateKey(Pg::Serial id);
 
 	/**
 	 * Result columns: id, handle, issuer_common_name, not_after

src/certdb/CoCertDatabase.cxx
@@ -33,6 +33,7 @@
 #include "CoCertDatabase.hxx"
 #include "Queries.hxx"
 #include "FromResult.hxx"
+#include "lib/openssl/UniqueCertKey.hxx"
 #include "pg/CoQuery.hxx"
 #include "co/Task.hxx"
 
@@ -50,7 +51,7 @@ struct CoQueryWrapper {
 	}
 };
 
-Co::Task<std::pair<UniqueX509, UniqueEVP_PKEY>>
+Co::Task<UniqueCertKey>
 CoGetServerCertificateKey(Pg::AsyncConnection &connection,
 			  const CertDatabaseConfig &config,
 			  const char *name, const char *special)
@@ -63,7 +64,7 @@ CoGetServerCertificateKey(Pg::AsyncConnection &connection,
 		// TODO do both queries, use the most recent record
 		result = co_await FindServerCertificateKeyByAltName(q, name, special);
 		if (result.GetRowCount() == 0)
-			co_return std::make_pair(nullptr, nullptr);
+			co_return UniqueCertKey{};
 	}
 
 	co_return LoadCertificateKey(config, result, 0, 0);

src/certdb/CoCertDatabase.hxx
@@ -32,17 +32,15 @@
 
 #pragma once
 
-#include "lib/openssl/UniqueEVP.hxx"
-#include "lib/openssl/UniqueX509.hxx"
-
 #include <utility>
 
 struct CertDatabaseConfig;
+struct UniqueCertKey;
 
 namespace Pg { class AsyncConnection; }
 namespace Co { template<typename T> class Task; }
 
-Co::Task<std::pair<UniqueX509, UniqueEVP_PKEY>>
+Co::Task<UniqueCertKey>
 CoGetServerCertificateKey(Pg::AsyncConnection &connection,
 			  const CertDatabaseConfig &config,
 			  const char *name, const char *special);

src/certdb/FromResult.cxx
@@ -35,6 +35,7 @@
 #include "pg/Result.hxx"
 #include "lib/openssl/Certificate.hxx"
 #include "lib/openssl/Key.hxx"
+#include "lib/openssl/UniqueCertKey.hxx"
 
 UniqueX509
 LoadCertificate(const Pg::Result &result, unsigned row, unsigned column)
@@ -66,15 +67,17 @@ LoadWrappedKey(const CertDatabaseConfig &config,
 	return DecodeDerKey(key_der);
 }
 
-std::pair<UniqueX509, UniqueEVP_PKEY>
+UniqueCertKey
 LoadCertificateKey(const CertDatabaseConfig &config,
 		   const Pg::Result &result, unsigned row, unsigned column)
 {
-	auto pair = std::make_pair(LoadCertificate(result, row, column),
-				   LoadWrappedKey(config, result, row, column + 1));
+	UniqueCertKey ck{
+		LoadCertificate(result, row, column),
+		LoadWrappedKey(config, result, row, column + 1),
+	};
 
-	if (!MatchModulus(*pair.first, *pair.second))
+	if (!MatchModulus(*ck.cert, *ck.key))
 		throw std::runtime_error("Key does not match certificate");
 
-	return pair;
+	return ck;
 }

src/certdb/FromResult.hxx
@@ -38,6 +38,7 @@
 #include <utility>
 
 struct CertDatabaseConfig;
+struct UniqueCertKey;
 namespace Pg { class Result; }
 
 UniqueX509
@@ -47,6 +48,6 @@ UniqueEVP_PKEY
 LoadWrappedKey(const CertDatabaseConfig &config,
 	       const Pg::Result &result, unsigned row, unsigned column);
 
-std::pair<UniqueX509, UniqueEVP_PKEY>
+UniqueCertKey
 LoadCertificateKey(const CertDatabaseConfig &config,
 		   const Pg::Result &result, unsigned row, unsigned column);

src/certdb/Main.cxx
@@ -43,6 +43,7 @@
 #include "lib/openssl/LoadFile.hxx"
 #include "lib/openssl/Name.hxx"
 #include "lib/openssl/UniqueEVP.hxx"
+#include "lib/openssl/UniqueCertKey.hxx"
 #include "lib/openssl/Error.hxx"
 #include "lb/Config.hxx"
 #include "system/Urandom.hxx"
@@ -139,14 +140,14 @@ ReloadCertificate(const CertDatabaseConfig &db_config, const char *handle)
 	CertDatabase db(db_config);
 
 	auto cert_key = db.GetServerCertificateKeyByHandle(handle);
-	if (!cert_key.second)
+	if (!cert_key)
 		throw "Certificate not found";
 
 	WrapKeyHelper wrap_key_helper;
 	const auto wrap_key = wrap_key_helper.SetEncryptKey(db_config);
 
 	db.LoadServerCertificate(handle, nullptr,
-				 *cert_key.first, *cert_key.second,
+				 *cert_key.cert, *cert_key.key,
 				 wrap_key.first, wrap_key.second);
 }
 
@@ -183,7 +184,7 @@ GetCertificate(const CertDatabaseConfig &db_config, const char *handle)
 static UniqueEVP_PKEY
 FindKeyByName(CertDatabase &db, const char *common_name)
 {
-	return db.GetServerCertificateKey(common_name, nullptr).second;
+	return db.GetServerCertificateKey(common_name, nullptr).key;
 }
 
 static void

commit ae59daec26f27f004d8141fd621b48472b651f7c

certdb/AcmeMain: fix inverted check
Regression by commit 8327fa66b23 (v17.0.92).

src/certdb/AcmeMain.cxx
@@ -487,7 +487,7 @@ AcmeRenewCert(const CertDatabaseConfig &db_config, const AcmeConfig &config,
 		throw "Neither --alpn nor --challenge-directory nor --dns-txt-program specified";
 
 	const auto old_cert_key = db.GetServerCertificateKeyByHandle(handle);
-	if (old_cert_key)
+	if (!old_cert_key)
 		throw "Old certificate not found in database";
 
 	auto &old_cert = *old_cert_key.cert;