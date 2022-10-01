f62f92dac4edf262315fffbd43a1be4b3a91de35
Max Kellermann <mk@cm4all.com>
2022-10-01 13:16:23 +0200

    fs/NopThreadSocketFilter: use SliceFifoBuffer::MoveFromAllowBothNull()
    The MoveFrom() method may fail if the destination SliceFifoBuffer is
    not allocated.

--- src/fs/NopThreadSocketFilter.cxx
+++ src/fs/NopThreadSocketFilter.cxx
@@ -44,8 +44,8 @@ NopThreadSocketFilter::Run(ThreadSocketFilterInternal &f)
 {
 	const std::scoped_lock lock{f.mutex};
 	f.handshaking = false;
-	f.decrypted_input.MoveFrom(f.encrypted_input);
-	f.encrypted_output.MoveFrom(f.plain_output);
+	f.decrypted_input.MoveFromAllowBothNull(f.encrypted_input);
+	f.encrypted_output.MoveFromAllowBothNull(f.plain_output);
 }
 
 /*
