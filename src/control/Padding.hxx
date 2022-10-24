795649d6184b6c86b7bb750ef322a275f16efd53
Max Kellermann <mk@cm4all.com>
2022-10-24 18:33:06 +0200

    control/Padding: add static_assert()

--- src/control/Padding.hxx
+++ src/control/Padding.hxx
@@ -48,10 +48,24 @@ ControlPaddingSize(std::size_t size) noexcept
 	return (3 - ((size - 1) & 0x3));
 }
 
+static_assert(ControlPaddingSize(0) == 0);
+static_assert(ControlPaddingSize(1) == 3);
+static_assert(ControlPaddingSize(2) == 2);
+static_assert(ControlPaddingSize(3) == 1);
+static_assert(ControlPaddingSize(4) == 0);
+static_assert(ControlPaddingSize(5) == 3);
+
 constexpr std::size_t
 PadControlSize(std::size_t size) noexcept
 {
 	return ((size + 3) | 3) - 3;
 }
+
+static_assert(PadControlSize(0) == 0);
+static_assert(PadControlSize(1) == 4);
+static_assert(PadControlSize(2) == 4);
+static_assert(PadControlSize(3) == 4);
+static_assert(PadControlSize(4) == 4);
+static_assert(PadControlSize(5) == 8);
 
 } // namespace BengProxy
