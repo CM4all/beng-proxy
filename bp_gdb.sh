96da575ca0def10b92f27d8594d4a5d61d8d3cf9
Max Kellermann <mk@cm4all.com>
2022-11-30 20:20:27 +0100

    remove a bunch of obsolete build scripts

--- bp_gdb.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -e
-ninja -C output/asan cm4all-beng-proxy
-exec gdb -x gdbrun --args ./output/asan/cm4all-beng-proxy -vvvvv "$@"
--- lb_gdb.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -e
-ninja -C output/asan cm4all-beng-lb
-exec gdb -x gdbrun --args ./output/asan/cm4all-beng-lb -vvvvv "$@"
