CM4all/beng-proxy

commit cf5e167f313c2d6e396a8620ba643becd76308e6

translation/Response: use std::shared_ptr<std::vector> for layout_items

src/bp/Handler.cxx
@@ -497,6 +497,7 @@ Request::RepeatTranslation(UniquePoolPtr<TranslateResponse> _response) noexcept
 
 	if (response.layout.data() != nullptr) {
 		/* repeat request with LAYOUT mirrored */
+		assert(response.layout_items);
 
 		if (++translate.n_layout > 4) {
 			LogDispatchError(HttpStatus::BAD_GATEWAY,
@@ -516,7 +517,7 @@ Request::RepeatTranslation(UniquePoolPtr<TranslateResponse> _response) noexcept
 		}
 
 		translate.request.layout = response.layout;
-		translate.request.layout_item = FindLayoutItem(response.layout_items,
+		translate.request.layout_item = FindLayoutItem(*response.layout_items,
 							       uri);
 	}
 

src/translation/Cache.cxx
@@ -591,8 +591,6 @@ tcache_uri_key(AllocatorPtr alloc, const char *uri, const char *host,
 		b.emplace_back(layout_buffer, UriEscape(layout_buffer, layout));
 
 		if (layout_item != nullptr) {
-			assert(layout_item->value != nullptr);
-
 			switch (layout_item->type) {
 			case TranslationLayoutItem::Type::BASE:
 				b.push_back("--");

src/translation/Layout.cxx
@@ -51,7 +51,7 @@ TranslationLayoutItem::Match(const char *uri) const noexcept
 
 	case Type::REGEX:
 		try {
-			return UniqueRegex{value, true, false}.Match(uri);
+			return UniqueRegex{value.c_str(), true, false}.Match(uri);
 		} catch (...) {
 			// TODO: what to do?  We should reject this
 			// translation response

src/translation/Layout.hxx
@@ -32,6 +32,8 @@
 
 #pragma once
 
+#include <string>
+
 class AllocatorPtr;
 
 /**
@@ -45,7 +47,7 @@ struct TranslationLayoutItem {
 		REGEX,
 	} type;
 
-	const char *value;
+	std::string value;
 
 	TranslationLayoutItem() = default;
 

test/t_tcache.cxx
@@ -145,6 +145,16 @@ AllEquals(const T &a, const T &b) noexcept
 	return i == end(a);
 }
 
+template<typename T>
+static bool
+AllEquals(const std::shared_ptr<T> &a, const std::shared_ptr<T> &b) noexcept
+{
+	if (!a || !b)
+		return (!a) == (!b);
+
+	return AllEquals(*a, *b);
+}
+
 static bool
 operator==(const MountNamespaceOptions &a,
 	   const MountNamespaceOptions &b) noexcept
@@ -281,7 +291,7 @@ AllEquals(WidgetView *a, WidgetView *b) noexcept
 static bool
 operator==(const TranslationLayoutItem &a, const TranslationLayoutItem &b) noexcept
 {
-	return a.type == b.type && StringIsEqual(a.value, b.value);
+	return a.type == b.type && a.value == b.value;
 }
 
 static bool

test/tconstruct.hxx
@@ -116,12 +116,11 @@ struct MakeResponse : TranslateResponse {
 	MakeResponse &&Layout(std::string_view value,
 			      std::initializer_list<const char *> items) && noexcept {
 		layout = AsBytes(value);
+		layout_items = std::make_shared<std::vector<TranslationLayoutItem>>();
 
-		auto *li = alloc.NewArray<TranslationLayoutItem>(items.size());
-		for (auto *dest = li; const char *src : items)
-			*dest++ = {TranslationLayoutItem::Type::BASE, src};
+		for (const char *src : items)
+			layout_items->emplace_back(TranslationLayoutItem::Type::BASE, src);
 
-		layout_items = {li, items.size()};
 		return std::move(*this);
 	}
 

commit ad7f05e5a2737ed3ebac2ff16f3070b27dc605e6

bp/Request: hold reference to layout_items
Fixes use-after-free caused by cf5e167f313c2d6e396a8620ba643becd76308e6

src/bp/Handler.cxx
@@ -517,6 +517,7 @@ Request::RepeatTranslation(UniquePoolPtr<TranslateResponse> _response) noexcept
 		}
 
 		translate.request.layout = response.layout;
+		translate.layout_items = response.layout_items;
 		translate.request.layout_item = FindLayoutItem(*response.layout_items,
 							       uri);
 	}

src/bp/Request.hxx
@@ -177,6 +177,13 @@ private:
 		UniquePoolPtr<TranslateResponse> previous;
 
 		/**
+		 * This field holds a reference to the
+		 * TranslateResponse::layout_items that
+		 * request.layout_item points into.
+		 */
+		std::shared_ptr<std::vector<TranslationLayoutItem>> layout_items;
+
+		/**
 		 * Number of LIKE_HOST packets followed so far.  This
 		 * variable is used for loop detection.
 		 */