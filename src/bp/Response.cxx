CM4all/beng-proxy

commit 59f0658df9e0fbfbc820fb972e9d8990688bc059

http/Headers: refactor Get() to MapContains()/AnyContains()

src/bp/Response.cxx
@@ -116,7 +116,7 @@ Request::AutoDeflate(HttpHeaders &response_headers,
 		   flags for us to check */
 	} else if (translate.response->auto_deflate &&
 		   http_client_accepts_encoding(request.headers, "deflate") &&
-		   response_headers.Get("content-encoding") == nullptr) {
+		   !response_headers.ContainsContentEncoding()) {
 		auto available = response_body.GetAvailable(false);
 		if (available < 0 || available >= 512) {
 			compressed = true;
@@ -127,7 +127,7 @@ Request::AutoDeflate(HttpHeaders &response_headers,
 #ifdef HAVE_BROTLI
 	} else if (translate.response->auto_brotli &&
 		   http_client_accepts_encoding(request.headers, "br") &&
-		   response_headers.Get("content-encoding") == nullptr) {
+		   !response_headers.ContainsContentEncoding()) {
 		auto available = response_body.GetAvailable(false);
 		if (available < 0 || available >= 512) {
 			compressed = true;
@@ -137,7 +137,7 @@ Request::AutoDeflate(HttpHeaders &response_headers,
 #endif
 	} else if (translate.response->auto_gzip &&
 		   http_client_accepts_encoding(request.headers, "gzip") &&
-		   response_headers.Get("content-encoding") == nullptr) {
+		   response_headers.ContainsContentEncoding()) {
 		auto available = response_body.GetAvailable(false);
 		if (available < 0 || available >= 512) {
 			compressed = true;
@@ -614,7 +614,7 @@ Request::DispatchResponseDirect(HttpStatus status, HttpHeaders headers,
 
 	if (translate.response &&
 	    translate.response->send_csrf_token) {
-		if (headers.Get("access-control-allow-origin") != nullptr) {
+		if (headers.MapContains("access-control-allow-origin")) {
 			/* if this CORS header indicates that other origins may
 			   send requests, then this undermindes our CSRF
 			   protection; thus, enabling both CORS headers and

src/http/Headers.hxx
@@ -57,9 +57,17 @@ public:
 		return std::move(map);
 	}
 
+	/**
+	 * Does the #StringMap contain a header with the specified
+	 * name?
+	 */
 	[[gnu::pure]]
-	const char *Get(const char *key) const noexcept {
-		return map.Get(key);
+	bool MapContains(const char *key) const noexcept {
+		return map.Contains(key);
+	}
+
+	bool ContainsContentEncoding() const noexcept {
+		return MapContains("content-encoding");
 	}
 
 	GrowingBuffer &GetBuffer() noexcept {

commit 39459ac51959fbbaf70fc733e1b76eade5a38711

bp/Response: fix inverted check for AUTO_GZIP
Regression by commit 59f0658df9e

src/bp/Response.cxx
@@ -199,7 +199,7 @@ Request::AutoDeflate(HttpHeaders &response_headers,
 				  });
 	} else if (translate.response->auto_gzip &&
 		   http_client_accepts_encoding(request.headers, "gzip") &&
-		   response_headers.ContainsContentEncoding()) {
+		   !response_headers.ContainsContentEncoding()) {
 		MaybeAutoCompress(instance.encoding_cache.get(), pool, resource_tag,
 				  response_headers, response_body, "gzip",
 				  [this](auto &&i){