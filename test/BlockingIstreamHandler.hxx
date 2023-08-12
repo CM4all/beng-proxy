7307c2996bc609142aef1399574de7c2649523f6
Max Kellermann <mk@cm4all.com>
2023-08-12 17:17:18 +0200

    test/t_istream_replace: add another test case

---
+++ test/BlockingIstreamHandler.hxx
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: BSD-2-Clause
+// Copyright CM4all GmbH
+// author: Max Kellermann <mk@cm4all.com>
+
+#include "istream/Handler.hxx"
+
+#include <cassert>
+
+class BlockingIstreamHandler : public IstreamHandler {
+public:
+	enum class State {
+		OPEN,
+		EOF_,
+		ERROR
+	} state = State::OPEN;
+
+	/* virtual methods from class IstreamHandler */
+
+	std::size_t OnData(std::span<const std::byte>) noexcept override {
+		assert(state = State::OPEN);
+		return 0;
+	}
+
+	void OnEof() noexcept override {
+		assert(state = State::OPEN);
+		state = State::EOF_;
+	}
+
+	void OnError(std::exception_ptr) noexcept override {
+		assert(state = State::OPEN);
+		state = State::ERROR;
+	}
+};
--- test/t_istream_replace.cxx
+++ test/t_istream_replace.cxx
@@ -4,9 +4,16 @@
 
 #include "IstreamFilterTest.hxx"
 #include "istream/ReplaceIstream.hxx"
+#include "istream/LengthIstream.hxx"
+#include "istream/OptionalIstream.hxx"
+#include "istream/PauseIstream.hxx"
 #include "istream/istream_string.hxx"
 #include "istream/UnusedPtr.hxx"
 #include "istream/New.hxx"
+#include "util/SpanCast.hxx"
+#include "BlockingIstreamHandler.hxx"
+
+using std::string_view_literals::operator""sv;
 
 class IstreamReplaceTestTraits {
 public:
@@ -61,3 +68,187 @@ public:
 
 INSTANTIATE_TYPED_TEST_CASE_P(Replace2, IstreamFilterTest,
 			      IstreamReplace2TestTraits);
+
+TEST(ReplaceIstream, Buckets)
+{
+	const ScopeFbPoolInit fb_pool_init;
+	PInstance instance;
+	BlockingIstreamHandler handler;
+
+	auto &pool = instance.root_pool;
+
+	auto part1 = istream_string_new(pool, "abcdefghijk");
+	auto part2 = istream_string_new(pool, "lmnopqrstuvwxyz");
+
+	auto [pause, pause_control] = NewPauseIstream(pool, instance.event_loop,
+						      std::move(part2));
+
+	auto length = NewIstreamPtr<LengthIstream>(pool, std::move(pause), 15);
+
+	auto *replace = NewIstream<ReplaceIstream>(pool, instance.event_loop,
+						   NewConcatIstream(pool, std::move(part1),
+								    std::move(length)));
+	replace->SetHandler(handler);
+
+	{
+		IstreamBucketList list;
+		replace->FillBucketList(list);
+		EXPECT_TRUE(list.IsEmpty());
+		EXPECT_TRUE(list.HasMore());
+	}
+
+	EXPECT_EQ(replace->GetAvailable(false), -1);
+	EXPECT_EQ(replace->GetAvailable(true), 0);
+
+	/* add one (blocking) replacement: all data up to this
+	   replacement should be available */
+
+	auto [i1, c1] = istream_optional_new(pool, istream_string_new(pool, "123"));
+	replace->Add(3, 4, std::move(i1));
+
+	{
+		IstreamBucketList list;
+		replace->FillBucketList(list);
+		EXPECT_FALSE(list.IsEmpty());
+		EXPECT_TRUE(list.HasMore());
+
+		auto i = list.begin();
+		ASSERT_NE(i, list.end());
+		EXPECT_TRUE(i->IsBuffer());
+		EXPECT_EQ(ToStringView(i->GetBuffer()), "abc");
+
+		++i;
+		ASSERT_EQ(i, list.end());
+	}
+
+	EXPECT_EQ(replace->GetAvailable(false), -1);
+	EXPECT_EQ(replace->GetAvailable(true), 3);
+
+	/* unblock this replacement */
+
+	c1->Resume();
+
+	{
+		IstreamBucketList list;
+		replace->FillBucketList(list);
+		EXPECT_FALSE(list.IsEmpty());
+		EXPECT_TRUE(list.HasMore());
+
+		auto i = list.begin();
+		ASSERT_NE(i, list.end());
+		EXPECT_TRUE(i->IsBuffer());
+		EXPECT_EQ(ToStringView(i->GetBuffer()), "abc");
+
+		++i;
+		ASSERT_NE(i, list.end());
+		EXPECT_TRUE(i->IsBuffer());
+		EXPECT_EQ(ToStringView(i->GetBuffer()), "123");
+
+		++i;
+		ASSERT_EQ(i, list.end());
+	}
+
+	EXPECT_EQ(replace->GetAvailable(false), -1);
+	EXPECT_EQ(replace->GetAvailable(true), 6);
+
+	/* increase the "settled" position */
+
+	replace->Settle(6);
+
+	{
+		IstreamBucketList list;
+		replace->FillBucketList(list);
+		EXPECT_FALSE(list.IsEmpty());
+		EXPECT_TRUE(list.HasMore());
+
+		auto i = list.begin();
+		ASSERT_NE(i, list.end());
+		EXPECT_TRUE(i->IsBuffer());
+		EXPECT_EQ(ToStringView(i->GetBuffer()), "abc");
+
+		++i;
+		ASSERT_NE(i, list.end());
+		EXPECT_TRUE(i->IsBuffer());
+		EXPECT_EQ(ToStringView(i->GetBuffer()), "123");
+
+		++i;
+		ASSERT_NE(i, list.end());
+		EXPECT_TRUE(i->IsBuffer());
+		EXPECT_EQ(ToStringView(i->GetBuffer()), "ef");
+
+		++i;
+		ASSERT_EQ(i, list.end());
+	}
+
+	EXPECT_EQ(replace->GetAvailable(false), -1);
+	EXPECT_EQ(replace->GetAvailable(true), 8);
+
+	/* finish */
+
+	replace->Finish();
+
+	{
+		IstreamBucketList list;
+		replace->FillBucketList(list);
+		EXPECT_FALSE(list.IsEmpty());
+		EXPECT_TRUE(list.HasMore());
+
+		auto i = list.begin();
+		ASSERT_NE(i, list.end());
+		EXPECT_TRUE(i->IsBuffer());
+		EXPECT_EQ(ToStringView(i->GetBuffer()), "abc");
+
+		++i;
+		ASSERT_NE(i, list.end());
+		EXPECT_TRUE(i->IsBuffer());
+		EXPECT_EQ(ToStringView(i->GetBuffer()), "123");
+
+		++i;
+		ASSERT_NE(i, list.end());
+		EXPECT_TRUE(i->IsBuffer());
+		EXPECT_EQ(ToStringView(i->GetBuffer()), "efghijk");
+
+		++i;
+		ASSERT_EQ(i, list.end());
+	}
+
+	EXPECT_EQ(replace->GetAvailable(false), 28);
+	EXPECT_EQ(replace->GetAvailable(true), 28);
+
+	/* unpause */
+
+	pause_control->Resume();
+
+	{
+		IstreamBucketList list;
+		replace->FillBucketList(list);
+		EXPECT_FALSE(list.IsEmpty());
+		EXPECT_FALSE(list.HasMore());
+
+		auto i = list.begin();
+		ASSERT_NE(i, list.end());
+		EXPECT_TRUE(i->IsBuffer());
+		EXPECT_EQ(ToStringView(i->GetBuffer()), "abc");
+
+		++i;
+		ASSERT_NE(i, list.end());
+		EXPECT_TRUE(i->IsBuffer());
+		EXPECT_EQ(ToStringView(i->GetBuffer()), "123");
+
+		++i;
+		ASSERT_NE(i, list.end());
+		EXPECT_TRUE(i->IsBuffer());
+		EXPECT_EQ(ToStringView(i->GetBuffer()), "efghijklmnopqrstuvwxyz");
+
+		++i;
+		ASSERT_EQ(i, list.end());
+	}
+
+	EXPECT_EQ(replace->GetAvailable(false), 28);
+	EXPECT_EQ(replace->GetAvailable(true), 28);
+
+	/* cleanup */
+
+	EXPECT_EQ(handler.state, BlockingIstreamHandler::State::OPEN);
+	replace->Close();
+}
