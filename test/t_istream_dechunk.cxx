b7c69da0759dec21bac47d5430f6d2fc618d9208
Max Kellermann <mk@cm4all.com>
2023-08-14 20:11:21 +0200

    test/t_istream_dechunk: add more chunks
    There are now more chunks than fit into DechunkIstream::chunks.  Due
    to a bug in commit d02c369348059803d421f64aec29f72d9a341b0a, this test
    fails.

--- test/t_istream_dechunk.cxx
+++ test/t_istream_dechunk.cxx
@@ -21,11 +21,21 @@ class IstreamDechunkTestTraits {
 
 public:
 	static constexpr IstreamFilterTestOptions options{
-		.expected_result = "foo",
+		.expected_result = "foo123456789",
 	};
 
 	UnusedIstreamPtr CreateInput(struct pool &pool) const noexcept {
-		return istream_string_new(pool, "3\r\nfoo\r\n0\r\n\r\n ");
+		return istream_string_new(pool, "3\r\nfoo\r\n"
+					  "1\r\n1\r\n"
+					  "1\r\n2\r\n"
+					  "1\r\n3\r\n"
+					  "1\r\n4\r\n"
+					  "1\r\n5\r\n"
+					  "1\r\n6\r\n"
+					  "1\r\n7\r\n"
+					  "1\r\n8\r\n"
+					  "1\r\n9\r\n"
+					  "0\r\n\r\n ");
 	}
 
 	UnusedIstreamPtr CreateTest(EventLoop &event_loop, struct pool &pool,
