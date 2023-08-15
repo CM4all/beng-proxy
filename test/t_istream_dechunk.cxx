c0f52164c1979b3f8bd29fab2f4eb1326123d28c
Max Kellermann <mk@cm4all.com>
2023-08-15 12:47:52 +0200

    test/t_istream_dechunk: add another test where EOF chunk does not fit in array
    Currently fails.

--- test/t_istream_dechunk.cxx
+++ test/t_istream_dechunk.cxx
@@ -58,3 +58,44 @@ public:
 
 INSTANTIATE_TYPED_TEST_CASE_P(Dechunk, IstreamFilterTest,
 			      IstreamDechunkTestTraits);
+
+/**
+ * A variant with exactly the number of chunks so the EOF chunk
+ * doesn't fit into the "chunks" array.
+ */
+class IstreamDechunk2TestTraits {
+	class MyDechunkHandler final : public DechunkHandler {
+		void OnDechunkEndSeen() noexcept override {}
+
+		DechunkInputAction OnDechunkEnd() noexcept override {
+			return DechunkInputAction::CLOSE;
+		}
+	};
+
+public:
+	static constexpr IstreamFilterTestOptions options{
+		.expected_result = "12345678",
+	};
+
+	UnusedIstreamPtr CreateInput(struct pool &pool) const noexcept {
+		return istream_string_new(pool, "1\r\n1\r\n"
+					  "1\r\n2\r\n"
+					  "1\r\n3\r\n"
+					  "1\r\n4\r\n"
+					  "1\r\n5\r\n"
+					  "1\r\n6\r\n"
+					  "1\r\n7\r\n"
+					  "1\r\n8\r\n"
+					  "0\r\n\r\n ");
+	}
+
+	UnusedIstreamPtr CreateTest(EventLoop &event_loop, struct pool &pool,
+				    UnusedIstreamPtr input) const noexcept {
+		auto *handler = NewFromPool<MyDechunkHandler>(pool);
+		return istream_dechunk_new(pool, std::move(input),
+					   event_loop, *handler);
+	}
+};
+
+INSTANTIATE_TYPED_TEST_CASE_P(Dechunk2, IstreamFilterTest,
+			      IstreamDechunk2TestTraits);
