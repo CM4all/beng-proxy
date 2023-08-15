CM4all/beng-proxy

commit d6fc31785756272881c541eb74c21b63e84c8061

http/Client: close the client in OnDechunkEnd()
Fixes memory leak if a chunked response body gets closed after reading
buckets.

src/http/Client.cxx
@@ -139,6 +139,14 @@ class HttpClient final : BufferedSocketHandler, IstreamSink, Cancellable, Destru
 			HttpBodyReader::OnDechunkEndSeen();
 			GetClient().SocketDone();
 		}
+
+		DechunkInputAction OnDechunkEnd() noexcept final {
+			assert(!GetClient().IsConnected());
+
+			GetClient().stopwatch.RecordEvent("end");
+			GetClient().Destroy();
+			return DechunkInputAction::DESTROYED;
+		}
 	};
 
 	struct pool &pool;

src/http/Body.hxx
@@ -250,5 +250,5 @@ public:
 protected:
 	/* virtual methods from class DechunkHandler */
 	void OnDechunkEndSeen() noexcept override;
-	DechunkInputAction OnDechunkEnd() noexcept final;
+	DechunkInputAction OnDechunkEnd() noexcept override;
 };

src/istream/DechunkIstream.cxx
@@ -213,6 +213,7 @@ DechunkIstream::InvokeDechunkEnd() noexcept
 
 	switch (action) {
 	case DechunkHandler::DechunkInputAction::ABANDON:
+	case DechunkHandler::DechunkInputAction::DESTROYED:
 		ClearInput();
 		break;
 
@@ -361,6 +362,7 @@ DechunkIstream::OnData(std::span<const std::byte> src) noexcept
 		case DechunkHandler::DechunkInputAction::ABANDON:
 			break;
 
+		case DechunkHandler::DechunkInputAction::DESTROYED:
 		case DechunkHandler::DechunkInputAction::CLOSE:
 			return 0;
 		}

src/istream/DechunkIstream.hxx
@@ -29,6 +29,11 @@ public:
 		 * Close the #DechunkIstream.
 		 */
 		CLOSE,
+
+		/**
+		 * The method has destroyed the #DechunkIstream.
+		 */
+		DESTROYED,
 	};
 
 	/**

commit 609a7612d9154d66d57276bda716bfc9328a9d4a

http/Client: defer the SocketDone() call out of OnDechunkEndSeen()
Fixes a use-after-free bug caused by commit
d6fc31785756272881c541eb74c21b63e84c8061 (see code comment).

src/http/Client.cxx
@@ -28,6 +28,7 @@
 #include "AllocatorPtr.hxx"
 #include "lib/fmt/RuntimeError.hxx"
 #include "lib/fmt/ToBuffer.hxx"
+#include "event/DeferEvent.hxx"
 #include "system/Error.hxx"
 #include "io/Iovec.hxx"
 #include "io/Logger.hxx"
@@ -137,12 +138,17 @@ class HttpClient final : BufferedSocketHandler, IstreamSink, Cancellable, Destru
 		/* virtual methods from class DechunkHandler */
 		void OnDechunkEndSeen() noexcept final {
 			HttpBodyReader::OnDechunkEndSeen();
-			GetClient().SocketDone();
+
+			/* the socket is not needed anymore, but we
+			   can't release it right now because it would
+			   invalidate the buffer pointers
+			   DechunkIstream is currently working with,
+			   therefore defer the SocketDone() call */
+			GetClient().defer_socket_done.Schedule();
 		}
 
 		DechunkInputAction OnDechunkEnd() noexcept final {
-			assert(!GetClient().IsConnected());
-
+			GetClient().SocketDone();
 			GetClient().stopwatch.RecordEvent("end");
 			GetClient().Destroy();
 			return DechunkInputAction::DESTROYED;
@@ -156,6 +162,8 @@ class HttpClient final : BufferedSocketHandler, IstreamSink, Cancellable, Destru
 	const StopwatchPtr stopwatch;
 
 	EventLoop &event_loop;
+
+	DeferEvent defer_socket_done{event_loop, BIND_THIS_METHOD(SocketDone)};
 
 	/* I/O */
 	FilteredSocketLease socket;
@@ -303,6 +311,8 @@ private:
 	void SocketDone() noexcept {
 		assert(response.state == Response::State::END ||
 		       response_body_reader.IsSocketDone(socket));
+
+		defer_socket_done.Cancel();
 
 		if (IsConnected())
 			/* we don't need the socket anymore, we've got everything we