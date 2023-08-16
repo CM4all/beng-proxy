bcc1f740730c2862750c48e266f3723ab6d0badf
Max Kellermann <mk@cm4all.com>
2023-08-16 11:25:00 +0200

    test/TestLengthIstream: add test for input that is too long and too short

--- test/TestLengthIstream.cxx
+++ test/TestLengthIstream.cxx
@@ -8,6 +8,9 @@
 #include "istream/UnusedPtr.hxx"
 #include "istream/Handler.hxx"
 #include "istream/New.hxx"
+#include "istream/ZeroIstream.hxx"
+#include "istream/FourIstream.hxx"
+#include "istream/HeadIstream.hxx"
 #include "pool/pool.hxx"
 
 using std::string_view_literals::operator""sv;
@@ -30,3 +33,48 @@ public:
 
 INSTANTIATE_TYPED_TEST_CASE_P(Length, IstreamFilterTest,
 			      IstreamLengthTestTraits);
+
+static auto
+CreateZero(struct pool &pool, std::size_t size) noexcept
+{
+	return istream_head_new(pool, istream_four_new(&pool, istream_zero_new(pool)),
+				size, false);
+}
+
+TEST(LengthIstream, TooLong_Buckets)
+{
+	Instance instance;
+
+	auto pool = pool_new_linear(instance.root_pool, "test", 8192);
+
+	auto istream = NewIstreamPtr<LengthIstream>(pool,
+						    CreateZero(pool, 65),
+						    64);
+
+	Context ctx(instance, std::move(pool), {}, std::move(istream));
+
+	try {
+		while (ctx.ReadBuckets(3)) {}
+		FAIL();
+	} catch (...) {
+	}
+}
+
+TEST(LengthIstream, TooShort_Buckets)
+{
+	Instance instance;
+
+	auto pool = pool_new_linear(instance.root_pool, "test", 8192);
+
+	auto istream = NewIstreamPtr<LengthIstream>(pool,
+						    CreateZero(pool, 63),
+						    64);
+
+	Context ctx(instance, std::move(pool), {}, std::move(istream));
+
+	try {
+		while (ctx.ReadBuckets(3)) {}
+		FAIL();
+	} catch (...) {
+	}
+}
