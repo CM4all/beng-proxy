44f62e2f8cd66df16720e3c325977d0e400aecc3
Max Kellermann <mk@cm4all.com>
2023-09-15 09:27:57 +0200

    demo/cgi-bin/directory_index.py: convert to Python 3 (print function)

--- demo/cgi-bin/directory_index.py
+++ demo/cgi-bin/directory_index.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 from os import environ, listdir
 
-print "Content-Type: text/plain"
-print
+print("Content-Type: text/plain")
+print()
 
 for i in listdir(environ['DIRECTORY']):
-    print i
+    print(i)
