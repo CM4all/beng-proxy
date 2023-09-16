ea9d36896c500db99e1779f35c5022b46f85e4b1
Max Kellermann <mk@cm4all.com>
2023-09-16 21:41:02 +0200

    memory/GrowingBuffer: remove obsolete FMT_VERSION check

--- src/memory/GrowingBuffer.hxx
+++ src/memory/GrowingBuffer.hxx
@@ -192,12 +192,7 @@ public:
 
 	template<typename S, typename... Args>
 	void Fmt(const S &format_str, Args&&... args) noexcept {
-#if FMT_VERSION >= 90000
 		return VFmt(format_str, fmt::make_format_args(args...));
-#else
-		return VFmt(fmt::to_string_view(format_str),
-			    fmt::make_args_checked<Args...>(format_str, args...));
-#endif
 	}
 
 	void AppendMoveFrom(GrowingBuffer &&src) noexcept;
