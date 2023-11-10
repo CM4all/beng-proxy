CM4all/beng-proxy

commit cedde9ce3812599fb0a9e7cbdb7d1e0f6cce4bc4

certdb/WrapKey: convert WrapKeyHelper to class WrapKey

src/certdb/CertDatabase.cxx
@@ -148,7 +148,7 @@ bool
 CertDatabase::LoadServerCertificate(const char *handle, const char *special,
 				    X509 &cert, const EVP_PKEY &key,
 				    const char *key_wrap_name,
-				    AES_KEY *wrap_key)
+				    WrapKey *wrap_key)
 {
 	const auto common_name = GetCommonName(cert);
 	assert(common_name != nullptr);
@@ -162,8 +162,8 @@ CertDatabase::LoadServerCertificate(const char *handle, const char *special,
 	Pg::BinaryValue key_der(key_buffer.get());
 
 	AllocatedArray<std::byte> wrapped;
-	if (wrap_key != nullptr)
-		key_der = wrapped = WrapKey(key_der, wrap_key);
+	if (key_wrap_name != nullptr)
+		key_der = wrapped = wrap_key->Decrypt(key_der);
 
 	const auto alt_names = GetSubjectAltNames(cert);
 
@@ -310,14 +310,14 @@ void
 CertDatabase::InsertAcmeAccount(bool staging,
 				const char *email, const char *location,
 				EVP_PKEY &key, const char *key_wrap_name,
-				AES_KEY *wrap_key)
+				WrapKey *wrap_key)
 {
 	const SslBuffer key_buffer(key);
 	Pg::BinaryValue key_der(key_buffer.get());
 
 	AllocatedArray<std::byte> wrapped;
-	if (wrap_key != nullptr)
-		key_der = wrapped = WrapKey(key_der, wrap_key);
+	if (key_wrap_name != nullptr)
+		key_der = wrapped = wrap_key->Encrypt(key_der);
 
 	conn.ExecuteParams("INSERT INTO acme_account("
 			   "staging, email, location, key_der, key_wrap_name) "

src/certdb/AcmeAlpn.cxx
@@ -104,11 +104,10 @@ Alpn01ChallengeRecord::Commit(const CertDatabaseConfig &db_config)
 	if (!X509_sign(cert.get(), cert_key.get(), EVP_sha256()))
 		throw SslError("X509_sign() failed");
 
-	WrapKeyHelper wrap_key_helper;
-	const auto wrap_key = wrap_key_helper.SetEncryptKey(db_config);
+	auto [wrap_key_name, wrap_key] = WrapKey::MakeEncryptKey(db_config);
 
 	db.LoadServerCertificate(handle.c_str(), "acme-alpn-tls-01",
 				 *cert, *cert_key,
-				 wrap_key.first, wrap_key.second);
+				 wrap_key_name, &wrap_key);
 	db.NotifyModified();
 }

src/certdb/AcmeMain.cxx
@@ -440,12 +440,11 @@ AcmeNewOrder(const CertDatabaseConfig &db_config, const AcmeConfig &config,
 	const auto cert = client.DownloadCertificate(account_key, order);
 	progress();
 
-	WrapKeyHelper wrap_key_helper;
-	const auto wrap_key = wrap_key_helper.SetEncryptKey(db_config);
+	auto [wrap_key_name, wrap_key] = WrapKey::MakeEncryptKey(db_config);
 
 	db.DoSerializableRepeat(8, [&](){
 		db.LoadServerCertificate(handle, nullptr, *cert, *cert_key,
-					 wrap_key.first, wrap_key.second);
+					 wrap_key_name, &wrap_key);
 	});
 
 	db.NotifyModified();
@@ -537,12 +536,11 @@ AcmeRenewCert(const CertDatabaseConfig &db_config, const AcmeConfig &config,
 	const auto cert = client.DownloadCertificate(account_key, order);
 	progress();
 
-	WrapKeyHelper wrap_key_helper;
-	const auto wrap_key = wrap_key_helper.SetEncryptKey(db_config);
+	auto [wrap_key_name, wrap_key] = WrapKey::MakeEncryptKey(db_config);
 
 	db.DoSerializableRepeat(8, [&](){
 		db.LoadServerCertificate(handle, nullptr, *cert, new_key,
-					 wrap_key.first, wrap_key.second);
+					 wrap_key_name, &wrap_key);
 	});
 
 	db.NotifyModified();
@@ -678,12 +676,11 @@ Acme(ConstBuffer<const char *> args)
 			const auto db_config = LoadPatchCertDatabaseConfig();
 			CertDatabase db(db_config);
 
-			WrapKeyHelper wrap_key_helper;
-			const auto wrap_key = wrap_key_helper.SetEncryptKey(db_config);
+			auto [wrap_key_name, wrap_key] = WrapKey::MakeEncryptKey(db_config);
 
 			db.InsertAcmeAccount(config.staging, email,
 					     account.location.c_str(), *key,
-					     wrap_key.first, wrap_key.second);
+					     wrap_key_name, &wrap_key);
 
 			fmt::print("{}\n", account.location);
 		} else {
@@ -731,14 +728,13 @@ Acme(ConstBuffer<const char *> args)
 		if (account.status != AcmeAccount::Status::VALID)
 			throw Usage("Account is not valid");
 
-		WrapKeyHelper wrap_key_helper;
-		const auto wrap_key = wrap_key_helper.SetEncryptKey(db_config);
+		auto [wrap_key_name, wrap_key] = WrapKey::MakeEncryptKey(db_config);
 
 		CertDatabase db(db_config);
 		db.InsertAcmeAccount(config.staging,
 				     account.GetEmail(),
 				     account.location.c_str(), *key,
-				     wrap_key.first, wrap_key.second);
+				     wrap_key_name, &wrap_key);
 
 		PrintAccount(account);
 	} else if (StringIsEqual(cmd, "new-order")) {

src/certdb/CertDatabase.hxx
@@ -12,9 +12,9 @@
 #include <span>
 #include <string>
 
-typedef struct aes_key_st AES_KEY;
 struct CertDatabaseConfig;
 struct UniqueCertKey;
+class WrapKey;
 
 class CertDatabase {
 	const CertDatabaseConfig &config;
@@ -113,7 +113,7 @@ public:
 	bool LoadServerCertificate(const char *handle, const char *special,
 				   X509 &cert, const EVP_PKEY &key,
 				   const char *key_wrap_name,
-				   AES_KEY *wrap_key);
+				   WrapKey *wrap_key);
 
 	UniqueX509 GetServerCertificateByHandle(const char *handle);
 
@@ -273,7 +273,7 @@ public:
 	void InsertAcmeAccount(bool staging,
 			       const char *email, const char *location,
 			       EVP_PKEY &key, const char *key_wrap_name,
-			       AES_KEY *wrap_key);
+			       WrapKey *wrap_key);
 
 	void TouchAcmeAccount(const char *id);
 

src/certdb/FromResult.cxx
@@ -34,7 +34,8 @@ LoadWrappedKey(const CertDatabaseConfig &config,
 		/* the private key is encrypted; descrypt it using the AES key
 		   from the configuration file */
 		const auto key_wrap_name = result.GetValue(row, column + 1);
-		key_der = unwrapped = UnwrapKey(key_der, config, key_wrap_name);
+		auto wrap_key = WrapKey::MakeDecryptKey(config, key_wrap_name);
+		key_der = unwrapped = wrap_key.Decrypt(key_der);
 	}
 
 	return DecodeDerKey(key_der);

src/certdb/Main.cxx
@@ -105,8 +105,7 @@ LoadCertificate(const CertDatabaseConfig &db_config,
 	if (!MatchModulus(*cert, *key))
 		throw "Key and certificate do not match.";
 
-	WrapKeyHelper wrap_key_helper;
-	const auto wrap_key = wrap_key_helper.SetEncryptKey(db_config);
+	auto [wrap_key_name, wrap_key] = WrapKey::MakeEncryptKey(db_config);
 
 	CertDatabase db(db_config);
 
@@ -114,8 +113,8 @@ LoadCertificate(const CertDatabaseConfig &db_config,
 
 	db.DoSerializableRepeat(8, [&](){
 		inserted = db.LoadServerCertificate(handle, nullptr,
-						    *cert, *key, wrap_key.first,
-						    wrap_key.second);
+						    *cert, *key, wrap_key_name,
+						    &wrap_key);
 	});
 
 	fmt::print("{}: {}\n", inserted ? "insert"sv : "update"sv,
@@ -132,12 +131,11 @@ ReloadCertificate(const CertDatabaseConfig &db_config, const char *handle)
 	if (!cert_key)
 		throw "Certificate not found";
 
-	WrapKeyHelper wrap_key_helper;
-	const auto wrap_key = wrap_key_helper.SetEncryptKey(db_config);
+	auto [wrap_key_name, wrap_key] = WrapKey::MakeEncryptKey(db_config);
 
 	db.LoadServerCertificate(handle, nullptr,
 				 *cert_key.cert, *cert_key.key,
-				 wrap_key.first, wrap_key.second);
+				 wrap_key_name, &wrap_key);
 }
 
 static void

src/certdb/WrapKey.cxx
@@ -17,60 +17,64 @@
 // TODO
 #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 
-AES_KEY *
-WrapKeyHelper::SetEncryptKey(const std::span<const std::byte, 32> key)
+WrapKey
+WrapKey::MakeEncryptKey(const std::span<const std::byte, 32> src)
 {
-	if (AES_set_encrypt_key(reinterpret_cast<const unsigned char *>(key.data()),
-				key.size() * 8, &buffer) != 0)
+	WrapKey key;
+	if (AES_set_encrypt_key(reinterpret_cast<const unsigned char *>(src.data()),
+				src.size() * 8, &key.key) != 0)
 		throw SslError("AES_set_encrypt_key() failed");
 
-	return &buffer;
+	return key;
 }
 
-AES_KEY *
-WrapKeyHelper::SetEncryptKey(const CertDatabaseConfig &config,
-			     std::string_view name)
+WrapKey
+WrapKey::MakeEncryptKey(const CertDatabaseConfig &config,
+			std::string_view name)
 {
 	const auto i = config.wrap_keys.find(name);
 	if (i == config.wrap_keys.end())
 		throw FmtRuntimeError("No such wrap_key: {}", name);
 
-	return SetEncryptKey(i->second);
+	return MakeEncryptKey(i->second);
 }
 
-std::pair<const char *, AES_KEY *>
-WrapKeyHelper::SetEncryptKey(const CertDatabaseConfig &config)
+std::pair<const char *, WrapKey>
+WrapKey::MakeEncryptKey(const CertDatabaseConfig &config)
 {
 	if (config.default_wrap_key.empty())
-		return std::make_pair(nullptr, nullptr);
+		return {};
 
-	return std::make_pair(config.default_wrap_key.c_str(),
-			      SetEncryptKey(config, config.default_wrap_key));
+	return {
+		config.default_wrap_key.c_str(),
+		MakeEncryptKey(config, config.default_wrap_key),
+	};
 }
 
-AES_KEY *
-WrapKeyHelper::SetDecryptKey(const std::span<const std::byte, 32> key)
+WrapKey
+WrapKey::MakeDecryptKey(const std::span<const std::byte, 32> src)
 {
-	if (AES_set_decrypt_key(reinterpret_cast<const unsigned char *>(key.data()),
-				key.size() * 8, &buffer) != 0)
+	WrapKey key;
+	if (AES_set_decrypt_key(reinterpret_cast<const unsigned char *>(src.data()),
+				src.size() * 8, &key.key) != 0)
 		throw SslError("AES_set_decrypt_key() failed");
 
-	return &buffer;
+	return key;
 }
 
-AES_KEY *
-WrapKeyHelper::SetDecryptKey(const CertDatabaseConfig &config,
-			     std::string_view name)
+WrapKey
+WrapKey::MakeDecryptKey(const CertDatabaseConfig &config,
+			std::string_view name)
 {
 	const auto i = config.wrap_keys.find(name);
 	if (i == config.wrap_keys.end())
 		throw FmtRuntimeError("No such wrap_key: {}", name);
 
-	return SetDecryptKey(i->second);
+	return MakeDecryptKey(i->second);
 }
 
 AllocatedArray<std::byte>
-WrapKey(std::span<const std::byte> src, AES_KEY *wrap_key)
+WrapKey::Encrypt(std::span<const std::byte> src)
 {
 	AllocatedArray<std::byte> padded;
 	size_t padded_size = ((src.size() - 1) | 7) + 1;
@@ -86,7 +90,7 @@ WrapKey(std::span<const std::byte> src, AES_KEY *wrap_key)
 	}
 
 	AllocatedArray<std::byte> dest{src.size() + 8};
-	int result = AES_wrap_key(wrap_key, nullptr,
+	int result = AES_wrap_key(&key, nullptr,
 				  (unsigned char *)dest.data(),
 				  (const unsigned char *)src.data(),
 				  src.size());
@@ -98,24 +102,15 @@ WrapKey(std::span<const std::byte> src, AES_KEY *wrap_key)
 }
 
 AllocatedArray<std::byte>
-UnwrapKey(std::span<const std::byte> src,
-	  const CertDatabaseConfig &config, std::string_view key_wrap_name)
+WrapKey::Decrypt(std::span<const std::byte> src)
 {
 	if (src.size() <= 8)
 		throw std::runtime_error("Malformed wrapped key");
 
-	auto i = config.wrap_keys.find(key_wrap_name);
-	if (i == config.wrap_keys.end())
-		throw FmtRuntimeError("No such wrap_key: {}", key_wrap_name);
-
-	WrapKeyHelper wrap_key_helper;
-	const auto wrap_key =
-		wrap_key_helper.SetDecryptKey(config, key_wrap_name);
-
 	ERR_clear_error();
 
 	AllocatedArray<std::byte> dest{src.size() - 8};
-	int r = AES_unwrap_key(wrap_key, nullptr,
+	int r = AES_unwrap_key(&key, nullptr,
 			       (unsigned char *)dest.data(),
 			       (const unsigned char *)src.data(),
 			       src.size());

src/certdb/WrapKey.hxx
@@ -20,24 +20,21 @@ template<typename> class AllocatedArray;
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 
-class WrapKeyHelper {
-	AES_KEY buffer;
+class WrapKey {
+	AES_KEY key;
 
 public:
-	AES_KEY *SetEncryptKey(const std::span<const std::byte, 32> key);
-	AES_KEY *SetEncryptKey(const CertDatabaseConfig &config,
-			       std::string_view name);
-	std::pair<const char *, AES_KEY *> SetEncryptKey(const CertDatabaseConfig &config);
-	AES_KEY *SetDecryptKey(const std::span<const std::byte, 32> key);
-	AES_KEY *SetDecryptKey(const CertDatabaseConfig &config,
-			       std::string_view name);
+	static WrapKey MakeEncryptKey(const std::span<const std::byte, 32> src);
+	static WrapKey MakeEncryptKey(const CertDatabaseConfig &config,
+				      std::string_view name);
+	static std::pair<const char *, WrapKey> MakeEncryptKey(const CertDatabaseConfig &config);
+
+	static WrapKey MakeDecryptKey(const std::span<const std::byte, 32> src);
+	static WrapKey MakeDecryptKey(const CertDatabaseConfig &config,
+				      std::string_view name);
+
+	AllocatedArray<std::byte> Encrypt(std::span<const std::byte> src);
+	AllocatedArray<std::byte> Decrypt(std::span<const std::byte> src);
 };
 
 #pragma GCC diagnostic pop
-
-AllocatedArray<std::byte>
-WrapKey(std::span<const std::byte> src, AES_KEY *wrap_key);
-
-AllocatedArray<std::byte>
-UnwrapKey(std::span<const std::byte> src,
-	  const CertDatabaseConfig &config, std::string_view key_wrap_name);

commit ffe04d16075c3d52c7a24b3c572a2df3d4586a7f

certdb/CertDatabase: fix Decrypt/Encrypt mixup in LoadServerCertificate()
Regression by commit cedde9ce3812599fb0a9e7cbdb7d1e0f6cce4bc4

src/certdb/CertDatabase.cxx
@@ -163,7 +163,7 @@ CertDatabase::LoadServerCertificate(const char *handle, const char *special,
 
 	AllocatedArray<std::byte> wrapped;
 	if (key_wrap_name != nullptr)
-		key_der = wrapped = wrap_key->Decrypt(key_der);
+		key_der = wrapped = wrap_key->Encrypt(key_der);
 
 	const auto alt_names = GetSubjectAltNames(cert);
 