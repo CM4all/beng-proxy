8b673aa98a6bfd03a2100b8b919331444987214e
Max Kellermann <mk@cm4all.com>
2023-12-01 18:21:02 +0100

    test/TestUriPRelative: use EXPECT_ instead of ASSERT_

--- test/TestUriPRelative.cxx
+++ test/TestUriPRelative.cxx
@@ -13,26 +13,26 @@ TEST(UriRelativeTest, Compress)
 	TestPool pool;
 	AllocatorPtr alloc(pool);
 
-	ASSERT_STREQ(uri_compress(alloc, "/foo/bar"), "/foo/bar");
-	ASSERT_STREQ(uri_compress(alloc, "/foo/./bar"), "/foo/bar");
-	ASSERT_STREQ(uri_compress(alloc, "/./foo/bar"), "/foo/bar");
-	ASSERT_STREQ(uri_compress(alloc, "/foo/bar/./"), "/foo/bar/");
-	ASSERT_STREQ(uri_compress(alloc, "./foo/bar/"), "foo/bar/");
-	ASSERT_STREQ(uri_compress(alloc, "/foo//bar/"), "/foo/bar/");
-	ASSERT_STREQ(uri_compress(alloc, "/foo///bar/"), "/foo/bar/");
-	ASSERT_STREQ(uri_compress(alloc, "/1/2/../3/"), "/1/3/");
-	ASSERT_STREQ(uri_compress(alloc, "/1/2/../../3/"), "/3/");
-	ASSERT_STREQ(uri_compress(alloc, "foo/../bar"), "bar");
-	ASSERT_STREQ(uri_compress(alloc, "foo//../bar"), "bar");
-	ASSERT_STREQ(uri_compress(alloc, "foo/.."), "");
-	ASSERT_STREQ(uri_compress(alloc, "foo/../."), "");
+	EXPECT_STREQ(uri_compress(alloc, "/foo/bar"), "/foo/bar");
+	EXPECT_STREQ(uri_compress(alloc, "/foo/./bar"), "/foo/bar");
+	EXPECT_STREQ(uri_compress(alloc, "/./foo/bar"), "/foo/bar");
+	EXPECT_STREQ(uri_compress(alloc, "/foo/bar/./"), "/foo/bar/");
+	EXPECT_STREQ(uri_compress(alloc, "./foo/bar/"), "foo/bar/");
+	EXPECT_STREQ(uri_compress(alloc, "/foo//bar/"), "/foo/bar/");
+	EXPECT_STREQ(uri_compress(alloc, "/foo///bar/"), "/foo/bar/");
+	EXPECT_STREQ(uri_compress(alloc, "/1/2/../3/"), "/1/3/");
+	EXPECT_STREQ(uri_compress(alloc, "/1/2/../../3/"), "/3/");
+	EXPECT_STREQ(uri_compress(alloc, "foo/../bar"), "bar");
+	EXPECT_STREQ(uri_compress(alloc, "foo//../bar"), "bar");
+	EXPECT_STREQ(uri_compress(alloc, "foo/.."), "");
+	EXPECT_STREQ(uri_compress(alloc, "foo/../."), "");
 
-	ASSERT_EQ(uri_compress(alloc, "/1/2/../../../3/"), nullptr);
-	ASSERT_EQ(uri_compress(alloc, "/../"), nullptr);
-	ASSERT_EQ(uri_compress(alloc, "/a/../../"), nullptr);
-	ASSERT_EQ(uri_compress(alloc, "/.."), nullptr);
-	ASSERT_EQ(uri_compress(alloc, ".."), nullptr);
-	ASSERT_STREQ(uri_compress(alloc, "/1/2/.."), "/1/");
+	EXPECT_EQ(uri_compress(alloc, "/1/2/../../../3/"), nullptr);
+	EXPECT_EQ(uri_compress(alloc, "/../"), nullptr);
+	EXPECT_EQ(uri_compress(alloc, "/a/../../"), nullptr);
+	EXPECT_EQ(uri_compress(alloc, "/.."), nullptr);
+	EXPECT_EQ(uri_compress(alloc, ".."), nullptr);
+	EXPECT_STREQ(uri_compress(alloc, "/1/2/.."), "/1/");
 }
 
 TEST(UriRelativeTest, Absolute)
@@ -40,44 +40,44 @@ TEST(UriRelativeTest, Absolute)
 	TestPool pool;
 	AllocatorPtr alloc(pool);
 
-	ASSERT_STREQ(uri_absolute(alloc, "http://localhost/", "foo"), "http://localhost/foo");
-	ASSERT_STREQ(uri_absolute(alloc, "http://localhost/bar", "foo"), "http://localhost/foo");
-	ASSERT_STREQ(uri_absolute(alloc, "http://localhost/bar/", "foo"), "http://localhost/bar/foo");
-	ASSERT_STREQ(uri_absolute(alloc, "http://localhost/bar/", "/foo"), "http://localhost/foo");
-	ASSERT_STREQ(uri_absolute(alloc, "http://localhost/bar/",
+	EXPECT_STREQ(uri_absolute(alloc, "http://localhost/", "foo"), "http://localhost/foo");
+	EXPECT_STREQ(uri_absolute(alloc, "http://localhost/bar", "foo"), "http://localhost/foo");
+	EXPECT_STREQ(uri_absolute(alloc, "http://localhost/bar/", "foo"), "http://localhost/bar/foo");
+	EXPECT_STREQ(uri_absolute(alloc, "http://localhost/bar/", "/foo"), "http://localhost/foo");
+	EXPECT_STREQ(uri_absolute(alloc, "http://localhost/bar/",
 				  "http://localhost/bar/foo"),
 		     "http://localhost/bar/foo");
-	ASSERT_STREQ(uri_absolute(alloc, "http://localhost/bar/",
+	EXPECT_STREQ(uri_absolute(alloc, "http://localhost/bar/",
 				  "http://localhost/foo"),
 		     "http://localhost/foo");
-	ASSERT_STREQ(uri_absolute(alloc, "http://localhost", "foo"),
+	EXPECT_STREQ(uri_absolute(alloc, "http://localhost", "foo"),
 		     "http://localhost/foo");
-	ASSERT_STREQ(uri_absolute(alloc, "/", "foo"), "/foo");
-	ASSERT_STREQ(uri_absolute(alloc, "/bar", "foo"), "/foo");
-	ASSERT_STREQ(uri_absolute(alloc, "/bar/", "foo"), "/bar/foo");
-	ASSERT_STREQ(uri_absolute(alloc, "/bar/", "/foo"), "/foo");
-	ASSERT_STREQ(uri_absolute(alloc, "/bar", "?foo"), "/bar?foo");
+	EXPECT_STREQ(uri_absolute(alloc, "/", "foo"), "/foo");
+	EXPECT_STREQ(uri_absolute(alloc, "/bar", "foo"), "/foo");
+	EXPECT_STREQ(uri_absolute(alloc, "/bar/", "foo"), "/bar/foo");
+	EXPECT_STREQ(uri_absolute(alloc, "/bar/", "/foo"), "/foo");
+	EXPECT_STREQ(uri_absolute(alloc, "/bar", "?foo"), "/bar?foo");
 
-	ASSERT_STREQ(uri_absolute(alloc, "http://localhost/foo/",
+	EXPECT_STREQ(uri_absolute(alloc, "http://localhost/foo/",
 				  "//example.com/bar"),
 		     "http://example.com/bar");
 
-	ASSERT_STREQ(uri_absolute(alloc, "ftp://localhost/foo/",
+	EXPECT_STREQ(uri_absolute(alloc, "ftp://localhost/foo/",
 				  "//example.com/bar"),
 		     "ftp://example.com/bar");
 
-	ASSERT_STREQ(uri_absolute(alloc, "/foo/", "//example.com/bar"),
+	EXPECT_STREQ(uri_absolute(alloc, "/foo/", "//example.com/bar"),
 		     "//example.com/bar");
 
-	ASSERT_STREQ(uri_absolute(alloc, "//example.com/foo/", "bar"),
+	EXPECT_STREQ(uri_absolute(alloc, "//example.com/foo/", "bar"),
 		     "//example.com/foo/bar");
 
-	ASSERT_STREQ(uri_absolute(alloc, "//example.com/foo/", "/bar"),
+	EXPECT_STREQ(uri_absolute(alloc, "//example.com/foo/", "/bar"),
 		     "//example.com/bar");
 
-	ASSERT_STREQ(uri_absolute(alloc, "//example.com", "bar"),
+	EXPECT_STREQ(uri_absolute(alloc, "//example.com", "bar"),
 		     "//example.com/bar");
 
-	ASSERT_STREQ(uri_absolute(alloc, "//example.com", "/bar"),
+	EXPECT_STREQ(uri_absolute(alloc, "//example.com", "/bar"),
 		     "//example.com/bar");
 }
