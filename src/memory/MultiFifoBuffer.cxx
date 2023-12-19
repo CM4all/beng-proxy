c78e95030569413558d09ede38ed58d280e68a53
Max Kellermann <mk@cm4all.com>
2023-12-19 19:46:12 +0100

    memory/MultiFifoBuffer: move code to class BufferQueue in libcommon

--- src/memory/MultiFifoBuffer.cxx
+++ src/memory/MultiFifoBuffer.cxx
@@ -5,94 +5,9 @@
 #include "MultiFifoBuffer.hxx"
 #include "istream/Bucket.hxx"
 
-#include <algorithm>
-
-MultiFifoBuffer::MultiFifoBuffer() noexcept = default;
-MultiFifoBuffer::~MultiFifoBuffer() noexcept = default;
-
-void
-MultiFifoBuffer::Push(std::span<const std::byte> src) noexcept
-{
-	/* try to append to the last existing buffer (if there is
-	   any) */
-	if (!buffers.empty()) {
-		auto &b = buffers.back();
-		assert(b.IsDefined());
-
-		const std::size_t nbytes = b.MoveFrom(src);
-		src = src.subspan(nbytes);
-	}
-
-	/* create more buffers for remaining data */
-	while (!src.empty()) {
-		buffers.emplace_back();
-		auto &b = buffers.back();
-		b.Allocate();
-
-		const std::size_t nbytes = b.MoveFrom(src);
-		src = src.subspan(nbytes);
-	}
-}
-
-size_t
-MultiFifoBuffer::GetAvailable() const noexcept
-{
-	size_t result = 0;
-	for (const auto &i : buffers)
-		result += i.GetAvailable();
-	return result;
-}
-
-std::span<const std::byte>
-MultiFifoBuffer::Read() const noexcept
-{
-	if (buffers.empty())
-		return {};
-
-	return buffers.front().Read();
-}
-
-void
-MultiFifoBuffer::Consume(size_t nbytes) noexcept
-{
-	if (nbytes == 0)
-		return;
-
-	assert(!buffers.empty());
-
-	auto &b = buffers.front();
-	assert(b.IsDefined());
-	assert(b.GetAvailable() >= nbytes);
-	b.Consume(nbytes);
-
-	if (b.empty())
-		buffers.pop_front();
-}
-
 void
 MultiFifoBuffer::FillBucketList(IstreamBucketList &list) const noexcept
 {
-	for (const auto &i : buffers)
-		list.Push(i.Read());
-}
-
-size_t
-MultiFifoBuffer::Skip(size_t nbytes) noexcept
-{
-	size_t result = 0;
-	while (!empty()) {
-		auto &b = buffers.front();
-		const size_t available = b.GetAvailable();
-		size_t consume = std::min(nbytes, available);
-		result += consume;
-		nbytes -= consume;
-		if (consume < available) {
-			b.Consume(consume);
-			break;
-		}
-
-		buffers.pop_front();
-	}
-
-	return result;
+	for (const std::span<const std::byte> i : *this)
+		list.Push(i);
 }
--- src/memory/MultiFifoBuffer.hxx
+++ src/memory/MultiFifoBuffer.hxx
@@ -4,41 +4,12 @@
 
 #pragma once
 
+#include "memory/BufferQueue.hxx"
 #include "DefaultFifoBuffer.hxx"
-
-#include <list>
-#include <span>
 
 class IstreamBucketList;
 
-class MultiFifoBuffer {
-	std::list<DefaultFifoBuffer> buffers;
-
+class MultiFifoBuffer : public BufferQueue {
 public:
-	MultiFifoBuffer() noexcept;
-	~MultiFifoBuffer() noexcept;
-
-	MultiFifoBuffer(MultiFifoBuffer &&) noexcept = default;
-	MultiFifoBuffer &operator=(MultiFifoBuffer &&) noexcept = default;
-
-	[[gnu::pure]]
-	bool empty() const noexcept {
-		return buffers.empty();
-	}
-
-	void Push(std::span<const std::byte> src) noexcept;
-
-	[[gnu::pure]]
-	size_t GetAvailable() const noexcept;
-
-	std::span<const std::byte> Read() const noexcept;
-	void Consume(size_t nbytes) noexcept;
-
 	void FillBucketList(IstreamBucketList &list) const noexcept;
-
-	/**
-	 * Like Consume(), but can run across several buffers and the
-	 * parameter is allowed to be larger than GetAvailable().
-	 */
-	size_t Skip(size_t nbytes) noexcept;
 };
