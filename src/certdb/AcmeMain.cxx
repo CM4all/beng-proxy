f3b83d2ef6b0e892a1f7529ad91ec213dbbd67e9
Max Kellermann <mk@cm4all.com>
2024-02-21 11:24:40 +0100

    certdb/AcmeMain: move code to CRequest.hxx

--- src/certdb/AcmeMain.cxx
+++ src/certdb/AcmeMain.cxx
@@ -3,6 +3,7 @@
 // author: Max Kellermann <mk@cm4all.com>
 
 #include "AcmeMain.hxx"
+#include "CRequest.hxx"
 #include "Main.hxx"
 #include "Progress.hxx"
 #include "AcmeUtil.hxx"
@@ -20,14 +21,10 @@
 #include "CertDatabase.hxx"
 #include "WrapKey.hxx"
 #include "lib/fmt/RuntimeError.hxx"
-#include "lib/openssl/Edit.hxx"
 #include "lib/openssl/Key.hxx"
 #include "lib/openssl/AltName.hxx"
 #include "lib/openssl/Name.hxx"
-#include "lib/openssl/GeneralName.hxx"
-#include "lib/openssl/UniqueX509.hxx"
 #include "lib/openssl/UniqueCertKey.hxx"
-#include "lib/openssl/Error.hxx"
 #include "util/AllocatedString.hxx"
 #include "util/ConstBuffer.hxx"
 #include "util/StringAPI.hxx"
@@ -49,95 +46,6 @@ GetAcmeAccountKey(AcmeConfig &config, CertDatabase &db)
 		return AcmeKey(std::move(account.key));
 	} else
 		return AcmeKey(config.account_key_path.c_str());
-}
-
-static void
-CopyCommonName(X509_REQ &req, X509 &src)
-{
-	X509_NAME *src_subject = X509_get_subject_name(&src);
-	if (src_subject == nullptr)
-		return;
-
-	int i = X509_NAME_get_index_by_NID(src_subject, NID_commonName, -1);
-	if (i < 0)
-		return;
-
-	auto *common_name = X509_NAME_get_entry(src_subject, i);
-	auto *dest_subject = X509_REQ_get_subject_name(&req);
-	X509_NAME_add_entry(dest_subject, common_name, -1, 0);
-}
-
-/**
- * Add a subject_alt_name extension for each host name in the list.
- */
-template<typename L>
-static void
-AddDnsAltNames(X509_REQ &req, const L &hosts)
-{
-	OpenSSL::UniqueGeneralNames ns;
-	for (const auto &host : hosts)
-		ns.push_back(OpenSSL::ToDnsName(host.c_str()));
-
-	AddAltNames(req, ns);
-}
-
-/**
- * Copy the subject_alt_name extension from the source certificate to
- * the request.
- */
-static void
-CopyDnsAltNames(X509_REQ &req, X509 &src)
-{
-	int i = X509_get_ext_by_NID(&src, NID_subject_alt_name, -1);
-	if (i < 0)
-		/* no subject_alt_name found, no-op */
-		return;
-
-	auto ext = X509_get_ext(&src, i);
-	if (ext == nullptr)
-		return;
-
-	OpenSSL::UniqueGeneralNames gn(reinterpret_cast<GENERAL_NAMES *>(X509V3_EXT_d2i(ext)));
-	if (!gn)
-		return;
-
-	AddAltNames(req, gn);
-}
-
-static UniqueX509_REQ
-MakeCertRequest(EVP_PKEY &key, const std::set<std::string> &alt_hosts)
-{
-	UniqueX509_REQ req(X509_REQ_new());
-	if (req == nullptr)
-		throw "X509_REQ_new() failed";
-
-	if (!alt_hosts.empty())
-		AddDnsAltNames(*req, alt_hosts);
-
-	X509_REQ_set_pubkey(req.get(), &key);
-
-	if (!X509_REQ_sign(req.get(), &key, EVP_sha256()))
-		throw SslError("X509_REQ_sign() failed");
-
-	return req;
-}
-
-static UniqueX509_REQ
-MakeCertRequest(EVP_PKEY &key, X509 &src)
-{
-	UniqueX509_REQ req(X509_REQ_new());
-	if (req == nullptr)
-		throw "X509_REQ_new() failed";
-
-	CopyCommonName(*req, src);
-	CopyDnsAltNames(*req, src);
-
-	X509_REQ_set_pubkey(req.get(), &key);
-
-	if (!X509_REQ_sign(req.get(), &key, EVP_sha256()))
-		throw SslError("X509_REQ_sign() failed");
-
-	return req;
 }
 
 using Alpn01ChallengeRecordPtr = std::shared_ptr<Alpn01ChallengeRecord>;
---
+++ src/certdb/CRequest.hxx
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: BSD-2-Clause
+// Copyright CM4all GmbH
+// author: Max Kellermann <mk@cm4all.com>
+
+#pragma once
+
+#include "lib/openssl/Edit.hxx"
+#include "lib/openssl/Error.hxx"
+#include "lib/openssl/GeneralName.hxx"
+#include "lib/openssl/UniqueX509.hxx"
+
+static void
+CopyCommonName(X509_REQ &req, X509 &src)
+{
+	X509_NAME *src_subject = X509_get_subject_name(&src);
+	if (src_subject == nullptr)
+		return;
+
+	int i = X509_NAME_get_index_by_NID(src_subject, NID_commonName, -1);
+	if (i < 0)
+		return;
+
+	auto *common_name = X509_NAME_get_entry(src_subject, i);
+	auto *dest_subject = X509_REQ_get_subject_name(&req);
+	X509_NAME_add_entry(dest_subject, common_name, -1, 0);
+}
+
+/**
+ * Add a subject_alt_name extension for each host name in the list.
+ */
+static void
+AddDnsAltNames(X509_REQ &req, const auto &hosts)
+{
+	OpenSSL::UniqueGeneralNames ns;
+	for (const auto &host : hosts)
+		ns.push_back(OpenSSL::ToDnsName(host.c_str()));
+
+	AddAltNames(req, ns);
+}
+
+/**
+ * Copy the subject_alt_name extension from the source certificate to
+ * the request.
+ */
+static void
+CopyDnsAltNames(X509_REQ &req, X509 &src)
+{
+	int i = X509_get_ext_by_NID(&src, NID_subject_alt_name, -1);
+	if (i < 0)
+		/* no subject_alt_name found, no-op */
+		return;
+
+	auto ext = X509_get_ext(&src, i);
+	if (ext == nullptr)
+		return;
+
+	OpenSSL::UniqueGeneralNames gn(reinterpret_cast<GENERAL_NAMES *>(X509V3_EXT_d2i(ext)));
+	if (!gn)
+		return;
+
+	AddAltNames(req, gn);
+}
+
+static UniqueX509_REQ
+MakeCertRequest(EVP_PKEY &key, const auto &alt_hosts)
+{
+	UniqueX509_REQ req(X509_REQ_new());
+	if (req == nullptr)
+		throw "X509_REQ_new() failed";
+
+	if (!alt_hosts.empty())
+		AddDnsAltNames(*req, alt_hosts);
+
+	X509_REQ_set_pubkey(req.get(), &key);
+
+	if (!X509_REQ_sign(req.get(), &key, EVP_sha256()))
+		throw SslError("X509_REQ_sign() failed");
+
+	return req;
+}
+
+static UniqueX509_REQ
+MakeCertRequest(EVP_PKEY &key, X509 &src)
+{
+	UniqueX509_REQ req(X509_REQ_new());
+	if (req == nullptr)
+		throw "X509_REQ_new() failed";
+
+	CopyCommonName(*req, src);
+	CopyDnsAltNames(*req, src);
+
+	X509_REQ_set_pubkey(req.get(), &key);
+
+	if (!X509_REQ_sign(req.get(), &key, EVP_sha256()))
+		throw SslError("X509_REQ_sign() failed");
+
+	return req;
+}
