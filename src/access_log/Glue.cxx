CM4all/beng-proxy

commit c75fedfdd28abefd099aca9840011d9b93514ef3

net/log/Datagram: remove constructors, add setters

src/access_log/Glue.cxx
@@ -93,16 +93,20 @@ AccessLogGlue::Log(std::chrono::system_clock::time_point now,
 		}
 	}
 
-	Net::Log::Datagram d(Net::Log::FromSystem(now),
-			     request.method, request.uri,
-			     remote_host,
-			     host,
-			     site,
-			     referer, user_agent,
-			     status, content_length,
-			     bytes_received, bytes_sent,
-			     std::chrono::duration_cast<Net::Log::Duration>(duration));
-	d.forwarded_to = forwarded_to;
+	const auto d = Net::Log::Datagram{
+		.timestamp = Net::Log::FromSystem(now),
+		.remote_host = remote_host,
+		.host = host,
+		.site = site,
+		.forwarded_to = forwarded_to,
+		.http_uri = request.uri,
+		.http_referer = referer,
+		.user_agent = user_agent,
+		.http_method = request.method,
+		.http_status = status,
+	}.SetLength(content_length)
+		.SetTraffic(bytes_received, bytes_sent)
+		.SetDuration(std::chrono::duration_cast<Net::Log::Duration>(duration));
 
 	Log(d);
 }

commit 8850a2d6fc1f12a25887dd56f511f9375fce1b34

access_log/Glue: call SetLength() only if there is a Content-Length
Fixes bogus 18446744073709551615 lengths (== (uint64_t)-1).

Regression was caused by c75fedfdd28abefd099aca9840011d9b93514ef3; I
got confused because the old Datagram constructor had a ">=0" check
which got lost in that commit.

src/access_log/Glue.cxx
@@ -94,7 +94,7 @@ AccessLogGlue::Log(std::chrono::system_clock::time_point now,
 		}
 	}
 
-	const auto d = Net::Log::Datagram{
+	auto d = Net::Log::Datagram{
 		.timestamp = Net::Log::FromSystem(now),
 		.remote_host = remote_host,
 		.host = host,
@@ -106,9 +106,12 @@ AccessLogGlue::Log(std::chrono::system_clock::time_point now,
 		.user_agent = user_agent,
 		.http_method = request.method,
 		.http_status = status,
-	}.SetLength(content_length)
+	}
 		.SetTraffic(bytes_received, bytes_sent)
 		.SetDuration(std::chrono::duration_cast<Net::Log::Duration>(duration));
+
+	if (content_length >= 0)
+		d.SetLength(content_length);
 
 	Log(d);
 }