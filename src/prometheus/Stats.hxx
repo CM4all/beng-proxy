777bb43506891b0f4b0426a09b74ad5a3dea8200
Max Kellermann <mk@cm4all.com>
2024-04-26 19:18:26 +0200

    prometheus/Stats: indent with tabs

--- src/prometheus/Stats.hxx
+++ src/prometheus/Stats.hxx
@@ -13,65 +13,65 @@ namespace BengControl { struct Stats; }
 namespace Prometheus {
 
 struct Stats {
-    /**
-     * Number of open incoming connections.
-     */
-    uint_least32_t incoming_connections;
+	/**
+	 * Number of open incoming connections.
+	 */
+	uint_least32_t incoming_connections;
 
-    /**
-     * Number of open outgoing connections.
-     */
-    uint_least32_t outgoing_connections;
+	/**
+	 * Number of open outgoing connections.
+	 */
+	uint_least32_t outgoing_connections;
 
-    /**
-     * Number of child processes.
-     */
-    uint_least32_t children;
+	/**
+	 * Number of child processes.
+	 */
+	uint_least32_t children;
 
-    /**
-     * Number of sessions.
-     */
-    uint_least32_t sessions;
+	/**
+	 * Number of sessions.
+	 */
+	uint_least32_t sessions;
 
-    /**
-     * Total number of incoming HTTP requests that were received since
-     * the server was started.
-     */
-    uint_least64_t http_requests;
+	/**
+	 * Total number of incoming HTTP requests that were received since
+	 * the server was started.
+	 */
+	uint_least64_t http_requests;
 
-    /**
-     * The total allocated size of the translation cache in the
-     * server's memory [bytes].
-     */
-    uint_least64_t translation_cache_size;
+	/**
+	 * The total allocated size of the translation cache in the
+	 * server's memory [bytes].
+	 */
+	uint_least64_t translation_cache_size;
 
-    /**
-     * The total allocated size of the HTTP cache in the server's
-     * memory [bytes].
-     */
-    uint_least64_t http_cache_size;
+	/**
+	 * The total allocated size of the HTTP cache in the server's
+	 * memory [bytes].
+	 */
+	uint_least64_t http_cache_size;
 
-    /**
-     * The total allocated size of the filter cache in the server's
-     * memory [bytes].
-     */
-    uint_least64_t filter_cache_size;
+	/**
+	 * The total allocated size of the filter cache in the server's
+	 * memory [bytes].
+	 */
+	uint_least64_t filter_cache_size;
 
-    uint_least64_t translation_cache_brutto_size;
-    uint_least64_t http_cache_brutto_size;
-    uint_least64_t filter_cache_brutto_size;
+	uint_least64_t translation_cache_brutto_size;
+	uint_least64_t http_cache_brutto_size;
+	uint_least64_t filter_cache_brutto_size;
 
-    /**
-     * Total size of I/O buffers.
-     */
-    uint_least64_t io_buffers_size, io_buffers_brutto_size;
+	/**
+	 * Total size of I/O buffers.
+	 */
+	uint_least64_t io_buffers_size, io_buffers_brutto_size;
 
-    /**
-     * In- and outgoing HTTP traffic since
-     * the server was started.
-     */
-    uint_least64_t http_traffic_received;
-    uint_least64_t http_traffic_sent;
+	/**
+	 * In- and outgoing HTTP traffic since
+	 * the server was started.
+	 */
+	uint_least64_t http_traffic_received;
+	uint_least64_t http_traffic_sent;
 };
 
 void
