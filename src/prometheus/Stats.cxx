218a7d034f69b33d64383e706a090d561421d1fb
Max Kellermann <mk@cm4all.com>
2024-04-26 20:00:46 +0200

    prometheus/Stats: move code to CacheStats/AllocatorStats functions

--- src/prometheus/Stats.cxx
+++ src/prometheus/Stats.cxx
@@ -6,7 +6,30 @@
 #include "net/control/Protocol.hxx"
 #include "memory/GrowingBuffer.hxx"
 
+using std::string_view_literals::operator""sv;
+
 namespace Prometheus {
+
+static void
+Write(GrowingBuffer &buffer, std::string_view metric_name,
+      std::string_view process, std::string_view type,
+      const AllocatorStats &stats) noexcept
+{
+	buffer.Fmt(R"(
+{}{{process={:?},type={:?},metric="netto"}} {}
+{}{{process={:?},type={:?},metric="brutto"}} {}
+)",
+		   metric_name, process, type, stats.netto_size,
+		   metric_name, process, type, stats.brutto_size);
+}
+
+static void
+Write(GrowingBuffer &buffer,
+      std::string_view process, std::string_view type,
+      const CacheStats &stats) noexcept
+{
+	Write(buffer, "beng_proxy_cache_size"sv, process, type, stats.allocator);
+}
 
 void
 Write(GrowingBuffer &buffer, std::string_view process,
@@ -33,27 +56,16 @@ beng_proxy_connections{{process={:?},direction="in"}} {}
 beng_proxy_connections{{process={:?},direction="out"}} {}
 beng_proxy_children{{process={:?}}} {}
 beng_proxy_sessions{{process={:?}}} {}
-beng_proxy_cache_size{{process={:?},type="translation",metric="netto"}} {}
-beng_proxy_cache_size{{process={:?},type="translation",metric="brutto"}} {}
-beng_proxy_cache_size{{process={:?},type="http",metric="netto"}} {}
-beng_proxy_cache_size{{process={:?},type="http",metric="brutto"}} {}
-beng_proxy_cache_size{{process={:?},type="filter",metric="netto"}} {}
-beng_proxy_cache_size{{process={:?},type="filter",metric="brutto"}} {}
-beng_proxy_buffer_size{{process={:?},type="io",metric="netto"}} {}
-beng_proxy_buffer_size{{process={:?},type="io",metric="brutto"}} {}
 )",
 	       process, stats.incoming_connections,
 	       process, stats.outgoing_connections,
 	       process, stats.children,
-	       process, stats.sessions,
-	       process, stats.translation_cache.allocator.netto_size,
-	       process, stats.translation_cache.allocator.brutto_size,
-	       process, stats.http_cache.allocator.netto_size,
-	       process, stats.http_cache.allocator.brutto_size,
-	       process, stats.filter_cache.allocator.netto_size,
-	       process, stats.filter_cache.allocator.brutto_size,
-	       process, stats.io_buffers.netto_size,
-	       process, stats.io_buffers.brutto_size);
+	       process, stats.sessions);
+
+	Write(buffer, process, "translation"sv, stats.translation_cache);
+	Write(buffer, process, "http"sv, stats.http_cache);
+	Write(buffer, process, "filter"sv, stats.filter_cache);
+	Write(buffer, "beng_proxy_buffer_size"sv, process, "io"sv, stats.io_buffers);
 }
 
 } // namespace Prometheus
