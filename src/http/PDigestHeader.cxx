95935f92d254c90ec543ce4cf0f2639df79ffba2
Max Kellermann <mk@cm4all.com>
2024-05-10 14:10:30 +0200

    http/PDigestHeader: switch from BLAKE2b to SHA-256
    Let's be RFC compliant here.

--- src/http/PDigestHeader.cxx
+++ src/http/PDigestHeader.cxx
@@ -4,6 +4,7 @@
 
 #include "PDigestHeader.hxx"
 #include "AllocatorPtr.hxx"
+#include "lib/sodium/SHA256.hxx"
 #include "util/HexFormat.hxx"
 #include "util/SpanCast.hxx"
 
@@ -13,22 +14,9 @@ using std::string_view_literals::operator""sv;
 
 [[gnu::pure]]
 static auto
-GenericHash(std::span<const std::byte> src) noexcept
-{
-	std::array<std::byte, crypto_generichash_BYTES> result;
-	crypto_generichash(reinterpret_cast<unsigned char *>(result.data()),
-			   result.size(),
-			   reinterpret_cast<const unsigned char *>(src.data()),
-			   src.size(),
-			   nullptr, 0);
-	return result;
-}
-
-[[gnu::pure]]
-static auto
 GenericHashHex(std::span<const std::byte> src) noexcept
 {
-	const auto hash = GenericHash(src);
+	const auto hash = SHA256(src);
 	return HexFormat(std::span{hash});
 }
 
@@ -36,11 +24,5 @@ const char *
 GenerateDigestHeader(AllocatorPtr alloc, std::span<const std::byte> src) noexcept
 {
 	const auto hash_hex = GenericHashHex(src);
-
-	/* "blake2b" is not an allowed value according to
-	   https://www.iana.org/assignments/http-dig-alg/http-dig-alg.xhtml
-	   but let's use it anyway, because it's the default algorithm
-	   implemented by libsodium and we use this only for
-	   experiments with our EncodingCache */
-	return alloc.Concat("blake2b="sv, ToStringView(hash_hex));
+	return alloc.Concat("sha-256="sv, ToStringView(hash_hex));
 }
