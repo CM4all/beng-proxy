5f8a48ecb55aa1d54dd6b17a5ae3d39213225e26
Max Kellermann <max.kellermann@ionos.com>
2024-07-26 12:32:54 +0200

    spawn/Stats: add counters for killed and exited processes

--- src/prometheus/SpawnStats.cxx
+++ src/prometheus/SpawnStats.cxx
@@ -21,15 +21,25 @@ Write(GrowingBuffer &buffer, std::string_view process,
 # HELP beng_proxy_spawn_errors Total number of child processes that failed to spawn
 # TYPE beng_proxy_spawn_errors counter
 
+# HELP beng_proxy_children_killed Total number of child processes that were killed with a signal
+# TYPE beng_proxy_children_killed counter
+
+# HELP beng_proxy_children_exited Total number of child processes that have exited
+# TYPE beng_proxy_children_exited counter
+
 # HELP beng_proxy_children Number of child processes
 # TYPE beng_proxy_children gauge
 
 beng_proxy_children_spawned{{process={:?}}} {}
 beng_proxy_spawn_errors{{process={:?}}} {}
+beng_proxy_children_killed{{process={:?}}} {}
+beng_proxy_children_exited{{process={:?}}} {}
 beng_proxy_children{{process={:?}}} {}
 )"sv,
 		   process, stats.spawned,
 		   process, stats.errors,
+		   process, stats.killed,
+		   process, stats.exited,
 		   process, stats.alive);
 }
 
