720f044575bc170f346434ed0fd2009af3ca8570
Max Kellermann <max.kellermann@ionos.com>
2024-11-28 14:37:40 +0100

    lb/Headers: discard duplicate Via/X-Forwarded-For headers
    The "trust_xff" setting could be circumvented by sending multitple
    "X-Forwarded-For" headers.  We need to use the new method RemoveAll()
    to remove all such headers and add only one new (rewritten) header.

--- src/lb/Headers.cxx
+++ src/lb/Headers.cxx
@@ -11,7 +11,7 @@ static void
 forward_via(AllocatorPtr alloc, StringMap &headers,
 	    const char *local_host) noexcept
 {
-	const char *p = headers.Remove(via_header);
+	const char *p = headers.RemoveAll(via_header);
 	if (p == nullptr) {
 		if (local_host != nullptr)
 			headers.Add(alloc, via_header, alloc.Concat("1.1 ", local_host));
@@ -27,7 +27,7 @@ static void
 forward_xff(AllocatorPtr alloc, StringMap &headers,
 	    const char *remote_host) noexcept
 {
-	const char *p = headers.Remove(x_forwarded_for_header);
+	const char *p = headers.RemoveAll(x_forwarded_for_header);
 	if (p == nullptr) {
 		if (remote_host != nullptr)
 			headers.Add(alloc, x_forwarded_for_header, remote_host);
