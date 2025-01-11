daf1171c3fd6db03542bbc5a2ba674ac54207cd4
Max Kellermann <max.kellermann@ionos.com>
2025-01-11 07:14:47 +0100

    uri/RedirectHttps: use fmt instead of snprintf()

--- src/uri/RedirectHttps.cxx
+++ src/uri/RedirectHttps.cxx
@@ -3,19 +3,18 @@
 // author: Max Kellermann <mk@cm4all.com>
 
 #include "RedirectHttps.hxx"
+#include "lib/fmt/Unsafe.hxx"
 #include "net/HostParser.hxx"
 #include "AllocatorPtr.hxx"
-
-#include <stdio.h>
 
 const char *
 MakeHttpsRedirect(AllocatorPtr alloc, const char *_host, uint16_t port,
 		  const char *uri) noexcept
 {
 	char port_buffer[16];
-	size_t port_length = 0;
+	std::string_view port_sv{};
 	if (port != 0 && port != 443)
-		port_length = sprintf(port_buffer, ":%u", port);
+		port_sv = FmtUnsafeSV(port_buffer, ":{}", port);
 
 	auto eh = ExtractHost(_host);
 	auto host = eh.host.data() != nullptr
@@ -26,12 +25,12 @@ MakeHttpsRedirect(AllocatorPtr alloc, const char *_host, uint16_t port,
 	static constexpr char b = ']';
 	const size_t is_ipv6 =
 		std::find(eh.host.begin(), eh.host.end(), ':') != eh.host.end();
-	const size_t need_brackets = is_ipv6 && port_length > 0;
+	const size_t need_brackets = is_ipv6 && !port_sv.empty();
 
 	return alloc.Concat("https://",
 			    std::string_view{&a, need_brackets},
 			    host,
 			    std::string_view{&b, need_brackets},
-			    std::string_view{port_buffer, port_length},
+			    port_sv,
 			    uri);
 }
