5b5a98c89b6ff14fdd25d823cd59547fdc977ce5
Max Kellermann <max.kellermann@ionos.com>
2025-01-13 21:04:17 +0100

    net/BufferedSocketLease: new class

---
+++ src/net/BufferedSocketLease.cxx
@@ -0,0 +1,312 @@
+// SPDX-License-Identifier: BSD-2-Clause
+// Copyright CM4all GmbH
+// author: Max Kellermann <mk@cm4all.com>
+
+#include "BufferedSocketLease.hxx"
+#include "memory/fb_pool.hxx"
+#include "net/SocketProtocolError.hxx"
+
+BufferedSocketLease::BufferedSocketLease(BufferedSocket &_socket, Lease &lease,
+					 Event::Duration write_timeout,
+					 BufferedSocketHandler &_handler) noexcept
+	:socket(&_socket), lease_ref(lease),
+	 handler(_handler)
+{
+	socket->Reinit(write_timeout, *this);
+}
+
+BufferedSocketLease::~BufferedSocketLease() noexcept
+{
+	assert(IsReleased());
+}
+
+inline void
+BufferedSocketLease::MoveSocketInput() noexcept
+{
+	assert(input.empty());
+
+	input.MoveFromAllowBothNull(socket->GetInputBuffer());
+	assert(socket->GetAvailable() == 0);
+}
+
+void
+BufferedSocketLease::Release(bool preserve, PutAction action) noexcept
+{
+	assert(!IsReleased());
+	assert(lease_ref);
+
+	if (preserve)
+		MoveSocketInput();
+
+	socket->SetDirect(false);
+
+	action = lease_ref.Release(action);
+	if (handler_info != nullptr) {
+#ifndef NDEBUG
+		handler_info->released = true;
+#endif
+		handler_info->action = action;
+	}
+
+	socket = nullptr;
+}
+
+bool
+BufferedSocketLease::IsEmpty() const noexcept
+{
+	if (IsReleased())
+		return IsReleasedEmpty();
+	else
+		return socket->IsEmpty();
+}
+
+std::size_t
+BufferedSocketLease::GetAvailable() const noexcept
+{
+	if (IsReleased())
+		return input.GetAvailable();
+	else
+		return socket->GetAvailable();
+}
+
+std::span<std::byte>
+BufferedSocketLease::ReadBuffer() const noexcept
+{
+	return IsReleased()
+		? input.Read()
+		: socket->ReadBuffer();
+}
+
+void
+BufferedSocketLease::DisposeConsumed(std::size_t nbytes) noexcept
+{
+	if (IsReleased())
+		input.Consume(nbytes);
+	else
+		socket->DisposeConsumed(nbytes);
+}
+
+void
+BufferedSocketLease::AfterConsumed() noexcept
+{
+	if (!IsReleased())
+		socket->AfterConsumed();
+}
+
+bool
+BufferedSocketLease::ReadReleased() noexcept
+{
+	const std::size_t remaining = input.GetAvailable();
+
+	switch (handler.OnBufferedData()) {
+	case BufferedResult::OK:
+		if (IsReleasedEmpty()) {
+			try {
+				if (!handler.OnBufferedEnd())
+					return false;
+			} catch (...) {
+				handler.OnBufferedError(std::current_exception());
+				return false;
+			}
+		}
+
+		if (input.GetAvailable() >= remaining)
+			/* no data was consumed */
+			return true;
+
+		break;
+
+	case BufferedResult::MORE:
+		if (IsReleasedEmpty()) {
+			handler.OnBufferedError(std::make_exception_ptr(SocketClosedPrematurelyError{}));
+			return false;
+		}
+
+		break;
+
+	case BufferedResult::AGAIN:
+		break;
+
+	case BufferedResult::DESTROYED:
+		return false;
+	}
+
+	return true;
+}
+
+BufferedReadResult
+BufferedSocketLease::Read() noexcept
+{
+	if (IsReleased())
+		return ReadReleased()
+			? BufferedReadResult::DISCONNECTED
+			: BufferedReadResult::DESTROYED;
+
+	const DestructObserver destructed{destruct_anchor};
+
+	auto result = socket->Read();
+
+	if (result == BufferedReadResult::DESTROYED && !destructed) {
+		/* BufferedSocket::Read() may return DESTROYED if we
+		   have just released our lease, but this lease has
+		   not been destroyed: translate the return value ot
+		   DISCONNECTED instead */
+		assert(IsReleased());
+		result = BufferedReadResult::DISCONNECTED;
+	} else if (destructed)
+		/* the BufferedSocket is alive, but the lease has been
+		   destroyed */
+		result = BufferedReadResult::DESTROYED;
+
+	return result;
+}
+
+BufferedResult
+BufferedSocketLease::OnBufferedData()
+{
+	HandlerInfo info;
+
+	while (true) {
+		assert(handler_info == nullptr);
+		handler_info = &info;
+
+		const auto result = handler.OnBufferedData();
+
+		if (result != BufferedResult::DESTROYED) {
+			assert(handler_info == &info);
+			handler_info = nullptr;
+		}
+
+		if (result == BufferedResult::DESTROYED) {
+			assert(info.released);
+
+			return info.action == PutAction::DESTROY
+				? BufferedResult::DESTROYED
+				/* the BufferedSocketLease was
+				   destroyed, but the BufferedSocket
+				   is still alive (in the
+				   BufferedSocketStock) */
+				: BufferedResult::OK;
+		}
+
+		if (!IsReleased())
+			return result;
+
+		assert(info.released);
+
+		/* since the BufferedSocket is gone already, we must handle
+		   the AGAIN result codes here */
+
+		if (result != BufferedResult::AGAIN && !IsEmpty()) {
+			/* if the socket has been released, we must
+			   always report OK/DESTROYED to the released
+			   BufferedSocket instance, even if our
+			   handler still wants to consume the
+			   remaining buffer */
+			return info.action == PutAction::DESTROY
+				? BufferedResult::DESTROYED
+				: BufferedResult::OK;
+		}
+	}
+}
+
+DirectResult
+BufferedSocketLease::OnBufferedDirect(SocketDescriptor fd, FdType fd_type)
+{
+	assert(handler_info == nullptr);
+	HandlerInfo info;
+	handler_info = &info;
+
+	auto result = handler.OnBufferedDirect(fd, fd_type);
+
+	if (result != DirectResult::CLOSED) {
+		assert(handler_info == &info);
+		handler_info = nullptr;
+	}
+
+	if (result == DirectResult::CLOSED) {
+		assert(info.released);
+
+		if (info.action != PutAction::DESTROY)
+			/* the BufferedSocketLease was destroyed, but
+			   the BufferedSocket is still alive (in the
+			   BufferedSocketStock) */
+			result = DirectResult::OK;
+	}
+
+	return result;
+}
+
+bool
+BufferedSocketLease::OnBufferedHangup() noexcept
+{
+	return handler.OnBufferedHangup();
+}
+
+bool
+BufferedSocketLease::OnBufferedClosed() noexcept
+{
+	auto result = handler.OnBufferedClosed();
+	if (result && IsReleased()) {
+		result = false;
+
+		if (handler.OnBufferedRemaining(GetAvailable()) &&
+		    ReadReleased() &&
+		    IsReleasedEmpty()) {
+			try {
+				if (!handler.OnBufferedEnd())
+					return false;
+			} catch (...) {
+				handler.OnBufferedError(std::current_exception());
+				return false;
+			}
+		}
+	}
+
+	return result;
+}
+
+bool
+BufferedSocketLease::OnBufferedRemaining(std::size_t remaining) noexcept
+{
+	auto result = handler.OnBufferedRemaining(remaining);
+	if (result && IsReleased())
+		result = false;
+	return result;
+}
+
+bool
+BufferedSocketLease::OnBufferedEnd()
+{
+	return handler.OnBufferedEnd();
+}
+
+bool
+BufferedSocketLease::OnBufferedWrite()
+{
+	return handler.OnBufferedWrite();
+}
+
+bool
+BufferedSocketLease::OnBufferedDrained() noexcept
+{
+	return handler.OnBufferedDrained();
+}
+
+bool
+BufferedSocketLease::OnBufferedTimeout() noexcept
+{
+	return handler.OnBufferedTimeout();
+}
+
+enum write_result
+BufferedSocketLease::OnBufferedBroken() noexcept
+{
+	return handler.OnBufferedBroken();
+}
+
+void
+BufferedSocketLease::OnBufferedError(std::exception_ptr e) noexcept
+{
+	return handler.OnBufferedError(e);
+}
---
+++ src/net/BufferedSocketLease.hxx
@@ -0,0 +1,173 @@
+// SPDX-License-Identifier: BSD-2-Clause
+// Copyright CM4all GmbH
+// author: Max Kellermann <mk@cm4all.com>
+
+#pragma once
+
+#include "event/net/BufferedSocket.hxx"
+#include "util/DestructObserver.hxx"
+#include "lease.hxx"
+
+#include <array>
+
+#include <sys/uio.h> // for struct iovec
+
+/**
+ * Wrapper for a #BufferedSocket which may be released at some point.
+ * After that, remaining data in the input buffer can still be read.
+ *
+ * This class acts a #BufferedSocketHandler proxy to filter result
+ * codes, when the socket has been released in the middle of a handler
+ * method.
+ */
+class BufferedSocketLease final : BufferedSocketHandler {
+	/* needed for translating the BufferedSocket::Read() return
+	   value */
+	DestructAnchor destruct_anchor;
+
+	BufferedSocket *socket;
+	LeasePtr lease_ref;
+
+	BufferedSocketHandler &handler;
+
+	SliceFifoBuffer input;
+
+	struct HandlerInfo {
+#ifndef NDEBUG
+		bool released = false;
+#endif
+		PutAction action;
+	};
+
+	HandlerInfo *handler_info = nullptr;
+
+public:
+	BufferedSocketLease(BufferedSocket &_socket, Lease &lease,
+			    Event::Duration write_timeout,
+			    BufferedSocketHandler &_handler) noexcept;
+
+	~BufferedSocketLease() noexcept;
+
+	[[gnu::pure]]
+	bool IsConnected() const noexcept {
+		return socket != nullptr && socket->IsConnected();
+	}
+
+	void Close() const noexcept {
+		socket->Close();
+	}
+
+#ifndef NDEBUG
+	[[gnu::pure]]
+	bool HasEnded() const noexcept {
+		assert(!IsReleased());
+
+		return socket->HasEnded();
+	}
+#endif
+
+	/**
+	 * @param preserve preserve the contents of the input buffer for
+	 * further consumption?
+	 */
+	void Release(bool preserve, PutAction action) noexcept;
+
+	bool IsReleased() const noexcept {
+		return socket == nullptr;
+	}
+
+	[[gnu::pure]]
+	FdType GetType() const noexcept {
+		assert(!IsReleased());
+
+		return socket->GetType();
+	}
+
+	void SetDirect(bool _direct) noexcept {
+		assert(!IsReleased());
+
+		socket->SetDirect(_direct);
+	}
+
+	[[gnu::pure]]
+	bool IsEmpty() const noexcept;
+
+	[[gnu::pure]]
+	std::size_t GetAvailable() const noexcept;
+
+	std::span<std::byte> ReadBuffer() const noexcept;
+
+	void DisposeConsumed(std::size_t nbytes) noexcept;
+	void AfterConsumed() noexcept;
+
+	BufferedReadResult Read() noexcept;
+
+	void ScheduleRead() noexcept {
+		socket->ScheduleRead();
+	}
+
+	ssize_t Write(std::span<const std::byte>  src) noexcept {
+		assert(!IsReleased());
+
+		return socket->Write(src);
+	}
+
+	void DeferWrite() noexcept {
+		assert(!IsReleased());
+
+		socket->DeferWrite();
+	}
+
+	void ScheduleWrite() noexcept {
+		assert(!IsReleased());
+
+		socket->ScheduleWrite();
+	}
+
+	void UnscheduleWrite() noexcept {
+		assert(!IsReleased());
+
+		socket->UnscheduleWrite();
+	}
+
+	ssize_t WriteV(std::span<const struct iovec> v) noexcept {
+		assert(!IsReleased());
+
+		return socket->WriteV(v);
+	}
+
+	ssize_t WriteFrom(FileDescriptor fd, FdType fd_type, off_t *offset,
+			  std::size_t length) noexcept {
+		assert(!IsReleased());
+
+		return socket->WriteFrom(fd, fd_type, offset, length);
+	}
+
+private:
+	/**
+	 * Move data from the #BufferedSocket input buffers to our #input
+	 * buffers.  This is done prior to releasing the #BufferedSocket
+	 * to be able to continue reading pending input.
+	 */
+	void MoveSocketInput() noexcept;
+
+	bool IsReleasedEmpty() const noexcept {
+		return input.empty();
+	}
+
+	bool ReadReleased() noexcept;
+
+	/* virtual methods from class BufferedSocketHandler */
+	BufferedResult OnBufferedData() override;
+	DirectResult OnBufferedDirect(SocketDescriptor fd,
+				      FdType fd_type) override;
+	bool OnBufferedHangup() noexcept override;
+	bool OnBufferedClosed() noexcept override;
+	bool OnBufferedRemaining(std::size_t remaining) noexcept override;
+	bool OnBufferedEnd() override;
+	bool OnBufferedWrite() override;
+	bool OnBufferedDrained() noexcept override;
+	bool OnBufferedTimeout() noexcept override;
+	enum write_result OnBufferedBroken() noexcept override;
+	void OnBufferedError(std::exception_ptr e) noexcept override;
+};
