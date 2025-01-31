CM4all/beng-proxy

commit 3570067b9dfb825c374aa545f1512043e0798e10

bp/FileHandler: use the FdCache

src/bp/FileHandler.cxx
@@ -32,6 +32,7 @@
 
 #include <assert.h>
 #include <fcntl.h>
+#include <linux/openat2.h> // for RESOLVE_*
 #include <sys/stat.h>
 
 using std::string_view_literals::operator""sv;
@@ -101,18 +102,17 @@ Request::CheckFileAddress(const FileAddress &address) noexcept
 }
 
 inline void
-Request::Handler::File::Close(UringGlue &uring) noexcept
+Request::Handler::File::Close() noexcept
 {
 	open_address = nullptr;
 	error = 0;
-
-	if (fd.IsDefined())
-		uring.Close(fd.Release());
+	fd = FileDescriptor::Undefined();
+	fd_lease = {};
 }
 
 void
-Request::DispatchFile(const char *path, UniqueFileDescriptor fd,
-		      const struct statx &st,
+Request::DispatchFile(const char *path, FileDescriptor fd,
+		      const struct statx &st, SharedLease &&lease,
 		      const struct file_request &file_request) noexcept
 {
 	const TranslateResponse &tr = *translate.response;
@@ -175,8 +175,6 @@ Request::DispatchFile(const char *path, UniqueFileDescriptor fd,
 
 	/* finished, dispatch this response */
 
-	auto *shared_fd = NewFromPool<SharedFd>(pool, std::move(fd));
-
 	DispatchResponse(status, std::move(headers),
 #ifdef HAVE_URING
 			 instance.uring
@@ -188,15 +186,15 @@ Request::DispatchFile(const char *path, UniqueFileDescriptor fd,
 			       network filesystem) I/O */
 			    ? NewUringSpliceIstream(instance.event_loop, *instance.uring, instance.pipe_stock,
 						    pool, path,
-						    shared_fd->Get(), *shared_fd,
+						    fd, std::move(lease),
 						    start_offset, end_offset)
 			    : NewUringIstream(*instance.uring, pool, path,
-					      shared_fd->Get(), *shared_fd,
+					      fd, std::move(lease),
 					      start_offset, end_offset))
 			 :
 #endif
 			 istream_file_fd_new(instance.event_loop, pool, path,
-					     shared_fd->Get(), *shared_fd,
+					     fd, std::move(lease),
 					     start_offset, end_offset));
 }
 
@@ -362,22 +360,23 @@ Request::ProbeNextPrecompressed() noexcept
 	}
 
 	const struct file_request file_request(p.original_st.stx_size);
-	DispatchFile(address.path, std::move(p.original_fd), p.original_st,
+	DispatchFile(address.path, p.original_fd, p.original_st, std::move(p.original_lease),
 		     file_request);
 }
 
 inline void
-Request::ProbePrecompressed(UniqueFileDescriptor fd,
-			    const struct statx &st) noexcept
+Request::ProbePrecompressed(FileDescriptor fd, const struct statx &st,
+			    SharedLease &&lease) noexcept
 {
-	handler.file.precompressed = UniquePoolPtr<Request::Handler::File::Precompressed>::Make(pool, std::move(fd), st);
+	handler.file.precompressed = UniquePoolPtr<Request::Handler::File::Precompressed>::Make(pool, fd, st, std::move(lease));
 	ProbeNextPrecompressed();
 }
 
 inline bool
 Request::MaybeEmulateModAuthEasy(const FileAddress &address,
-				 UniqueFileDescriptor &fd,
-				 const struct statx &st) noexcept
+				 FileDescriptor fd,
+				 const struct statx &st,
+				 SharedLease &lease) noexcept
 {
 	assert(S_ISREG(st.stx_mode));
 
@@ -397,14 +396,13 @@ Request::MaybeEmulateModAuthEasy(const FileAddress &address,
 	if (strstr(base, "/pr_0001/public_html") == nullptr)
 		return false;
 
-	return EmulateModAuthEasy(address, fd, st);
+	return EmulateModAuthEasy(address, fd, st, lease);
 }
 
 inline void
-Request::OnOpenStat(UniqueFileDescriptor fd,
-		    struct statx &st) noexcept
+Request::OnOpenStat(FileDescriptor fd, const struct statx &st, SharedLease _lease) noexcept
 {
-	HandleFileAddress(*handler.file.address, std::move(fd), st);
+	HandleFileAddress(*handler.file.address, fd, st, std::move(_lease));
 }
 
 inline void
@@ -435,8 +433,8 @@ Request::HandleFileAddress(const FileAddress &address) noexcept
 
 		if (handler.file.fd.IsDefined())
 			/* file has already been opened */
-			HandleFileAddress(address, std::move(handler.file.fd),
-					  handler.file.stx);
+			HandleFileAddress(address, handler.file.fd,
+					  handler.file.stx, std::move(handler.file.fd_lease));
 		else
 			OnOpenStatError(handler.file.error);
 	} else
@@ -444,19 +442,37 @@ Request::HandleFileAddress(const FileAddress &address) noexcept
 }
 
 void
-Request::HandleFileAddressAfterBase(FileDescriptor base) noexcept
+Request::HandleFileAddressAfterBase(FileDescriptor base, std::string_view strip_base) noexcept
 {
-	instance.uring.OpenStat(pool,
-				{base, StripBase(handler.file.address->path)},
-				BIND_THIS_METHOD(OnOpenStat),
-				BIND_THIS_METHOD(OnOpenStatError),
-				cancel_ptr);
+	const FileAddress &address = *handler.file.address;
+
+	const char *path = address.base != nullptr
+		? AllocatorPtr{pool}.Concat(address.base, address.path)
+		: address.path;
+
+	static constexpr struct open_how open_read_only{
+		.flags = O_RDONLY|O_NOCTTY|O_CLOEXEC|O_NONBLOCK,
+		.resolve = RESOLVE_NO_MAGICLINKS,
+	};
+
+	static constexpr struct open_how open_read_only_beneath{
+		.flags = O_RDONLY|O_NOCTTY|O_CLOEXEC|O_NONBLOCK,
+		.resolve = RESOLVE_BENEATH|RESOLVE_NO_MAGICLINKS,
+	};
+
+	instance.fd_cache.Get(base, strip_base, path,
+			      base.IsDefined() ? open_read_only_beneath : open_read_only,
+			      STATX_TYPE|STATX_MTIME|STATX_INO|STATX_SIZE,
+			      BIND_THIS_METHOD(OnOpenStat),
+			      BIND_THIS_METHOD(OnOpenStatError),
+			      cancel_ptr);
 }
 
 void
 Request::HandleFileAddress(const FileAddress &address,
-			   UniqueFileDescriptor fd,
-			   const struct statx &st) noexcept
+			   FileDescriptor fd,
+			   const struct statx &st,
+			   SharedLease &&lease) noexcept
 {
 	/* check request method */
 
@@ -474,20 +490,17 @@ Request::HandleFileAddress(const FileAddress &address,
 		DispatchResponse(HttpStatus::OK, {},
 				 NewFdIstream(instance.event_loop,
 					      pool, address.path,
-					      std::move(fd),
+					      fd.Duplicate(),
 					      FdType::FD_CHARDEV));
 		return;
 	}
 
 	if (!S_ISREG(st.stx_mode)) {
-		instance.uring.Close(fd.Release());
 		DispatchError(HttpStatus::NOT_FOUND, "Not a regular file");
 		return;
 	}
 
-	if (MaybeEmulateModAuthEasy(address, fd, st)) {
-		if (fd.IsDefined())
-			instance.uring.Close(fd.Release());
+	if (MaybeEmulateModAuthEasy(address, fd, st, lease)) {
 		return;
 	}
 
@@ -496,7 +509,6 @@ Request::HandleFileAddress(const FileAddress &address,
 	/* request options */
 
 	if (!EvaluateFileRequest(fd, st, file_request)) {
-		instance.uring.Close(fd.Release());
 		return;
 	}
 
@@ -504,23 +516,23 @@ Request::HandleFileAddress(const FileAddress &address,
 
 	if (file_request.range.type == HttpRangeRequest::Type::NONE &&
 	    !IsTransformationEnabled()) {
-		ProbePrecompressed(std::move(fd), st);
+		ProbePrecompressed(fd, st, std::move(lease));
 		return;
 	}
 
 	/* build the response */
 
-	DispatchFile(address.path, std::move(fd), st, file_request);
+	DispatchFile(address.path, fd, st, std::move(lease), file_request);
 }
 
 void
-Request::OnStatOpenStatSuccess(UniqueFileDescriptor fd,
-			       struct statx &st) noexcept
+Request::OnStatOpenStatSuccess(FileDescriptor fd, const struct statx &st, SharedLease lease) noexcept
 {
 	assert(!handler.file.fd.IsDefined());
 	assert(handler.file.error == 0);
 
-	handler.file.fd = std::move(fd);
+	handler.file.fd_lease = std::move(lease);
+	handler.file.fd = fd;
 	handler.file.stx = st;
 	handler.file.open_address = handler.file.address;
 
@@ -540,17 +552,27 @@ Request::OnStatOpenStatError(int error) noexcept
 }
 
 void
-Request::StatFileAddressAfterBase(FileDescriptor base) noexcept
+Request::StatFileAddressAfterBase(FileDescriptor base, std::string_view strip_base) noexcept
 {
 	assert(!handler.file.fd.IsDefined());
 
 	const FileAddress &address = *handler.file.address;
 
-	instance.uring.OpenStat(pool,
-				{base, StripBase(address.path)},
-				BIND_THIS_METHOD(OnStatOpenStatSuccess),
-				BIND_THIS_METHOD(OnStatOpenStatError),
-				cancel_ptr);
+	const char *path = address.base != nullptr
+		? AllocatorPtr{pool}.Concat(address.base, address.path)
+		: address.path;
+
+	static constexpr struct open_how open_read_only{
+		.flags = O_RDONLY|O_NOCTTY|O_CLOEXEC|O_NONBLOCK,
+		.resolve = RESOLVE_BENEATH|RESOLVE_NO_MAGICLINKS,
+	};
+
+	instance.fd_cache.Get(base, strip_base, path,
+			      open_read_only,
+			      STATX_TYPE|STATX_MTIME|STATX_INO|STATX_SIZE,
+			      BIND_THIS_METHOD(OnStatOpenStatSuccess),
+			      BIND_THIS_METHOD(OnStatOpenStatError),
+			      cancel_ptr);
 }
 
 void
@@ -566,7 +588,7 @@ Request::StatFileAddress(const FileAddress &address,
 		else
 			(this->*on_error)(handler.file.error);
 	} else {
-		handler.file.Close(instance.uring);
+		handler.file.Close();
 
 		handler.file.address = &address;
 		handler.file.on_stat_success = on_success;

src/bp/Base.cxx
@@ -31,11 +31,14 @@ NormalizePath(std::string_view path) noexcept
 inline void
 Request::OnBaseOpen(FileDescriptor fd, const struct statx &, SharedLease lease) noexcept
 {
+	const auto &address = *handler.file.address;
+	assert(address.base != nullptr);
+
 	handler.file.base = fd;
 	handler.file.base_lease = std::move(lease);
 	handler.file.base_relative = {};
 
-	(this->*handler.file.open_base_callback)(fd);
+	(this->*handler.file.open_base_callback)(fd, address.base);
 }
 
 inline void
@@ -63,7 +66,7 @@ Request::OnBeneathOpen(FileDescriptor fd, const struct statx &, SharedLease leas
 		handler.file.base_relative = base;
 	}
 
-	(this->*handler.file.open_base_callback)(fd);
+	(this->*handler.file.open_base_callback)(fd, address.beneath);
 }
 
 inline void
@@ -107,7 +110,7 @@ Request::OpenBase(const FileAddress &address,
 	else if (address.base != nullptr)
 		OpenBase(address.base, callback);
 	else
-		(this->*callback)(FileDescriptor::Undefined());
+		(this->*callback)(FileDescriptor::Undefined(), {});
 }
 
 inline void
@@ -122,7 +125,7 @@ Request::OpenBase(const ResourceAddress &address,
 	case ResourceAddress::Type::FASTCGI:
 	case ResourceAddress::Type::WAS:
 	case ResourceAddress::Type::LHTTP:
-		(this->*callback)(FileDescriptor::Undefined());
+		(this->*callback)(FileDescriptor::Undefined(), {});
 		break;
 
 	case ResourceAddress::Type::LOCAL:

src/bp/EmulateModAuthEasy.cxx
@@ -13,7 +13,6 @@
 #include "translation/Vary.hxx"
 #include "istream/FileIstream.hxx"
 #include "io/FileDescriptor.hxx"
-#include "io/SharedFd.hxx"
 #include "util/StringCompare.hxx"
 #include "util/StringStrip.hxx"
 #include "util/CharUtil.hxx"
@@ -210,8 +209,9 @@ CheckAccessFileFor(FileDescriptor directory, std::string_view base_relative,
 
 bool
 Request::EmulateModAuthEasy(const FileAddress &address,
-			    UniqueFileDescriptor &fd,
-			    const struct statx &st) noexcept
+			    FileDescriptor fd,
+			    const struct statx &st,
+			    SharedLease &lease) noexcept
 {
 	if (!CheckAccessFileFor(handler.file.base, handler.file.base_relative,
 				request.headers,
@@ -278,14 +278,12 @@ Request::EmulateModAuthEasy(const FileAddress &address,
 			      instance.config.use_xattr);
 	write_translation_vary_header(headers2, tr);
 
-	auto *shared_fd = NewFromPool<SharedFd>(pool, std::move(fd));
-
 	auto status = tr.status == HttpStatus{} ? HttpStatus::OK : tr.status;
 
 	DispatchResponse(status, std::move(headers),
 			 istream_file_fd_new(instance.event_loop, pool,
 					     address.path,
-					     shared_fd->Get(), *shared_fd,
+					     fd, std::move(lease),
 					     0, st.stx_size));
 
 	return true;

src/bp/Precompressed.hxx
@@ -5,6 +5,7 @@
 #pragma once
 
 #include "Request.hxx"
+#include "util/SharedLease.hxx"
 
 #include <sys/stat.h>
 
@@ -13,9 +14,11 @@ struct Request::Handler::File::Precompressed {
 
 	const char *encoding;
 
+	SharedLease original_lease;
+
 	const struct statx original_st;
 
-	UniqueFileDescriptor original_fd;
+	FileDescriptor original_fd;
 
 	enum Stat {
 #ifdef HAVE_BROTLI
@@ -26,6 +29,6 @@ struct Request::Handler::File::Precompressed {
 		END
 	} state{};
 
-	Precompressed(UniqueFileDescriptor &&_fd, const struct statx &_st) noexcept
-		:original_st(_st), original_fd(std::move(_fd)) {}
+	Precompressed(FileDescriptor _fd, const struct statx &_st, SharedLease &&_lease) noexcept
+		:original_lease(std::move(_lease)), original_st(_st), original_fd(_fd) {}
 };

src/bp/Request.cxx
@@ -33,16 +33,7 @@ Request::Request(BpConnection &_connection,
 	session_id.Clear();
 }
 
-Request::~Request() noexcept
-{
-	if (handler.file.fd.IsDefined())
-		instance.uring.Close(handler.file.fd.Release());
-
-	if (handler.file.precompressed) {
-		if (handler.file.precompressed->original_fd.IsDefined())
-			instance.uring.Close(handler.file.precompressed->original_fd.Release());
-	}
-}
+Request::~Request() noexcept = default;
 
 TranslationService &
 Request::GetTranslationService() const noexcept

src/bp/Request.hxx
@@ -20,7 +20,7 @@
 #include "widget/View.hxx"
 #include "http/ResponseHandler.hxx"
 #include "io/Logger.hxx"
-#include "io/UniqueFileDescriptor.hxx"
+#include "io/FileDescriptor.hxx"
 #include "co/InvokeTask.hxx"
 #include "util/Cancellable.hxx"
 #include "util/SharedLease.hxx"
@@ -255,7 +255,7 @@ private:
 			 */
 			const FileAddress *open_address = nullptr;
 
-			SharedLease beneath_lease, base_lease;
+			SharedLease beneath_lease, base_lease, fd_lease;
 
 			FileDescriptor base;
 
@@ -264,7 +264,7 @@ private:
 			 * descriptor for the file referred to by
 			 * #base.
 			 */
-			UniqueFileDescriptor fd;
+			FileDescriptor fd;
 
 			/**
 			 * If non-zero, then this is the error that
@@ -296,7 +296,7 @@ private:
 			struct Precompressed;
 			UniquePoolPtr<Precompressed> precompressed;
 
-			using OpenBaseCallback = void (Request:: *)(FileDescriptor fd) noexcept;
+			using OpenBaseCallback = void (Request:: *)(FileDescriptor fd, std::string_view strip_base) noexcept;
 			OpenBaseCallback open_base_callback;
 
 			using StatSuccessCallback = void (Request:: *)(const struct statx &st) noexcept;
@@ -309,7 +309,7 @@ private:
 			 * Clear #open_address, #fd, #error as if
 			 * StatFileAddress() had never been called.
 			 */
-			void Close(UringGlue &uring) noexcept;
+			void Close() noexcept;
 		} file;
 
 		struct {
@@ -595,8 +595,8 @@ private:
 	bool EvaluateFileRequest(FileDescriptor fd, const struct statx &st,
 				 struct file_request &file_request) noexcept;
 
-	void DispatchFile(const char *path, UniqueFileDescriptor fd,
-			  const struct statx &st,
+	void DispatchFile(const char *path, FileDescriptor fd,
+			  const struct statx &st, SharedLease &&lease,
 			  const struct file_request &file_request) noexcept;
 
 	bool DispatchCompressedFile(const char *path, FileDescriptor fd,
@@ -611,23 +611,25 @@ private:
 				     const char *suffix) noexcept;
 
 	bool EmulateModAuthEasy(const FileAddress &address,
-				UniqueFileDescriptor &fd,
-				const struct statx &st) noexcept;
+				FileDescriptor fd,
+				const struct statx &st,
+				SharedLease &lease) noexcept;
 
 	bool MaybeEmulateModAuthEasy(const FileAddress &address,
-				     UniqueFileDescriptor &fd,
-				     const struct statx &st) noexcept;
+				     FileDescriptor fd,
+				     const struct statx &st,
+				     SharedLease &lease) noexcept;
 
 	void HandleFileAddress(const FileAddress &address) noexcept;
-	void HandleFileAddressAfterBase(FileDescriptor base) noexcept;
+	void HandleFileAddressAfterBase(FileDescriptor base, std::string_view strip_base) noexcept;
 	void HandleFileAddress(const FileAddress &address,
-			       UniqueFileDescriptor fd,
-			       const struct statx &st) noexcept;
+			       FileDescriptor fd,
+			       const struct statx &st,
+			       SharedLease &&lease) noexcept;
 
-	void OnStatOpenStatSuccess(UniqueFileDescriptor fd,
-				   struct statx &st) noexcept;
+	void OnStatOpenStatSuccess(FileDescriptor fd, const struct statx &st, SharedLease lease) noexcept;
 	void OnStatOpenStatError(int error) noexcept;
-	void StatFileAddressAfterBase(FileDescriptor base) noexcept;
+	void StatFileAddressAfterBase(FileDescriptor base, std::string_view strip_base) noexcept;
 	void StatFileAddress(const FileAddress &address,
 			     Handler::File::StatSuccessCallback on_success,
 			     Handler::File::StatErrorCallback on_error) noexcept;
@@ -956,7 +958,7 @@ private:
 	void CheckDirectoryIndex(UniquePoolPtr<TranslateResponse> response) noexcept;
 	void CheckDirectoryIndex(UniquePoolPtr<TranslateResponse> _response, FileDescriptor base) noexcept;
 	void CheckDirectoryIndex(UniquePoolPtr<TranslateResponse> response, FileAt file) noexcept;
-	void OnDirectoryIndexBaseOpen(FileDescriptor fd) noexcept;
+	void OnDirectoryIndexBaseOpen(FileDescriptor fd, std::string_view strip_base) noexcept;
 	void OnDirectoryIndexStat(const struct statx &st) noexcept;
 	void OnDirectoryIndexStatError(int error) noexcept;
 	void SubmitDirectoryIndex(const TranslateResponse &response) noexcept;
@@ -966,7 +968,7 @@ private:
 	void CheckFileNotFound(UniquePoolPtr<TranslateResponse> response) noexcept;
 	void CheckFileNotFound(UniquePoolPtr<TranslateResponse> response, FileDescriptor base) noexcept;
 	void CheckFileNotFound(UniquePoolPtr<TranslateResponse> response, FileAt file) noexcept;
-	void OnFileNotFoundBaseOpen(FileDescriptor fd) noexcept;
+	void OnFileNotFoundBaseOpen(FileDescriptor fd, std::string_view strip_base) noexcept;
 	void OnFileNotFoundStat(const struct statx &st) noexcept;
 	void OnFileNotFoundStatError(int error) noexcept;
 	void SubmitFileNotFound(const TranslateResponse &response) noexcept;
@@ -978,7 +980,7 @@ private:
 	void OnEnotdirStat(const struct statx &st) noexcept;
 	void OnEnotdirStatError(int error) noexcept;
 
-	void OnEnotdirBaseOpen(FileDescriptor fd) noexcept;
+	void OnEnotdirBaseOpen(FileDescriptor fd, std::string_view strip_base) noexcept;
 
 	/**
 	 * The #TranslateResponse contains #TRANSLATE_ENOTDIR.  Check this
@@ -1027,8 +1029,7 @@ private:
 	[[gnu::pure]]
 	const char *StripBase(const char *path) const noexcept;
 
-	void ProbePrecompressed(UniqueFileDescriptor fd,
-				const struct statx &st) noexcept;
+	void ProbePrecompressed(FileDescriptor fd, const struct statx &st, SharedLease &&lease) noexcept;
 	void ProbeNextPrecompressed() noexcept;
 	void OnPrecompressedOpenStat(UniqueFileDescriptor fd,
 				     struct statx &st) noexcept;
@@ -1062,8 +1063,7 @@ private:
 	void OnDelegateError(std::exception_ptr ep) noexcept override;
 
 	/* handler methods for UringOpenStat() */
-	void OnOpenStat(UniqueFileDescriptor fd,
-			struct statx &st) noexcept;
+	void OnOpenStat(FileDescriptor fd, const struct statx &stx, SharedLease _lease) noexcept;
 	void OnOpenStatError(int error) noexcept;
 
 	/* virtual methods from class SuffixRegistryHandler */

src/bp/file_directory_index.cxx
@@ -73,7 +73,7 @@ Request::CheckDirectoryIndex(UniquePoolPtr<TranslateResponse> _response, FileDes
 }
 
 void
-Request::OnDirectoryIndexBaseOpen(FileDescriptor fd) noexcept
+Request::OnDirectoryIndexBaseOpen(FileDescriptor fd, [[maybe_unused]] std::string_view strip_base) noexcept
 {
 	assert(translate.pending_response);
 

src/bp/file_enotdir.cxx
@@ -84,7 +84,7 @@ Request::CheckFileEnotdir(UniquePoolPtr<TranslateResponse> _response, FileAt fil
 }
 
 void
-Request::OnEnotdirBaseOpen(FileDescriptor fd) noexcept
+Request::OnEnotdirBaseOpen(FileDescriptor fd, [[maybe_unused]] std::string_view strip_base) noexcept
 {
 	assert(translate.pending_response);
 

commit 68fccc9c46b5951e9bef8f16e52aca6993fc3e15

bp/FileHandler: use RESOLVE_BENEATH only if a BASE was defined
Fixes another regression by commit 3570067b9dfb825c374aa545f1512043e0798e10

src/bp/FileHandler.cxx
@@ -562,11 +562,16 @@ Request::StatFileAddressAfterBase(FileDescriptor base, std::string_view strip_ba
 
 	static constexpr struct open_how open_read_only{
 		.flags = O_RDONLY|O_NOCTTY|O_CLOEXEC|O_NONBLOCK,
+		.resolve = RESOLVE_NO_MAGICLINKS,
+	};
+
+	static constexpr struct open_how open_read_only_beneath{
+		.flags = O_RDONLY|O_NOCTTY|O_CLOEXEC|O_NONBLOCK,
 		.resolve = RESOLVE_BENEATH|RESOLVE_NO_MAGICLINKS,
 	};
 
 	instance.fd_cache.Get(base, strip_base, path,
-			      open_read_only,
+			      base.IsDefined() ? open_read_only_beneath : open_read_only,
 			      STATX_TYPE|STATX_MTIME|STATX_INO|STATX_SIZE,
 			      BIND_THIS_METHOD(OnStatOpenStatSuccess),
 			      BIND_THIS_METHOD(OnStatOpenStatError),