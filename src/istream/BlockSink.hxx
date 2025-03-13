bf512c1b6eb418f97f11476ee607283eabed66ff
Max Kellermann <max.kellermann@ionos.com>
2025-03-13 16:37:21 +0100

    test/istream/TestConcatIstream: unit new unit test "SecondReady"

---
+++ src/istream/BlockSink.hxx
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: BSD-2-Clause
+// Copyright CM4all GmbH
+// author: Max Kellermann <mk@cm4all.com>
+
+#include "Sink.hxx"
+
+/**
+ * An #IstreamSink that blocks everything.
+ */
+class BlockSink final : public IstreamSink {
+public:
+	BlockSink(UnusedIstreamPtr &&_input) noexcept
+		:IstreamSink(std::move(_input)) {}
+
+	~BlockSink() noexcept {
+		if (HasInput())
+			CloseInput();
+	}
+
+	/* virtual methods from class IstreamHandler */
+
+	std::size_t OnData(std::span<const std::byte>) noexcept override {
+		return 0;
+	}
+
+	void OnEof() noexcept override {
+		ClearInput();
+	}
+
+	void OnError(std::exception_ptr) noexcept override {
+		ClearInput();
+	}
+};
--- test/istream/TestConcatIstream.cxx
+++ test/istream/TestConcatIstream.cxx
@@ -3,9 +3,13 @@
 // author: Max Kellermann <mk@cm4all.com>
 
 #include "IstreamFilterTest.hxx"
+#include "istream/BlockIstream.hxx"
 #include "istream/ConcatIstream.hxx"
+#include "istream/DelayedIstream.hxx"
 #include "istream/istream_string.hxx"
+#include "istream/istream_null.hxx"
 #include "istream/istream.hxx"
+#include "istream/BlockSink.hxx"
 #include "istream/UnusedPtr.hxx"
 
 class IstreamCatTestTraits {
@@ -26,3 +30,26 @@ public:
 
 INSTANTIATE_TYPED_TEST_SUITE_P(Cat, IstreamFilterTest,
 			       IstreamCatTestTraits);
+
+/**
+ * Test for a bug introduced by commit 5cb558a4cd9a9ddd8380581265
+ * fixed by commit d5decf0585667354cf19d88250
+ */
+TEST(ConcatIstreamTest, SecondReady)
+{
+	PInstance instance;
+	auto pool = pool_new_linear(instance.root_pool, "test", 8192);
+
+	auto [delayed, delayed_control] = istream_delayed_new(*pool, instance.event_loop);
+
+	BlockSink sink{NewConcatIstream(*pool, istream_block_new(*pool), std::move(delayed))};
+
+	instance.event_loop.Run();
+
+	delayed_control.Set(istream_null_new(*pool));
+
+	instance.event_loop.Run();
+
+	pool.reset();
+	pool_commit();
+}
