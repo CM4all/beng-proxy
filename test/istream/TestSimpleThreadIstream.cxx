c7468796605825b9b90fac337c0989e03edc3ba2
Max Kellermann <max.kellermann@ionos.com>
2025-05-20 13:27:02 +0200

    test/TestSimpleThreadIstream: new unit test

---
+++ test/istream/TestSimpleThreadIstream.cxx
@@ -0,0 +1,223 @@
+// SPDX-License-Identifier: BSD-2-Clause
+// Copyright CM4all GmbH
+// author: Max Kellermann <mk@cm4all.com>
+
+#include "IstreamFilterTest.hxx"
+#include "istream/SimpleThreadIstreamFilter.hxx"
+#include "istream/UnusedPtr.hxx"
+#include "istream/istream_string.hxx"
+#include "thread/Pool.hxx"
+
+/* most of this is a copy of TestThreadIstream.cxx refactored to use
+   SimpleThreadIstreamFilter */
+
+class NopSimpleThreadIstreamFilter final : public SimpleThreadIstreamFilter {
+public:
+	/* virtual methods from class SimpleThreadIstreamFilter */
+	Result SimpleRun(SliceFifoBuffer &input, SliceFifoBuffer &output,
+			 Params) override {
+		output.MoveFromAllowBothNull(input);
+		return {.drained = true};
+	}
+};
+
+class NopSimpleThreadIstreamTestTraits {
+	mutable EventLoop *event_loop_ = nullptr;
+
+public:
+	static constexpr IstreamFilterTestOptions options{
+		.expected_result = "foobar",
+	};
+
+	~NopSimpleThreadIstreamTestTraits() noexcept {
+		// invoke all pending ThreadJob::Done() calls
+		if (event_loop_ != nullptr)
+			event_loop_->Run();
+
+		thread_pool_stop();
+		thread_pool_join();
+		thread_pool_deinit();
+	}
+
+	UnusedIstreamPtr CreateInput(struct pool &pool) const noexcept {
+		return istream_string_new(pool, "foobar");
+	}
+
+	UnusedIstreamPtr CreateTest(EventLoop &event_loop, struct pool &pool,
+				    UnusedIstreamPtr input) const noexcept {
+		event_loop_ = &event_loop;
+
+		thread_pool_set_volatile();
+		return NewThreadIstream(pool, thread_pool_get_queue(event_loop),
+					std::move(input),
+					std::make_unique<NopSimpleThreadIstreamFilter>());
+	}
+};
+
+INSTANTIATE_TYPED_TEST_SUITE_P(SimpleThreadIstreamFilterNop, IstreamFilterTest,
+			       NopSimpleThreadIstreamTestTraits);
+
+/**
+ * Like #NopSimpleThreadIstreamFilter, but inserts a header and a footer
+ * byte.
+ */
+class FooSimpleThreadIstreamFilter final : public SimpleThreadIstreamFilter {
+	bool header_sent = false, trailer_sent = false;
+
+public:
+	Result SimpleRun(SliceFifoBuffer &input, SliceFifoBuffer &output,
+			 Params params) override {
+		if (!header_sent) {
+			auto w = output.Write();
+			if (w.empty())
+				return {.drained = false};
+
+			w.front() = std::byte{'H'};
+			output.Append(1);
+
+			header_sent = true;
+		}
+
+		/* sleep a bit to check whether main thread wakeups
+		   work properly */
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+		output.MoveFromAllowSrcNull(input);
+
+		if (input.empty() && params.finish && !trailer_sent) {
+			auto w = output.Write();
+			if (w.empty())
+				return {.drained = false};
+
+			w.front() = std::byte{'T'};
+			output.Append(1);
+
+			trailer_sent = true;
+		}
+
+		return {.drained = trailer_sent};
+	}
+};
+
+class FooSimpleThreadIstreamTestTraits {
+	mutable EventLoop *event_loop_ = nullptr;
+
+public:
+	static constexpr IstreamFilterTestOptions options{
+		.expected_result = "HfoobarT",
+	};
+
+	~FooSimpleThreadIstreamTestTraits() noexcept {
+		// invoke all pending ThreadJob::Done() calls
+		if (event_loop_ != nullptr)
+			event_loop_->Run();
+
+		thread_pool_stop();
+		thread_pool_join();
+		thread_pool_deinit();
+	}
+
+	UnusedIstreamPtr CreateInput(struct pool &pool) const noexcept {
+		return istream_string_new(pool, "foobar");
+	}
+
+	UnusedIstreamPtr CreateTest(EventLoop &event_loop, struct pool &pool,
+				    UnusedIstreamPtr input) const noexcept {
+		event_loop_ = &event_loop;
+
+		thread_pool_set_volatile();
+		return NewThreadIstream(pool, thread_pool_get_queue(event_loop),
+					std::move(input),
+					std::make_unique<FooSimpleThreadIstreamFilter>());
+	}
+};
+
+INSTANTIATE_TYPED_TEST_SUITE_P(SimpleThreadIstreamFoo, IstreamFilterTest,
+			       FooSimpleThreadIstreamTestTraits);
+
+/**
+ * A filter that returns each input byte 4093 times.  The goal is to
+ * have stalls due to a full output buffer see how
+ * #SimpleThreadIstreamFilter deals with this.
+ */
+class ExplodeSimpleThreadIstreamFilter final : public SimpleThreadIstreamFilter {
+	std::size_t remaining = 0;
+
+	std::byte value;
+
+public:
+	Result SimpleRun(SliceFifoBuffer &input, SliceFifoBuffer &output,
+			 Params) override {
+		while (true) {
+			if (remaining == 0) {
+				const auto r = input.Read();
+				if (r.empty())
+					return {.drained = true};
+
+				value = r.front();
+				input.Consume(1);
+				remaining = 4093;
+			}
+
+			auto w = output.Write();
+			if (w.empty())
+				return {.drained = false};
+
+			std::size_t n = std::min(remaining, w.size());
+			std::fill_n(w.begin(), n, value);
+			output.Append(n);
+			remaining -= n;
+		}
+	}
+};
+
+template<std::size_t size>
+static constexpr auto MakeExplodedBuffer(const char (&src)[size]) noexcept {
+	constexpr std::size_t length = size - 1;
+	std::array<char, length * 4093 + 1> buffer{};
+	auto o = buffer.begin();
+	for (std::size_t i = 0; i < length; ++i)
+		o = std::fill_n(o, 4093, src[i]);
+	*o = '\0';
+
+	return buffer;
+}
+
+class ExplodeOutputIstreamTestTraits {
+	mutable EventLoop *event_loop_ = nullptr;
+
+	static constexpr char input_string[] = "0123456789abcdefghijklmnopqrstuvwxyz";
+	static constexpr auto result = MakeExplodedBuffer(input_string);
+
+public:
+	static constexpr IstreamFilterTestOptions options{
+		.expected_result = result.data(),
+	};
+
+	~ExplodeOutputIstreamTestTraits() noexcept {
+		// invoke all pending ThreadJob::Done() calls
+		if (event_loop_ != nullptr)
+			event_loop_->Run();
+
+		thread_pool_stop();
+		thread_pool_join();
+		thread_pool_deinit();
+	}
+
+	UnusedIstreamPtr CreateInput(struct pool &pool) const noexcept {
+		return istream_string_new(pool, input_string);
+	}
+
+	UnusedIstreamPtr CreateTest(EventLoop &event_loop, struct pool &pool,
+				    UnusedIstreamPtr input) const noexcept {
+		event_loop_ = &event_loop;
+
+		thread_pool_set_volatile();
+		return NewThreadIstream(pool, thread_pool_get_queue(event_loop),
+					std::move(input),
+					std::make_unique<ExplodeSimpleThreadIstreamFilter>());
+	}
+};
+
+INSTANTIATE_TYPED_TEST_SUITE_P(SimpleThreadIstreamExplode, IstreamFilterTest,
+			       ExplodeOutputIstreamTestTraits);
