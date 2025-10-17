CM4all/beng-proxy

commit e105d8b3bba2f86abe9ad6b00128d7af78072e6f

istream/replace: remove empty substitutions in _FillBucketList()
Fixes a stalled transfer when an empty substitution (istream_null) is
at the front of the list and an incomplete widget follows directly
after that.  The _FillBucketList() call will return no data, thus
_ConsumeBucketList() does not get called and the empty substitution is
not removed.  The next Substitution::OnIstreamReady() call has no
effect because it's not the active substitution.

To fix this, we need to remove empty substitution in _FillBucketList()
without relying on _ConsumeBucketList() to be called eventually.

src/istream/ReplaceIstream.cxx
@@ -526,6 +526,23 @@ ReplaceIstream::_FillBucketList(IstreamBucketList &list)
 			throw;
 		}
 
+		if (tmp.IsEmpty() && !tmp.HasMore() && s->IsActive()) {
+			/* this (active) substitution is empty -
+			   remove it and try again; getting rid of
+			   empty substitutions is important because
+			   otherwise OnIstreamReady() calls from the
+			   following substitutions would be ignored;
+			   while reading buckets, nobody else would do
+			   that */
+			assert(fill_position == position);
+			assert(end == fill_position);
+
+			ToNextSubstitution(s);
+
+			// TODO refactor the loop to avoid this recursive call
+			return _FillBucketList(list);
+		}
+
 		list.SpliceBuffersFrom(std::move(tmp));
 		if (tmp.HasMore())
 			return;

commit 97663468b06bd78e56bd3a23a3646d4cb3871e3a

istream/replace: reset the "more" flag before recursing
Fixes regression from commit e105d8b3bba2f86abe9ad6b00128d7af78072e6f
which caused an assertion failure.  We need to restore the
IstreamBucketList to the state before this method was entered before
doing the recursive call (which was supposed to be a temporary
workaround anyway, but an ugly one).

src/istream/ReplaceIstream.cxx
@@ -540,6 +540,7 @@ ReplaceIstream::_FillBucketList(IstreamBucketList &list)
 			ToNextSubstitution(s);
 
 			// TODO refactor the loop to avoid this recursive call
+			list.SetMore(false);
 			return _FillBucketList(list);
 		}
 