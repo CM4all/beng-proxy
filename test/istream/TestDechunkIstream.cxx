763e32acbafca53a1d51414e523618bc4085028a
Max Kellermann <max.kellermann@ionos.com>
2025-11-11 14:35:02 +0100

    test/istream/TestDechunkIstream: add tests with DelayedIstream
    This causes all processing to be done from inside InvokeReady().  This
    currently crashes with assertion failure for
    DechunkInputAction::ABANDONED.

--- test/istream/TestDechunkIstream.cxx
+++ test/istream/TestDechunkIstream.cxx
@@ -6,6 +6,7 @@
 #include "../FlushEventLoop.hxx"
 #include "../RecordingStringSinkHandler.hxx"
 #include "istream/DechunkIstream.hxx"
+#include "istream/DelayedIstream.hxx"
 #include "istream/ByteIstream.hxx"
 #include "istream/FourIstream.hxx"
 #include "istream/istream_string.hxx"
@@ -119,13 +120,28 @@ public:
 INSTANTIATE_TYPED_TEST_SUITE_P(Dechunk2, IstreamFilterTest,
 			       IstreamDechunk2TestTraits);
 
+/**
+ * @param delayed_input wrap the input Istream in a DelayedIstream?
+ * This causes all processing to be done from inside InvokeReady() and
+ * tests this code path.
+ */
 static void
 TestAction(EventLoop &event_loop, struct pool &pool,
-	   DechunkHandler::DechunkInputAction action, bool buckets)
+	   DechunkHandler::DechunkInputAction action, bool buckets, bool delayed_input=false)
 {
-	auto input = istream_string_new(pool, "3\r\nFOO\r\n0\r\n\r\nBAR"sv);
+	auto real_input = istream_string_new(pool, "3\r\nFOO\r\n0\r\n\r\nBAR"sv);
 	if (!buckets)
-		input = NewIstreamPtr<NoBucketIstream>(pool, std::move(input));
+		real_input = NewIstreamPtr<NoBucketIstream>(pool, std::move(real_input));
+
+	UnusedIstreamPtr input;
+	DelayedIstreamControl *delayed_control;
+	if (delayed_input) {
+		auto delayed = istream_delayed_new(pool, event_loop);
+		input = std::move(delayed.first);
+		delayed_control = &delayed.second;
+	} else
+		input = std::move(real_input);
+
 
 	MyDechunkHandler dechunk_handler{action};
 
@@ -149,11 +165,19 @@ TestAction(EventLoop &event_loop, struct pool &pool,
 	EXPECT_EQ(dechunk_handler.state, MyDechunkHandler::State::INITIAL);
 	EXPECT_TRUE(handler.IsAlive());
 
-	ReadStringSink(sink);
-	EXPECT_GE(dechunk_handler.state, MyDechunkHandler::State::END_SEEN);
+	if (delayed_input) {
+		delayed_control->Set(std::move(real_input));
 
-	if (!buckets && handler.IsAlive())
+		/* here, DelayedIstream::DeferredRead() will call
+		   InvokeReady() */
 		FlushPending(event_loop);
+	} else {
+		ReadStringSink(sink);
+		EXPECT_GE(dechunk_handler.state, MyDechunkHandler::State::END_SEEN);
+
+		if (!buckets && handler.IsAlive())
+			FlushPending(event_loop);
+	}
 
 	EXPECT_EQ(dechunk_handler.state, MyDechunkHandler::State::END);
 	ASSERT_FALSE(handler.IsAlive());
@@ -164,13 +188,13 @@ TestAction(EventLoop &event_loop, struct pool &pool,
 }
 
 static void
-TestAction(DechunkHandler::DechunkInputAction action, bool buckets)
+TestAction(DechunkHandler::DechunkInputAction action, bool buckets, bool delayed_input=false)
 {
 	PInstance instance;
 
 	{
 		const auto pool = pool_new_linear(instance.root_pool, "test", 8192);
-		TestAction(instance.event_loop, pool, action, buckets);
+		TestAction(instance.event_loop, pool, action, buckets, delayed_input);
 		pool_trash(pool);
 	}
 
@@ -190,6 +214,11 @@ TEST(DechunkIstream, AbandonActionBuckets)
 	TestAction(DechunkHandler::DechunkInputAction::ABANDON, true);
 }
 
+TEST(DechunkIstream, AbandonActionBuckets2)
+{
+	TestAction(DechunkHandler::DechunkInputAction::ABANDON, true, true);
+}
+
 /**
  * Test DechunkInputAction::CLOSE.
  */
@@ -203,6 +232,11 @@ TEST(DechunkIstream, CloseActionBuckets)
 	TestAction(DechunkHandler::DechunkInputAction::CLOSE, true);
 }
 
+TEST(DechunkIstream, CloseActionBuckets2)
+{
+	TestAction(DechunkHandler::DechunkInputAction::CLOSE, true, true);
+}
+
 /**
  * Test DechunkInputAction::DESTROYED.
  */
@@ -214,4 +248,9 @@ TEST(DechunkIstream, DestroyedAction)
 TEST(DechunkIstream, DestroyedActionBuckets)
 {
 	TestAction(DechunkHandler::DechunkInputAction::DESTROYED, true);
+}
+
+TEST(DechunkIstream, DestroyedActionBuckets2)
+{
+	TestAction(DechunkHandler::DechunkInputAction::DESTROYED, true, true);
 }
