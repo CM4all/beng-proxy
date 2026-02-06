80aaaeb16a647ceb71ed230f80e86f8a7c9a86d3
Max Kellermann <max.kellermann@ionos.com>
2026-02-06 19:14:54 +0100

    net/NetworkList: use class InetAddress, no dynamic object size
    The dynamic object size was probably not worth it, and a fixed size
    may allow other optimizations.

--- src/net/NetworkList.cxx
+++ src/net/NetworkList.cxx
@@ -3,6 +3,7 @@
 // author: Max Kellermann <max.kellermann@ionos.com>
 
 #include "NetworkList.hxx"
+#include "net/InetAddress.hxx"
 #include "net/MaskedSocketAddress.hxx"
 #include "AllocatorPtr.hxx"
 
@@ -17,12 +18,11 @@ struct NetworkList::Item : IntrusiveForwardListHook
 {
 	uint_least8_t prefix_length;
 
-	struct sockaddr address;
+	InetAddress address;
 
-	explicit Item(SocketAddress src, uint_least8_t _prefix_length) noexcept
-		:prefix_length(_prefix_length)
+	explicit Item(const auto &src, uint_least8_t _prefix_length) noexcept
+		:prefix_length(_prefix_length), address(src)
 	{
-		memcpy(&address, src.GetAddress(), src.GetSize());
 	}
 
 	static SocketAddress::size_type AddressSize(const struct sockaddr &address) noexcept {
@@ -38,27 +38,8 @@ struct NetworkList::Item : IntrusiveForwardListHook
 		}
 	}
 
-	SocketAddress::size_type AddressSize() const noexcept {
-		return AddressSize(address);
-	}
-
-	operator SocketAddress() const noexcept {
-		if (const auto size = AddressSize(); size > 0)
-			return {&address, size};
-		else
-			return nullptr;
-	}
-
-	static std::size_t ObjectSize(std::size_t address_size) noexcept {
-		return sizeof(Item) - sizeof(address) + address_size;
-	}
-
-	std::size_t ObjectSize() const noexcept {
-		return ObjectSize(AddressSize(address));
-	}
-
 	Item *Dup(AllocatorPtr alloc) const noexcept {
-		return (Item *)alloc.Dup(this, ObjectSize());
+		return alloc.New<Item>(address, prefix_length);
 	}
 
 	[[gnu::pure]]
@@ -79,27 +60,30 @@ NetworkList::Add(AllocatorPtr alloc, SocketAddress address, uint_least8_t prefix
 {
 	assert(!address.IsNull());
 
-	const auto address_size = Item::AddressSize(*address.GetAddress());
-	if (address_size == 0)
-		throw std::invalid_argument{"Unsupported address family"};
-
-	if (address_size != address.GetSize())
-		throw std::invalid_argument{"Bad address size"};
-
 	if (prefix_length > MaskedSocketAddress::MaximumPrefixLength(address))
 		throw std::invalid_argument{"Bad network prefix length"};
 
-	if (!MaskedSocketAddress::IsValidPrefixLength(address, prefix_length))
-		throw std::invalid_argument{"Bad network address"};
+	Item *item;
+	switch (address.GetFamily()) {
+	case AF_INET:
+		item = alloc.New<Item>(IPv4Address::Cast(address), prefix_length);
+		break;
 
-	auto *item = alloc.NewVar<Item>(Item::ObjectSize(address_size), address, prefix_length);
+	case AF_INET6:
+		item = alloc.New<Item>(IPv6Address::Cast(address), prefix_length);
+		break;
+
+	default:
+		throw std::invalid_argument{"Unsupported address family"};
+	}
+
 	list.push_front(*item);
 }
 
 inline bool
 NetworkList::Item::Contains(SocketAddress other) const noexcept
 {
-	return MaskedSocketAddress::Matches(*this, prefix_length, other);
+	return MaskedSocketAddress::Matches(address, prefix_length, other);
 }
 
 bool
