CM4all/beng-proxy

commit c48b4226c157ac9c0f6e1e52adc1f0ad0db8ae97

bp/Request: fix ENOTDIR with only a trailing slash
Since the trailing slash has been stripped already, no actual ENOTDIR
occurs, but we really need to treat it like ENOTDIR.

src/bp/file_enotdir.cxx
@@ -58,9 +58,26 @@ Request::SubmitEnotdir(const TranslateResponse &response) noexcept
 }
 
 inline void
-Request::OnEnotdirStat([[maybe_unused]] const struct statx &st) noexcept
+Request::OnEnotdirStat(const struct statx &st) noexcept
 {
 	assert(translate.pending_response);
+
+	if (!S_ISDIR(st.stx_mode)) {
+		const auto &response = *translate.pending_response;
+		const auto &address = response.address.GetFile();
+		const std::string_view path{address.path};
+		assert(!path.empty());
+
+		if (path.back() == '/') {
+			/* this trailing slash was stripped by
+			  StatFileAddressAfterBase(), but since this
+			  turns out to not be a directory, we handle
+			  this as if it were ENOTDIR */
+
+			if (!SubmitEnotdir(response))
+				return;
+		}
+	}
 
 	OnTranslateResponseAfterEnotdir(std::move(translate.pending_response));
 }

commit c9132756940e8df98f60362a17e4b469b809bf35

bp/file_enotdir: check for trailing slash only when address is a local file
Fixes assertion failure when ENOTDIR is used with WAS addresses;
regression by commit c48b4226c157ac9c0f6e1e52adc1f0ad0db8ae97

src/bp/file_enotdir.cxx
@@ -62,7 +62,8 @@ Request::OnEnotdirStat(const struct statx &st) noexcept
 {
 	assert(translate.pending_response);
 
-	if (!S_ISDIR(st.stx_mode)) {
+	if (!S_ISDIR(st.stx_mode) &&
+	    translate.pending_response->address.type == ResourceAddress::Type::LOCAL) {
 		const auto &response = *translate.pending_response;
 		const auto &address = response.address.GetFile();
 		const std::string_view path{address.path};