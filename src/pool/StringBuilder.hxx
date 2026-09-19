642607a38bc31133371a3e374599e56165281bcf
Max Kellermann <max.kellermann@ionos.com>
2026-09-19 10:52:31 +0000

    pool/StringBuilder: support more items than the fixed capacity
    The number of strings appended to the PoolStringBuilder is not
    bounded; appending more than the capacity of the StaticVector could
    lead to stack overflow.

--- src/pool/StringBuilder.hxx
+++ src/pool/StringBuilder.hxx
@@ -8,26 +8,41 @@
 #include "util/StaticVector.hxx"
 
 #include <string_view>
+#include <vector>
 
 template<size_t MAX>
 class PoolStringBuilder {
 	StaticVector<std::string_view, MAX> items;
 
+	/**
+	 * If we have more than #MAX items, they will land in this
+	 * dynamic std::vector.  This usually doesn't happen, but just
+	 * in case there are many APPEND/PAIR packets to be
+	 * appended...
+	 */
+	std::vector<std::string_view> overflow;
+
 public:
 	void push_back(std::string_view s) noexcept {
-		items.push_back(s);
+		if (items.full()) [[unlikely]]
+			overflow.push_back(s);
+		else
+			items.push_back(s);
 	}
 
 	template<typename... Args>
 	void emplace_back(Args&&... args) noexcept {
-		items.emplace_back(std::forward<Args>(args)...);
+		if (items.full()) [[unlikely]]
+			overflow.emplace_back(std::forward<Args>(args)...);
+		else
+			items.emplace_back(std::forward<Args>(args)...);
 	}
 
 	char *operator()(AllocatorPtr alloc) const noexcept {
-		return alloc.Concat(items);
+		return alloc.Concat(items, overflow);
 	}
 
 	std::string_view MakeView(AllocatorPtr alloc) const noexcept {
-		return alloc.ConcatView(items);
+		return alloc.ConcatView(items, overflow);
 	}
 };
