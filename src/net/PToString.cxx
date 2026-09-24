dc4b846e86595420e0bb99df3dc29aab63020548
Max Kellermann <max.kellermann@ionos.com>
2026-09-24 20:48:42 +0200

    net/PToString: include cleanup

--- src/net/PToString.cxx
+++ src/net/PToString.cxx
@@ -4,7 +4,6 @@
 
 #include "PToString.hxx"
 #include "InterfaceNameCache.hxx"
-#include "pool/pool.hxx"
 #include "net/SocketAddress.hxx"
 #include "net/FormatAddress.hxx"
 #include "net/IPv4Address.hxx"
